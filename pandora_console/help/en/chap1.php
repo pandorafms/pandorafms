bf28867b70095e68ea4b0a9687f29b47633d4f90
raulmateos <noreply@pandorafms.org>
2006-04-20 17:40:45 +0000

    -Add new help
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@11 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/help/en/chap1.php
@@ -0,0 +1,56 @@
+<?php
+// Pandora - The Free Monitoring System
+// This code is protected by GPL license.
+// Este codigo esta protegido por la licencia GPL.
+// Sancho Lerena <slerena@gmail.com>, 2003-2006
+// Raul Mateos <raulofpandora@gmail.com>, 2004-2006
+?>
+<html>
+<head>
+<title>Pandora - The Free Monitoring System Help - I. Introduction</title>
+<link rel="stylesheet" href="../../include/styles/pandora.css" type="text/css">
+<style>
+div.logo {float:left;}
+div.toc {padding-left: 200px;}
+div.rayah {clear:both; border-top: 1px solid #708090; width: 100%;}
+</style>
+</head>
+
+<body>
+
+<div class='logo'>
+<img src="../../images/logo_menu.gif" alt='logo'><h1>Pandora Help v1.2</h1>
+</div>
+<div class="toc">
+<h1><a href="toc.php">Table of Contents</a> » <a href="chap2.php">2. Users</a></h1>
+
+</div>
+<div class="rayah">
+<p align='right'>Pandora is a GPL Software Project. &copy; Sancho Lerena 2003-2005, David villanueva 2004-2005, Alex Arnal 2005, Ra&uacute;l Mateos 2004-2005.</p>
+</div>
+
+<a name="1"><h1>1. Introduction</h1></a>
+
+<h2><a name="11">1.1. Pandora Administration</a></h2>
+
+<p>The web console is the tool used to administrate Pandora. It is written in PHP and doesn't
+need any third party software to work –neither FLASH, JAVA, nor ActiveX
+plug-ins. The Web console can be run over any web browser supporting HTML and
+CSS (IE5+ or Mozilla 4+).</p>
+
+<p class="center">
+<img src="images/image001.png" alt="image001">
+</p>
+
+<p>Pandora administration is divided in the following main elements:</p>
+<ul>
+<li>Pandora users</li>
+<li>Agents</li>
+<li>Alerts</li>
+<li>Incidents</li>
+<li>Server configuration</li>
+<li>Database maintenance</li>
+</ul>
+
+</body>
+</html>
---
+++ pandora_console/help/es/toc.php
@@ -0,0 +1,92 @@
+<?php
+// Pandora - The Free Monitoring System
+// This code is protected by GPL license.
+// Este codigo esta protegido por la licencia GPL.
+// Sancho Lerena <slerena@gmail.com>, 2003-2006
+// Raul Mateos <raulofpandora@gmail.com>, 2004-2006
+?>
+<html>
+<head>
+<title>Pandora - Sistema de monitorizaci&oacute;n de Software Libre - Ayuda - &Iacute;ndice</title>
+<link rel="stylesheet" href="../../include/styles/pandora.css" type="text/css">
+<style>
+dd.help_t1 {margin-left: 10px; margin-top: 6px; margin-bottom: 5px; font-size: 13px; font-weight: bold}
+dd.help_t2 {margin-left: 28px; margin-top: 3px;}
+dd.help_t3 {margin-left: 55px; margin-top: 4px;}
+dd.help_t4 {margin-left: 92px; margin-top: 3px; margin-bottom: 2px;}
+</style>
+</head>
+
+<body>
+<center>
+<img src="../../images/logo_menu.gif" alt='logo'>
+
+<p><h1>Ayuda de Pandora 1.2</h1></p>
+<p>(Actualizado el 20 de abril de 2006).</p>
+<p>Pandora es un proyecto de software GPL. <br>&copy; Sancho Lerena 2003-2006, David Villanueva 2004-2006 y Ra&uacute;l Mateos 2004-2006.</p>
+</center>
+<br>
+<h1>&Iacute;ndice</h1>
+
+<dl>
+
+<dd class="help_t1"><a href="chap1.php#1"><h1>1. Introduction</h1></a></dd>
+<dd class="help_t2"><a href="chap1.php#11">1.1. Administración de Pandora</a></dd>
+
+<dd class="help_t1"><a href="chap2.php#2"><h1>2. Usuarios</h1></a></dd>
+<dd class="help_t2"><a href="chap2.php#21">2.1. Gesti&oacute;n de perfiles</a></dd>
+<dd class="help_t2"><a href="chap2.php#22">2.2. A&ntilde;adir un usuario</a></dd>
+<dd class="help_t2"><a href="chap2.php#23">2.3. Borrar un usuario</a></dd>
+<dd class="help_t2"><a href="chap2.php#24">2.4. Estad&iacute;sticas</a></dd>
+
+<dd class="help_t1"><a href="chap2.php#3"><h1>3. Agentes</h1></a></dd>
+<dd class="help_t2"><a href="chap2.php#31">3.1. Gesti&oacute;n de grupos</a></dd>
+<dd class="help_t2"><a href="chap2.php#32">3.2. A&ntilde;adir un agente</a></dd>
+<dd class="help_t3"><a href="chap3.php#321">3.2.1. Asignaci&oacute;n de m&oacute;dulos</a></dd>
+<dd class="help_t3"><a href="chap3.php#322">3.2.2. Alertas</a></dd>
+<dd class="help_t4"><a href="chap3.php#3221">3.2.2.1. Creaci&oacute;n de alertas</a></dd>
+<dd class="help_t4"><a href="chap3.php#3222">3.2.2.2. Asignaci&oacute;n de alertas</a></dd>
+<dd class="help_t3"><a href="chap3.php#323">3.2.3. Gesti&oacute;n de m&oacute;dulos y alertas de agentes</a></dd>
+<dd class="help_t2"><a href="chap2.php#33">3.3. Monitorizaci&oacute;n de agentes</a></dd>
+<dd class="help_t3"><a href="chap3.php#331">3.3.1. Ver agentes</a></dd>
+<dd class="help_t3"><a href="chap3.php#322">3.3.2. Acceso a los datos de un agente concreto</a></dd>
+<dd class="help_t4"><a href="chap3.php#3321">3.3.2.1. Informaci&oacute;n general de un agente</a></dd>
+<dd class="help_t4"><a href="chap3.php#3322">3.3.2.2. Muestra de &uacute;ltimos datos obtenidos</a></dd>
+<dd class="help_t4"><a href="chap3.php#3323">3.3.2.3. Lista completa de monitores</a></dd>
+<dd class="help_t4"><a href="chap3.php#3324">3.3.2.4. Lista completa de alertas</a></dd>
+<dd class="help_t3"><a href="chap3.php#333">3.3.3. Detalle de grupos</a></dd>
+<dd class="help_t3"><a href="chap3.php#334">3.3.4. Ver monitores</a></dd>
+<dd class="help_t3"><a href="chap3.php#335">3.3.5. Detalle de alertas</a></dd>
+<dd class="help_t3"><a href="chap3.php#336">3.3.6. Exportar satos</a></dd>
+<dd class="help_t3"><a href="chap3.php#337">3.3.7. Estad&iacute;sticas</a></dd>
+
+<dd class="help_t1"><a href="chap2.php#4"><h1>4. Gesti&oacute;n de incidentes</h1></a></dd>
+<dd class="help_t2"><a href="chap2.php#41">4.1. A&ntilde;adir un incidente</a></dd>
+<dd class="help_t2"><a href="chap2.php#42">4.2. Seguimiento de un incidente</a></dd>
+<dd class="help_t3"><a href="chap3.php#421">4.2.1. A&ntilde;adir notas a un incidente</a></dd>
+<dd class="help_t3"><a href="chap3.php#422">4.2.2. A&ntilde;adir archivos a un incidente</a></dd>
+<dd class="help_t2"><a href="chap2.php#43">4.3. Buscar incidente</a></dd>
+<dd class="help_t2"><a href="chap2.php#44">4.4. Estad&iacute;sticas</a></dd>
+
+<dd class="help_t1"><a href="chap2.php#5"><h1>5. Eventos</h1></a></dd>
+<dd class="help_t2"><a href="chap2.php#51">5.1. Estad&iacute;sticas</a></dd>
+
+<dd class="help_t1"><a href="chap2.php#6"><h1>6. Auditor&iacute;a del sistema</h1></a></dd>
+<dd class="help_t2"><a href="chap2.php#61">6.1. Estad&iacute;sticas</a></dd>
+
+<dd class="help_t1"><a href="chap2.php#7"><h1>7. Configuraci&oacute;n del servidor</h1></a></dd>
+<dd class="help_t2"><a href="chap2.php#71">7.1. Enlaces</a></dd>
+
+<dd class="help_t1"><a href="chap2.php#8"><h1>8. Mantenimiento de la Base de Datos</h1></a></dd>
+<dd class="help_t2"><a href="chap2.php#81">8.1. Informaci&oacute;n de la Base de Datos</a></dd>
+<dd class="help_t2"><a href="chap2.php#82">8.2. Purgado manual de la Base de Datos</a></dd>
+<dd class="help_t2"><a href="chap2.php#83">8.3. Depuraci&oacute;n de Datos procedentes de los agentes</a></dd>
+<dd class="help_t3"><a href="chap3.php#831">8.3.1. Depuraci&oacute;n de datos concretos de un m&oacute;dulo</a></dd>
+<dd class="help_t3"><a href="chap3.php#832">8.3.2. Depuraci&oacute;n de todos los datos de un agente</a></dd>
+<dd class="help_t2"><a href="chap2.php#84">8.4. Depuraci&oacute;n de datos procedentes del sistema</a></dd>
+<dd class="help_t3"><a href="chap3.php#841">8.4.1. Depuraci&oacute;n de datos de auditor&iacute;a</a></dd>
+<dd class="help_t3"><a href="chap3.php#842">8.4.2. Depuraci&oacute;n de datos de eventos</a></dd>
+</dl>
+
+</body>
+</html>

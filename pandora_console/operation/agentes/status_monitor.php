af06c72d1f00bdaf5157ea7baf6f24be826b96d3
raulmateos <noreply@pandorafms.org>
2006-07-09 14:20:10 +0000

    - Update info and clean code in estado_alertas.php, estado_monitores.php and status_monitor.php (Pandora Console)
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@111 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/agentes/status_monitor.php
+++ pandora_console/operation/agentes/status_monitor.php
@@ -1,9 +1,20 @@
 <?php
-// Pandora - The Free Monitoring System
-// This code is protected by GPL license.
-// Este codigo esta protegido por la licencia GPL.
-// Sancho Lerena <slerena@gmail.com>, 2003-2006
-// Raul Mateos <raulofpandora@gmail.com>, 2005-2006
+// Pandora - the Free monitoring system
+// ====================================
+// Copyright (c) 2004-2006 Sancho Lerena, slerena@gmail.com
+// Copyright (c) 2005-2006 Artica Soluciones Tecnologicas S.L, info@artica.es
+// Copyright (c) 2004-2006 Raul Mateos Martin, raulofpandora@gmail.com
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 // Load global vars
 require("include/config.php");
@@ -94,7 +105,6 @@ if (comprueba_login() == 0) {
 	$color =1;
 	$result=mysql_query($sql);
 	if (mysql_num_rows($result)){
-		$string='';
 		while ($row=mysql_fetch_array($result)){ //while there are agents	
 			if ($row["disabled"] == 0) {
 				if ((isset($ag_modulename)) && ($ag_modulename != "---"))

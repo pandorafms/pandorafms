75a907d218e35dd395da0ea4c38f1ff2ceb20946
raulmateos <noreply@pandorafms.org>
2006-07-11 10:03:18 +0000

    (Pandora Console)
    * message.php: Add new text about authors. Add new help icon; At this moment help for this item isn't available. Add a line between table and button (cosmetic change)
    
    * incident_detail: Add new text about authors. Add a line between table and button. Add different colors between rows. Change button position (cosmetic changes).
    
    * incident_note: Add new text about authors. Add a line between table and button. Add different colors between rows (cosmetic changes).
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@121 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/incidents/incident_note.php
+++ pandora_console/operation/incidents/incident_note.php
@@ -1,30 +1,42 @@
 <?php
-// Pandora - The Free Monitoring System
-// This code is protected by GPL license.
-// Este codigo esta protegido por la licencia GPL.
-// Sancho Lerena <slerena@gmail.com>, 2003-2005
 
-// Cargamos variables globales
+// Pandora - the Free monitoring system
+// ====================================
+// Copyright (c) 2004-2006 Sancho Lerena, slerena@gmail.com
+// Copyright (c) 2005-2006 Artica Soluciones Tecnologicas S.L, info@artica.es
+// Copyright (c) 2004-2006 Raul Mateos Martin, raulofpandora@gmail.com
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+// Load global vars
 require("include/config.php");
-//require("include/functions.php");
-//require("include/functions_db.php");
+
 if (comprueba_login() == 0) {
 
 	$id_inc = $_GET["id_inc"];
 	$ahora=date("Y/m/d H:i:s");
 
-	// Crear Nota
+	// Create Note
 	echo "<h2>".$lang_label["incident_manag"]."</h2>";
-	echo "<h3>".$lang_label["note_title"]." #".$id_inc." </h3>";
+	echo "<h3>".$lang_label["note_title"]." #".$id_inc."<a href='help/".$help_code."/chap3.php#331' target='_help' class='help'>&nbsp;<span>".$lang_label["help"]."</span></a></h3>";
 	echo "<table cellpadding=3 cellspacing=3 border=0><form name='nota' method='post' action='index.php?sec=incidencias&sec2=operation/incidents/incident_detail&insertar_nota=1&id=".$id_inc."'>";
-	echo "<tr><td class='datos'><b>".$lang_label["date"]."</b>";
+	echo "<tr><td class='lb' rowspan='2' width='5'><td class='datos'><b>".$lang_label["date"]."</b>";
 	echo "<td class='datos'>".$ahora;
 	echo "<input type='hidden' name='timestamp' value='".$ahora."'>";
 	echo "<input type='hidden' name='id_inc' value='".$id_inc."'>";
-	echo '<tr><td colspan=2 class="datos"><textarea name="nota" rows="20" cols="85">';
+	echo '<tr><td colspan="3" class="datos2"><textarea name="nota" rows="20" cols="85">';
 	echo '</textarea>';
-	echo '<tr><td colspan=2 class="datos" align="right"><input name="addnote" type="submit" class="sub" value="'.$lang_label["add"].'">';
+	echo '<tr><td colspan="3"><div class="raya"></div></td></tr>';
+	echo '<tr><td colspan="3" align="right"><input name="addnote" type="submit" class="sub" value="'.$lang_label["add"].'">';
 	echo '</table>';
 
-
-} // fin pagina
+} // end page

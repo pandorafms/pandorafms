e46b6c27e466f4aa916641f0e30376bfc1f91868
raulmateos <noreply@pandorafms.org>
2006-07-12 13:43:41 +0000

    (Pandora Console)
    * datos_agente.php. Add new text about authors. Add a line between table and button. Add different colors between rows (cosmetic changes). Add Help.
    
    * user.php. Add new text about authors. Clean code.
    
    * menu.php. Add highlight when datos_agente selected.
    
    * pandora.css. Add new text about authors.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@132 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/users/user.php
+++ pandora_console/operation/users/user.php
@@ -1,20 +1,30 @@
 <?php
 
-// Pandora - The Free Monitoring System
-// This code is protected by GPL license.
-// Este codigo esta protegido por la licencia GPL.
-// Sancho Lerena <slerena@gmail.com>, 2003-2006
-// Raul Mateos <raulofpandora@gmail.com>, 2005-2006
+// Pandora - the Free monitoring system
+// ====================================
+// Copyright (c) 2004-2006 Sancho Lerena, slerena@gmail.com
+// Copyright (c) 2005-2006 Artica Soluciones Tecnologicas S.L, info@artica.es
+// Copyright (c) 2004-2006 Raul Mateos Martin, raulofpandora@gmail.com
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 // Load global vars
 require("include/config.php");
-//require("include/functions.php");
-//require("include/functions_db.php");
+
 if (comprueba_login() == 0) {
 
 ?>
 
-<h2><?php echo $lang_label["users_"] ?></h3>
+<h2><?php echo $lang_label["users_"] ?></h2>
 <h3><?php echo $lang_label["users"] ?><a href='help/<?php echo $help_code ?>/chap2.php#2' target='_help' class='help'>&nbsp;<span><?php echo $lang_label["help"] ?></span></a></h3>
 
 <table cellpadding="3" cellspacing="3" width="700">

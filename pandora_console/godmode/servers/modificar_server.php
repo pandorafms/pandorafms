f26600cc932b948ddd30bd28b3eefa8a322f5f4f
raulmateos <noreply@pandorafms.org>
2006-07-17 08:17:58 +0000

    (Pandora Console)
    * Some headers changed to show the "official" GPL Header info
    
    * modificar_server.php. Add highlights. Add line before the submit button. Add help.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@136 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/servers/modificar_server.php
+++ pandora_console/godmode/servers/modificar_server.php
@@ -1,13 +1,25 @@
 <?php
 
-// Pandora - The Free Monitoring System
-// This code is protected by GPL license.
-// Este codigo esta protegido por la licencia GPL.
-// Sancho Lerena <slerena@gmail.com>, 2003-2006
-// Raul Mateos <raulofpandora@gmail.com>, 2005-2006
+// Pandora - the Free monitoring system
+// ====================================
+// Copyright (c) 2004-2006 Sancho Lerena, slerena@gmail.com
+// Copyright (c) 2005-2006 Artica Soluciones Tecnológicas S.L, info@artica.es
+// Copyright (c) 2004-2006 Raul Mateos Martin, raulofpandora@gmail.com
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-// Cargamos variables globales
+// Load global vars
 require("include/config.php");
+
 if (comprueba_login() == 0) {
  	if ((give_acl($id_user, 0, "AR")==1) or (give_acl($id_user,0,"AW")) or (dame_admin($id_user)==1)) {
 
@@ -32,8 +44,7 @@ if (comprueba_login() == 0) {
 	if (isset($_GET["server"])) {
 		$id_server=entrada_limpia($_GET["server"]);
 		echo "<h2>".$lang_label["view_servers"]."</h2>";
-		echo "<h3>".$lang_label["update_server"]."</h3>";
-		echo "<br>";
+		echo "<h3>".$lang_label["update_server"]."<a href='help/".$help_code."/chap2.php#22' target='_help' class='help'>&nbsp;<span>".$lang_label["help"]."</span></a></h3>";
 
 	$query="SELECT * FROM tserver WHERE id_server=".$id_server;
 	$result=mysql_query($query);
@@ -51,15 +62,16 @@ if (comprueba_login() == 0) {
 		$checksum = $row["checksum"];
 		$description = $row["description"];
 		echo '<form name="servers" method="POST" action="index.php?sec=gserver&sec2=godmode/servers/modificar_server&update=1">';
-		echo "<table cellpadding='3' cellspacing='3' witdh='550'>";
-		echo "<th rowspan='4' width='5'></th>";
-		echo "<tr><td class='datos'>".$lang_label["name"]."<td class='datos'><input type='text' name='name' value='".$name."'>";
-		echo "<tr><td class='datos'>".$lang_label['ip_address']."<td class='datos'><input type='text' name='address' value='".$address."'>";
-		echo "<tr><td class='datos'>".$lang_label['description']."<td class='datos'><input type='text' name='description' value='".$description."'><input type='hidden' name='server' value='".entrada_limpia($_GET["server"])."'></input>";
+		echo "<table cellpadding='3' cellspacing='3' width='450'>";
+		echo "<tr><td class='lb' rowspan='3' width='5'>";
+		echo "<td class='datos'>".$lang_label["name"]."</td><td class='datos'><input type='text' name='name' value='".$name."' class='w200'>";
+		echo "<tr><td class='datos2'>".$lang_label['ip_address']."</td><td class='datos2'><input type='text' name='address' value='".$address."' class='w200'>";
+		echo "<tr><td class='datos'>".$lang_label['description']."<td class='datos'><input type='text' name='description' value='".$description."' class='w200'><input type='hidden' name='server' value='".entrada_limpia($_GET["server"])."></input>";
 	}
 	else {
 		echo '<font class="red">'.$lang_label["no_server"].'</font>';
 		}
+	echo '<tr><td colspan="3"><div class="raya"></div></td></tr>';
 	echo '<tr><td colspan="3" align="right">';
 	echo '<input type="submit" class="sub" value="'.$lang_label["update"].'"></table>';
 	}
@@ -68,12 +80,11 @@ if (comprueba_login() == 0) {
 	$sql='SELECT * FROM tserver';
 	
 	echo "<h2>".$lang_label["view_servers"]."</h2>";
-	echo "<h3>".$lang_label["manage_servers"]."</h3>";
+	echo "<h3>".$lang_label["manage_servers"]."<a href='help/".$help_code."/chap2.php#22' target='_help' class='help'>&nbsp;<span>".$lang_label["help"]."</span></a></h3>";
 
 	// Connect DataBase
 	$result=mysql_query($sql);
-	if (mysql_num_rows($result)){
-	echo "<br>";	
+	if (mysql_num_rows($result)){	
 	echo "<table cellpadding='3' cellspacing='3' witdh=550>";
 	echo "<tr><th class='datos'>".$lang_label["name"];
 	echo "<th class='datos'>".$lang_label['status'];
@@ -112,7 +123,7 @@ if (comprueba_login() == 0) {
 			}
 			echo "<tr><td class='$tdcolor'>";
 			echo "<b>$name</b>";
-			echo "<td class='datos' align='middle'>";
+			echo "<td class='$tdcolor' align='middle'>";
 			if ($status ==0){
 				echo "<img src='images/dot_red.gif'>";
 			} else {

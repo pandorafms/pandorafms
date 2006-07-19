8de8a0f71d1771881c1097e487477016d7335dfe
raulmateos <noreply@pandorafms.org>
2006-07-19 22:01:47 +0000

    * Some headers changed to show the "official" GPL Header info. No new features or solved bugs
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@143 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/users/user_statistics.php
+++ pandora_console/operation/users/user_statistics.php
@@ -1,17 +1,28 @@
 <?php
-// Pandora - The Free Monitoring System
-// This code is protected by GPL license.
-// Este codigo esta protegido por la licencia GPL.
-// Sancho Lerena <slerena@gmail.com>, 2003-2005
 
-// Cargamos variables globales
+// Pandora - the Free monitoring system
+// ====================================
+// Copyright (c) 2004-2006 Sancho Lerena, slerena@gmail.com
+// Copyright (c) 2005-2006 Artica Soluciones Tecnológicas S.L, info@artica.es
+// Copyright (c) 2004-2006 Raul Mateos Martin, raulofpandora@gmail.com
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+// Load global vars
 require("include/config.php");
-//require("include/functions.php");
-//require("include/functions_db.php");
+
 if (comprueba_login() == 0) {
 echo "<h2>".$lang_label["users"]."</h2>";
 echo "<h3>".$lang_label["users_statistics"]."<a href='help/".$help_code."/chap2.php#24' target='_help' class='help'>&nbsp;<span>".$lang_label["help"]."</span></a></h3>";
 echo '<img src="reporting/fgraph.php?tipo=user_activity" border=0>';
-echo "<br><br>";
 }
 ?>

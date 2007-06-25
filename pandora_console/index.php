e18fb2e2e1c150ce7dfdce78679bc654d96c2eed
slerena <slerena@gmail.com>
2007-06-25 17:33:22 +0000

    2007-06-25  Sancho Lerena  <slerena@artica.es>
            First commit for Beta2 :-)
    
            * reporting/fgraph.php: Fixed problem with combined graph that
            makes long time graphs more smaller and slightly visible.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@544 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/index.php
+++ pandora_console/index.php
@@ -25,8 +25,8 @@
 // Pandora FMS 1.x uses Pear Image::Graph code
 
 //Pandora Version, if not defined here it would take from config.php
-$build_version="PC070622";
-$pandora_version="v1.3 Beta 1";
+$build_version="PC070623";
+$pandora_version="v1.3 Beta 2";
 global $build_version;
 global $pandora_version;
 

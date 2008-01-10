2664626ebecb01fb7a2f1c7be21aaf19696ebeec
slerena <slerena@gmail.com>
2008-01-10 17:59:04 +0000

    2008-01-10 Sancho Lerena <slerena@gmail.com>
            * pandoradbdata_13_to_14.sql: Correct build and version.
    
            * tactical.php: Fixed problem with networkserver stats.
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@701 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/pandoradbdata_13_to_14.sql
+++ pandora_console/pandoradbdata_13_to_14.sql
@@ -2,8 +2,8 @@
 
 -- New data
 
-UPDATE tconfig SET value = '1.4dev' WHERE token = 'db_scheme_version';
-UPDATE tconfig SET value = '1.4dev' WHERE token = 'db_scheme_build';
+UPDATE tconfig SET value = '1.4-dev' WHERE token = 'db_scheme_version';
+UPDATE tconfig SET value = 'PD080110' WHERE token = 'db_scheme_build';
 
 INSERT INTO `ttipo_modulo` VALUES (100,'keep_alive',-1,'KeepAlive','mod_keepalive.png'), (19, 'image_jpg',4,'Image JPG data', 'mod_image_jpg.png'), (20, 'image_png',4,'Image PNG data', 'mod_image_png.png'), (21, 'async_proc', 5, 'Asyncronous proc data', 'mod_async_proc.png'), (22, 'async_data', 5, 'Asyncronous numeric data', 'mod_async_data.png'), (23, 'async_string', 5, 'Asyncronous string data', 'mod_async_string.png');
 

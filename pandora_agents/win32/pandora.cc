7d1bba6c49b806f6c3d7373c8af6805189f14ee4
ramonn <noreply@pandorafms.org>
2008-06-10 11:18:11 +0000

    2008-06-10  Ramon Novoa  <rnovoa@artica.es>
    	* win32/pandora.cc: Updated version information.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@849 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("1.3.1(Build 080429)")
+#define PANDORA_VERSION ("2.0(Build 080610)")
 
 string pandora_path;
 string pandora_dir;

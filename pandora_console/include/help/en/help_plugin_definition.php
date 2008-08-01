c657f95ba3d02fc5b5696665f97805525a63a8cb
marostegui <noreply@pandorafms.org>
2008-08-01 06:17:34 +0000

    2008-08-01  Manuel Arostegui <marostegui@artica.es>
    	* include/help/en/help_plugin_definition.php: Language corrections.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@995 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_plugin_definition.php
+++ pandora_console/include/help/en/help_plugin_definition.php
@@ -1,8 +1,8 @@
 <h1>Plugin registration</h1>
 
-Pandora FMS Plugin registration tool is used to define what parameters needs Pandora FMS Plugin server to use in each plugin, and what kind of data pass after that parameter.
+Pandora FMS Plugin registration tool is used to define what parameters Pandora FMS Plugin server needs to be used in each plugin, and what kind of data will pass after that parameter.
 <br><br>
-For example, you have a plugin to check Informix tablespace called "Aleph", under IP address "192.168.50.2" and with username "Calabria" and password "malcolm45". This plugin could return if tablespace it's ok, number of queries per second, load, fragmentation level and memory usage.
+For example, if you have a plugin to check Informix tablespace called "Aleph", under IP address "192.168.50.2" and with username "Calabria" and password "malcolm45". This plugin could return if tablespace is ok, the number of queries per second, load, fragmentation level and memory usage.
 <br><br>
 This plugin has the following interface:
 <br>
@@ -10,7 +10,7 @@ This plugin has the following interface:
 	informix_plugin_pandora -H ip_address -U user -P password -T tablespace -O operation
 </pre>
 <br>
-Operation could be status, qps, load, fragment and memory. It returns a single value, used by Pandora FMS.  To define this plugin in pandora, you need to fill fields in this way:
+Operation could be status, qps, load, fragment and memory. It returns a single value, used by Pandora FMS.  To define this plugin in pandora, you would need to fill fields as follows:
 <br><br>
 
 <table cellpadding=4 cellspacing=4 class=databox width=80%>
@@ -22,7 +22,7 @@ Operation could be status, qps, load, fragment and memory. It returns a single v
 <td>IP Address option:<td> -H
 
 <tr>
-<td>Port option<td>Let it blank
+<td>Port option<td>Left it blank
 
 <tr>
 <td>User option<td>-U
@@ -33,9 +33,9 @@ Operation could be status, qps, load, fragment and memory. It returns a single v
 </table>
 <br>
 
-When you need to create a module that uses this plugin, you need to choose plugin (this new plugin will apear in the combo to select it). And you only need to fill IP Address, Username, and Password. Pandora FMS will put this data into appopiate fields when exec external plugin.
+If you needed to create a module that uses this plugin, you would need to choose the plugin (this new plugin would apear in the combo to be selected). Afterwards only to fill the IP Address, Username, and Password would be required. Pandora FMS will put this data into the appropiate fields when executing external plugins.
 <br><br>
-There is always some kind of parameters that cannot be "generic", in this example, you have "tablespace" parameter. This is very particular for Informix, but each example could have it's own exception. You have a field called "Plugin parameter", that it's used to pass "as is" to the plugin. In this particular case you need to put "-T tablespace" there. 
+There are always some sort of parameters which cannot be "generic", in this scenario, you would have "tablespace" parameter. This one is very particular for Informix, but each example could have its own exception. You would have a field called "Plugin parameter", that is used to pass "as is" to the plugin. In this particular case you would need to put "-T tablespace" there. 
 <br><br>
 If you want to use another tablespace, just create another module with different string after "-T".
 <br><br>

af105fa8455e15bea18e6fe958aceb5fe1062fa3
Esteban Sanchez <esteban.sanchez@gmail.com>
2008-08-04 08:53:10 +0000

    2008-08-04  Esteban Sánchez  <estebans@artica.es>
            * extensions/hello.php: Fixed old function name. Activate the 
            extension by default. Style correction.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@999 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/extensions/hello.php
+++ pandora_console/extensions/hello.php
@@ -1,16 +1,15 @@
 <?php
 
-/* Remove this statement to enabled the extension */
-return;
+/* You can safely delete this file */
 
 function hello_extension_main () {
 	/* Here you can do almost all you want! */
-        echo 'Hello world!';
+	echo 'Hello world!';
 }
 
 /* This adds a option in the operation menu */
 add_operation_menu_option ('Hello plugin!');
 
 /* This sets the function to be called when the extension is selected in the operation menu */
-add_plugin_main_function ('hello_extension_main');
+add_extension_main_function ('hello_extension_main');
 ?>

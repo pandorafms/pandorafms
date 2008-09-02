c6316bdcc9779cdeb882293b3f6813c56284672f
slerena <slerena@gmail.com>
2008-09-02 11:55:54 +0000

    2008-09-02  Sancho Lerena <slerena@gmail.com>
            *  extensions/update_manager*: Updated GPL header. Added 
            more text about Update Manager anonymous information gathering.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1067 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/extensions/update_manager/lib/libupdate_manager_utils.php
+++ pandora_console/extensions/update_manager/lib/libupdate_manager_utils.php
@@ -1,5 +1,22 @@
 <?php
 
+// Pandora FMS - the Flexible Monitoring System
+// ============================================
+// Copyright (c) 2008 Artica Soluciones Tecnologicas, http://www.artica.es
+// Please see http://pandora.sourceforge.net for full contribution list
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation for version 2.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 function add_prefix (&$string, $key, $prefix) {
 	$string = $prefix.'/'.$string;
 }

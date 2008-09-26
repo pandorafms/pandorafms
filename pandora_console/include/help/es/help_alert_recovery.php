4d85159761514e72ce11ab7219feaa3697bd0039
jorgonzalez <aloriel@gmail.com>
2008-09-26 10:48:12 +0000

    2008-09-25  Jorge Gonzalez  <jorgegonz@artica.es>
            * include/help/es: created directory for Spanish help files.
    
            * include/help/es/help_map_builder.php,
              include/help/es/help_alert_recovery.php,
              include/help/es/help_network_component.php,
              include/help/es/help_module_definition.php: Added some Spanish
              help files.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1118 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/es/help_alert_recovery.php
@@ -0,0 +1,3 @@
+<h1>Recuperación de alerta</h1>
+
+Define si Pandora FMS lanza otra alerta cuando la condición de la alerta se recupera. Tiene el mismo «campo1» pero añade «[RECOVER]]» al «campo2» y al «campo3». De forma predeterminada está desactivada.

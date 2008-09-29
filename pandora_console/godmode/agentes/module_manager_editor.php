ab77462b0f4eb34a3bdceb645ab193d6ba65070b
guruevi <noreply@pandorafms.org>
2008-09-29 15:36:03 +0000

    2008-09-29  Evi Vanoost  <vanooste@rcbi.rochester.edu>
            * godmode/agentes/module_manager_editor_data.php,
            godmode/agentes/module_manager_editor.php,
            godmode/agentes/configurar_agente.php: Style updates. These files now
            use the database functions and can be used as a template for the
            others. Resolved bug #2124706 where a blank page would come up in
            case the module type would be wrong. Now displays an error message
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1122 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/agentes/module_manager_editor.php
+++ pandora_console/godmode/agentes/module_manager_editor.php
@@ -19,14 +19,13 @@
 
 
 // General startup for established session
-global $config;
 check_login();
 
 // Specific ACL check
-if (give_acl($config["id_user"], 0, "AW")!=1) {
-    audit_db($config["id_user"], $REMOTE_ADDR, "ACL Violation","Trying to access agent manager");
-    require ($config["homedir"]."/general/noaccess.php");
-    exit;
+if (give_acl ($config["id_user"], 0, "AW") != 1) {
+	audit_db($config["id_user"], $REMOTE_ADDR, "ACL Violation","Trying to access agent manager");
+	require ($config["homedir"]."/general/noaccess.php");
+	exit;
 }
 
 // Following variables come from module_manager.php -> configurar_agente.php :
@@ -34,7 +33,7 @@ if (give_acl($config["id_user"], 0, "AW")!=1) {
 // $form_moduletype: could be [1] Agent module/Data server, [2] network server, [4] plugin server, [6] wmiserver, or [5] predictionserver
 // $moduletype: helper to fix get/post method; copy of $form_moduletype just to edit modules, not to create them
 
-if (($form_moduletype == "") && ($moduletype != "")){
+if (($form_moduletype == "") && ($moduletype != "")) {
 	switch ($moduletype) {
 		case "1":	
 			$form_moduletype = "dataserver";
@@ -51,91 +50,100 @@ if (($form_moduletype == "") && ($moduletype != "")){
 		case "6":
 			$form_moduletype = "wmiserver";
 			break;
+		//This will make sure that blank pages will
+		//have at least some debug info in them
+		default:
+			echo '<h3 class="error">DEBUG: Invalid module type specified in '.__FILE__.':'.__LINE__.'</h3>';
+			echo 'Most likely you have recently upgraded from an earlier version of Pandora and either <br />
+				1) forgot to use the database converter<br />
+				2) used a bad version of the database converter (see Bugreport #2124706 for the solution)<br />
+				3) found a new bug - please report a way to duplicate this error';
+			return; //We return control to the invoking script so the page finishes rendering
 	}
 }
 
-// Get form (GET)
-$form_network_component = get_parameter_get("form_network_component", "");
-if($form_network_component == "")
-	$form_network_component = get_parameter_post("form_network_component", "");
+// Get form
+$form_network_component = get_parameter ("form_network_component", "");
 
 // Using network component to fill some fields
-if (($form_moduletype == "networkserver" || $form_moduletype == "wmiserver") && ($form_network_component != "") && (!isset($_POST['crtbutton'])) && (!isset($_POST['oid']))){
-    // Preload data from template
-    $row = get_db_row ("tnetwork_component", 'id_nc', $form_network_component);
-    if ($row == 0){
-        unmanaged_error("Cannot load tnetwork_component reference from previous page");
-    }
-    $form_id_tipo_modulo = $row["type"];
-    $form_id_module_group = $row["id_module_group"];
-    $form_name = $row["name"];
-    $form_descripcion = $row["description"];
-    $form_tcp_send = $row["tcp_send"];
-    $form_tcp_rcv = $row["tcp_rcv"];
-    $form_tcp_port = $row["tcp_port"];
-    $form_snmp_community = $row["snmp_community"];
-    $form_snmp_oid = $row["snmp_oid"];
-    $form_id_module_group = $row["id_module_group"];
-    $form_interval = $row["module_interval"];
-    $form_maxvalue = $row["max"];
-    $form_minvalue = $row["min"];
-    $form_max_timeout = $row["max_timeout"];
-    $form_id_export = 0;
-    $form_disabled = 0;
-    $form_plugin_user = $row["plugin_user"];
-    $form_plugin_pass = $row["plugin_pass"];
-    $form_plugin_parameter = $row["plugin_parameter"];
-    $form_prediction_module = "";
-    $form_id_plugin = "";
-    $form_post_process = "";
-} elseif (!isset($_POST['oid'])){
-    // Clean up specific network modules fields
-    $form_name = "";
-    $form_description = "";
-    $form_id_module_group = 1;
-    $form_id_tipo_modulo = 1;
-    $form_post_process = "";
-    $form_max_timeout = "";
-    $form_minvalue = "";
-    $form_maxvalue = "";
-    $form_interval = "";
-    $form_prediction_module = "";
-    $form_id_plugin = "";
-    $form_id_export = "";
-    $form_disabled= "0";
-    $form_tcp_send = "";
-    $form_tcp_rcv = "";
-    $form_tcp_port = "";
-    $form_snmp_community = "public";
-    $form_snmp_oid = "";
-    $form_ip_target = $direccion_agente; // taken from configurar_agente.php
-    $form_plugin_user = "";
-    $form_plugin_pass = "";
-    $form_plugin_parameter = "";
+if (($form_moduletype == "networkserver" || $form_moduletype == "wmiserver") && ($form_network_component != "") && (!isset($_POST['crtbutton'])) && (!isset($_POST['oid']))) {
+	// Preload data from template
+	$row = get_db_row ("tnetwork_component", 'id_nc', $form_network_component);
+	if (empty ($row))
+		unmanaged_error ("Cannot load tnetwork_component reference from previous page");
+
+	$form_id_tipo_modulo = $row["type"];
+	$form_id_module_group = $row["id_module_group"];
+	$form_name = $row["name"];
+	$form_descripcion = $row["description"];
+	$form_tcp_send = $row["tcp_send"];
+	$form_tcp_rcv = $row["tcp_rcv"];
+	$form_tcp_port = $row["tcp_port"];
+	$form_snmp_community = $row["snmp_community"];
+	$form_snmp_oid = $row["snmp_oid"];
+	$form_id_module_group = $row["id_module_group"];
+	$form_interval = $row["module_interval"];
+	$form_maxvalue = $row["max"];
+	$form_minvalue = $row["min"];
+	$form_max_timeout = $row["max_timeout"];
+	$form_id_export = 0;
+	$form_disabled = 0;
+	$form_plugin_user = $row["plugin_user"];
+	$form_plugin_pass = $row["plugin_pass"];
+	$form_plugin_parameter = $row["plugin_parameter"];
+	$form_prediction_module = "";
+	$form_id_plugin = "";
+	$form_post_process = "";
+	
+} elseif (!isset($_POST['oid'])) {
+	// Clean up specific network modules fields
+	$form_name = "";
+	$form_description = "";
+	$form_id_module_group = 1;
+	$form_id_tipo_modulo = 1;
+	$form_post_process = "";
+	$form_max_timeout = "";
+	$form_minvalue = "";
+	$form_maxvalue = "";
+	$form_interval = "";
+	$form_prediction_module = "";
+	$form_id_plugin = "";
+	$form_id_export = "";
+	$form_disabled= "0";
+	$form_tcp_send = "";
+	$form_tcp_rcv = "";
+	$form_tcp_port = "";
+	$form_snmp_community = "public";
+	$form_snmp_oid = "";
+	$form_ip_target = $direccion_agente; // taken from configurar_agente.php
+	$form_plugin_user = "";
+	$form_plugin_pass = "";
+	$form_plugin_parameter = "";
 }
 
-// Data Server
-if ($form_moduletype == "dataserver"){
-    include $config["homedir"]."/godmode/agentes/module_manager_editor_data.php";
+switch ($form_moduletype) {
+	case "dataserver":
+		include $config["homedir"]."/godmode/agentes/module_manager_editor_data.php";
+		break;
+	case "networkserver":
+		include $config["homedir"]."/godmode/agentes/module_manager_editor_network.php";
+		break;
+	case "pluginserver":
+		include $config["homedir"]."/godmode/agentes/module_manager_editor_plugin.php";
+		break;
+	case "predictionserver":
+		include $config["homedir"]."/godmode/agentes/module_manager_editor_prediction.php";
+		break;
+	case "wmiserver":
+		include $config["homedir"]."/godmode/agentes/module_manager_editor_wmi.php";
+		break;
+	default:
+		echo '<h3 class="error">DEBUG: Invalid module type specified in '.__FILE__.':'.__LINE__.'</h3>';
+		echo 'Most likely you have recently upgraded from an earlier version of Pandora and either <br />
+			1) forgot to use the database converter<br />
+			2) used a bad version of the database converter (see Bugreport #2124706 for the solution)<br />
+			3) found a new bug - please report a way to duplicate this error';
+		return;
+																				
 }
-
-// Network server
-if ($form_moduletype == "networkserver"){
-    include $config["homedir"]."/godmode/agentes/module_manager_editor_network.php";
-}
-
-// Plugin server
-if ($form_moduletype == "pluginserver"){
-    include $config["homedir"]."/godmode/agentes/module_manager_editor_plugin.php";
-}
-
-// Prediction server
-if ($form_moduletype == "predictionserver"){
-    include $config["homedir"]."/godmode/agentes/module_manager_editor_prediction.php";
-}
-
-// WMI server
-if ($form_moduletype == "wmiserver"){
-    include $config["homedir"]."/godmode/agentes/module_manager_editor_wmi.php";
-}
-
+?>

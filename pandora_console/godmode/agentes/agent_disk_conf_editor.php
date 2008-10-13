a508f64b7e07c0d6b64ae257ca73100a1e59055b
slerena <slerena@gmail.com>
2008-10-13 17:13:34 +0000

    2008-10-13  Sancho Lerena  <slerena@artica.es>
    	* agent_disk_conf_editor.php: Now load enterprise code
    	using a normalized code.
    
    	* setup.php: Removed some features of enterprise on setup.
    
    	* functions.php: Added enterprise load code and load hook 
    	functions.
    
    	* datos_agente.php: Fixed stupid problem with string data
    	deletion.
    
    	* custom_reporting.php:  Now load enterprise code
    	using a normalized code.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1147 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/agentes/agent_disk_conf_editor.php
+++ pandora_console/godmode/agentes/agent_disk_conf_editor.php
@@ -1,10 +1,12 @@
 <?PHP
 
-echo "<h3>This feature is not included on OpenSource version. Please visit our website to learn more about the advanced features of <a href='http://pandorafms.com'>Pandora FMS Enterprise edition</a></h3>";
+if (enterprise_include ("godmode/agentes/agent_disk_conf_editor.php"))
+	return;
 
-	// Footer
-	echo "</div><div id='foot'>";
-	include ("general/footer.php");
-	echo "</div>";
+echo "<h3>This feature is not included on OpenSource version. Please visit our website to learn more about the advanced features of <a href='http://pandorafms.com'>Pandora FMS Enterprise edition</a></h3>";
+// Footer
+echo "</div><div id='foot'>";
+include ("general/footer.php");
+echo "</div>";
 
 ?>

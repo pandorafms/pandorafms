8fad28d9468980efa8af8105904a748387466325
guruevi <noreply@pandorafms.org>
2008-10-20 17:25:37 +0000

    2008-10-20  Evi Vanoost  <vanooste@rcbi.rochester.edu>
            * pandoradb_data.sql, pandora_db.sql: id_npc was rendered unnecessary
            with the last rewrite of the profiles page and id_np should really be
            the key since that what we're selecting/sorting on.
    
            * operation/events/events.php: Fixed Bug #2174790 and fixed the
            brackets being in all the wrong places. Also added checks so that only
            admins can retrieve system events
    
            * include/functions_db.php: Added an option to get_user_groups so that
            other privileges than AR can be retrieved.
    
            * operation/visual_console/index.php: Added ACL check. Security issue 
            reported in bug #2174790
    
            * operation/menu.php: Added ACL check. Security issue reported in bug 
            #2174790
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1177 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/visual_console/index.php
+++ pandora_console/operation/visual_console/index.php
@@ -19,19 +19,15 @@
 
 
 // Login check
-global $REMOTE_ADDR;
-
 check_login ();
 
-echo "<h2>".__('Visual console')." &gt; ";
-echo __('Summary')."</h2>";
+echo "<h2>".__('Visual console')." &gt; ".__('Summary')."</h2>";
+
 
 $layouts = get_db_all_rows_in_table ('tlayout','name');
 
-if (sizeof ($layouts) == 0) {
-	echo "<div class='nf'>".__('There are no defined layouts')."</div>";
-	return;
-}
+if ($layouts === false)
+	$layouts = array ();
 
 $table->width = '500px';
 $table->data = array ();
@@ -41,22 +37,28 @@ $table->head[1] = __('Group');
 $table->head[2] = __('Elements');
 $table->align = array ();
 $table->align[2] = 'center';
-if ($layouts){
-	foreach ($layouts as $layout) {
-		$data = array ();
-		
-		$data[0] = '<a href="index.php?sec=visualc&sec2=operation/visual_console/render_view&id='.
-			$layout['id'].'">'.$layout['name'].'</a>';
-		$data[1] = '<img src="images/'.dame_grupo_icono($layout["id_group"]).'.png" 
-			title="'.dame_nombre_grupo ($layout["id_group"]).'"> ';
-		$data[1] .= dame_nombre_grupo ($layout["id_group"]);
-		$data[2] = get_db_value ('COUNT(*)', 'tlayout_data', 'id_layout', $layout['id']);
-		
-		array_push ($table->data, $data);
+
+foreach ($layouts as $layout) {
+	if (!give_acl ($config["id_user"], $layout["id_group"], "AR")) {
+		continue;
 	}
+	$data = array ();
+	
+	$data[0] = '<a href="index.php?sec=visualc&sec2=operation/visual_console/render_view&id='.
+		$layout['id'].'">'.$layout['name'].'</a>';
+	$data[1] = '<img src="images/'.dame_grupo_icono($layout["id_group"]).'.png" 
+		title="'.dame_nombre_grupo ($layout["id_group"]).'"> ';
+	$data[1] .= dame_nombre_grupo ($layout["id_group"]);
+	$data[2] = get_db_value ('COUNT(*)', 'tlayout_data', 'id_layout', $layout['id']);
+		
+	array_push ($table->data, $data);
+}
+
+if (!empty ($table->data)) {
 	print_table ($table);
 } else {
-	echo '<div class="nf">'.__('There are no visual consoles defined').'</div>';
+	echo '<div class="nf">'.__('No layouts found').'</div>';
 }
+unset ($table);
 
 ?>

1a144f11cba7654df07c7d47b201f97f0968a5fd
slerena <slerena@gmail.com>
2008-10-30 13:15:18 +0000

    2008-10-30  Sancho Lerena <slerena@gmail.com>
            * lib/PandoraFMS/DB.pm:  Some people reports problems with
            server stop detection. This could help, because has optimized
            way to detect servers, but is not a bugfix because I cannot
            reproduce the problem.
    
            * Config.pm: Updated version (2.1)
    
            * server/Launcher scripts: Updated version (2.1)
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1205 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -38,8 +38,8 @@ our @EXPORT = qw(
 # There is no global vars, all variables (setup) passed as hash reference
 
 # version: Defines actual version of Pandora Server for this module only
-my $pandora_version = "2.0";
-my $pandora_build="PS081014";
+my $pandora_version = "2.1-dev";
+my $pandora_build="PS081030";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash

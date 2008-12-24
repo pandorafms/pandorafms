c463b65c29de7f0a0d8e3103f0041b7637d0aafc
raulmateos <noreply@pandorafms.org>
2008-12-24 12:51:59 +0000

    2008-12-24  Raul Mateos  <raulofpandora@gmail.com>
    	* index.php: Updated build, again due to the use of the new custom ID.
    
    	* reporting/fgraph2.php: Test for ezcomponents.
    
    	* operation/events/event_statistics.php: Use of fgraph2 if test var is
    	set in the URL. It uses ezComponents, that need to be installed. 
    	This shows a 3D chart. Probably not will be used, but it's an idea
    	about how to improve graphs in pandora. To test, just add &test to the
    	event_statistics URL.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1308 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/index.php
+++ pandora_console/index.php
@@ -20,7 +20,7 @@
 // Silk icon set 1.3 (cc) Mark James, http://www.famfamfam.com/lab/icons/silk/
 // Pandora FMS uses Pear Image::Graph code
 
-$build_version="PC081221";
+$build_version="PC081224";
 $pandora_version="v2.1-dev";
 global $build_version;
 global $pandora_version;

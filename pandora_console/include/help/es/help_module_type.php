19125f7d0e0a0dd23b09e92b4a7c8a46db7ef080
jorgonzalez <aloriel@gmail.com>
2009-01-21 13:33:23 +0000

    2009-01-21  Jorge Gonzalez  <jorgegonz@svn.gnome.org>
            * en/help_wmiquery.php: Modified html tags.
    
            * es/help_plugin_parameters.php, es/help_wmiquery.php,
              es/help_snmpcommunity.php, es/help_wmikey.php,
              es/help_module_type.php, es/help_wminamespace.php,
              es/help_duplicateconfig.php, es/help_wmifield.php: Added Spanish  
              help files.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_2.0@1382 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/es/help_module_type.php
@@ -0,0 +1,26 @@
+<h1>Tipo de módulo</h1>
+
+La siguiente lista describe los tipos de módulos:
+<ul>
+	<li>async_data: Datos numéricos asíncronos</li>
+	<li>async_inc: Datos incrementales asíncronos</li>
+	<li>async_proc: Datos asíncronos booleanos</li>
+	<li>async_string: Datos asíncronos tipo cadena</li>
+	<li>generic_data: Módulo genérico para adquirir datos numéricos</li>
+	<li>generic_data_inc: Módulo genérico para adquirir datos numéricos incrementales</li>
+	<li>generic_data_string: Módulo genérico para adquirir datos alfanuméricos</li>
+	<li>generic_proc: Módulo genérico para adquirir datos booleanos</li>
+	<li>image_jpg: Imagen JPG</li>
+	<li>image_png: Imagen PNG</li>
+	<li>keep_alive: KeepAlive</li>
+	<li>remote_icmp: Agente de red remoto ICMP (latencia)</li>
+	<li>remote_icmp_proc: Agente de red remoto ICMP, datos booleanos</li>
+	<li>remote_snmp: Agente de red remoto SNMP, datos numéricos</li>
+	<li>remote_snmp_inc: Agente de red remoto SNMP, datos incrementales</li>
+	<li>remote_snmp_proc: Agente de red remoto SNMP, datos booleanos</li>
+	<li>remote_snmp_string: Agente de red remoto SNMP, datos alfanuméricos</li>
+	<li>remote_tcp: Agente de red remoto TCP, datos numéricos</li>
+	<li>remote_tcp_inc: Agente de red remoto TCP, incremental data</li>
+	<li>remote_tcp_proc: Agente de red remoto TCP, datos booleanos</li>
+	<li>remote_tcp_string: Agente de red remoto TCP, datos alfanuméricos</li>
+</ul>

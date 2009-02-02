784c3d422b116f3f21486a3e2ea7f35c1844b0de
Esteban Sanchez <esteban.sanchez@gmail.com>
2009-02-02 15:22:06 +0000

    2009-02-02  Esteban Sanchez  <estebans@artica.es>
    	* godmode/agentes/configurar_agente.php: Removed an unneccesary "k"
    	character on top of file.
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_2.0@1416 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/agentes/configurar_agente.php
+++ pandora_console/godmode/agentes/configurar_agente.php
@@ -16,7 +16,6 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-
 // Load global vars
 require("include/config.php");
 

801344cb7e3bbf0acb8b36092153d72f0ef639f9
guruevi <noreply@pandorafms.org>
2009-02-10 20:29:45 +0000

    2009-02-10  Evi Vanoost  <vanooste@rcbi.rochester.edu>
    	* general/main_menu.php: Modified it to go with Esteban's idea
    	
    	* include/styles/menu.css: Modified it to go with Esteban's idea
    
    	* include/javascript/jquery.pandora.js: Removed previous jquery stuff
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1441 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/javascript/jquery.pandora.js
+++ pandora_console/include/javascript/jquery.pandora.js
@@ -1,18 +1,4 @@
 $(document).ready (function () {
-	/* Menu activation */
-	$(".toggle").click (function () {
-		parents = $(this).parents ("li");
-		if ($(parents).hasClass ("has_submenu_visible")) {
-			$(".submenu", parents).hide ();
-			$(parents).removeClass ("has_submenu_visible");
-			$.cookie ($(parents).attr ("id"), null);
-			return;
-		}
-		$(parents).addClass ("has_submenu_visible");
-		$(".submenu", parents).show ();
-		$.cookie ($(parents).attr ("id"), true);
-	});
-
 	$.fn.check = function () {
 		return this.each (function () {
 			this.checked = true;

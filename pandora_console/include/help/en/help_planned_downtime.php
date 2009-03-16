160cf02a1cf671c60274b8a291852a5cdee7aab1
jorgonzalez <aloriel@gmail.com>
2009-03-16 11:12:40 +0000

    2009-03-16  Jorge Gonzalez  <jorgegonz@artica.es>
            * include/help/en/help_planned_downtime.php,
              include/help/es/help_planned_downtime.php: Added more information
              about the scheduled downtime.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1538 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_planned_downtime.php
+++ pandora_console/include/help/en/help_planned_downtime.php
@@ -1,11 +1,11 @@
-<h1>Planned downtimes</h1>
+<h1>Scheduled downtimes</h1>
 
 <p>
 This tool is used to plan non-monitoring periods of time. This is useful if you know, for example, that a group of systems will be disconnected in a specific time. This helps to avoid false alarms. 
 </p>
 <p>
-It's very easy to setup, you specify start date/time of planned downtime and an end date/time. You can include in that downtime a list of agents.
+It's very easy to setup, you specify start date/time of a scheduled downtime and an end date/time. After setting the first fields you must save the Scheduled downtime and edit it, to set the agents which are going to be disconnected. You can also edit the rest of the fields once editing the Scheduled downtime entry.
 </p>
 <p>
-When planned downtime starts, Pandora FMS automatically disable all agents assigned to this downtime and no alerts or data are processed. When downtime ends, Pandora FMS will be enable all agents assigned to this downtime. You cannot delete or modify a downtime instance when it's fired, you need to wait for ending before doing anything in this downtime instance. Of course you can manually, enable an agent using the agent configuration dialog.
+When scheduled downtime starts, Pandora FMS automatically disable all agents assigned to this downtime and no alerts or data are processed. When downtime ends, Pandora FMS will be enable all agents assigned to this downtime. You cannot delete or modify a downtime instance when it's fired, you need to wait for ending before doing anything in this downtime instance. Of course you can manually, enable an agent using the agent configuration dialog.
 </p>

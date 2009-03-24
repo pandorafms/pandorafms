2257df6d99245d2dce6d9353e41f9488478d3155
esanchezm <noreply@pandorafms.org>
2009-03-24 14:06:23 +0000

    2009-03-24  Esteban Sanchez  <estebans@artica.es>
            * udp_server/udp_server.cc, udp_server/udp_server.h: Replaced
            blankspaces with tabs.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1556 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/win32/udp_server/udp_server.h
+++ pandora_agents/win32/udp_server/udp_server.h
@@ -14,10 +14,10 @@ namespace Pandora {
 		public:
 			UDP_Server (Pandora_Windows_Service *service, string address, string auth_address, unsigned int port);
 			~UDP_Server ();
-            unsigned long getAddress ();
-            unsigned long getAuthAddress ();
-            unsigned long getPort ();
-            Pandora_Windows_Service *getService ();
+			unsigned long getAddress ();
+			unsigned long getAuthAddress ();
+			unsigned long getPort ();
+			Pandora_Windows_Service *getService ();
 			unsigned char isRunning ();
 
 			int start ();
@@ -25,13 +25,13 @@ namespace Pandora {
 
 		private:
 			unsigned long address;
-            unsigned long auth_address;
+			unsigned long auth_address;
 			unsigned long port;
 			unsigned char running;
 			Pandora_Windows_Service *service;
 	};
 
-    void listen (UDP_Server *server);
+	void listen (UDP_Server *server);
 	int process_command (Pandora_Windows_Service *service, char *command);
 }
 

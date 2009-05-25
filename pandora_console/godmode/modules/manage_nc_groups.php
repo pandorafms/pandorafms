d4b2f7863d0bffefe9de87866e50c0398e853b0a
Esteban Sanchez <esteban.sanchez@gmail.com>
2009-05-25 13:29:48 +0000

    2009-05-25  Esteban Sanchez  <estebans@artica.es>
    	* general/ui/agents_list.php: Fixed pagination javascript selector
    	to avoid collisions.
    
    	* godmode/modules/manage_nc_groups.php,
    	godmode/modules/manage_nc_groups_form.php: Rewritten to use pandora
    	functions. Code cleanup.
    
    	* godmode/modules/manage_network_components.php: Fixed string search
    	query.
    
    	* include/functions_network_components.php: Added
    	get_network_component_group().
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1705 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/modules/manage_nc_groups.php
+++ pandora_console/godmode/modules/manage_nc_groups.php
@@ -25,15 +25,17 @@ if (! give_acl ($config['id_user'], 0, "PM")) {
 	audit_db ($config['id_user'], $REMOTE_ADDR, "ACL Violation",
 		"Trying to access SNMP Group Management");
 	require ("general/noaccess.php");
-	exit;
+	return;
 }
+
+require_once ('include/functions_network_components.php');
 
 $create = (bool) get_parameter ('create');
 $update = (bool) get_parameter ('update');
 $delete = (bool) get_parameter ('delete');
-
-echo '<h2>'.__('Module management').' &raquo; '. __('Component group management').'</h2>';
-
+$new = (bool) get_parameter ('new');
+$id = (int) get_parameter ('id');
+	
 if ($create) {
 	$name = (string) get_parameter ('name');
 	$parent = (int) get_parameter ('parent');
@@ -47,7 +49,6 @@ if ($create) {
 }
 
 if ($update) {
-	$id = (int) get_parameter ('id_sg');
 	$name = (string) get_parameter ('name');
 	$parent = (int) get_parameter ('parent');
 	
@@ -60,9 +61,7 @@ if ($update) {
 		__('Not updated. Error updating data'));
 }
 
-if ($delete) { // if delete
-	$id = (int) get_parameter ('id_sg');
-	
+if ($delete) {
 	$result = process_sql_delete ('tnetwork_component_group',
 		array ('id_sg' => $id));
 	print_result_message ($result,
@@ -70,40 +69,72 @@ if ($delete) { // if delete
 		__('Not deleted. Error deleting data'));
 }
 
+if ($id || $new) {
+	require_once ('manage_nc_groups_form.php');
+	return;
+}
+
+echo '<h2>'.__('Module management').' &raquo; '. __('Component group management').'</h2>';
+
+$url = get_url_refresh (array ('offset' => false,
+	'create' => false,
+	'update' => false,
+	'delete' => false,
+	'new' => false,
+	'crt' => false,
+	'upd' => false,
+	'id_sg' => false));
+
 $table->width = '90%';
 $table->head = array ();
 $table->head[0] = __('Name');
 $table->head[1] = __('Parent');
-$table->head[2] = __('Delete');
+$table->head[2] = '';
 $table->style = array ();
 $table->style[0] = 'font-weight: bold';
 $table->align = array ();
 $table->align[2] = 'center';
+$table->size = array ();
+$table->size[0] = '50%';
+$table->size[1] = '50%';
+$table->size[2] = '40px';
 $table->data = array ();
 
-$groups = get_db_all_rows_filter ('tnetwork_component_group',
-	array ('order' => 'parent'));
+$total_groups = get_db_all_rows_filter ('tnetwork_component_group', false, 'COUNT(*) AS total');
+$total_groups = $total_groups[0]['total'];
+
+$filter = array ();
+$filter['offset'] = (int) get_parameter ('offset');
+$filter['limit'] = (int) $config['block_size'];
+
+$groups = get_db_all_rows_filter ('tnetwork_component_group', $filter);
 if ($groups === false)
 	$groups = array ();
+
+pagination ($total_groups, $url);
 
 foreach ($groups as $group) {
 	$data = array ();
 	
-	$data[0] = '<a href="index.php?sec=gmodules&sec2=godmode/modules/manage_nc_groups_form&edit=1&id_sg='.$group["id_sg"].'">'.$group["name"].'</a>';
+	$data[0] = '<a href="index.php?sec=gmodules&sec2=godmode/modules/manage_nc_groups&id='.$group['id_sg'].'">'.$group['name'].'</a>';
 	
-	$data[1] = get_network_component_group_name ($group["parent"]);
-	$data[2] = '<a href="index.php?sec=gmodules&sec2=godmode/modules/manage_nc_groups&delete=1&id_sg='.$group["id_sg"].'"
-		onClick="if (!confirm(\''.__('Are you sure?').'\')) return false;">
-		<img src="images/cross.png"></a>';
+	$data[1] = get_network_component_group_name ($group['parent']);
+	
+	$data[2] = '<form method="post" onsubmit="if (! confirm (\''.__('Are you sure?').'\')) return false">';
+	$data[2] .= print_input_hidden ('delete', 1, true);
+	$data[2] .= print_input_hidden ('id', $group['id_sg'], true);
+	$data[2] .= print_input_image ('del', 'images/cross.png', 1, '', true,
+		array ('title' => __('Delete')));
+	$data[2] .= '</form>';
 	
 	array_push ($table->data, $data);
 }
 
 print_table ($table);
 
-echo '<form method="post" action="index.php?sec=gmodules&sec2=godmode/modules/manage_nc_groups_form">';
+echo '<form method="post">';
 echo '<div class="action-buttons" style="width: '.$table->width.'">';
-print_input_hidden ('create', 1);
+print_input_hidden ('new', 1);
 print_submit_button (__('Create'), 'crt', false, 'class="sub next"');
 echo '</div>';
 echo '</form>';

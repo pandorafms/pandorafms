690f984748d3c030f9bc52c42b39c618682e051b
slerena <slerena@gmail.com>
2009-06-08 18:17:33 +0000

    2009-06-08  Sancho Lerena <slerena@artica.es>
    	* Massive header update.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1735 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/events/events_rss.php
+++ pandora_console/operation/events/events_rss.php
@@ -1,20 +1,19 @@
 <?php
-// Pandora FMS - the Flexible monitoring system
-// ========================================
-// Copyright (c) 2004-2007 Sancho Lerena, slerena@openideas.info
-// Copyright (c) 2005-2007 Artica Soluciones Tecnologicas
-// Copyright (c) 2008-2009 Evi Vanoost, vanooste@rcbi.rochester.edu
+
+// Pandora FMS - http://pandorafms.com
+// ==================================================
+// Copyright (c) 2005-2009 Artica Soluciones Tecnologicas
+// Please see http://pandorafms.org for full contribution list
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation version 2
+// as published by the Free Software Foundation for version 2.
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 
 ini_set ('display_errors', 0); //Don't display other errors, messes up XML
 header("Content-Type: application/xml; charset=UTF-8"); //Send header before starting to output

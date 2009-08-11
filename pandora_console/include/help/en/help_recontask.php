9a2aa063baa5443fed138699bc482fe2fa031711
mdtrooper <tres.14159@gmail.com>
2009-08-11 15:36:47 +0000

    2009-08-11  Miguel de Dios <miguel.dedios@artica.es>
    	* godmode/servers/manage_recontask_form.php,
    	godmode/servers/manage_recontask.php, include/help/en/help_recontask.php,
    	operation/servers/view_server_detail.php: change strings for more easy
    	understand.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1837 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_recontask.php
+++ pandora_console/include/help/en/help_recontask.php
@@ -7,5 +7,5 @@ To create a Recon Task, you must fill this form. Please fill all the fields, pay
 "Network" refers to the network segment, and are defined using <b>CIDR</b> format. For example 192.168.1.0/24 refers to <i>all C class on 192.168.1.0</i>, wich includes all the addresses in the scope: 192.168.1.0 - 192.168.1.255.
 </p>
 <p>
-"Network profile" are predefined profiles for the reconnaissance task. It will add such modules defined in the profile to the newly discovered machines in the network.
+"Module template" are predefined profiles for the reconnaissance task. It will add such modules defined in the profile to the newly discovered machines in the network.
 </p>

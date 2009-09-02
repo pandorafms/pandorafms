61ac3d3dddbe810b969d4537d342161bd1f69e34
Ramon Novoa <rnovoa@artica.es>
2009-09-02 15:53:05 +0000

    2009-09-02  Ramon Novoa  <rnovoa@artica.es>
    	* include/help/en/help_alert_macros.php,
              include/help/es/help_alert_macros.php: Added to repository. Alert
              macro help files.
    
            * godmode/alerts/configure_alert_template.php: Show alert macro help.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1908 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_alert_macros.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Alert macros</h1>
+
+<p>
+The following macros are available:
+<ul>
+<li>_field1_: User defined field 1.</li>
+<li>_field2_: User defined field 2.</li>
+<li>_field3_: User defined field 3.</li>
+<li>_agent_: Name of the agent that fired the alert.</li>
+<li>_address_: Address of the agent that fired the alert.</li>
+<li>_timestamp_: Time when the alert was fired.</li>
+<li>_data_: Module data that caused the alert to fire.</li>
+<li>_alert_description_: Alert description.</li>
+<li>_alert_threshold_: Alert threshold.</li>
+<li>_alert_times_fired_: Number of times the alert has been fired.</li>
+</ul>
+<p>
+</p>
+Example: Agent _agent_ error: _alert_description_.
+</p>

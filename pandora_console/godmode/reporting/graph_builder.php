195bcc7930beaf495153cd9838fa304883ef4992
mdtrooper <tres.14159@gmail.com>
2009-09-14 14:38:13 +0000

    2009-09-14  Miguel de Dios  <miguel.dedios@artica.es>
    	* operation/agentes/exportdata.php, godmode/agentes/agent_manager.php,
    	godmode/reporting/map_builder.php, godmode/reporting/graph_builder.php,
    	godmode/alerts/alert_list.php: fix the bug that the div of autocomplete
    	don't show.
    	Fixes: 2856945
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1938 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/reporting/graph_builder.php
+++ pandora_console/godmode/reporting/graph_builder.php
@@ -590,7 +590,7 @@ $(document).ready (function () {
 					if (total == 0)
 						$("#text-id_agent").css ('background-color', '#cc0000');
 					else
-						$("#text-id_agent").css ('background-color', 'none');
+						$("#text-id_agent").css ('background-color', '');
 					if (data == "")
 						return false;
 					return data[0]+'<br><span class="ac_extra_field"><?php echo __("IP") ?>: '+data[1]+'</span>';

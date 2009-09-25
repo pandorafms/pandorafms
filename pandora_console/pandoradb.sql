a1be60d8373fc9da9b80017c11d0e992eec2dab7
slerena <slerena@gmail.com>
2009-09-25 00:21:00 +0000

    2009-09-25  Sancho lerena <slerena@artica.es>
            * pandoradb.sql: SUSE is not very happy with some comments, fixed.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1973 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/pandoradb.sql
+++ pandora_console/pandoradb.sql
@@ -596,17 +596,17 @@ CREATE TABLE IF NOT EXISTS `tserver` (
 	KEY `status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---server types:
---0 data
---1 network
---2 snmp trap console
---3 recon
---4 plugin
---5 prediction
---6 wmi
---7 export
---8 inventory
---9 web
+-- server types:
+-- 0 data
+-- 1 network
+-- 2 snmp trap console
+-- 3 recon
+-- 4 plugin
+-- 5 prediction
+-- 6 wmi
+-- 7 export
+-- 8 inventory
+-- 9 web
 -- TODO: drop 2.x xxxx_server fields, unused since server_type exists.
 
 CREATE TABLE IF NOT EXISTS `tsesion` (

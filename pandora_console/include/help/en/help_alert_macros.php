9451d48603817e767a77e6c40a9ab17cb213386a
slerena <slerena@gmail.com>
2009-09-28 16:06:06 +0000

    2009-09-28  Sancho Lerena <slerena@artica.es>
            * include/help/en/help_alert_macros.php: Updated macros help
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@1987 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_alert_macros.php
+++ pandora_console/include/help/en/help_alert_macros.php
@@ -8,18 +8,22 @@
 <p>
 The following macros are available:
 <ul>
-<li>_field1_: User defined field 1.</li>
-<li>_field2_: User defined field 2.</li>
-<li>_field3_: User defined field 3.</li>
-<li>_agent_: Name of the agent that fired the alert.</li>
-<li>_address_: Address of the agent that fired the alert.</li>
-<li>_timestamp_: Time when the alert was fired.</li>
-<li>_data_: Module data that caused the alert to fire.</li>
-<li>_alert_description_: Alert description.</li>
-<li>_alert_threshold_: Alert threshold.</li>
-<li>_alert_times_fired_: Number of times the alert has been fired.</li>
+<li>_field1_ : User defined field 1.</li>
+<li>_field2_ : User defined field 2.</li>
+<li>_field3_ : User defined field 3.</li>
+<li>_agent_ : Name of the agent that fired the alert.</li>
+<li>_address_ : Address of the agent that fired the alert.</li>
+<li>_timestamp_ : Time when the alert was fired (yy-mm-dd hh:mm:ss).</li>
+<li>_data_ : Module data that caused the alert to fire.</li>
+<li>_alert_description_ : Alert description.</li>
+<li>_alert_threshold_ : Alert threshold.</li>
+<li>_alert_times_fired_ : Number of times the alert has been fired.</li>
+<li>_module_ : Module name</li>
+<li>_alert_ : Alert name </li>
+<li>_alert_priority_ : Numerical alert priorityu </li>
+<li>_id_agent_ : Id of agent, useful to build direct URL to redirect to a Pandora FMS console webpage.
 </ul>
 <p>
 </p>
-Example: Agent _agent_ error: _alert_description_.
+Example: Agent _agent_ has fired alert _alert_ with data _data_
 </p>

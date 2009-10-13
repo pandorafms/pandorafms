80160587a7ff4fecea572f60483cb2a9bf7ca5f3
slerena <slerena@gmail.com>
2009-10-13 22:47:53 +0000

    2009-10-13  Sancho Lerena <slerena@artica.es>
            * util/pandora_server: Incremented shutting down grace time to 60 secs
            because in some systems could take a lot of time to close all threads.
    
            * conf/pandora_server.conf: Better defaults for Pandora FMS server default
            install.
    
            * Config.pm: Hey, this is officially RC1....
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2018 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -36,8 +36,8 @@ our @EXPORT = qw(
     );
 
 # version: Defines actual version of Pandora Server for this module only
-my $pandora_version = "3.0-dev";
-my $pandora_build = "PS090928";
+my $pandora_version = "3.0RC1";
+my $pandora_build = "PS091013";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash

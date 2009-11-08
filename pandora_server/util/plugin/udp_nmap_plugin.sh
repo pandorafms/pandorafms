4aaf65d5391f356ab967f55da7fe2e44c9c32404
slerena <slerena@gmail.com>
2009-11-08 01:38:04 +0000

    2009-11-07  Sancho Lerena <slerena@artica.es>
    	* pandora_server_installer: A few bunch of upgrades for SUSE, added
    	logrotate.d and some minor fixes.
    
    	* pandora_server_upgrade: Some minor fixes.
    
    	* PandoraFMS/Tools.pm: Fixes to internal SMTP Auth mechanism. 
    
    	* util/pandora_logrotate: Much better configuration for pandora 
    	logrotate config, this also rotate pandora_server.error and delete
    	files after 5 days.
    
    	* util/pandora_server: more fixes and small improvements to daemon
    	script: Removed Mysql dependency on LSB, added variable for let user
    	to choose timeout before KILL, and now checks for especific .conf file
    	on detecting PID, this should work with several servers running in the
    	same server.
    
    	* util/plugin/ssh_pandoraplugin.sh: rewritten to use real parameters
    	and be more verbose and easy to integrate in Pandora FMS Plugin server.
    
    	* util/plugin/udp_nmap_plugin.sh: Same as above.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2097 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/util/plugin/udp_nmap_plugin.sh
+++ pandora_server/util/plugin/udp_nmap_plugin.sh
@@ -1,5 +1,54 @@
 #!/bin/bash
-# This is called like -p xxx -h xxxx
-HOST=$4
-PORT=$2
+# UDP Scan (using nmap) Pandora FMS Server plugin
+# (c) Sancho Lerena 2008-2009
+
+
+# Default values
+PORT=""
+HOST=""
+
+function help {
+	echo -e "UDP Port Plugin for Pandora FMS Plugin server. http://pandorafms.com" 
+	echo -e "Syntax:" 
+	echo -e "\t\t-p port"
+	echo -e "\t\t-t hostname / target IP"
+	echo -e "Samples:"
+	echo "   ./udp_nmap_plugin.sh -p 137 -t 192.168.5.20"
+	echo ""
+	echo -e "Please note that -p accepts nmap multiport syntax (like: 135,138,139,200-300)\n\n"
+	exit
+}
+
+if [ $# -eq 0 ]
+then
+	help
+fi
+
+
+# Main parsing code
+
+while getopts ":hp:t:" optname
+  do
+    case "$optname" in
+      "h")
+	        help
+	;;
+      "p")
+		PORT=$OPTARG
+        ;;
+      "t")
+		HOST=$OPTARG
+        ;;
+       ?)
+		help
+		;;
+      default) 
+		help
+	;;
+     
+    esac
+done
+
+# execution
 nmap -T5 -p $PORT -sU $HOST | grep open | wc -l
+

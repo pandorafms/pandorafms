b090979cfc734588708e66ed52bea0ee029e85da
slerena <slerena@gmail.com>
2009-11-12 19:47:08 +0000

    2009-11-12  Sancho lerena <slerena@artica.es>
            * include/help/en/help_web_checks.php: Added missing help.
    
            * include/help/en/help_alert_macros.php: Added missing macros.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2106 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_web_checks.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>WEB Monitoring</h1>
+
+<p>
+Advanced WEB Monitoring is a feature done by the Goliat/WEB Server in Pandora FMS Enterprise version.
+<br><br>
+This is a sample of GOLIAT Webcheck module:
+<br>
+<pre>
+
+
+task_begin
+post http://galaga.artica.es/pandora/index.php?login=1
+variable_name nick
+variable_value demo
+variable_name pass
+variable_value demo
+cookie 1
+resource 1
+task_end
+
+task_begin
+get http://galaga.artica.es/pandora/index.php?sec=messages&sec2=operation/messages/message
+cookie 1
+resource 1
+check_string Read messages
+task_end
+
+
+</pre>
+</p>

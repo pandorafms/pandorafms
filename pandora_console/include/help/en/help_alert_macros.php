08959d32004e46b274806029248d1a437262c728
slerena <slerena@gmail.com>
2009-11-12 19:47:08 +0000

    2009-11-12  Sancho lerena <slerena@artica.es>
            * include/help/en/help_web_checks.php: Added missing help.
    
            * include/help/en/help_alert_macros.php: Added missing macros.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2106 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_alert_macros.php
+++ pandora_console/include/help/en/help_alert_macros.php
@@ -12,6 +12,8 @@ The following macros are available:
 <li>_field2_ : User defined field 2.</li>
 <li>_field3_ : User defined field 3.</li>
 <li>_agent_ : Name of the agent that fired the alert.</li>
+<li>_agentdescription_ : Description of the agent who fired alert</li>
+<li>_agentgroup_ : Agent group name </li>
 <li>_address_ : Address of the agent that fired the alert.</li>
 <li>_timestamp_ : Time when the alert was fired (yy-mm-dd hh:mm:ss).</li>
 <li>_data_ : Module data that caused the alert to fire.</li>
@@ -19,9 +21,13 @@ The following macros are available:
 <li>_alert_threshold_ : Alert threshold.</li>
 <li>_alert_times_fired_ : Number of times the alert has been fired.</li>
 <li>_module_ : Module name</li>
+<li>_moduledescription_ : Description of the module who fired the alert </li>
 <li>_alert_name_ : Alert name </li>
 <li>_alert_priority_ : Numerical alert priorityu </li>
 <li>_id_agent_ : Id of agent, useful to build direct URL to redirect to a Pandora FMS console webpage.
+<li>_id_alert_ : Numerical ID of the alert (unique), used to correlate on third party software</li>
+
+
 </ul>
 <p>
 </p>

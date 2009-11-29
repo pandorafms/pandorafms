54582319899852b1e03c0654f64d3ed1316033c3
raulmateos <noreply@pandorafms.org>
2009-11-29 13:35:16 +0000

    2009-11-29  Raul Mateos  <raulofpandora@gmail.com>
    	* linux/pandora_agent_installer: Added Ubuntu specific stop scripts.
    	Added link removing with update-rc.d. Changed temp directory (it
    	added duplicated data_out directories). Use tabs instead spaces.
    
    	* linux/pandora_agent_daemon: Use log file in /var/log/pandora/ directory.
    
    	* linux/pandora_agent.conf: Changed temp directory to the one created
    	by installer.
    
    	* linux/DEBIAN/control, DEBIAN/make_deb_package.sh: Changed string from rc2
    	to RC3.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2141 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/linux/DEBIAN/make_deb_package.sh
+++ pandora_agents/linux/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_agent_version="3.0.0.rc2"
+pandora_agent_version="3.0.0.RC3"
 
 echo "Test if you has the tools for to make the packages."
 whereis dpkg-deb | cut -d":" -f2 | grep dpkg-deb > /dev/null

55199847b524224a9dfc2266f6c557370171c84a
raulmateos <noreply@pandorafms.org>
2009-12-08 14:48:59 +0000

    2009-12-08  Raul Mateos  <raulofpandora@gmail.com>
    	* include/auth/*.php: added style to error when accessing the files
    	directly.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2177 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/auth/dev.php
+++ pandora_console/include/auth/dev.php
@@ -18,7 +18,39 @@
 
 
 if (!isset ($config)) {
-	die ('You cannot access this file directly!');
+	die ('
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Pandora FMS - The Flexible Monitoring System - Console error</title>
+<meta http-equiv="expires" content="0">
+<meta http-equiv="content-type" content="text/html; charset=utf8">
+<meta name="resource-type" content="document">
+<meta name="distribution" content="global">
+<meta name="author" content="Sancho Lerena">
+<meta name="copyright" content="This is GPL software. Created by Sancho Lerena and others">
+<meta name="keywords" content="pandora, monitoring, system, GPL, software">
+<meta name="robots" content="index, follow">
+<link rel="icon" href="../../images/pandora.ico" type="image/ico">
+<link rel="stylesheet" href="../styles/pandora.css" type="text/css">
+</head>
+<body>
+<div id="main" style="float:left; margin-left: 100px">
+<div align="center">
+<div id="login_f">
+	<h1 id="log_f" class="error">You cannot access this file</h1>
+	<div>
+		<img src="../../images/pandora_logo.png" border="0"></a>
+	</div>
+	<div class="msg">
+		<span class="error"><b>ERROR:</b>
+		You can\'t access this file directly!</span>
+	</div>
+</div>
+</div>
+</body>
+</html>
+');
 }
 
 $config["user_can_update_password"] = false;

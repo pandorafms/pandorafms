359c487eb94c6ed755e740f22b8a9cd7ee07addd
slerena <slerena@gmail.com>
2009-12-18 18:25:38 +0000

    2009-12-18  Sancho lerena <slerena@artica.es>
    	* pandora_console.spec: Ypdated build.
    
    	* pandora_console_upgrade: Applied patch for more verbose information, submitted in 
    	Sourceforge.
    
    	* pandoradb_data.sql: Updated version to FINAL build and version ;-)
    
    	* include/config_process.php: Updated build.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2221 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC091216';
+$build_version = 'PC091218';
 $pandora_version = 'v3.0';
 
 /* Help to debug problems. Override global PHP configuration */

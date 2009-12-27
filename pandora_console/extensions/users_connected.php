ffe5971a9a39c65a893131eac5f7326f9d81c630
raulmateos <noreply@pandorafms.org>
2009-12-27 21:31:54 +0000

    2009-12-27  Raúl Mateos  <raulofpandora@gmail.com>
    	* extensions/users/menu.png: New file
    
    	* extensions/users_connected.php : Added icon and changed menu section. Added new godmode function that allows the user to be edited.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2244 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/extensions/users_connected.php
+++ pandora_console/extensions/users_connected.php
@@ -56,9 +56,54 @@ function users_extension_main () {
 	print_table ($table);
 
 }
+function users_extension_main_god () {
+	echo "<h2>".__('Extensions'). " &raquo; ".__("Users connected"). "</h2>";
 
-add_operation_menu_option(__('Users connected'), 'estado_server',"");
+	$sql = "SELECT id_usuario, ip_origen, fecha, accion FROM tsesion WHERE descripcion = 'Logged in' AND utimestamp > (UNIX_TIMESTAMP(NOW()) - 3600) GROUP BY id_usuario, ip_origen, accion";
 
+	$rows = get_db_all_rows_sql ($sql);
+	if (empty ($rows)) {
+		$rows = array ();
+	}
+
+	$table->cellpadding = 4;
+	$table->cellspacing = 4;
+	$table->width = 600;
+	$table->class = "databox";
+	$table->size = array ();
+	$table->data = array ();
+	$table->head = array ();
+
+	$table->head[0] = __('User');
+	$table->head[1] = __('IP');
+	$table->head[2] = __('Date');
+
+	$rowPair = true;
+	$iterator = 0;
+
+	// Get data
+	foreach ($rows as $row) {
+		if ($rowPair)
+			$table->rowclass[$iterator] = 'rowPair';
+		else
+			$table->rowclass[$iterator] = 'rowOdd';
+		$rowPair = !$rowPair;
+		$iterator++;
+
+		$data = array ();
+		$data[0] = '<a href="index.php?sec=gusuarios&amp;sec2=godmode/users/configure_user&amp;id='.$row["id_usuario"].'">'.$row["id_usuario"].'</a>';
+		$data[1] = $row["ip_origen"];
+		$data[2] = $row["fecha"];
+		array_push ($table->data, $data);
+	}
+
+	print_table ($table);
+
+}
+add_godmode_menu_option (__('Users connected'), 'UM','gusuarios',"users/icon.png");
+add_operation_menu_option(__('Users connected'), 'usuarios',"users/icon.png");
+
+add_extension_godmode_function ('users_extension_main_god');
 add_extension_main_function ('users_extension_main');
 
 ?>

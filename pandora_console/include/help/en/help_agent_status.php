77075b5ab121aed938bc7617d27d6c050d44f51a
raulmateos <noreply@pandorafms.org>
2010-02-01 18:01:05 +0000

    2010-02-01  Raúl Mateos <raulofpandora@gmail.com>
    	* operation/agentes/estado_agente.php: Changed colour order for 
    	module status.
    
    	* include/help/en/help_agent_status.php: Improved help.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2325 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_agent_status.php
+++ pandora_console/include/help/en/help_agent_status.php
@@ -5,25 +5,42 @@
 ?>
 <h1>Agent status view </h1>
 
-Possible values of an agent status are:
+Possible color values of <b>modules</b> are:
+<br><br>
+<b>
+Number of modules
+: <span class="red">Number of critical modules</span>
+: <span class="yellow">Number of warning modules</span>
+: <span class="green">Number of normal modules</span>
+: <span class="grey">Number of down modules</span>
+</b>
+<br><br>
+Possible values of an <b>agent status</b> are:
 
 <br><br>
 
-<table width=750px><tr>
-	<td class="f9i"><img src="../images/status_sets/default/agent_ok.png" title="All Monitors OK" alt="All Monitors OK"><td>All Monitors OK</td>
-	<td class="f9i"><img src="../images/status_sets/default/module_critical.png" title="At least one monitor fails" alt="At least one monitor fails"><td>At least one monitor fails</td>
-	<td class="f9i"><img src="../images/status_sets/default/module_warning.png" title="Change between Green/Red state" alt="Change between Green/Red state"><td>Change between Green/Red state</td>
+<table width="750px">
+<tr>
+	<td class="f9i"><img src="../images/status_sets/default/module_critical.png" title="At least one monitor fails" alt="At least one monitor fails"><img src="../images/status_sets/faces/module_critical.png" title="At least one monitor fails" alt="At least one monitor fails"><td>At least one monitor fails</td>
+	<td class="f9i"><img src="../images/status_sets/default/module_warning.png" title="Change between Green/Red state" alt="Change between Green/Red state"><img src="../images/status_sets/faces/module_warning.png" title="Change between Green/Red state" alt="Change between Green/Red state"><td>Change between Green/Red state</td>
+	<td class="f9i"><img src="../images/status_sets/default/agent_ok.png" title="All Monitors OK" alt="All Monitors OK"><img src="../images/status_sets/faces/agent_ok.png" title="All Monitors OK" alt="All Monitors OK"><td>All Monitors OK</td>
 
-	<td class="f9i"><img src="../images/status_sets/default/alert_fired.png" title="Alert fired" alt="Alert fired"><td>Alert fired</td>
-	<td class="f9i"><img src="../images/status_sets/default/alert_disabled.png" title="Alert disabled" alt="Alert disabled"><td>Alerts disabled</td>
+</tr><tr>
+	<td class="f9i"><img src="../images/status_sets/default/agent_no_monitors.png" title="Agent without monitors" alt="Agent without monitors"><img src="../images/status_sets/faces/agent_no_monitors.png" title="Agent without monitors" alt="Agent without monitors"><td>Agent without monitors</td>
+	<td class="f9i"><img src="../images/status_sets/default/agent_no_data.png" title="Agent without data" alt="Agent without data"><img src="../images/status_sets/faces/agent_no_data.png" title="Agent without data" alt="Agent without data"><td>Agent without data</td>
+	<td class="f9i"><img src="../images/status_sets/default/agent_down.png" title="Agent down" alt="Agent down"><img src="../images/status_sets/faces/agent_down.png" title="Agent down" alt="Agent down"><td>Agent down</td>
+</tr>
+</table>
 
-	</tr><tr>
+<br><br>
+Possible values of <b>alert status</b> are:
 
-	<td class="f9i"><img src="../images/status_sets/default/agent_no_monitors.png" title="Agent without monitors" alt="Agent without monitors"><td>Agent without monitors</td>
-	<td class="f9i"><img src="../images/status_sets/default/agent_no_data.png" title="Agent without data" alt="Agent without data"><td>Agent without data</td>
-	<td class="f9i"><img src="../images/status_sets/default/agent_down.png" title="Agent down" alt="Agent down"><td>Agent down</td>
-	
-	<td class="f9i"><img src="../images/status_sets/default/alert_not_fired.png" title="Alert not fired" alt="Alert not fired"><td>Alert not fired</td>
+<br><br>
+<table width="450px">
+<tr>
+	<td class="f9i"><img src="../images/status_sets/default/alert_fired.png" title="Alert fired" alt="Alert fired"><img src="../images/status_sets/faces/alert_fired.png" title="Alert fired" alt="Alert fired"><td>Alert fired</td>
+	<td class="f9i"><img src="../images/status_sets/default/alert_disabled.png" title="Alert disabled" alt="Alert disabled"><img src="../images/status_sets/faces/alert_disabled.png" title="Alert disabled" alt="Alert disabled"><td>Alerts disabled</td>
+	<td class="f9i"><img src="../images/status_sets/default/alert_not_fired.png" title="Alert not fired" alt="Alert not fired"><img src="../images/status_sets/faces/alert_not_fired.png" title="Alert not fired" alt="Alert not fired"><td>Alert not fired</td>
 
-	</tr>
-	</table>
+</tr>
+</table>

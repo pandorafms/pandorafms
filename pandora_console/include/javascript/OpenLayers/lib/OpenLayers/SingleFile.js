01245adaeeb6c8dd18ebb96174150c9eeadaf4c5
mdtrooper <tres.14159@gmail.com>
2010-03-02 15:51:33 +0000

    2010-03-02  Miguel de Dios  <miguel.dedios@artica.es>
    	* include/javascript/OpenLayers/lib/*, 
    	include/javascript/OpenLayers/news.txt, 
    	include/javascript/OpenLayers/readme.txt,
    	include/javascript/OpenLayers/repository-license.txt,
    	include/javascript/OpenLayers/release-license.txt: delete unused files of
    	OpenLayers lib.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2449 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/javascript/OpenLayers/lib/OpenLayers/SingleFile.js
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2006-2008 MetaCarta, Inc., published under the Clear BSD
- * license.  See http://svn.openlayers.org/trunk/openlayers/license.txt for the
- * full text of the license. */
-
-var OpenLayers = {
-    singleFile: true
-};
-
-

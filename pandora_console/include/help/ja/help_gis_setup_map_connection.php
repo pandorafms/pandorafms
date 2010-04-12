9fc72dcf2af333cfd39727fb4877cc8c251a03fd
jsatoh <junichi@rworks.jp>
2010-04-12 02:32:50 +0000

    2010-04-12  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/ja/help_configure_gis_map.php,
    	include/help/ja/help_gis_map_builder.php,
    	include/help/ja/help_gis_setup_map_connection.php,
    	include/help/ja/help_setup_gis_index.php: Added the GIS help files
    	in Japanese.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2555 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/ja/help_gis_setup_map_connection.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package Include/help/ja
+ */
+?>
+<h1>GIS 利用マップ設定</h1>
+
+<p>
+このページは、管理者が<strong>利用する GIS マップサーバ</strong>を設定するためのものです。
+</p>
+
+<h2>マップの種類</h2>
+<p>
+現在、Pandora FMS は、OpenStreetマップ、Google マップ、静的画像の 3種類のマップをサポートしています。
+</p>
+<h3>Open Street マップ</h3>
+<p>
+Open Street マップを利用するためには、以下に示す Open Street マップサーバにアクセスできる必要があります。
+(詳細は、<a href="http://wiki.openstreetmap.org/wiki/ja:Main_Page">http://wiki.openstreetmap.org/wiki/ja:Main_Page</a> を参照してください。また、<a href="http://wiki.openstreetmap.org/wiki/ja:Mapnik">http://wiki.openstreetmap.org/wiki/ja:Mapnik</a> にサンプルがあります。)
+<pre>
+http://tile.openstreetmap.org/${z}/${x}/${y}.png
+</pre>
+OpenStreetマップの利用は、<a href="http://wiki.openstreetmap.org/wiki/Licence">ライセンス</a>に従っています。
+</p>
+</p>
+<h3>Google マップ</h3>
+<p>
+最初に Google MAPS API に登録し、フリーの API キーを取得する必要があります。
+これに関しては、以下を参照してください。<br/>
+<a href="http://code.google.com/intl/ja/apis/maps/signup.html">http://code.google.com/intl/ja/apis/maps/signup.html</a></p>
+<p>Google API キーは次のようになっています。</p>
+<pre>
+ABQIAAAAZuJY-VSG4gOH73b6mcUw1hTfSvFQRXGUGjHx8f036YCF-UKjgxT9lUhqOJx7KDHSnFnt46qnj89SOQ
+</pre>
+<h3>静的画像</h3>
+<p>
+一つの画像ソースとして、静的画像ファイル(PNG など)を利用することも可能です。
+これを利用するには、<strong>URL</strong>、画像の<strong>位置情報</strong>、そして、<strong>高さ</strong>と<strong>幅</strong>を設定する必要があります。
+</p>

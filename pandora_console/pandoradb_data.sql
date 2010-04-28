3f1a98cb70538019290c450823f0787194e753d3
slerena <slerena@gmail.com>
2010-04-28 18:59:11 +0000

    2010-04-28  Sancho Lerena <slerena@artica.es>
            * pandoradb_data.sql: Updated version to 3.1
    
            * pandoradb_migrate_v3.0_to_v3.1.sql: Fix, set log4x correct type id.
    
            * include/functions_gis.php: Fixed a notice by undefined value.
    
            * images/gis_maps/icons: Deleted several non-useful icons.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2629 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/pandoradb_data.sql
+++ pandora_console/pandoradb_data.sql
@@ -14,7 +14,7 @@
 -- along with this program; if not, write to the Free Software
 -- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
--- Database Data for Pandora FMS 3.0
+-- Database Data for Pandora FMS 3.1
 
 -- PLEASE NO NOT USE MULTILINE COMMENTS 
 -- Because Pandora Installer don't understand them

d40500cad3c4224199f131212f8a692049cfe940
mdtrooper <tres.14159@gmail.com>
2010-04-29 08:21:43 +0000

    2010-04-29 Miguel de Dios  <miguel.dedios@artica.es>
    	* godmode/reporting/reporting_builder.php,
    	godmode/reporting/reporting_builder.item_editor.php: fixed typo mistake in
    	the field "id_agent_module".
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2632 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/reporting/reporting_builder.php
+++ pandora_console/godmode/reporting/reporting_builder.php
@@ -168,7 +168,7 @@ switch ($action) {
 						$values['id_agent'] = get_parameter('id_agent');
 						$values['id_gs'] = get_parameter('id_custom_graph');
 						$values['text'] = get_parameter('text');
-						$values['id_agent_module'] = get_parameter('id_agente_modulo');
+						$values['id_agent_module'] = get_parameter('id_agent_module');
 						$values['type'] = get_parameter('type', null);
 						
 						if ($values['type'] == 'sql') {
@@ -196,7 +196,7 @@ switch ($action) {
 						$values['id_agent'] = get_parameter('id_agent');
 						$values['id_gs'] = get_parameter('id_custom_graph');
 						$values['text'] = get_parameter('text');
-						$values['id_agent_module'] = get_parameter('id_agente_modulo');
+						$values['id_agent_module'] = get_parameter('id_agent_module');
 						
 						if ($values['type'] == 'sql') {
 							$values['treport_custom_sql_id'] = get_parameter('id_custom');

a15281dcde9866a475d37edbded92b8cfc14216f
raulmateos <noreply@pandorafms.org>
2010-05-15 22:36:34 +0000

    2010-05-16  Raul Mateos  <raulofpandora@gmail.com>
    	* linux/pandora_agent_installer, linux/DEBIAN/make_deb_package.sh,
    	linux/DEBIAN/control, linux/pandora_agent, linux/pandora_agent_daemon,
    	linux/pandora_agent.conf: Updated build. Updated year.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2733 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/linux/DEBIAN/make_deb_package.sh
+++ pandora_agents/linux/DEBIAN/make_deb_package.sh
@@ -2,7 +2,7 @@
 
 #Pandora FMS- http:#pandorafms.com
 # ==================================================
-# Copyright (c) 2005-2009 Artica Soluciones Tecnologicas
+# Copyright (c) 2005-2010 Artica Soluciones Tecnologicas
 # Please see http:#pandorafms.org for full contribution list
 
 # This program is free software; you can redistribute it and/or
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_agent_version="3.0.0"
+pandora_agent_version="3.1.0"
 
 echo "Test if you has the tools for to make the packages."
 whereis dpkg-deb | cut -d":" -f2 | grep dpkg-deb > /dev/null

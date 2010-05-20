8727aee060055ad296c7e02a78e370cb07274bb0
Ramon Novoa <rnovoa@artica.es>
2010-05-20 13:42:47 +0000

    2010-05-20  Ramon Novoa  <rnovoa@artica.es>
    	* build_src.sh, build_deb.sh, build_rpm.sh: Added to repository. 
              Sancho's newest build scripts.
    
            * build_rpm_packages.sh: Removed from repository. Old build script.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2773 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ extras/build_deb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CODEHOME=~/code/pandora/trunk
+CODEHOME_ENT=~/code/artica/code
+RPMHOME=/usr/src/packages
+
+mkdir -p $RPMHOME/DEB
+
+echo "Creating DEB packages in $RPMHOME/DEB"
+cd $CODEHOME/pandora_console/DEBIAN
+bash ./make_deb_package.sh -pear
+cd ..
+mv *.deb $RPMHOME/DEB
+
+cd $CODEHOME/pandora_server/DEBIAN
+bash ./make_deb_package.sh -cpan
+cd ..
+mv *.deb $RPMHOME/DEB
+
+cd $CODEHOME/pandora_agents/unix/DEBIAN
+bash ./make_deb_package.sh
+cd ..
+mv *.deb $RPMHOME/DEB
+
+cd $CODEHOME_ENT/pandora/trunk/pandora_console/DEBIAN
+bash ./make_deb_package.sh
+cd ..
+mv *.deb $RPMHOME/DEB
+
+cd $CODEHOME_ENT/pandora/trunk/pandora_server/PandoraFMS-Enterprise/DEBIAN
+bash ./make_deb_package.sh
+cd ..
+mv *.deb $RPMHOME/DEB
+
--- extras/build_src.sh
+++ extras/build_src.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# YOU NEED TO ALTER THIS PATHS for match paths of your system
-
-# CONFIG BEGINS HERE
 CODEHOME=~/code/pandora/trunk
-CODEHOME_ENT=~/code/artica/code/
+CODEHOME_ENT=~/code/artica/code
 RPMHOME=/usr/src/packages
-VERSION=3.0.0
-
-# CONFIG ENDS HERE
+VERSION=$(grep 'my $pandora_version =' ../pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
 
 echo "Creating source tarballs (/usr/src/rpm/SOURCES)"
-rm -Rf $RPMHOME=/SOURCES/pandorafms_*.tar.gz
+rm -Rf /usr/src/rpm/SOURCES/pandorafms_*.tar.gz
 
 cd $CODEHOME/pandora_agents
 sudo tar zcvf $RPMHOME/SOURCES/pandorafms_agent-$VERSION.tar.gz --exclude \.svn --exclude nohup linux
@@ -23,15 +18,22 @@ sudo tar zcvf $RPMHOME/SOURCES/pandorafms_server-$VERSION.tar.gz --exclude \.svn
 # Console OpenSource
 cd $CODEHOME
 tar zcvf $RPMHOME/SOURCES/pandorafms_console-$VERSION.tar.gz --exclude \.svn --exclude config.php --exclude enterprise pandora_console
+
 # Console Enterprise
 cd $CODEHOME_ENT/pandora/trunk/pandora_console
 sudo tar zcvf $RPMHOME/SOURCES/pandorafms_console_enterprise-$VERSION.tar.gz --exclude \.svn enterprise/*
+
 # Server OpenSource
 cd $CODEHOME_ENT/pandora/trunk/pandora_server/
 sudo tar zcvf $RPMHOME/SOURCES/pandorafms_server_enterprise-$VERSION.tar.gz --exclude \.svn  PandoraFMS-Enterprise
+
 # Updatemanager Client keygen
 cd $CODEHOME_ENT/updatemanager/keygen
 sudo tar cvzf $RPMHOME/SOURCES/pandorafms_keygen-$VERSION.tar.gz --exclude .svn --exclude keygen --exclude keygen.i386.static --exclude pandora_keygen.spec pandora
+
+echo " "
+echo " ABORTING RPM Creation, only tarball packages build "
+exit
 
 echo "Creating RPMs  at $RPMHOME/RPMS"
 cd $CODEHOME

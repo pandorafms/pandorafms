ad378b25aea5975e2d1f18a1704ad7264ed9f3a9
Ramon Novoa <rnovoa@artica.es>
2010-05-21 11:58:30 +0000

    2010-05-21  Ramon Novoa  <rnovoa@artica.es>
    	* build_src.sh: Get the right version for the keygen.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2784 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- extras/build_src.sh
+++ extras/build_src.sh
@@ -3,7 +3,8 @@
 CODEHOME=~/code/pandora/trunk
 CODEHOME_ENT=~/code/artica/code
 RPMHOME=/usr/src/packages
-VERSION=$(grep 'my $pandora_version =' ../pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
+VERSION=$(grep 'my $pandora_version =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
+KEYGEN_VERSION=$(grep "%define version" $CODEHOME_ENT/updatemanager/keygen/pandora/pandora_keygen.spec | awk '{print $3}')
 
 echo "Creating source tarballs (/usr/src/rpm/SOURCES)"
 rm -Rf /usr/src/rpm/SOURCES/pandorafms_*.tar.gz
@@ -29,7 +30,7 @@ sudo tar zcvf $RPMHOME/SOURCES/pandorafms_server_enterprise-$VERSION.tar.gz --ex
 
 # Updatemanager Client keygen
 cd $CODEHOME_ENT/updatemanager/keygen
-sudo tar cvzf $RPMHOME/SOURCES/pandorafms_keygen-$VERSION.tar.gz --exclude .svn --exclude keygen --exclude keygen.i386.static --exclude pandora_keygen.spec pandora
+sudo tar cvzf $RPMHOME/SOURCES/pandorafms_keygen-$KEYGEN_VERSION.tar.gz --exclude .svn --exclude keygen --exclude keygen.i386.static --exclude pandora_keygen.spec pandora
 
 echo " "
 echo " ABORTING RPM Creation, only tarball packages build "

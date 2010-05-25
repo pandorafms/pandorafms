3c277e1e564d5bf40f648761fc9cd59195e2f1fe
raulmateos <noreply@pandorafms.org>
2010-05-25 18:09:27 +0000

    2010-05-25 Raul Mateos <raulofpandora@gmail.com>
    	* extensions/update_manager/lib/*.php: Updated year, clean code.
    
    	* extensions/update_manager/lib/libupdate_manager.php: Improved usability.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@2799 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/extensions/update_manager/lib/libupdate_manager_client.php
+++ pandora_console/extensions/update_manager/lib/libupdate_manager_client.php
@@ -2,7 +2,7 @@
 
 // Pandora FMS - http://pandorafms.com
 // ==================================================
-// Copyright (c) 2005-2009 Artica Soluciones Tecnologicas
+// Copyright (c) 2005-2010 Artica Soluciones Tecnologicas
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
--- pandora_console/extensions/update_manager/lib/libupdate_manager_utils.php
+++ pandora_console/extensions/update_manager/lib/libupdate_manager_utils.php
@@ -2,7 +2,7 @@
 
 // Pandora FMS - http://pandorafms.com
 // ==================================================
-// Copyright (c) 2005-2009 Artica Soluciones Tecnologicas
+// Copyright (c) 2005-2010 Artica Soluciones Tecnologicas
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -26,7 +26,7 @@ function is_binary ($filepath) {
 	return false;
 }
 
-function directory_to_array ($directory, $ignores =  NULL, $only_binary_files = false) {
+function directory_to_array ($directory, $ignores = NULL, $only_binary_files = false) {
 	if (! $ignores)
 		$ignores = array ('.', '..');
 	

c8776a428f079c709dbda1127c4fc6b2c372757d
darode <drd.sqki@gmail.com>
2010-07-13 10:36:38 +0000

    2010-07-13  Darío Rodríguez <dario.rodriguez@artica.es>
    	* include/help/en/help_reporting_main_tab.php: Added help for report
    	creator for English.
    	* include/help/es/help_reporting_main_tab.php: Added help for report
    	creator for Spanish.
    	Fixes: #3013763
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3000 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_reporting_main_tab.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Report builder</h1>
+
+To create a report, you must first fill the form providing:
+<ul>
+	<li>A name</li>
+	<li>A group</li>
+	<li>A description</li>
+</ul>
+Then you must press the botton with the text Create.
+<br /><br />
+To edit the properties of an existing report just change the form field and press the Update botton. 
---
+++ pandora_console/include/help/es/help_reporting_main_tab.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Creador de informes</h1>
+
+Para crear un informe, primero debe llenar el formulario proporcionando:
+<ul>
+	<li>Un nombre</li>
+	<li>Un grupo</li>
+	<li>Una descripción</li>
+</ul>
+Luego se debe presionar el boton con el texto Crear.
+<br /><br />
+Para editar las propiedades de un informe existente sólo cambia el campo de formulario y pulse el boton Actualizar.

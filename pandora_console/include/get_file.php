3216f2fffc63e1b5fa99a6dcbb45d5a7dc458b2e
mdtrooper <tres.14159@gmail.com>
2010-08-02 10:36:55 +0000

    2010-08-02  Miguel de Dios  <miguel.dedios@artica.es> 	* include/functions_filemanager.php: cleaned source code, changed the show 	to buttons of actions (create file, upload...) to out the list of files. And 	check if the directory is writeable to show this buttons.
    	* include/get_file.php: added file to work as mediator between the Pandora
    	and file system, and added a some security checks as check login and others.
    	
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3083 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/get_file.php
@@ -0,0 +1,55 @@
+<?php
+// Pandora FMS - http://pandorafms.com
+// ==================================================
+// Copyright (c) 2005-2010 Artica Soluciones Tecnologicas
+// Please see http://pandorafms.org for full contribution list
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the  GNU Lesser General Public License
+// as published by the Free Software Foundation; version 2
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+require_once('functions.php');
+require_once('functions_filemanager.php');
+
+session_start();
+
+require_once ("config.php");
+global $config;
+
+session_write_close ();
+
+check_login ();
+
+$styleError = "background:url(\"../images/err.png\") no-repeat scroll 0 0 transparent; padding:4px 1px 6px 30px; color:#CC0000;";
+
+$file = get_parameter('file', null);
+$chunks = explode('/', $file); 
+$nameFile = end($chunks);
+
+$hash = get_parameter('hash', null);
+
+$testHash = md5($file . $config['dbpass']);
+
+if ($hash != $testHash) {
+	echo "<h3 style='" . $styleError . "'>".__('Security error. Please contact the administrator.')."</h3>";
+}
+else if (!empty($file) && !empty($hash)) {
+	$file = $_SERVER['DOCUMENT_ROOT'] . $file;
+
+	if (!file_exists($file)){
+		echo "<h3 style='" . $styleError . "'>".__("File is missing in disk storage. Please contact the administrator.")."</h3>";
+	}
+	else {
+		header('Content-type: aplication/octet-stream;');
+		header('Content-type: ' . mime_content_type($file) . ';');
+		header("Content-Length: " . filesize($file));
+		header('Content-Disposition: attachment; filename="' . $nameFile . '"');
+		readfile($file);
+	}
+}
+?>

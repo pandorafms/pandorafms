ea31a214c685e32dcf505a7773687aa197072488
mdtrooper <tres.14159@gmail.com>
2010-09-22 16:52:29 +0000

    2010-09-22  Miguel de Dios  <miguel.dedios@artica.es>
    	* ChangeLog: fixed previous commit of jobmate that erase some lines of
    	ChangeLog. 
    	
    	* index.php: cleaned source code style.
    	
    	* mobile/include/style/main.css: added style for the login mobile page.
    	
    	* mobile/include/user.class.php: the "loginShow" function have a nice
    	login box with some style.
    	
    	* mobile/images/transparent_pixel.gif, mobile/images/key.gif: added image
    	files for the login page. The key image is created for "barretr" in
    	http://www.openclipart.org/detail/3330.
    	
    	* mobile/index.php: changes for to show logout and new login box.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3275 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/mobile/include/user.class.php
+++ pandora_console/mobile/include/user.class.php
@@ -54,7 +54,7 @@ class User {
 			$password = $this->system->getRequest('password', null);
 		}
 		
-		if (process_user_login($user, $password)) {
+		if (process_user_login($user, $password) !== false) {
 			$this->logged = true;
 			$this->user = $user;
 		}
@@ -70,13 +70,93 @@ class User {
 		$this->logged = false;
 	}
 	
-	public function login() {
-		echo "<form method='post'>";
+	public function showLogin($text = '') {
+		global $pandora_version;
+		
+		echo "<form action='index.php' method='post'>";
 		print_input_hidden('action', 'login');
-		print_input_text('user', $this->user, __('User'), 10, 20);
-		print_input_password('password', '', __('Password'), 10, 20);
-		print_submit_button(__('Login'), 'login', false, 'class="sub next"');
-		echo "</form>";		
+		?>
+		<div id="center_div">
+			<div id="table_version_negative_position_div">
+				<table cellspacing="0" style="margin: 10px;" id="table_version">
+					<tr>
+						<td style="height: 120px; width: 200px; background: #fff; width: 200px; height: 120px;" colspan="2" rowspan="2">
+							<table id="form_table" cellspacing="0" style="border: 2px solid #6DC62D; width: 100%; height: 100%; background: url('../images/pandora_logo.png') bottom left no-repeat #fff;">
+								<tr>
+									<td style="color: #036A3A; height: 20px;" colspan="2" valign="top" align="left"><?php echo $pandora_version;?> <?php echo $text;?></td>
+									<td style="width: 80px; height: 80px;" valign="bottom" align="left" rowspan="4">
+										<?php
+										print_submit_button(__(''), 'login', false, 'class="login_button" alt="' . __('Login') . '" title="' . __('Login') . '"');
+										?>
+									</td>
+								</tr>
+								<tr>
+									<td style="width: 20px; height: 25px;">&nbsp;</td>
+									<td valign="top" align="left"><?php print_input_text('user', $this->user, __('User'), 10, 20);?></td>
+								</tr>
+								<tr>
+									<td style="width: 20px; height: 15px;">&nbsp;</td>
+									<td valign="top" align="left"><?php print_input_password('password', '', __('Password'), 10, 20);?></td>
+								</tr>
+								<tr>
+									<td style="height: 20px;">&nbsp;</td>
+									<td style="height: 20px;">&nbsp;</td>
+								</tr>
+								<tr>
+									<td>&nbsp;</td>
+									<td>&nbsp;</td>
+									<td>&nbsp;</td>
+								</tr>
+							</table>
+						</td>
+						<td style="height: 15px; width: 15px; background: #fff;">&nbsp;</td>
+					</tr>
+					<tr>
+						<td style="width: 15px; height: 90px; background: #036A3A;">&nbsp;</td>
+					</tr>
+					<tr>
+						<td style="height: 15px; width: 15px; background: #fff;">&nbsp;</td>
+						<td style="width: 200px; height: 15px; background: #036A3A; color: #fff; font-size: 9px; text-align: right;" colspan="2">
+							<?php echo '<b style="font-size: 9px;">' . __('Your IP').':</b>' . $this->system->getConfig("remote_addr") . '&nbsp;';?>
+						</td>
+					</tr>
+					<tr>
+						<td style="background: white; height: 0px;">&nbsp;</td>
+						<td style="background: white; height: 0px;">&nbsp;</td>
+						<td style="background: white; height: 0px;">&nbsp;</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<?php
+		echo "</form>";
+		
+		/*
+		?>
+		<div id="center_div">
+			<div id="negative_position_div">
+				<div id="style_div">
+					<div id="shadow">
+						<p>
+						<?php
+						echo '<b>' . __('Your IP').':</b>' . $this->system->getConfig("remote_addr");
+						?>
+						</p>
+					</div>
+					<?php
+					echo "<form id='login_box' method='post' style=''>";
+					echo "<div id='version'>" . $pandora_version . "</div>";
+					print_input_hidden('action', 'login');
+					print_input_text('user', $this->user, __('User'), 10, 20);
+					print_input_password('password', '', __('Password'), 10, 20);
+					print_submit_button(__(''), 'login', false, 'onclick="javascript: click();" class="login_button" alt="' . __('Login') . '" title="' . __('Login') . '"');
+					echo "</form>";
+					?>
+				</div>
+			</div>
+		</div>
+		<?php
+		*/
 	}
 	
 	public function getIdUser() {

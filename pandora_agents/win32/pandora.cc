716988e50f0dda776bc63bfe7f62c4ebc9cc5d96
slerena <slerena@gmail.com>
2010-10-29 15:50:43 +0000

    2010-10-29  Sancho Lerena <slerena@artica.es>
        * win32/bin/util/mibs: Added default mibs for the small snmpagent
        * tool.
    
        * win32/bin/util/getsnmp.bat,
        win32/bin/util/snmpget.exe: Added tool to get snmp info from the
        agent using tool from a module_exec.
    
        * win32/installer/pandora.mpi: Updated installer to have snmpget and
        mibs in the install process.
    
        * win32/pandora_windows_service.cc: Fixed bug 3098165.
    
        * win32/pandora.cc: Updated version
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3482 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("3.1(Build 100608)")
+#define PANDORA_VERSION ("3.2 RC1 (Build 101029)")
 
 string pandora_path;
 string pandora_dir;

2ded23a69446b541c9d431de85e9ccbc2af090e0
raulmateos <noreply@pandorafms.org>
2010-11-03 17:56:20 +0000

    2010-11-03  Raul Mateos <raulofpandora@gmail.com>
    	* index.php: Updated year :-D
    	* general/footer.php, header.php: Updated year. Fixed some HTML strings to improve W3C validation. A lot of work to do in this area.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3510 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/index.php
+++ pandora_console/index.php
@@ -2,7 +2,7 @@
 
 //Pandora FMS- http://pandorafms.com
 // ==================================================
-// Copyright (c) 2005-2009 Artica Soluciones Tecnologicas
+// Copyright (c) 2005-2010 Artica Soluciones Tecnologicas
 // Please see http://pandorafms.org for full contribution list
 
 // This program is free software; you can redistribute it and/or

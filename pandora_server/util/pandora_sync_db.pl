388b03d2cf70baa2a3602518df4fc9ca590016a7
zarzuelo <noreply@pandorafms.org>
2010-11-05 13:38:56 +0000

    2010-11-05  Sergio Martin <sergio.martin@artica.es>
    	* util/pandora_sync_db.pl: Changed the tevento table
    	synchronization to sync it only when the flag sync_data 
    	is 1
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3541 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/util/pandora_sync_db.pl
+++ pandora_server/util/pandora_sync_db.pl
@@ -150,7 +150,7 @@ sub help_screen{
 
 sub pandora_sync_main ($$$) {
 	my ($dbh_source, $dbh_dest, $history_dbh) = @_;
-	
+
 	$|++;
 	my $success = 0;
 	my $percent;
@@ -178,7 +178,9 @@ sub pandora_sync_main ($$$) {
 		
 	# Mixed cases	
 	empty_table($dbh_dest, 'tlayout_data');
-	empty_table($dbh_dest, 'tevento');
+	if($sync_data == 1) {
+		empty_table($dbh_dest, 'tevento');
+	}
 	
 	print "\n\n";
 	
@@ -190,10 +192,13 @@ sub pandora_sync_main ($$$) {
 	@columns = ('id_agente_modulo', 'id_agent');
 	@types = ('module', 'agent');
 	enterprise_hook('sync_clone_table', [$dbh_source, $dbh_dest, 'tlayout_data', \@columns, \@types, @id_agent_comparation, @id_agentmodule_comparation, @id_server_export_comparation, @id_server_comparation]);
-	@columns = ('id_agentmodule', 'id_agente');
-	@types = ('module', 'agent');
-	enterprise_hook('sync_clone_table', [$dbh_source, $dbh_dest, 'tevento', \@columns, \@types, @id_agent_comparation, @id_agentmodule_comparation, @id_server_export_comparation, @id_server_comparation]);
-
+	
+	if($sync_data == 1) {
+		@columns = ('id_agentmodule', 'id_agente');
+		@types = ('module', 'agent');
+		enterprise_hook('sync_clone_table', [$dbh_source, $dbh_dest, 'tevento', \@columns, \@types, @id_agent_comparation, @id_agentmodule_comparation, @id_server_export_comparation, @id_server_comparation]);
+	}
+	
 	my $errors = $errors_agents + $errors_modules + $errors_servers + $errors_exportservers;
 
 	if($errors == 0) {

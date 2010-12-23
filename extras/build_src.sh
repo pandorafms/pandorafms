f417abae88c95b8cd591427a74dd16f529e6bd84
ramonn <noreply@pandorafms.org>
2010-12-23 12:34:02 +0000

    2010-12-23  Ramon Novoa  <rnovoa@artica.es>
    	* build_src.sh, build_windows_agent.sh,
    	  build_deb.sh, build_rpm.sh: Return error codes and exclude .po files.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3696 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- extras/build_src.sh
+++ extras/build_src.sh
@@ -5,29 +5,29 @@ RPMHOME=/usr/src/packages
 VERSION=$(grep 'my $pandora_version =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
 KEYGEN_VERSION=$(grep "%define version" $CODEHOME_ENT/updatemanager/keygen/pandora/pandora_keygen.spec | awk '{print $3}')
 
+if [ ! -d $RPMHOME/SOURCES ]; then
+	mkdir -p $RPMHOME/SOURCES || exit 1
+fi
+
 echo "Creating source tarballs in $RPMHOME/SOURCES"
-rm -Rf /usr/src/rpm/SOURCES/pandorafms_*.tar.gz
 
-cd $CODEHOME/pandora_agents
-tar zcvf $RPMHOME/SOURCES/pandorafms_agent-$VERSION.tar.gz --exclude \.svn --exclude nohup linux
-tar zvcf $RPMHOME/SOURCES/pandorafms_agent_unix-$VERSION.tar.gz --exclude \.svn --exclude nohup --exclude NT4 unix
+# Console
+cd $CODEHOME && tar zcvf $RPMHOME/SOURCES/pandorafms_console-$VERSION.tar.gz --exclude \.svn --exclude config.php --exclude enterprise --exclude \*\.po pandora_console || exit 1
 
-cd $CODEHOME
-tar zcvf $RPMHOME/SOURCES/pandorafms_server-$VERSION.tar.gz --exclude \.svn pandora_server
+# Server
+cd $CODEHOME && tar zcvf $RPMHOME/SOURCES/pandorafms_server-$VERSION.tar.gz --exclude \.svn pandora_server || exit 1
 
-# Console OpenSource
-cd $CODEHOME
-tar zcvf $RPMHOME/SOURCES/pandorafms_console-$VERSION.tar.gz --exclude \.svn --exclude config.php --exclude enterprise pandora_console
+# Linux and Unix agents
+cd $CODEHOME/pandora_agents && tar zcvf $RPMHOME/SOURCES/pandorafms_agent-$VERSION.tar.gz --exclude \.svn --exclude nohup linux && tar zvcf $RPMHOME/SOURCES/pandorafms_agent_unix-$VERSION.tar.gz --exclude \.svn --exclude nohup --exclude NT4 unix || exit 1
 
-# Console Enterprise
-cd $CODEHOME_ENT/pandora/trunk/pandora_console
-tar zcvf $RPMHOME/SOURCES/pandorafms_console_enterprise-$VERSION.tar.gz --exclude \.svn enterprise/*
+# Enterprise console
+cd $CODEHOME_ENT/pandora/trunk/pandora_console && tar zcvf $RPMHOME/SOURCES/pandorafms_console_enterprise-$VERSION.tar.gz --exclude \.svn enterprise/* || exit 1
 
-# Server OpenSource
-cd $CODEHOME_ENT/pandora/trunk/pandora_server/
-tar zcvf $RPMHOME/SOURCES/pandorafms_server_enterprise-$VERSION.tar.gz --exclude \.svn  PandoraFMS-Enterprise
+# Enterprise server
+cd $CODEHOME_ENT/pandora/trunk/pandora_server/ && tar zcvf $RPMHOME/SOURCES/pandorafms_server_enterprise-$VERSION.tar.gz --exclude \.svn  PandoraFMS-Enterprise || exit 1
 
-# Updatemanager Client keygen
-cd $CODEHOME_ENT/updatemanager/keygen
-tar cvzf $RPMHOME/SOURCES/pandorafms_keygen-$KEYGEN_VERSION.tar.gz --exclude .svn --exclude keygen --exclude keygen.i386.static --exclude pandora_keygen.spec pandora
+# Updatemanager keygen
+cd $CODEHOME_ENT/updatemanager/keygen && tar cvzf $RPMHOME/SOURCES/pandorafms_keygen-$KEYGEN_VERSION.tar.gz --exclude .svn --exclude keygen --exclude keygen.i386.static --exclude pandora_keygen.spec pandora || exit 1
+
+exit 0
 
--- extras/build_windows_agent.sh
+++ extras/build_windows_agent.sh
@@ -2,9 +2,13 @@
 CODEHOME=~/code/pandora/trunk
 RPMHOME=/usr/src/packages
 
-mkdir -p $RPMHOME/EXE
+if [ ! -d $RPMHOME/EXE ]; then
+	mkdir -p $RPMHOME/EXE || exit 1
+fi
 
 echo "Creating Pandora FMS Agent Windows installer in $RPMHOME/EXE"
+
+# Windows agent
 rm -rf $CODEHOME/pandora_agents/win32/installer/output/*.exe
 cd $CODEHOME/pandora_agents/win32
 ./build.sh

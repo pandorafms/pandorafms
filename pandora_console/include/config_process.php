313776ea8a5f01185b785c6ca02d00f06e520f0a
ramonn <noreply@pandorafms.org>
2010-12-27 18:28:10 +0000

    2010-12-27  Ramon Novoa  <rnovoa@artica.es>
    	* pandoradb_data.sql, include/config_process.php: Updated
    	  version strings for the 3.2 release.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3713 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,8 +22,8 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC101125';
-$pandora_version = 'v3.2rc2';
+$build_version = 'PC101227';
+$pandora_version = 'v3.2';
 
 /* Help to debug problems. Override global PHP configuration */
 if (!isset($develop_bypass)) $develop_bypass = 0;

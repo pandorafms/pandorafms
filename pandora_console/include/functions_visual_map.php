fca081ab216f76f29b7b28bcbc6b6df9a49b890b
juanmanuelr <noreply@pandorafms.org>
2011-01-10 18:33:26 +0000

    2010-01-10 Juan Manuel Ramon <juanmanuel.ramon@artica.es>
    	* pchart_graph.php: Remove axis values and legend in small graphs.
    	* functions_visual_map.php: Added marks (div borders) for beautify visual console graphs.
    	* fgraph.php: Dont show caption graphs in small graphs.
    	* functions.php: Added constants for visual console.
    
    	Fixes bug #3152673.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3730 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/functions_visual_map.php
+++ pandora_console/include/functions_visual_map.php
@@ -739,9 +739,9 @@ function print_pandora_visual_map ($id_layout, $show_links = true, $draw_lines =
 					
 					if ($show_links) {
 						if (($layout_data['id_layout_linked'] == "") || ($layout_data['id_layout_linked'] == 0)) {
-							echo '<a href="index.php?sec=estado&amp;sec2=operation/agentes/ver_agente&amp;id_agente='.$layout_data["id_agent"].'&amp;tab=data">';
+							echo '<div style="border-width:1px; border-style:solid; border-color:#808080"><a href="index.php?sec=estado&amp;sec2=operation/agentes/ver_agente&amp;id_agente='.$layout_data["id_agent"].'&amp;tab=data">';
 						} else {
-							echo '<a href="index.php?sec=visualc&amp;sec2=operation/visual_console/render_view&amp;pure='.$config["pure"].'&amp;id='.$layout_data['id_layout_linked'].'">';
+							echo '<div style="border-width:1px; border-style:solid; border-color:#808080"><a href="index.php?sec=visualc&amp;sec2=operation/visual_console/render_view&amp;pure='.$config["pure"].'&amp;id='.$layout_data['id_layout_linked'].'">';
 						}
 					}
 					if ($resizedMap)
@@ -753,7 +753,7 @@ function print_pandora_visual_map ($id_layout, $show_links = true, $draw_lines =
 					else
 						print_image ("include/fgraph.php?tipo=sparse&id=".$layout_data['id_agente_modulo']."&label=".safe_input ($layout_data['label'])."&height=".$layout_data['height']."&pure=1&width=".$layout_data['width']."&period=".$layout_data['period'], false, array ("title" => $layout_data['label'], "border" => 0));
 					echo "</a>";
-					echo "</div>";
+					echo "</div></div>";
 					break;
 			}
 			

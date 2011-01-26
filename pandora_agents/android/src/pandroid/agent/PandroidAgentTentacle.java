412b9a7a635c307657365a37750267844005376d
zarzuelo <zarzuelo@gmail.com>
2011-01-26 19:15:48 +0000

    2011-01-26  Sergio Martin <sergio.martin@artica.es>
    	* android
    	android/default.properties
    	android/.classpath
    	android/proguard.cfg
    	android/assets
    	android/.project
    	android/AndroidManifest.xml
    	android/src
    	android/src/pandroid
    	android/src/pandroid/agent
    	android/src/pandroid/agent/PandroidAgentTentacle.java
    	android/src/pandroid/agent/PandroidAgent.java
    	android/bin
    	android/bin/pandroid
    	android/bin/pandroid/agent
    	android/bin/pandroid/agent/PandroidAgent$8.class
    	android/bin/pandroid/agent/PandroidAgent$9.class
    	android/bin/pandroid/agent/PandroidAgent.class
    	android/bin/pandroid/agent/R$id.class
    	android/bin/pandroid/agent/PandroidAgent$MyLocationListener.class
    	android/bin/pandroid/agent/R$string.class
    	android/bin/pandroid/agent/tentacle_client.class
    	android/bin/pandroid/agent/R$attr.class
    	android/bin/pandroid/agent/R.class
    	android/bin/pandroid/agent/R$layout.class
    	android/bin/pandroid/agent/PandroidAgent$1.class
    	android/bin/pandroid/agent/PandroidAgent$2.class
    	android/bin/pandroid/agent/PandroidAgent$3.class
    	android/bin/pandroid/agent/PandroidAgent$4.class
    	android/bin/pandroid/agent/R$drawable.class
    	android/bin/pandroid/agent/PandroidAgent$5.class
    	android/bin/pandroid/agent/PandroidAgent$6.class
    	android/bin/pandroid/agent/PandroidAgent$7.class
    	android/bin/resources.ap_
    	android/bin/Copy of Copy of Pandroid Agent.apk
    	android/bin/classes.dex
    	android/res
    	android/res/values
    	android/res/values/strings.xml
    	android/res/drawable-hdpi
    	android/res/drawable-hdpi/logo.png
    	android/res/drawable-hdpi/icon.png
    	android/res/drawable-ldpi
    	android/res/drawable-ldpi/icon.png
    	android/res/drawable-mdpi
    	android/res/drawable-mdpi/icon.png
    	android/res/layout
    	android/res/layout/main.xml
    	android/gen
    	android/gen/pandroid
    	android/gen/pandroid/agent
    	android/gen/pandroid/agent/R.java: Created the
    	first android agent with report of battery level 
    	and basic GPS location coordinates report
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3779 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_agents/android/src/pandroid/agent/PandroidAgentTentacle.java
@@ -0,0 +1,181 @@
+package pandroid.agent;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.content.Context;
+import android.widget.Toast;
+
+class tentacle_client {
+
+	// Return 0 when success, -1 when error
+    public int tentacle_client(String args[]) {
+    	
+	    int port = 41121;
+	    String send = null;
+	    String serverResponse = null;
+	    String address = "127.0.0.1";
+	    String data = "";
+	    String parameter = "";
+	    String filePath = null;
+	    boolean verbose = false;
+	    File file = new File("");
+	    		   
+	    for (int i=0;i<args.length;i++) {
+	    	if(i == (args.length - 1)) {
+	    		filePath = args[i];
+	    	} else {
+	    		// Get the param if is -* format or empty string otherwise
+	    		String last_parameter = parameter;
+		    	parameter = isParameter(args[i]);
+
+		    	// If is not -* param check the previous param
+		    	if(parameter.equals("")) {
+		    		if(last_parameter.equals("-a")) {
+		    			address = args[i];
+		    		}
+		    		else if(last_parameter.equals("-p")) {
+		    			port = Integer.parseInt(args[i]);
+		    		}
+		    	}
+		    	
+		    	// The solo params are checked otherwise
+		    	if(parameter.equals("-v")) {
+	    			verbose = true;
+		    	}			    	
+	    	}
+	    }
+
+	    if(filePath == null) {
+	    	getError("Incorrect parameters. File path is necessary.");
+	    }
+	    
+	    getInfo("\n*** Starting tentacle client ***\n",verbose);
+	    
+	    Socket socketCliente = null;
+	    try {
+	    	socketCliente = new Socket(address, port);
+	    } catch (UnknownHostException e) {
+	    	getError("Host don't exists");
+	    	return -1;
+	    } catch (IOException e) {
+	    	getError("Could not connect: The host is down");
+	    	return -1;
+	    }
+
+	    DataOutputStream serverOutput = null;
+	    
+	    try {
+	    	serverOutput = new DataOutputStream(socketCliente.getOutputStream());
+	    } catch (IOException e1) {
+	    	getError("Could not get Data output stream");
+	    }
+	    
+	    BufferedReader serverInput = null;
+	    
+	    try {
+	    	serverInput = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+	    } catch (IOException e1) {
+	    	getError("Could not get Buffered reader");
+	    }
+	    
+	    try {
+         file = new File (filePath);
+         FileReader fr = new FileReader (file);
+         BufferedReader br = new BufferedReader(fr);
+
+		 getInfo("Reading the file '" + file.getName() + "' content...\n", verbose);
+         // Reading the file
+         String line;
+         while((line=br.readLine())!=null)
+            data += line + '\n';
+	    } catch (IOException e) {
+	    	getError("Could not get the file");
+	    }
+	    
+	    getInfo("*** Start of transference ***\n",verbose);
+	    // Send the file name and length
+	    try {
+	    	send = "SEND <" + file.getName() + "> SIZE " + Integer.toString(data.length()) + '\n';
+	    	getInfo("Client -> Server: " + send, verbose);
+	    	serverOutput.writeBytes(send);
+	    } catch (IOException e) {
+	    	getError("Could not write on server");
+	    }
+	    try {
+	    	serverResponse = serverInput.readLine();
+	    } catch (IOException e) {
+	    	getError("Could not get server response");
+	    }
+	    
+	    getInfo("Server -> Client: " + serverResponse + "\n", verbose);
+	    if (serverResponse.equals("SEND OK")) {
+		    try {
+		    	 getInfo("Client -> Server: [file data]\n", verbose);
+		    	 serverOutput.writeBytes(data);
+		    } catch (IOException e) {
+		    	getError("Could not write on server");
+		    }
+		    try {
+		    	serverResponse = serverInput.readLine();
+		    } catch (IOException e) {
+		    	getError("Could not get server response");
+		    }
+		    
+		    getInfo("Server -> Client: " + serverResponse + "\n", verbose);
+		    if (serverResponse.equals("SEND OK")) {
+			    try {
+			    	send = "QUIT\n";
+			    	getInfo("Client -> Server: " + send, verbose);
+			    	serverOutput.writeBytes("QUIT\n");
+			    } catch (IOException e) {
+			    	getError("Could not write on server");
+			    }
+			    getInfo("*** End of transference ***\n", verbose);
+		    }
+		    else {
+			    getError("Bad server response, execution aborted.\n");
+		    }
+	    }
+	    else {
+		    getError("Bad server response, execution aborted.\n");
+	    }
+	    
+	    return 0;
+    }
+    
+    private String isParameter(String str) {
+    	if(str.equals("-a") || str.equals("-p") || str.equals("-v")) {
+    		return str;
+    	}
+    	else {
+    		return "";
+    	}
+    }
+    
+    private void getError(String error_str) {
+    	log("[ERROR] " + error_str + '\n');
+    }
+    
+    private void getInfo(String error_str, boolean verbose) {
+    	if(verbose) {
+    		log("[INFO] " + error_str + '\n');
+    	}
+    }
+    
+    private void log (String msg) {
+    	//Context context = getApplicationContext();
+    	//int duration = Toast.LENGTH_SHORT;
+
+    	//Toast toast = Toast.makeText(context, msg, duration);
+    	//toast.show();
+    }
+}

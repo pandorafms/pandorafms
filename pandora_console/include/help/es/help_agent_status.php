73193744c68ba0abf25ba873b67099378a77541e
Ramon Novoa <rnovoa@artica.es>
2011-02-17 16:31:39 +0000

    2011-02-17  Ramon Novoa  <rnovoa@artica.es>
    	* include/help/en/help_agent_status.php,
    	  include/help/es/help_agent_status.php,
    	  include/help/ja/help_agent_status.php: The 'no monitors' status does
    	  not make sense now that all modules have a status.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@3902 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/es/help_agent_status.php
+++ pandora_console/include/help/es/help_agent_status.php
@@ -28,7 +28,6 @@ Los valores posibles del <b>estado de un agente </b> son:
 	<td class="f9i"><?php print_image("images/status_sets/default/agent_ok.png", false, array("title" => "All Monitors OK", "alt" => "All Monitors OK")); ?><?php print_image("images/status_sets/faces/agent_ok.png", false, array("title" => "All Monitors OK", "alt" => "All Monitors OK")); ?></td><td>Todos los monitores están OK</td>
 
 </tr><tr>
-	<td class="f9i"><?php print_image("images/status_sets/default/agent_no_monitors.png", false, array("title" => "Agent without monitors", "alt" => "Agent without monitors")); ?><?php print_image("images/status_sets/faces/agent_no_monitors.png", false, array("title" => "Agent without monitors", "alt" => "Agent without monitors")); ?></td><td>Agente sin monitores</td>
 	<td class="f9i"><?php print_image("images/status_sets/default/agent_no_data.png", false, array("title" => "Agent without data", "alt" => "Agent without data")); ?><?php print_image("images/status_sets/faces/agent_no_data.png", false, array("title" => "Agent without data", "alt" => "Agent without data")); ?></td><td>Agente sin datos</td>
 	<td class="f9i"><?php print_image("images/status_sets/default/agent_down.png", false, array("title" => "Agent down", "alt" => "Agent down")); ?><?php print_image("images/status_sets/faces/agent_down.png", false, array("title" => "Agent down", "alt" => "Agent down")); ?></td><td>Agente caído</td>
 </tr>

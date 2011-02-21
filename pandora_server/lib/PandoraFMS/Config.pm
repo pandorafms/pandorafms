8b0faf5e14afe64fb42c0760a64e0b53a427bfbb
slerena <slerena@gmail.com>
2011-02-21 11:47:21 +0000

    2011-02-22  Sancho Lerena <slerena@artica.es>
    	* DEBIAN/postinst: Fixed permissions on /incoming dirs for www-data
    	
    	* lib/PandoraFMS/Config.pm: Updated version.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_3.2@3938 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -41,7 +41,7 @@ our @EXPORT = qw(
 	);
 
 # version: Defines actual version of Pandora Server for this module only
-my $pandora_version = "3.2";
+my $pandora_version = "3.2.1";
 my $pandora_build = "101227";
 our $VERSION = $pandora_version." ".$pandora_build;
 

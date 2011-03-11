d023256912a91ad2033ed9e6b4c2e1c34d516fb7
mdtrooper <tres.14159@gmail.com>
2011-03-11 12:33:15 +0000

    2011-03-11 Miguel de Dios  <miguel.dedios@artica.es>
    	* extensions/agents_modules.php, operation/search_modules.php,
    	operation/agentes/status_monitor.php,
    	operation/agentes/estado_ultimopaquete.php,
    	operation/agentes/estado_monitores.php: fixed the label for the graphs in
    	new window.
    	
    	Fixes: #3203474
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4087 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/agentes/estado_ultimopaquete.php
+++ pandora_console/operation/agentes/estado_ultimopaquete.php
@@ -342,17 +342,17 @@ foreach ($modules as $module) {
 		$url = 'include/procesos.php?agente='.$module["id_agente_modulo"];
 		$win_handle=dechex(crc32($module["id_agente_modulo"].$module["nombre"]));
 		echo "<td class=".$tdcolor." width='78'>";
-		$graph_label = output_clean_strict ($module["nombre"]);
+		$graph_label = safe_output($module["nombre"]);
 		
-		echo "<a href='javascript:winopeng(\"operation/agentes/stat_win.php?type=$graph_type&period=2419200&id=".$module["id_agente_modulo"]."&label=".$graph_label."&refresh=180000\", \"month_".$win_handle."\")'>" . print_image('images/grafica_m.png' , true, array("border" => '0', "alt" => '')) . "</a>&nbsp;";
+		echo "<a href='javascript:winopeng(\"operation/agentes/stat_win.php?type=$graph_type&period=2419200&id=".$module["id_agente_modulo"]."&label=".base64_encode($graph_label)."&refresh=180000\", \"month_".$win_handle."\")'>" . print_image('images/grafica_m.png' , true, array("border" => '0', "alt" => '')) . "</a>&nbsp;";
 		
-		$link ="winopeng('operation/agentes/stat_win.php?type=$graph_type&period=604800&id=".$module["id_agente_modulo"]."&label=".$graph_label."&refresh=6000','week_".$win_handle."')";
+		$link ="winopeng('operation/agentes/stat_win.php?type=$graph_type&period=604800&id=".$module["id_agente_modulo"]."&label=".base64_encode($graph_label)."&refresh=6000','week_".$win_handle."')";
 		echo '<a href="javascript:'.$link.'">' . print_image("images/grafica_w.png", true, array("border" => '0', "alt" => '')) . '</a>&nbsp;';
 		
-		$link ="winopeng('operation/agentes/stat_win.php?type=$graph_type&period=86400&id=".$module["id_agente_modulo"]."&label=".$graph_label."&refresh=600','day_".$win_handle."')";
+		$link ="winopeng('operation/agentes/stat_win.php?type=$graph_type&period=86400&id=".$module["id_agente_modulo"]."&label=".base64_encode($graph_label)."&refresh=600','day_".$win_handle."')";
 		echo '<a href="javascript:'.$link.'">' . print_image("images/grafica_d.png", true, array("border" => '0', "alt" => '')) . '</a>&nbsp;';
 
-		$link ="winopeng('operation/agentes/stat_win.php?type=$graph_type&period=3600&id=".$module["id_agente_modulo"]."&label=".$graph_label."&refresh=60','hour_".$win_handle."')";
+		$link ="winopeng('operation/agentes/stat_win.php?type=$graph_type&period=3600&id=".$module["id_agente_modulo"]."&label=".base64_encode($graph_label)."&refresh=60','hour_".$win_handle."')";
 		echo '<a href="javascript:'.$link.'">' . print_image("images/grafica_h.png", true, array("border" => '0', "alt" => "")) . '</a>';
 
 	}

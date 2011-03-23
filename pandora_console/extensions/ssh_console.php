d4864a672b387d86159c1723068f0c65afbc0998
raulmateos <noreply@pandorafms.org>
2011-03-23 16:13:28 +0000

    2011-03-23  Raul Mateos <raulofpandora@gmail.com>
    	* extensions/ssh_console.php, extensions/vnc_view.php,
    	extensions/update_manager.php, extensions/users_connected.php,
    	extensions/extension_uploader.php, extensions/insert_data.php,
    	extensions/module_groups.php, extensions/plugin_registration.php,
    	extensions/agent_modules.php, extensions/resource_registration.php,
    	extensions/resource_exportation.php, extensions/dbmanager.php,
    	extensions/pandora_logs.php, general/*.php, ajax.php, 
    	operation/search_*.php, operation/menu.php, operation/extensions.php,
    	godmode/menu.php, godmode/extensions.php, godmode/admin_access_logs.php:
    	CReverted unwanted license changes.
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4126 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/extensions/ssh_console.php
+++ pandora_console/extensions/ssh_console.php
@@ -6,7 +6,7 @@
 // Please see http://pandorafms.org for full contribution list
 
 // This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public License
+// modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; version 2
 
 // This program is distributed in the hope that it will be useful,

1ad4276daf31abea98647f5fca45124ba3ddef00
mdtrooper <tres.14159@gmail.com>
2011-03-28 14:35:08 +0000

    2011-03-28 Miguel de Dios  <miguel.dedios@artica.es>
    	* pandoradb.sql, pandoradb.postgreSQL.sql, pandoradb.oracle.sql,
    	extras/pandoradb_migrate_v3.2_to_v4.0.sql: added column "style" for to
    	add style (as json encode) the many styles for the contents.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4138 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/pandoradb.oracle.sql
+++ pandora_console/pandoradb.oracle.sql
@@ -931,6 +931,7 @@ CREATE TABLE treport_content (
 	order_uptodown NUMBER(10, 0) default 0 NOT NULL,
 	show_graph NUMBER(10, 0) default 0 NOT NULL,
 	group_by_agent NUMBER(10, 0) default 0 NOT NULL
+	style NCLOB default '' NOT NULL
 );
 
 CREATE SEQUENCE treport_content_s INCREMENT BY 1 START WITH 1;

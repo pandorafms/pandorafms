47f4f0bf3330c86a2d470c6fa5825253dc98eb4c
zarzuelo <noreply@pandorafms.org>
2011-04-01 12:28:50 +0000

    2011-04-01  Sergio Martin <sergio.martin@artica.es>
    	* include/graphs/functions_pchart.php
    	include/graphs/pChart/pDraw.class.php
    	include/graphs/fgraph.php
    	include/graphs/functions_utils.php: Changed the send data method 
    	from sessions to files with serialize functions. Creation of a
    	utils functions file. Improvement of the pChart library adding 
    	attributes to avoid ticks and grids when the data under the 
    	xaxis is empty.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4160 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/graphs/functions_utils.php
@@ -0,0 +1,60 @@
+<?php
+// Copyright (c) 2011-2011 Ártica Soluciones Tecnológicas
+// http://www.artica.es  <info@artica.es>
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+function serialize_in_temp($array = array(), $serial_id = null) {
+	$json = json_encode($array);
+	
+	if($serial_id === null) {
+		$serial_id = uniqid();
+	}
+	
+	$file_path = sys_get_temp_dir()."/pandora_serialize_".$serial_id;
+		
+	if(file_put_contents($file_path, $json) === false) {
+		return false;
+	}
+
+	return $serial_id;
+}
+
+function unserialize_in_temp($serial_id = null, $delete = true) {
+	if($serial_id === null) {
+		return false;
+	}
+	
+	$file_path = sys_get_temp_dir()."/pandora_serialize_".$serial_id;
+
+	$content = file_get_contents($file_path);
+
+	if($content === false) {
+		return false;
+	}
+	
+	$array = json_decode($content, true);
+	
+	if($delete) {
+		unlink($file_path);
+	}
+
+	return $array;
+}
+
+function delete_unserialize_in_temp($serial_id = null) {
+	if($serial_id === null) {
+		return false;
+	}
+	
+	$file_path = sys_get_temp_dir()."/pandora_serialize_".$serial_id;
+		
+	return unlink($file_path);
+}
+?>

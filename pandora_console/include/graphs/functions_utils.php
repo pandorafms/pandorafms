7e770bba098ad1b961ba68fd7acfaaf3c4e861c6
mdtrooper <tres.14159@gmail.com>
2011-04-11 16:45:03 +0000

    2011-04-11 Miguel de Dios  <miguel.dedios@artica.es>
    	* godmode/reporting/graph_builder.main.php: changed to use constants.
    	
    	* include/graphs/fgraph.php: dded the show image when none data
    	and added the function "stacked_line_graph".
    	
    	* include/graphs/functions_pchart.php: fixed when haven't the $xaxisname and
    	$yaxisname. Fixed typo mistake.
    
    	* include/graphs/functions_utils.php: added function "stack_data".
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4186 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/graphs/functions_utils.php
+++ pandora_console/include/graphs/functions_utils.php
@@ -68,4 +68,35 @@ function reverse_data($array) {
 	
 	return $array2;
 }
+
+
+function stack_data(&$chart_data, &$legend = null, &$color = null) {
+	foreach ($chart_data as $val_x => $graphs) {
+		$prev_val = 0;
+		$key = 1000;
+		foreach ($graphs as $graph => $val_y) {
+			$chart_data[$val_x][$graph] += $prev_val;
+			$prev_val = $chart_data[$val_x][$graph];
+			$temp_data[$val_x][$key] = $chart_data[$val_x][$graph];
+			if (isset($color)) {
+				$temp_color[$key] = $color[$graph];
+			}
+			if (isset($legend)) {
+				$temp_legend[$key] = $legend[$graph];
+			}
+			$key--;
+		}
+		ksort($temp_data[$val_x]);
+	}
+	
+	$chart_data = $temp_data;
+	if (isset($legend)) {
+		$legend = $temp_legend;
+		ksort($legend);
+	}
+	if (isset($color)) {
+		$color = $temp_color;
+		ksort($color);
+	}
+}
 ?>

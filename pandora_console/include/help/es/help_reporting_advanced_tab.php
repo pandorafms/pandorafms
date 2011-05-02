5ffb96ebd532e41d517800be52ecc8161527b1f6
slerena <slerena@gmail.com>
2011-05-02 11:18:49 +0000

    2011-05-02 Sancho Lerena <slerena@artica.es>
            *  include/help/en/help_reporting_advanced_tab.php,
            include/help/es/help_reporting_advanced_tab.php: Missing help
            files.  
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4294 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/es/help_reporting_advanced_tab.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Opciones avanzadas</h1>
+<p>
+Puede usar una macro en la portada:<br><br>
+
+<b>(_DATETIME_)</b>  este token será reemplazado por la fecha y hora actual en el momento de generar el PDF.
+<br><br>
+Si tiene problemas visualizando una imagen, prueba a usar el formato PNG en vez de usar otros formatos.
+</p>

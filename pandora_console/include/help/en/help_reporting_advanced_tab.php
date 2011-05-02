b49989663fcd7496209c9deab3b6a216f52a0ac1
slerena <slerena@gmail.com>
2011-05-02 11:09:06 +0000

    2011-05-01 SAncho Lerena <slerena@artica.es>
            *  include/help/en/help_reporting_advanced_tab.php,
            include/help/es/help_reporting_advanced_tab.php: MIssing help
            files.
            
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_3.2@4293 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_reporting_advanced_tab.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Advanced reporting</h1>
+<p>
+You can use only a macro in the first page:  (_DATETIME_) this token will be replaced by the current date and time in the moment of generating the PDF.
+<br><br>
+If you have problems loading a image file, try using a PNG format instead JPG or other formats.
+</p>

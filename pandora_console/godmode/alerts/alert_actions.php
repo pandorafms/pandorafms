2676d688dbecb14438433dcc31d0073c8f11bbb2
mdtrooper <tres.14159@gmail.com>
2011-06-16 15:32:12 +0000

    2011-06-16 Miguel de Dios  <miguel.dedios@artica.es>
    	Merge from the branch.
    	
    	* godmode/alerts/alert_actions.php: fixed the url to copy the alert action.
    	The file didn't upload to repository.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4461 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/alerts/alert_actions.php
+++ pandora_console/godmode/alerts/alert_actions.php
@@ -285,7 +285,7 @@ foreach ($actions as $action) {
 	$data[0] = '<a href="index.php?sec=galertas&sec2=godmode/alerts/configure_alert_action&id='.$action['id'].'">'.
 		$action['name'].'</a>';
 	$data[1] = ui_print_group_icon ($action["id_group"], true) .'&nbsp;'. ui_print_truncate_text(groups_get_name ($action["id_group"], true));
-	$data[2] = '<a href="index.php?sec=galertas&sec2=godmode/alerts/alert_actions&copy_action=1&id='.$action['id'].'"
+	$data[2] = '<a href="index.php?sec=galertas&sec2=godmode/alerts/alert_actions&amp;copy_action=1&amp;id='.$action['id'].'"
 		onClick="if (!confirm(\''.__('Are you sure?').'\')) return false;">' .
 		html_print_image("images/copy.png", true) . '</a>';
 	$data[3] = '<a href="index.php?sec=galertas&sec2=godmode/alerts/alert_actions&delete_action=1&id='.$action['id'].'"

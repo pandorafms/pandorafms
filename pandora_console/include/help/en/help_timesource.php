1f60ae22992fb505e5e8faf6980272d259304c22
juanmanuelr <noreply@pandorafms.org>
2011-07-12 15:10:42 +0000

    2011-07-12 Juan Manuel Ramon <juanmanuel.ramon@artica.es>
    	* include/help/en/help_timesource.php
    	include/help/es/help_timesource.php
    	include/help/ja/help_timesource.php: Fixed bad sintax query.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4550 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_timesource.php
+++ pandora_console/include/help/en/help_timesource.php
@@ -40,13 +40,15 @@ global $config;
 
 switch ($config["dbtype"]) {
 	case "mysql":
-		$timestamp = db_get_value_sql("SELECT UNIX_TIMESTAMP();");
+		$timestamp = db_process_sql("SELECT UNIX_TIMESTAMP();");
+		$timestamp = $timestamp[0]['UNIX_TIMESTAMP()'];
 		break;
 	case "postgresql":
 		$timestamp = db_get_value_sql("SELECT ceil(date_part('epoch', CURRENT_TIMESTAMP));");
 		break;
 	case "oracle":
-		$timestamp = db_get_value_sql("SELECT ceil((sysdate - to_date('19700101000000','YYYYMMDDHH24MISS')) * (86400)) as dt FROM dual");
+		$timestamp = db_process_sql("SELECT ceil((sysdate - to_date('19700101000000','YYYYMMDDHH24MISS')) * (86400)) as dt FROM dual");
+		$timestamp = $timestamp[0]['dt'];
 		break;
 }
 ui_print_timestamp ($timestamp, false, $option);

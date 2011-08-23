a810b0c1c051ffff93cdbf914233224a1a1511f0
Aldrioth <tomaspalacios91@gmail.com>
2011-08-23 16:43:56 +0000

    2011-08-22  Tomas Palacios  <tomas.palacios@artica.es>
            * pandora_console/pandoradb_data.sql
            pandora_console/pandoradb.data.oracle.sql
            pandora_console/pandoradb.postgreSQL.sql:updated the
            default site news.
    
            Fixes: Bug ID 3396846
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4811 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/pandoradb.data.oracle.sql
+++ pandora_console/pandoradb.data.oracle.sql
@@ -301,8 +301,8 @@ END;;
 --
 BEGIN
 LOCK TABLE tnews IN EXCLUSIVE MODE;
-INSERT INTO tnews (id_news, author, subject, text, timestamp) VALUES (1,'admin','Welcome to Pandora FMS 3.2!','This is the new Pandora FMS Console. A lot of new features have been added since last version. Please read the documentation about it, and feel free to test any option.<br><br>The Pandora FMS Team.',current_timestamp);
-INSERT INTO tnews (id_news, author, subject, text, timestamp) VALUES (2,'admin','New GIS Feature','Try our new GIS representation, activate GIS in Setup to see new available options.',current_timestamp);
+INSERT INTO tnews (id_news, author, subject, text, timestamp) VALUES (1,'admin','Welcome to Pandora FMS 34.0','This is the new Pandora FMS Console. A lot of new features have been added since last version. Please read the documentation about it, and feel free to test any option.<br><br>The Pandora FMS Team.',current_timestamp);
+INSERT INTO tnews (id_news, author, subject, text, timestamp) VALUES (2,'admin','New Pandora FMS Agent Features','Feel free to test our new features for both Windows and Linux agents: Proxy and Broker modes.',current_timestamp);
 COMMIT;
 END;;
 

90b6ecf22676b8c3209686a44f0aefd0c39fd441
zarzuelo <zarzuelo@gmail.com>
2011-08-30 08:27:31 +0000

    2011-08-30  Sergio Martin <sergio.martin@artica.es>
    	* operation/agentes/estado_agente.php: Fixed URL injection 
    	vulnerability replacing safe_output for safe_output_html
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_3.2@4848 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/agentes/estado_agente.php
+++ pandora_console/operation/agentes/estado_agente.php
@@ -78,7 +78,7 @@ if (is_ajax ()) {
 
 // Take some parameters (GET)
 $group_id = (int) get_parameter ("group_id", 0);
-$search = safe_output(get_parameter ("search", ""));
+$search = safe_output_html(get_parameter ("search", ""));
 $offset = get_parameter('offset', 0);
 $refr = get_parameter('refr', 0);
 

ef32b9db1718e8c9115345e7b6562f4c58cff71a
jsatoh <junichi@rworks.jp>
2011-08-30 06:55:49 +0000

    2011-08-30  Junichi Satoh  <junichi@rworks.jp>
    	* godmode/massive/massive_enable_disable_alerts.php,
    	godmode/massive/massive_standby_alerts.php: Added group recursion
    	feature, it allows to search agents in child groups.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4847 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/massive/massive_standby_alerts.php
+++ pandora_console/godmode/massive/massive_standby_alerts.php
@@ -63,6 +63,7 @@ if (is_ajax ()) {
 $id_group = (int) get_parameter ('id_group');
 $id_agents = (array) get_parameter ('id_agents');
 $action = (string) get_parameter ('action', '');
+$recursion = get_parameter ('recursion');
 
 $result = false;
 
@@ -117,7 +118,9 @@ $table->style[0] = 'font-weight: bold; vertical-align:top';
 $table->style[2] = 'font-weight: bold';
 $table->size = array ();
 $table->size[0] = '15%';
-$table->size[1] = '85%';
+$table->size[1] = '55%';
+$table->size[2] = '15%';
+$table->size[3] = '15%';
 
 $table->data = array ();
 
@@ -127,6 +130,8 @@ $table->data[0][0] .= html_print_input_hidden('id_alert_template_not_standby', $
 $table->data[0][0] .= __('Group');
 $table->data[0][1] = html_print_select_groups(false, "AR", true, 'id_group', $id_group,
         '', '', '', true, false, true, '');
+$table->data[0][2] = __('Group recursion');
+$table->data[0][3] = html_print_checkbox ("recursion", 1, $recursion, true, false);
 
 $table->data[1][0] = __('Agents');
 $table->data[1][0] .= '<span id="agent_loading" class="invisible">';
@@ -179,8 +184,15 @@ ui_require_jquery_file ('pandora.controls');
 $(document).ready (function () {
 	clear_alert_fields();
 
+	var recursion;
+	$("#checkbox-recursion").click(function (){
+		recursion = this.checked ? 1 : 0;
+		$("#id_group").trigger("change");
+	});
+
 	$("#id_group").pandoraSelectGroupAgent ({
 		agentSelect: "select#id_agents",
+		recursion: function() {return recursion},
 		callbackPost: function () {
 			clear_alert_fields();
 		}

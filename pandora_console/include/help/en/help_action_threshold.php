df7de9e3b2d579bdcf65ba79ba690beed8c6fb1a
mdtrooper <tres.14159@gmail.com>
2011-09-05 16:12:06 +0000

    2011-09-05  Miguel de Dios  <miguel.dedios@artica.es>
    	* include/help/en/help_action_threshold.php,
    	include/help/es/help_action_threshold.php: added help for threshold from
    	the wiki documentation.
    	
    	Fixes: #3371385
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4908 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_action_threshold.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Time Threshold</h1>
+
+<p>
+Time Threshold
+</p>
+<p>
+Defines the time interval in which it is guaranteed that an alert is not going to be fired more times than the number fixed in Maximum number of alerts. If the defined interval is exceeded, an alert will not recover if it comes to an specific value, except if the alert Recover value would be activated. In this case it is recovered inmediatelly after receiving an specific value,regardless the threshold.
+</p>
---
+++ pandora_console/include/help/es/help_action_threshold.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Time Threshold</h1>
+
+<p>
+Time Threshold
+</p>
+<p>
+Define el intervalo de tiempo en el cual se garantiza que una alerta no se va a disparar más veces del número establecido en Numero máximo de alertas. Pasado el intervalo definido, una alerta se recupera si llega un valor correcto, salvo que esté activado el valor Recuperación de alerta, en cuyo caso se recupera inmediatamente después de recibir un valor correcto independientemente del umbral.
+</p>

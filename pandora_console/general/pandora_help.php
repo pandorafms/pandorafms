1da9951ce46593f4dddaf6ddc633f74123b272be
mdtrooper <tres.14159@gmail.com>
2011-09-06 16:22:40 +0000

    2011-09-06  Miguel de Dios  <miguel.dedios@artica.es>
    	* general/pandora_help.php: changet the text align of footer.
    	
    	Fixes: #3371432
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4924 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/general/pandora_help.php
+++ pandora_console/general/pandora_help.php
@@ -78,7 +78,7 @@ echo '<div style="font-size: 12px; margin-left: 30px; margin-right:25px;">';
 require_once ($help_file);
 echo '</div>';
 echo '<br /><br /><hr width="100%" size="1" />';
-echo '<div style="padding: 15px; background-color: #6E6E6E; font-family: verdana, arial; font-size: 11px;">';
+echo '<div style="text-align: center; padding: 15px; background-color: #6E6E6E; font-family: verdana, arial; font-size: 11px;">';
 include ('footer.php');
 echo '</div>';
 ?>

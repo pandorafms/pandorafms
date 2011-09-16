5face9a62da52f3f432a486c1790cab0168c1819
slerena <slerena@gmail.com>
2011-09-16 12:08:53 +0000

    2011-09-16  Sancho Lerena <slerena@artica.es>
            *  extras/build_src.sh: Fixed bad path.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4965 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- extras/build_src.sh
+++ extras/build_src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CODEHOME=~/code/pandora/trunk
-CODEHOME_ENT=~/code/artica
+CODEHOME_ENT=~/code/artica/code
 RPMHOME=/usr/src/packages
 VERSION=$(grep 'my $pandora_version =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
 KEYGEN_VERSION=$(grep "%define version" $CODEHOME_ENT/updatemanager/keygen/pandora/pandora_keygen.spec | awk '{print $3}')

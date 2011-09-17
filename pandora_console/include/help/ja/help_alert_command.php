18cfa9850bd31ab8e13c85934e2bf2959b7f6733
jsatoh <junichi@rworks.jp>
2011-09-17 14:46:04 +0000

    2011-09-17  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/ja/help_alert_action.php,
    	include/help/ja/help_alert_command.php,
    	include/help/ja/help_alert_compound.php,
    	include/help/ja/help_alert_template.php,
    	include/help/ja/help_collection_tab.php,
    	include/help/ja/help_collections.php,
    	include/help/ja/help_component_groups.php,
    	include/help/ja/help_event_alert.php,
    	include/help/ja/help_export_server.php,
    	include/help/ja/help_external_alert.php: Added new help files.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4967 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/ja/help_alert_command.php
@@ -0,0 +1,9 @@
+<?php
+/*
+* @package Include/help/ja
+*/
+?>
+
+<h1>アラートコマンド</h1>
+
+<p>&#34;しきい値を越えた場合&#34;の Pandora FMS の動作には、syslog への記録、e-mail、SMS 送信や、Pandora FMS が動作するマシンでの任意のスクリプトの実行など、さまざまな種類があります。</p>

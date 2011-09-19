e37f8716aa01884a8402fe2f87fab1a9edfef10f
jsatoh <junichi@rworks.jp>
2011-09-19 01:59:57 +0000

    2011-09-19  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/ja/help_gis_tab.php,
    	include/help/ja/help_graph_builder.php,
    	include/help/ja/help_graph_editor.php,
    	include/help/ja/help_graph_view.php,
    	include/help/ja/help_inventory_tab.php,
    	include/help/ja/help_local_component.php,
    	include/help/ja/help_module_linking.php,
    	include/help/ja/help_policy_agent.php,
    	include/help/ja/help_policy_queue.php,
    	include/help/ja/help_profile.php,
    	include/help/ja/help_reporting_global_tab.php,
    	include/help/ja/help_reporting_item_editor_tab.php,
    	include/help/ja/help_reporting_list_items_tab.php,
    	include/help/ja/help_reporting_preview_tab.php,
    	include/help/ja/help_reporting_wizard_sla_tab.php,
    	include/help/ja/help_reporting_wizard_tab.php: Added new help files.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4968 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/ja/help_reporting_wizard_sla_tab.php
@@ -0,0 +1,9 @@
+<?php
+/*
+* @package Include/help/ja
+*/
+?>
+
+<h1>SLA ウィザード</h1>
+
+<p>Pandora FMS でモニタリングしているサービスレベル (Service Level Agreement) を計測できます。このウィザードでは、複数のエージェントの SLA レポートを作成することができます。</p>

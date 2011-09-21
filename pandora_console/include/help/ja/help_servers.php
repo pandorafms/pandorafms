d6c0297a2a5996c38de8945638d7db05aaff2fde
jsatoh <junichi@rworks.jp>
2011-09-21 02:28:14 +0000

    2011-09-21  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/ja/help_servers.php,
    	include/help/ja/help_service_elements_management.php,
    	include/help/ja/help_service_management_edit.php,
    	include/help/ja/help_services_management.php,
    	include/help/ja/help_snmp_alert.php,
    	include/help/ja/help_snmp_alert_custom.php,
    	include/help/ja/help_snmp_explorer.php,
    	include/help/ja/help_template_tab.php,
    	include/help/ja/help_view_services_detail.php,
    	include/help/ja/help_visual_console_editor_data_tab.php,
    	include/help/ja/help_visual_console_editor_editor_tab.php,
    	include/help/ja/help_visual_console_editor_preview_tab.php,
    	include/help/ja/help_visual_console_editor_wizard_tab.php: Added new
    	help files.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@4974 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/ja/help_servers.php
@@ -0,0 +1,26 @@
+<?php
+/*
+* @package Include/help/ja
+*/
+?>
+
+<h1>サーバ管理</h1>
+
+<p>Pandora FMS サーバは、定義された監視を実行する要素です。監視をし結果に応じて状態を変更します。また、データの状態に応じてアラートを生成します。</p>
+
+<p>Pandora FMS のデータサーバは、冗長化および負荷分散が可能です。大きなシステムでは、大量の機能を扱ったり物理的に分散した情報を扱うために、さまざまな Pandora FMS サーバを同時に利用可能です。</p>
+
+<p>Pandora FMS サーバは、各要素に問題が無いか、また、アラートが定義されているかを常に確認します。何かあると、アラームに定義された SMS 送信、e-mail 送信、スクリプトの実行など、アクションを実行します。l</p>
+
+<ul>
+<li type="circle">データサーバ(Data Server)</li>
+<li type="circle">ネットワークサーバ(Network Server)</li>
+<li type="circle">SNMPサーバ(SNMP Server)</li>
+<li type="circle">WMIサーバ(WMI Server)</li>
+<li type="circle">自動検出サーバ(Recognition Server)</li>
+<li type="circle">プラグインサーバ(Plugins Server)</li>
+<li type="circle">予測サーバ(Prediction Server)</li>
+<li type="circle">WEBテストサーバ(WEB Test Server)</li>
+<li type="circle">エクスポートサーバ(Export Server)</li>
+<li type="circle">インベントリサーバ(Inventory Server)</li>
+</ul>

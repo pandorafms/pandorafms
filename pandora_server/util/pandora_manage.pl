7fa5d063617dbad41ab688cd9e9bab3546e79212
javilanz <javi.lanz@gmail.com>
2011-10-10 11:07:34 +0000

    2011-10-10 Javier Lanz <javier.lanz@artica.es>
            * util/pandora_manage.pl: Fixed a wee bug with 'group' parameter
            creating events
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5051 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -1096,7 +1096,7 @@ sub pandora_manage_main ($$$) {
 			
 			my $id_group;
 			
-			if (!defined($group_name) || $group_name == "All") {
+			if (!defined($group_name) || $group_name eq "All") {
 				$id_group = 0;
 			}
 			else {

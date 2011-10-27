20cbb8176bfc0425c23b567cbafe621f01d2cb99
Ramon Novoa <rnovoa@artica.es>
2011-10-27 16:50:37 +0000

    2011-10-27  Ramon Novoa  <rnovoa@artica.es>
    	* build_src.sh: Include build number in tarballs for nightly builds.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5088 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- extras/build_src.sh
+++ extras/build_src.sh
@@ -3,7 +3,13 @@ CODEHOME=~/code/pandora/trunk
 CODEHOME_ENT=~/code/artica/code
 RPMHOME=/usr/src/packages
 VERSION=$(grep 'my $pandora_version =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
+BUILD=$(grep 'my $pandora_build =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
 KEYGEN_VERSION=$(grep "%define version" $CODEHOME_ENT/updatemanager/keygen/pandora/pandora_keygen.spec | awk '{print $3}')
+
+# Add build string for nightly builds
+if [ "$1" == "nightly" ]; then
+	VERSION="$VERSION-$BUILD"
+fi
 
 if [ ! -d $RPMHOME/SOURCES ]; then
 	mkdir -p $RPMHOME/SOURCES || exit 1

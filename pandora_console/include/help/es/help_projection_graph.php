65e796eb388988753157ad9f42f98f9201c7e8b7
juanmanuelr <noreply@pandorafms.org>
2011-10-28 09:09:53 +0000

    2011-10-28 Juan Manuel Ramon <juanmanuel.ramon@artica.es>
    	* include/help/en/help_projection_graph.php
    	include/help/en/help_prediction_date.php
    	include/help/es/help_projection_graph.php
    	include/help/es/help_prediction_date.php
    	images/help/prediction_date.png
    	images/help/projection_periods.png
    	godmode/reporting/reporting_builder.item_editor.php: Added 
    	contextual help.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5094 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/es/help_projection_graph.php
@@ -0,0 +1,17 @@
+<h1>Projection graph</h1>
+
+
+<?php html_print_image("images/help/projection_periods.png", false, array('height' => '210')); ?>
+<br>
+<p>
+Projection graphs hacen una proyección de los datos del modulo en el futuro. La proyeccion se basa en una regresion lineal.
+</p>
+<p>
+Parametros importantes son:
+</p>
+<p>
+<b>Period</b>: Periodo de tiempo para hacer la estimacion basada en datos reales del modulo.
+</p>
+<p>
+<b>Projection period</b>: Periodo de tiempo en el futuro donde los datos del modulo seran estimados.
+</p>

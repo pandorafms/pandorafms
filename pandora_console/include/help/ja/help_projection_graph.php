bea6d3f94dffa46f913326d5d00f2644b5621148
jsatoh <junichi@rworks.jp>
2011-10-29 01:24:36 +0000

    2011-10-29  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/ja/help_projection_graph.php,
    	include/help/ja/help_prediction_date.php: Added new help files.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5101 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/ja/help_projection_graph.php
@@ -0,0 +1,17 @@
+<h1>予想グラフ</h1>
+
+
+<?php html_print_image("images/help/projection_periods.png", false, array('height' => '210')); ?>
+<br>
+<p>
+予想グラフは将来のモジュールデータを予想します。予想は線形回帰を利用しています。
+</p>
+<p>
+重要なパラメータは次の通りです。
+</p>
+<p>
+<b>間隔(Period)</b>: 予想に使うデータの時間間隔
+</p>
+<p>
+<b>予想間隔(Projection period)</b>: モジュールデータを予想する将来の時間間隔
+</p>

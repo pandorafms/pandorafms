f8105f8393b3ff753c89850b0b4d829ecd8a4ae1
juanmanuelr <noreply@pandorafms.org>
2011-11-11 08:35:21 +0000

    2011-11-11 Juan Manuel Ramon <juanmanuel.ramon@artica.es>
    	* godmode/massive/massive_edit_agents.php: Added id field to form in
    	this view.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5128 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/massive/massive_edit_agents.php
+++ pandora_console/godmode/massive/massive_edit_agents.php
@@ -208,8 +208,7 @@ $all_agents = agents_get_group_agents ($id_group, array('disabled' => 1), "none"
 $table->data[1][1] = html_print_select ($all_agents,
 	'id_agents[]', 0, false, '', '', true, true);
 
-echo '<form method="post" id="form_agents" action="index.php?sec=gmassive&sec2=godmode/massive/massive_operations&option=edit_agents">';
-//echo '<form method="post" action="index.php?sec=gmassive&sec2=godmode/massive/massive_operations&option=edit_agents">';
+echo '<form method="post" id="form_agent" action="index.php?sec=gmassive&sec2=godmode/massive/massive_operations&option=edit_agents">';
 html_print_table ($table);
 
 $nombre_agente = "";

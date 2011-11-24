5543a2da3aeecb3cd7a2d7161ca0a0a7a61e9cee
juanmanuelr <noreply@pandorafms.org>
2011-11-24 15:48:21 +0000

    2011-11-24 Juan Manuel Ramon <juanmanuel.ramon@artica.es>
    	* godmode/setup/setup.php: Added confirmation when "Use Enterprise 
    	ACL System" is checked.
    	
    	Fixes: #3438190
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5163 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/setup/setup.php
+++ pandora_console/godmode/setup/setup.php
@@ -214,6 +214,16 @@ function replaySound(type) {
 		$('#layer_sound_' + type).html("<embed src='" + $("#sound_" + type).val() + "' autostart='true' hidden='true' loop='true'>");
 	}
 }
+
+
+$(document).ready (function () {
+	$("#radiobtn0011").click(function(){
+			flag = $("#radiobtn0011").is(':checked');
+			if (flag == true){
+				<?php echo __("if (! confirm ('" . __('If Enterprise ACL System is enabled without rules you will lose access to Pandora FMS Console (even admin). Do you want to continue?') . "')) return false", true); ?>
+			}
+	});
+});
 </script>
 <?php
 

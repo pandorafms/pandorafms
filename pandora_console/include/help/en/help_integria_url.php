070c2337425f53f6b3db4889144b6176ff1ef74a
zarzuelo <noreply@pandorafms.org>
2011-11-24 18:16:26 +0000

    2011-11-24  Sergio Martin <sergio.martin@artica.es>
    	* include/help/en/help_integria_url.php
    	include/help/es/help_integria_url.php
    	godmode/setup/setup.php: Added help for integria api
    	url in setup
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5166 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_integria_url.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Integria URL</h1>
+
+<p>
+The URL of Integria must be the main path of the installation.
+<br><br>
+Example: http://192.169.75.4/integria
+</p>
---
+++ pandora_console/include/help/es/help_integria_url.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Include/help/es
+ */
+?>
+<h1>Integria URL</h1>
+
+<p>
+La URL de Integria debe ser la ruta principal de la instalación.
+<br><br>
+Ejemplo: http://192.169.75.4/integria
+</p>

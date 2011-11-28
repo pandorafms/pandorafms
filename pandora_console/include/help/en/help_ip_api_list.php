4d7a6cdb41420ce89e8a7bab04e3853b91eb9a84
zarzuelo <noreply@pandorafms.org>
2011-11-28 16:37:08 +0000

    2011-11-28  Sergio Martin <sergio.martin@artica.es>
    	* include/functions_api.php
    	include/help/en/help_ip_api_list.php
    	include/help/es/help_ip_api_list.php
    	godmode/setup/setup.php: Improved the IP ACL Access 
    	list to allow substrings for bug 3438385
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5187 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_ip_api_list.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>IP list with API access</h1>
+
+<p>
+Individual access: The list of IPs separate with carriage return.<br>
+Advanced access: Use "*" as wilcard.<br><br>
+
+Examples: <br>
+"192.168.20.*" will allows access to all the subnet 192.168.20.0/24<br>
+"*" will allows access to all the possible IP's
+</p>
---
+++ pandora_console/include/help/es/help_ip_api_list.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Include/help/es
+ */
+?>
+<h1>Lista de IPs con acceso al API</h1>
+
+<p>
+Acceso individual: La lista de IPs separadas por retorno de carro.<br>
+Acceso avanzado: Utilizar "*" como comodín.<br><br>
+
+Ejemplos: <br>
+"192.168.20.*" permitirá el acceso a toda la subred 192.168.20.0/24<br>
+"*" permitirá el acceso a todas las IP's
+</p>

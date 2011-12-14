401f257b214dc8c4a1cff2da49ba533154e9ce54
juanmanuelr <noreply@pandorafms.org>
2011-12-14 13:52:46 +0000

    2011-12-14 Juan Manuel Ramon <juanmanuel.ramon@artica.es>
    	* include/functions_events.php
    	godmode/agentes/module_manager_editor_common.php
    	godmode/agentes/module_manager.php: Some corrections over this 
    	views.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5259 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/agentes/module_manager_editor_common.php
+++ pandora_console/godmode/agentes/module_manager_editor_common.php
@@ -123,9 +123,9 @@ $table_simple->style[1] = 'width: 40%';
 $table_simple->style[2] = 'font-weight: bold; vertical-align: top';
 
 $table_simple->data[0][0] = __('Name');
-$table_simple->data[0][1] = html_print_input_text ('name', io_safe_output($name), '', 50, 100, true, $disabledBecauseInPolicy);
+$table_simple->data[0][1] = html_print_input_text ('name', io_safe_output($name), '', 45, 100, true, $disabledBecauseInPolicy);
 
-if (isset($id_agent_module) && isset($id_agente)) {
+if (!empty($id_agent_module) && isset($id_agente)) {
 	$table_simple->data[0][1] .= '&nbsp;<a href="index.php?sec=gagente&tab=module&sec2=godmode/agentes/configurar_agente&id_agente='.$id_agente.'&delete_module='.$id_agent_module.'"
 		onClick="if (!confirm(\' '.__('Are you sure?').'\')) return false;">';
 	$table_simple->data[0][1] .= html_print_image ('images/cross.png', true,

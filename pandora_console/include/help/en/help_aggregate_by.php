350243bd6c4c0bba04d3ed34a7724b95cb341e80
vgilc <noreply@pandorafms.org>
2011-12-21 14:39:50 +0000

    2011-12-21  Vanessa Gil  <vanessa.gil@artica.es> 	* include/functions_netflow.php 	  include/help/en/help_aggregate_by.php: added files to netflow 	traffic monitoring.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5290 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_aggregate_by.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Aggregate</h1>
+
+Aggregate by destination ip: flows are grouped to show the traffic for each different destination IP.<br>
+
+Aggregate by source ip: flows are grouped to show the traffic for each different source IP.<br>
+
+Aggregate by destination port: flows are grouped to show the traffic for each different destination port.<br>
+
+Aggregate by source port: flows are grouped to show the traffic for each different source port.<br>
+
+No aggregate: global traffic.<br>

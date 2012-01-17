dcdd53a9fc3823af2727c7a2ed1feaf1af4c858e
Ramon Novoa <rnovoa@artica.es>
2012-01-17 14:01:49 +0000

    2012-01-17  Ramon Novoa  <rnovoa@artica.es>
    	* include/functions_graph.php, include/functions_netflow.php,
    	  godmode/netflow/nf_edit_form.php, pandoradb.sql,
    	  pandoradb.postgreSQL.sql, pandoradb.oracle.sql: Added suport for
    	  custom filters. Small fixes.
    
    	* include/help/en/help_pcap_filter.php,
    	  include/help/es/help_pcap_filter.php: Added to repository. Nfdump
    	  filter syntax help files.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5382 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/es/help_pcap_filter.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Sintaxis de filtros de Nfdump</h1>
+
+La sintaxis de filtros es muy parecida a la de tcpdump. Por ejemplo:
+
+<ul>
+<li>Capturar tráfico hacia o desde 192.168.0.1:</li>
+<pre>
+host 192.168.0.1
+</pre>
+
+<ul>
+<li>Capturar tráfico hacia 192.168.0.1:</li>
+<pre>
+dst host 192.168.0.1
+</pre>
+
+<li>Capturar tráfico desde 192.168.0.0/24:</li>
+<pre>
+src net 192.168.0.0/24
+</pre>
+
+<li>Capturar tráfico HTTP y HTTPS:</li>
+<pre>
+(port 80) or (port 443)
+</pre>
+
+<li>Capturar todo el tráfico excepto DNS:</li>
+<pre>
+port not 53
+</pre>
+
+<li>Capturar el tráfico SSH hacia 192.168.0.1:</li>
+<pre>
+(port 22) and (dst host 192.168.0.1)
+</pre>

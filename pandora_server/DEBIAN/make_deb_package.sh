74aedbda445e8a1976403047f7122247280ce3cf
ramonn <noreply@pandorafms.org>
2012-01-24 18:46:00 +0000

    2012-01-24  Ramon Novoa  <rnovoa@artica.es>
    	* DEBIAN/make_deb_package.sh,
    	  pandora_server_installer,
    	  pandora_server.spec: Auto-create the netflow storage dir.
    
    	* bin/pandora_server: Aesthetic changes.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5418 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -76,6 +76,8 @@ then
 	chmod 770 temp_package/var/spool/pandora/data_in/md5
 	mkdir -p temp_package/var/spool/pandora/data_in/collections
 	chmod 770 temp_package/var/spool/pandora/data_in/collections
+	mkdir -p temp_package/var/spool/pandora/data_in/netflow
+	chmod 770 temp_package/var/spool/pandora/data_in/netflow
 	mkdir -p temp_package/var/log/pandora/
 	mkdir -p temp_package/usr/share/pandora_server/conf/
 	mkdir -p temp_package/usr/lib/perl5/

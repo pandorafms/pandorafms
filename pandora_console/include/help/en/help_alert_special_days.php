5c381513d0ebae3c8203708128662c1f1ad29f3b
jsatoh <junichi@rworks.jp>
2012-01-27 04:30:38 +0000

    2012-01-27  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/en/help_alert_special_days.php,
    	include/help/ja/help_alert_special_days.php: Added new help files
    	for special days feature.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5437 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_alert_special_days.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Special days list</h1>
+
+<p>Allows to define special days, holidays and special working days, for alert templates. They are treated as the same day of the week you selected.</p>
+
+<p>For example, assume that is a holiday, May 03, 2012. When you define '2012-05-03' with 'Sunday', that day is treated as the same as Sunday. The date format is YYYY-MM-DD. If you want to define same day every year, you can use '*' for YYYY.
+</p>
+
+<p>
+Note: To enable special days list, "Use special days list" should be set on the alert template (step2).
+</p>

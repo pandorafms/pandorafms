0666ec1b165064b583aabb891c19a7f3fb5cc317
Ramon Novoa <rnovoa@artica.es>
2012-01-27 10:36:22 +0000

    2012-01-27  Ramon Novoa  <rnovoa@artica.es>
    	* include/help/es/help_alert_special_days.php: Added to repository.
    	  Translated the special days help file to Spanish.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5439 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/es/help_alert_special_days.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Include/help/es
+ */
+?>
+<h1>Lista de días especiales</h1>
+
+<p>Permite definir días especiales, festivos y días laborables especiales, en las plantillas de alertas. Estos días serán tratados del mismo modo que el día de la semana seleccionado.</p>
+
+<p>Por ejemplo, supongamos que el 3 de Mayo de 2012 es festivo. Cuando definimos el '2012-05-03' como 'domingo', este día será tratado como si fuese un domingo. El formato de fecha es YYYY-MM-DD. Si no se quiere especificar el año, se puede sustituir YYYY por '*'.
+</p>
+
+<p>
+Nota: Para habilitar la lista de días especiales, "Utilizar lista de días especiales" tiene que estar activado en la plantilla de la alerta (paso2).
+</p>

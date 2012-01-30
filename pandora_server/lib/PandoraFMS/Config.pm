f87cd813302b5dd6e536f7565bef72c06a54d42a
ramonn <noreply@pandorafms.org>
2012-01-30 18:00:59 +0000

    2012-01-30  Ramon Novoa  <rnovoa@artica.es>
    	* lib/PandoraFMS/Config.pm: Updated version string.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5453 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -42,7 +42,7 @@ our @EXPORT = qw(
 	);
 
 # version: Defines actual version of Pandora Server for this module only
-my $pandora_version = "4.0.1";
+my $pandora_version = "4.1dev";
 my $pandora_build = "111213";
 our $VERSION = $pandora_version." ".$pandora_build;
 

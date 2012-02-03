ee832c31ea2be5f177e801397c2afb002c88518e
darode <drd.sqki@gmail.com>
2012-02-03 11:01:12 +0000

    2012-02-03 Dario Rodriguez <dario.rodriguez@artica.es>
            * general/main_menu.php: Fixed godmode menu header visualization.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5483 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/general/main_menu.php
+++ pandora_console/general/main_menu.php
@@ -23,7 +23,17 @@ if (! isset ($config["id_user"])) {
 echo '<div class="tit bg">:: '.__('Operation').' ::</div>';
 require ("operation/menu.php");
 
-echo '<div class="tit bg3">:: '.__('Administration').' ::</div>';
+//Check all enterprise ACL used in godmenu items to print menu headers
+if (check_acl ($config['id_user'], 0, "AW") ||
+        check_acl ($config['id_user'], 0, "PM") ||
+        check_acl ($config['id_user'], 0, "LM") ||
+        check_acl ($config['id_user'], 0, "UM") ||
+        check_acl ($config['id_user'], 0, "LW") ||
+        check_acl ($config['id_user'], 0, "IW") ||
+        check_acl ($config['id_user'], 0, "DW")) {
+        echo '<div class="tit bg3">:: '.__('Administration').' ::</div>';
+}
+
 require ("godmode/menu.php");
 
 require ("links_menu.php");

a63ec8d9c352b31217661aa8140b1ace751723bb
juanmanuelr <noreply@pandorafms.org>
2012-02-09 18:12:50 +0000

    2012-02-09 Juan Manuel Ramon <juanmanuel.ramon@artica.es>
    	* include/help/en/help_field_match_snmp.php
    	 include/help/es/help_field_match_snmp.php
    	 godmode/snmpconsole/snmp_alert.php: Changed order of fields in this 
    	 view.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5541 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_field_match_snmp.php
+++ pandora_console/include/help/en/help_field_match_snmp.php
@@ -4,6 +4,6 @@
 */
 ?>
 
-<h1>Fields match 1-6</h1>
+<h1>Custom OID/Data #1-6</h1>
 
 <p>There are regular expressions. It is possible to use selectors in order to select a part of the regular expression that will be load in _snmp_fx_ macro. You can use these macros in Field #1 (Alias, name), Field #2 (Single Line) and Field #3 (Full Text).</p>

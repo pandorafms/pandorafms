4ce050d7742f88d9b3601abc72563b2ea429116c
zarzuelo <noreply@pandorafms.org>
2012-03-16 12:03:59 +0000

    2012-03-16  Sergio Martin <sergio.martin@artica.es>
    	* include/functions_html.php
    	include/javascript/pandora.js: Fixed problem with two 
    	or more time controls at the same time adding uniq id
    	to selects
    
    	* godmode/agentes/module_manager_editor_common.php
    	godmode/reporting/reporting_builder.item_editor.php: Clean
    	code
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5785 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/agentes/module_manager_editor_common.php
+++ pandora_console/godmode/agentes/module_manager_editor_common.php
@@ -233,9 +233,6 @@ $table_advanced->data[1][4] = html_print_input_text ('module_ff_interval', $ff_i
 $table_advanced->data[2][0] = __('Interval').ui_print_help_tip (__('Module execution time interval.'), true);
 
 $table_advanced->colspan[2][1] = 2;
-//~ $table_advanced->data[2][1] = html_print_input_text ('module_interval', $interval,
-	//~ '', 5, 10, true, $disabledBecauseInPolicy).ui_print_help_tip (__('Module execution time interval (in secs).'), true);
-	//~ 
 $table_advanced->data[2][1] = html_print_extended_select_for_time ('module_interval' , $interval, '', '', '0', false, true);
 	
 $table_advanced->data[2][3] = __('Post process').' ' . ui_print_help_icon ('postprocess', true);

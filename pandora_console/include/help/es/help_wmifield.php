4005f73665849e0c0a190d431fdf91e8b63ea01c
vgilc <noreply@pandorafms.org>
2012-03-23 12:21:06 +0000

    2012-03-23  Vanessa Gil  <vanessa.gil@artica.es>
    	* include/help/en/help_wmifield.php
    	  include/help/es/help_wmifield.php: Fixed wmi field help.
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5822 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/es/help_wmifield.php
+++ pandora_console/include/help/es/help_wmifield.php
@@ -5,4 +5,4 @@
 ?>
 <h1>Campo número</h1>
 
-Número de fila que obtener desde el resultado de la consulta WQL (empezando desde cero).
+Número de columna que obtener desde el resultado de la consulta WQL (empezando desde cero).

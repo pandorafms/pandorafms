00eac177c54357eee41905879832c5db6feff8b3
zarzuelo <noreply@pandorafms.org>
2012-04-10 13:37:06 +0000

    2012-04-10  Sergio Martin <sergio.martin@artica.es>
    	* include/functions_agents.php: Fixed an error when try
    	to retrieve policy acl's without agents choosed
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5921 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/functions_agents.php
+++ pandora_console/include/functions_agents.php
@@ -192,6 +192,13 @@ function agents_get_alerts_simple ($id_agent = false, $filter = '', $options = f
 		$selectText = 'COUNT(talert_template_modules.id) AS count';
 	}
 
+	if(is_array($id_agent)) {
+		$extra_sql = enterprise_hook('policies_get_modules_sql_condition', array(reset($id_agent), 't3.', false));
+	}
+	else {
+		$extra_sql = '';
+	}
+	
 	$extra_sql = enterprise_hook('policies_get_modules_sql_condition', array(reset($id_agent), 't3.', false));
 	if ($extra_sql === ENTERPRISE_NOT_HOOK) {
 		$extra_sql = '';

2ddbea117c65487a045798dadc0c3429e57b13ad
juanmanuelr <noreply@pandorafms.org>
2012-04-16 11:24:47 +0000

    2012-04-16 Juan Manuel Ramon <juanmanuel.ramon@artica.es>
    	* include/styles/dialog.css
    	 include/styles/pandora.css
    	 operation/system_alert.php
    	 include/functions_config.php
    	 general/login_page.php: Modified style in UM license advises, also
    	 modified style in system alerts dialog. 
    
    	* include/functions_ui.php: Modified function ui_print_message to 
    	allow force the style.
    	
    	* include/styles/pandora_legacy.css: New css style with Pandora old
    	style "legacy". New style has 100% page width.
    
    	* images/spinner.gif
    	 images/spinner.png: Added new spinners with transparency.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@5982 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/system_alert.php
+++ pandora_console/operation/system_alert.php
@@ -2,7 +2,7 @@
 
 // TODO: Poner esto resizable y dragable: http://jqueryui.com/demos/dialog
 
-echo "<div style='margin-top: 30px; width: 550px; margin-left: 50px'>";
+echo "<div style='margin-top: 10px; width: 100%; height: 250px; overflow: auto; margin-left: 0px; text-align: left;'>";
 echo $_SESSION["alert_msg"];
 echo "</div>";
 

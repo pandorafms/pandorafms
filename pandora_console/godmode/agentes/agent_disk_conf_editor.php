e1d38ed950ee1de4d680ab55a81892c550a7bb16
darode <drd.sqki@gmail.com>
2012-04-18 09:23:45 +0000

    2012-04-18 Dario Rodriguez <dario.rodriguez@artica.es>
            * godmode/agentes/agent_disk_conf_editor.php: Print message
            using new message box
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@6008 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/agentes/agent_disk_conf_editor.php
+++ pandora_console/godmode/agentes/agent_disk_conf_editor.php
@@ -1,6 +1,10 @@
 <?php
-//TODO: Implement an open source version
 if (enterprise_include ("godmode/agentes/agent_disk_conf_editor.php") === ENTERPRISE_NOT_HOOK) {
-	echo "<h3>This feature is not included on the Open Source version. Please visit our website to learn more about the advanced features of <a href='http://pandorafms.com'>Pandora FMS Enterprise edition</a></h3>";
+	
+	$message = array("title" => "Pandora FMS Enterprise",
+					"message" => "<h3>This feature is not included on the Open Source version. Please visit our website to learn more about the advanced features of <a href='http://pandorafms.com'>Pandora FMS Enterprise edition</a></h3>",
+					"no_close" => 1);
+	
+	ui_print_info_message($message);
 }
 ?>

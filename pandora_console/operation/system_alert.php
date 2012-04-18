8ecadd3c909166392bdd4aff81e2b2d5ab2ac432
juanmanuelr <noreply@pandorafms.org>
2012-04-18 08:08:04 +0000

    2012-04-18 Juan Manuel Ramon <juanmanuel.ramon@artica.es>
    	* include/functions_html.php
    	 include/styles/pandora.css
    	 include/javascript/jquery.pandora.js
    	 include/functions_config.php
    	 include/functions_ui.php
    	 extensions/update_manager.php
    	 extensions/update_manager/load_updatemanager.php
    	 extensions/update_manager/lib/functions.ajax.php
    	 extensions/update_manager/settings.php
    	 operation/system_alert.php
    	 general/login_page.php
    	 godmode/setup/setup.php: Changed layout in modal windows for 
    	 license information also changed several styles.
    	 
    	 Merged from branches.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@6005 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/system_alert.php
+++ pandora_console/operation/system_alert.php
@@ -2,8 +2,6 @@
 
 // TODO: Poner esto resizable y dragable: http://jqueryui.com/demos/dialog
 
-echo "<div style='margin-top: 10px; width: 100%; height: 250px; overflow: auto; margin-left: 0px; text-align: left;'>";
 echo $_SESSION["alert_msg"];
-echo "</div>";
 
 ?>

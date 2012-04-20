eff00638edf8eeb66e23d44947d252f458c5b9bc
zarzuelo <noreply@pandorafms.org>
2012-04-20 11:09:43 +0000

    2012-04-20  Sergio Martin <sergio.martin@artica.es>
    	* include/help/en/help_network_map.php
    	include/help/es/help_network_map.php
    	operation/agentes/networkmap.php: Fixed bug changing the
    	map name and improved the usability of the settings form
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@6063 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_network_map.php
+++ pandora_console/include/help/en/help_network_map.php
@@ -6,7 +6,7 @@
 <h1>Network map</h1>
 <p>This is the Network map section.</p>
 
-<p>To save a created map, you must update it first.</p>
+<p>To save a created map, you must apply the changes first.</p>
 
 <p>To buid a submap, you must click on the edges.</p>
 
--- pandora_console/include/help/es/help_network_map.php
+++ pandora_console/include/help/es/help_network_map.php
@@ -6,6 +6,6 @@
 <h1>Mapa de red</h1>
 <p>Esta es la sección de Mapas de red.</p>
 
-<p>Para salvar un mapa creado, tu debes actualizarlo primero.</p>
+<p>Para salvar un mapa creado, debes aplicar los cambios primero.</p>
 
 <p>Para construir un submapa, debes hacer click en los bordes.</p>

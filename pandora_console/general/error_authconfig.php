16f3379b81dd7af6234549af3fb3c85865ea67c0
slerena <slerena@gmail.com>
2012-04-25 19:54:40 +0000

    2012-04-25  Sancho Lerena <slerena@artica.es>
    	* general/error_emptyconfig.php,
    	general/error_authconfig.php,
    	general/error_dbconfig.php: minimal changes on text to fit the error
    	window message.
    
    	* godmode/modules/manage_network_templates_form.php: Fixed description.
    	
    	* godmode/modules/manage_network_components.php: minimal layout changes.
    
    	* pandoradb_data.sql: New "default" values for network components, tags,
    	network (group) templates, ant agent custom fields.
    
    	* operation/agentes/estado_monitores.php: More size for the description 
    	in the agent main view.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@6173 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/general/error_authconfig.php
+++ pandora_console/general/error_authconfig.php
@@ -44,16 +44,16 @@
 		<tr><td align="left">
 			<a href="index.php"><img src="images/pandora_login.png" border="0" height="100px" alt="Pandora FMS"></a>
 		</td><td valign="bottom">
-			Cannot connect to the database, please check your database setup in the <b>include/config.php</b> file or read the documentation on how to setup Pandora FMS.<i><br /><br />
-			Probably one or more of your user, database or hostname values are incorrect or 
-			the database server is not running.</i><br /><br /><span class="error">
+			Cannot connect to the database, please check your database setup in the <b>include/config.php</b> file.<i><br/><br/>
+			Probably your database, hostname, user or password values are incorrect or 
+			the database server is not running.</i><br /><br /><span style='line-height: 18px' class="error">
 				<b>DB ERROR:</b>
 				<?php
 				echo db_get_last_error();
 				?></span>
 			</div>	
 			<div class="msg">
-			If you have modified auth system, problem could be because Pandora cannot override authorization variables from the config database. Remove them from your database by executing:<br><br><pre>DELETE FROM tconfig WHERE token = "auth";</pre>
+			If you have modified auth system, this problem could be because Pandora cannot override authorization variables from the config database. Remove them from your database by executing:<br><pre>DELETE FROM tconfig WHERE token = "auth";</pre>
 			</div>
 		</td></tr>
 		</table>

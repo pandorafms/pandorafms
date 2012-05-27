fcfab782477d6df69d29d843485dc9fb449dccd2
jsatoh <junichi@rworks.jp>
2012-05-27 02:20:43 +0000

    2012-05-27  Junichi Satoh  <junichi@rworks.jp>
    	* general/pandora_help.php: Fixed i18n translation does not work
    	in the help window.
    
    	* include/help/ja/help_main_help.php: Added new general
    	introduction help file.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@6348 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/ja/help_main_help.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Include/help/ja
+ */
+?>
+<h1>Pandora FMS - ヘルプ</h1>
+<h3>概要</h3>
+<p>
+これは、Pandora FMS コンソールのオンラインヘルプです。このヘルプは簡単に説明することを目的としたもので、Pandora FMS の使い方を教えることを意図しているわけではありません。Pandora FMS の公式ドキュメントは約 900ページにも及びます。全体を読む必要はありませんが、参照できるようにダウンロードしておくことをお勧めします。
+
+<br><br>
+<a href="http://pandorafms.com/pandora/doc/"><b>公式ドキュメントのダウンロード</b></a>
+ 
+</p>

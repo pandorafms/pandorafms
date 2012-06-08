93ebe1893e1c4696eb7d2b039836e21ec72cc6d2
darode <drd.sqki@gmail.com>
2012-06-08 12:04:17 +0000

    2012-06-08 Dario Rodriguez <dario.rodriguez@artica.es>
    	* lib/PandoraFMS/Core.pm: Fixed an error counting 
    	unknown agents in groups.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@6456 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/Core.pm
+++ pandora_server/lib/PandoraFMS/Core.pm
@@ -2418,11 +2418,11 @@ sub pandora_group_statistics ($$) {
 					AND tagente_estado.id_agente_modulo = tagente_modulo.id_agente_modulo AND tagente_modulo.disabled = 0 AND tagente_estado.utimestamp != 0");
 		$warning = 0 unless defined ($warning);
 		
-		$unknown = get_db_value ($dbh, "SELECT COUNT(tagente_estado.id_agente_estado)
-				FROM tagente_estado, tagente, tagente_modulo
-				WHERE tagente.id_grupo = $group AND tagente.disabled = 0
-					AND tagente_estado.id_agente = tagente.id_agente AND tagente_estado.estado = 3 
-					AND tagente_estado.id_agente_modulo = tagente_modulo.id_agente_modulo AND tagente_modulo.disabled = 0 AND tagente_estado.utimestamp != 0");
+		$unknown = get_db_value ($dbh, 		"SELECT COUNT(min_estado) FROM 
+		(SELECT MIN(tagente_estado.estado) as min_estado FROM tagente_estado, tagente, tagente_modulo 
+		WHERE tagente.disabled = 0 AND tagente_estado.utimestamp != 0 AND tagente_modulo.id_agente_modulo = tagente_estado.id_agente_modulo 
+		AND tagente_modulo.disabled = 0 AND tagente_estado.id_agente = tagente.id_agente AND tagente_estado.estado != 0 
+		AND tagente.id_grupo = $group GROUP BY tagente.id_agente HAVING min_estado = 3) AS S1");
 		$unknown = 0 unless defined ($unknown);
 		
 		$non_init = get_db_value ($dbh, "SELECT COUNT(tagente_estado.id_agente_estado)

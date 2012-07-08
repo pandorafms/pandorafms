83e1f18f6889808f25bfec2d8383c74046d67e81
jsatoh <junichi@rworks.jp>
2012-07-08 14:34:50 +0000

    2012-07-08  Junichi Satoh  <junichi@rworks.jp>
    	* extras/pandoradb_migrate_4.0.x_to_5.0.postgreSQL.sql,
    	pandoradb.postgreSQL.sql: Fixed SQL error with version 8.x of
    	PostgreSQL.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@6752 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/extras/pandoradb_migrate_4.0.x_to_5.0.postgreSQL.sql
+++ pandora_console/extras/pandoradb_migrate_4.0.x_to_5.0.postgreSQL.sql
@@ -245,7 +245,7 @@ INSERT INTO "tconfig" ("token", "value") VALUES
 -- -----------------------------------------------------
 -- Table `tpassword_history`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "tpassword_history" (
+CREATE TABLE "tpassword_history" (
   "id_pass"  INTEGER NOT NULL PRIMARY KEY,
   "id_user" varchar(60) NOT NULL,
   "password" varchar(45) default NULL,

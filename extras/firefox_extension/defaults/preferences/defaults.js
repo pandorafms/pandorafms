1cc1ff5223cabdeff5b16c0352cdfdf8d1206014
rharishan <noreply@pandorafms.org>
2012-08-15 19:15:17 +0000

    2012-08-16 Hareesan Rajendra  <rharishan@gmail.com>
            * firefox_extension : Contains files for Firefox extension.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@6863 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ extras/firefox_extension/defaults/preferences/defaults.js
@@ -0,0 +1,17 @@
+pref("pandora.ip_address","http://firefly.artica.es/pandora_demo" );
+pref("pandora.api_pass","doreik0");
+pref("pandora.user_name","demo");
+pref("pandora.pass", "demo");
+pref("pandora.critical",11);
+pref("pandora.informational",1);
+pref("pandora.maintenance",10);
+pref("pandora.normal",6);
+pref("pandora.warning",2);
+pref("pandora.events",20);
+pref("pandora.refresh",10);
+pref("pandora.new_events",0);
+pref("pandora.sound_alert",true);
+pref("pandora.changed", false);
+pref("pandora.data_check", true);
+pref("pandora.firstLoad", true);
+pref("pandora.data", null);

f33394789e69634ba96007c4df606b6211d9ffac
vgilc <vanessa.gil@artica.es>
2012-09-05 07:50:07 +0000

    2012-09-05  Vanessa Gil <vanessa.gil@artica.es>
    	* general/header.php
    	  include/functions_ui.php: Enable/disable autorefresh.
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@6933 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/general/header.php
+++ pandora_console/general/header.php
@@ -130,20 +130,23 @@ config_check();
 			<?php
 			// Autorefresh
 			$ignored_params = array ('agent_config' => false, 'code' => false);
-				if ($config["refr"]) {//autorefresh interval
-					$ignored_params['refr'] = 0;
-					if (($config['enable_refr']) || (($_GET['sec2'] == 'operation/agentes/tactical') || ($_GET['sec2'] == 'operation/agentes/estado_agente') ||
+			
+			if ($config['enable_refr']) {
+				$ignored_params['refr'] = 0;
+				echo '<a id="autorefresh" class="white_bold" href="' . ui_get_url_refresh ($ignored_params).'">' . html_print_image("images/page_refresh.png", true, array("class" => 'bot', "alt" => 'lightning')) . '&nbsp;'. __('Autorefresh'); 
+				echo ' (<span id="refrcounter">'.date ("i:s", $config["refr"]).'</span>)';
+				echo '</a>';
+			} else {
+
+				if (($config['refr']) && (($_GET['sec2'] == 'operation/agentes/tactical') || ($_GET['sec2'] == 'operation/agentes/estado_agente') ||
 					($_GET['sec2'] == 'operation/agentes/group_view') || ($_GET['sec2'] == 'operation/events/events') || 
-					($_GET['sec2'] == 'enterprise/dashboard/main_dashboard'))) { //enable autorefresh to all pages or default pages
-					
+					($_GET['sec2'] == 'enterprise/dashboard/main_dashboard'))) {
+
 						echo '<a id="autorefresh" class="white_bold" href="' . ui_get_url_refresh ($ignored_params).'">' . html_print_image("images/page_refresh.png", true, array("class" => 'bot', "alt" => 'lightning')) . '&nbsp;'. __('Autorefresh'); 
 						echo ' (<span id="refrcounter">'.date ("i:s", $config["refr"]).'</span>)';
 						echo '</a>';
-						
-					} 
-					else {
-						
-						$ignored_params['refr'] = '';
+				} else {
+					$ignored_params['refr'] = '';
 						echo '<a id="autorefresh" class="white_bold" href="' . ui_get_url_refresh ($ignored_params).'">' . html_print_image("images/page_refresh.png", true, array("class" => 'bot', "alt" => 'lightning')) . '&nbsp;'. __('Autorefresh').'</a>'; 
 						$values = array (
 							'5' => __('5 seconds'),
@@ -160,28 +163,9 @@ config_check();
 						html_print_select ($values, 'ref', '', '', __('Select'), '0', false, false, false);
 						unset ($values);
 						echo '</span>';
-					
 				}
 			}
-			else {
-				$ignored_params['refr'] = '';
-				echo '<a id="autorefresh" class="white_bold" href="' . ui_get_url_refresh ($ignored_params).'">' . html_print_image("images/page_refresh.png", true, array("class" => 'bot', "alt" => 'lightning')) . '&nbsp;'. __('Autorefresh').'</a>'; 
-				$values = array (
-					'5' => __('5 seconds'),
-					'10' => __('10 seconds'),
-					'15' => __('15 seconds'),
-					'30' => __('30 seconds'),
-					(string)SECONDS_1MINUTE => __('1 minute'),
-					(string)SECONDS_2MINUTES => __('2 minutes'),
-					(string)SECONDS_5MINUTES => __('5 minutes'),
-					(string)SECONDS_15MINUTES => __('15 minutes'),
-					(string)SECONDS_30MINUTES => __('30 minutes'),
-					(string)SECONDS_1HOUR => __('1 hour'));
-				echo '<span id="combo_refr" style="display: none">';
-				html_print_select ($values, 'ref', '', '', __('Select'), '0', false, false, false);
-				unset ($values);
-				echo '</span>';
-			}
+
 			?>
 		</td>
 		<td width='20%' rowspan='2'>

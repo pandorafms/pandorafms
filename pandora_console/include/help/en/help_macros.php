998b4cd0824e516c35b4dd48823983a85decd833
zarzuelo <noreply@pandorafms.org>
2012-09-10 13:01:56 +0000

    2012-09-10  Sergio Martin <sergio.martin@artica.es>
    	* include/help/en/help_macros.php
    	include/help/es/help_macros.php: Add help to 
    	new macros
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@6943 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_macros.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Macros</h1>
+
+Is possible configure macros in the module execution (module_exec) or in a plugin parameters.
+<br /><br />
+Each macro has 3 parameters:
+<ul>
+	<li>Description</li>
+	<li>Default value (optional)</li>
+	<li>Help (optional)</li>
+</ul>
+
+In example, to configure a module that returns the apache's running process
+number in a machine, we configure the next command:
+<br /><br />
+ps -A | grep apache2 | wc -l
+<br /><br />
+We can replace the name of the process by a macro:
+<br /><br />
+ps -A | grep _field1_ | wc -l
+<br /><br />
+And configure the parameters of the macro as:
+
+<ul>
+	<li>Description: Process</li>
+	<li>Default value: apache2</li>
+	<li>Help: Name of substring of the running processes counted by the module</li>
+</ul>
+
+When we configure the module from this component, will appear a text field "Process" 
+with a default value "apache2" that we can modify, and a help that will show more information
+to user.
+<br /><br />
+Is possible to configure as many macros as you want.
---
+++ pandora_console/include/help/es/help_macros.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package Include/help/es
+ */
+?>
+<h1>Macros</h1>
+
+Se pueden configurar macros en la ejecución del módulo (module_exec) o los parámetros de un plugin.
+<br /><br />
+Cada macro tiene 3 parámetros:
+<ul>
+	<li>Descripción</li>
+	<li>Valor por defecto (opcional)</li>
+	<li>Ayuda (opcional)</li>
+</ul>
+
+Por ejemplo, para configurar un módulo que devuelva el número de procesos 
+de apache corriendo en una máquina configuraremos el siguiente comando:
+<br /><br />
+ps -A | grep apache2 | wc -l
+<br /><br />
+Podemos sustituir el nombre del proceso por una macro:
+<br /><br />
+ps -A | grep _field1_ | wc -l
+<br /><br />
+Y configurar los parámetros de la macro de la siguiente manera:
+
+<ul>
+	<li>Descripción: Proceso</li>
+	<li>Valor por defecto: apache2</li>
+	<li>Ayuda: Nombre o subcadena de los procesos en ejecución contados por el módulo</li>
+</ul>
+
+Cuando configuremos el módulo a partir de este componente, aparecerá un campo de texto "Proceso" 
+con un valor por defecto "apache2" que podremos modificar, y una ayuda que indicará más información
+al usuario.
+<br /><br />
+Se pueden configurar tantas macros como se quiera.

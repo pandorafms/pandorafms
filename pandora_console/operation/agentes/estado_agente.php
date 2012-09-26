f3ad8d629d3c91f6772b7ca5536667bf28fe39f8
zarzuelo <noreply@pandorafms.org>
2012-09-26 16:54:25 +0000

    2012-09-26  Sergio Martin <sergio.martin@artica.es>
    	* include/functions_menu.php
    	operation/agentes/estado_agente.php: Add a manually list of the
    	pages (sec2) not accessibles from menu to link it to the
    	section where appear (sec). Fix a bad section
    	Merged from 4.0.2
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7003 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/agentes/estado_agente.php
+++ pandora_console/operation/agentes/estado_agente.php
@@ -217,7 +217,7 @@ html_print_submit_button (__('Search'), "srcbutton", '', array ("class" => "sub 
 echo '</td><td style="width:5%;">&nbsp;</form></td>';
 
 echo '<td>';
-echo '<form method="post" action="index.php?sec=estado&sec2=godmode/agentes/configurar_agente">';
+echo '<form method="post" action="index.php?sec=gagente&sec2=godmode/agentes/configurar_agente">';
 	html_print_input_hidden ('new_agent', 1);
 	html_print_submit_button (__('Create agent'), 'crt', false, 'class="sub next"');
 echo "</form>";

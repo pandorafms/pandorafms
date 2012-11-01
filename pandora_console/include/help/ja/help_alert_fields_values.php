72f178f5c021e707247c60bfdb326bb7d9159fc1
jsatoh <junichi@rworks.jp>
2012-11-01 03:13:17 +0000

    2012-11-01  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/ja/help_alert_fields_description.php,
    	include/help/ja/help_alert_fields_values.php: Added new help files.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7118 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/ja/help_alert_fields_values.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Include/help/ja
+ */
+?>
+<h1>フィールドの値</h1>
+
+値とタグの一覧を定義することにより、フィールドに設定可能な値を制限することができます。一覧を定義した場合、フィールドは選択メニューになります。
+<br><br>
+フォーマットは次の通りです。
+<br><br>
+value1,tag1;value2,tag2;value3,tag3
+<br><br>
+例:
+<br><br>
+1,Number one;2,Number two;3,Number three;4,Number four

f97cededf37ff469f6e3d7e1c66d978a01dd18b2
mdtrooper <tres.14159@gmail.com>
2012-11-20 19:27:36 +0000

    2012-11-20 Miguel de Dios <miguel.dedios@artica.es>
    	* extensions/update_manager/load_updatemanager.php,
    	extensions/update_manager/debug.php: fixed the execution of keygen
    	when there is some wrong in the execution the message for user is
    	more clean now show "Error in execution the keygen." instead the
    	message "Error Server authorisation rejected". And added other forms
    	to get debugData for the update.
    	
    	MERGED FROM THE BRANCH PANDORA_4.0
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7176 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/extensions/update_manager/debug.php
+++ pandora_console/extensions/update_manager/debug.php
@@ -18,14 +18,17 @@
 ////////////////////////////////////
 ///////PLEASE DONT TOUCH
 global $debug_update_manager;
+global $debug_mode_output;
 ////////////////////////////////////
 ////////////////////////////////////
 
 
 
 ////////////////////////////////////
-///////PLEASE ONLY CHANGE THIS VALUE
+///////PLEASE ONLY CHANGE THESE VALUES
 $debug_update_manager = 0;
+//There are "notice_php" and "logDebug".
+$debug_mode_output = "notice_php";
 ////////////////////////////////////
 ////////////////////////////////////
 
@@ -43,22 +46,31 @@ $debug_update_manager = 0;
 ///////PLEASE DONT TOUCH
 function print_debug_message_trace($message) {
 	global $debug_update_manager;
+	global $debug_mode_output;
 	
 	if ($debug_update_manager) {
-		trigger_error("PRINT DEBUG TRACE",
-			E_USER_NOTICE);
-		
-		trigger_error($message, E_USER_NOTICE);
-		
-		$backtrace = json_encode(debug_backtrace());
-		$backtrace_chunks = str_split($backtrace, 1024);
-		
-		trigger_error("INIT DEBUG BACKTRACE (JSON ENCODE) CHUNKS " .
-			count($backtrace_chunks), E_USER_NOTICE);
-		foreach ($backtrace_chunks as $chunk) 
-			trigger_error($chunk, E_USER_NOTICE);
-		trigger_error("END DEBUG BACKTRACE (JSON ENCODE)",
-			E_USER_NOTICE);
+		switch ($debug_mode_output) {
+			case "notice_php":
+				trigger_error("PRINT DEBUG TRACE",
+					E_USER_NOTICE);
+				
+				trigger_error($message, E_USER_NOTICE);
+				
+				$backtrace = json_encode(debug_backtrace());
+				$backtrace_chunks = str_split($backtrace, 1024);
+				
+				trigger_error("INIT DEBUG BACKTRACE (JSON ENCODE) CHUNKS " .
+					count($backtrace_chunks), E_USER_NOTICE);
+				foreach ($backtrace_chunks as $chunk) 
+					trigger_error($chunk, E_USER_NOTICE);
+				trigger_error("END DEBUG BACKTRACE (JSON ENCODE)",
+					E_USER_NOTICE);
+				break;
+			case "logDebug":
+				html_debug_print($message, true);
+				html_debug_print(debug_backtrace(), true);
+				break;
+		}
 	}
 }
 ////////////////////////////////////

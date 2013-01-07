f707b132c1a0352c276b5836996ecc285fa2c8ae
hkosaka <kosaka@rworks.jp>
2013-01-07 09:59:56 +0000

    2013-01-07  Hirofumi Kosaka  <kosaka@rworks.jp>
    	* godmode/reporting/graph_builder.php: Fixed bug that
    	unspecified graph name was displayed in the header line
    	when creating a new custom graph. Merged from branch 4.0.
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7359 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/reporting/graph_builder.php
+++ pandora_console/godmode/reporting/graph_builder.php
@@ -53,6 +53,8 @@ if (! check_acl ($config['id_user'], 0, "RW")) {
 	exit;
 }
 
+unset($name);
+
 $add_module = (bool) get_parameter ('add_module', false);
 $delete_module = (bool) get_parameter ('delete_module', false);
 $edit_graph = (bool) get_parameter('edit_graph', false);

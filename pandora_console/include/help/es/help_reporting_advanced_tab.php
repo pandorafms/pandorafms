e71b49a038d737e4757db867e4473c0a547b6008
darode <drd.sqki@gmail.com>
2013-01-08 10:56:30 +0000

    2013-01-08 Dario Rodriguez <dario.rodriguez@artica.es>
    	* include/help/es/help_reporting_advanced_tab.php,
    	include/help/ja/help_reporting_advanced_tab.php,
    	include/help/en/help_reporting_advanced_tab.php: Documented the new
    	macro for cover page.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@7363 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/es/help_reporting_advanced_tab.php
+++ pandora_console/include/help/es/help_reporting_advanced_tab.php
@@ -6,8 +6,8 @@
 <h1>Opciones avanzadas</h1>
 <p>
 Puede usar una macro en la portada:<br><br>
-
 <b>(_DATETIME_)</b>  este token será reemplazado por la fecha y hora actual en el momento de generar el PDF.
+<b>(_REPORT_NAME_)</b> este token será reemplazado por el nombre del informe.
 <br><br>
 Si tiene problemas visualizando una imagen, prueba a usar el formato PNG en vez de usar otros formatos.
 </p>

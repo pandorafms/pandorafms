366deb9a91cf8284b5f35efba1c01bc6523ebee9
darode <drd.sqki@gmail.com>
2013-01-08 11:05:14 +0000

    2013-01-08 Dario Rodriguez <dario.rodriguez@artica.es>
    	* godmode/reporting/reporting_builder.php: Added a default cover
    	for PDF reports.
    	include/help/ja/help_reporting_advanced_tab.php,
    	include/help/en/help_reporting_advanced_tab.php,
    	include/help/es/help_reporting_advanced_tab.php: Documented new macro
    	called REPORT_NAME
    
    	MERGED FROM 4.0.3
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7365 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_reporting_advanced_tab.php
+++ pandora_console/include/help/en/help_reporting_advanced_tab.php
@@ -5,7 +5,9 @@
 ?>
 <h1>Advanced reporting</h1>
 <p>
-You can use only a macro in the first page:  (_DATETIME_) this token will be replaced by the current date and time in the moment of generating the PDF.
+You can use only a macro in the first page: <br><br>
+<b>(_DATETIME_)</b> will be replaced by the current date and time in the moment of generating the PDF.<br>
+<b>(_REPORT_NAME_)</b> will be replaced by the current name of report.
 <br><br>
 If you have problems loading a image file, try using a PNG format instead JPG or other formats.
 </p>

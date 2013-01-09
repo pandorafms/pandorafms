8add742881a093a4d9925ca97602c2d51c35cfeb
slerena <slerena@gmail.com>
2013-01-09 18:44:59 +0000

    2012-01-09 Sancho Lerena <slerena@artica.es>
            * godmode/setup/setup.php: Fixed warning on enabling EACL.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@7407 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/setup/setup.php
+++ pandora_console/godmode/setup/setup.php
@@ -252,8 +252,8 @@ function replaySound(type) {
 
 
 $(document).ready (function () {
-	$("#radiobtn0011").click(function(){
-			flag = $("#radiobtn0011").is(':checked');
+	$("input[name='acl_enterprise']").click(function(){
+			flag = $(this).is(':checked');
 			if (flag == true){
 				<?php echo "if (! confirm ('" . __('If Enterprise ACL System is enabled without rules you will lose access to Pandora FMS Console (even admin). Do you want to continue?') . "')) return false" ?>
 			}

d40f8ab41384d1505bc669951549992df7fd27f0
jsatoh <junichi@rworks.jp>
2013-01-09 07:23:13 +0000

    2013-01-09  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/ja/help_reporting_advanced_tab.php: Translated
    	description for _REPORT_NAME_ into Japanese.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@7382 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/ja/help_reporting_advanced_tab.php
+++ pandora_console/include/help/ja/help_reporting_advanced_tab.php
@@ -6,7 +6,7 @@
 <h1>拡張レポート</h1>
 <p>
 <b>(_DATETIME_)</b> マクロは、最初のページで利用できます。これは、PDF を生成した時点の日時に置き換えられます。<br>
-<b>(_REPORT_NAME)</b> will be replaced by the current name of report.
+<b>(_REPORT_NAME_)</b> 現在のレポート名に置き換えられます。
 <br><br>
 画像ファイルの読み込みで問題がある場合は、JPG 等のフォーマットではなく、PNG フォーマットを使ってみてください。
 </p>

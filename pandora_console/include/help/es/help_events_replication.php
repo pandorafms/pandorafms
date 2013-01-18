16c725611ec7803c63cc4030fa73af635213ade0
zarzuelo <noreply@pandorafms.org>
2013-01-18 12:06:26 +0000

    2013-01-18  Sergio Martin <sergio.martin@artica.es>
    	* include/functions_config.php
    	include/help/en/help_events_replication.php
    	include/help/en/help_module_interval.php
    	include/help/en/help_events_history.php
    	include/help/es/help_events_replication.php
    	include/help/es/help_events_history.php
    	operation/events/events.php: Fixed bug #3599448 of
    	setup javascript error. 
    	Added help documents to events history and events 
    	replication. Typo fix in other help document
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7506 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/es/help_events_replication.php
@@ -0,0 +1,16 @@
+<?php
+/* Include package help/es
+*/
+?>
+
+<p> Cuando la replicación de eventos está activada, los eventos recibidos se copiarán a la base de datos remota de una metaconsola.
+<br><br>
+Es necesario configurar las credenciales de la base de datos de la metaconsola, así como el modo de replicación (todos los eventos o solo los validados) y el intervalo de replicación en segundos.
+<br><br>
+<b>NOTAS:</b>
+<br><br>
+El visor de eventos se deshabilita cuando se activa esta opción.
+<br><br>
+Para que los cambios en la configuración de replicación de eventos se hagan efectivos será necesario reiniciar el servidor.
+</p>
+

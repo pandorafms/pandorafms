9af286457eb27f04cd640ee18dda3533dea7c3ae
zarzuelo <noreply@pandorafms.org>
2013-02-12 16:33:01 +0000

    2013-02-12  Sergio Martin <sergio.martin@artica.es>
    	* include/help/en/help_warning_status.php
    	include/help/en/help_critical_status.php
    	include/help/es/help_warning_status.php
    	include/help/es/help_critical_status.php: Improved the
    	really poor help of warning and critical status for
    	bug 3600787
    
    	* godmode/agentes/module_manager_editor_common.php: Added
    	checks to hide de useless status threshold. Now
    	the min and max thresholds are shown only in numeric modules
    	and the string field only in string ones
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7634 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_warning_status.php
+++ pandora_console/include/help/en/help_warning_status.php
@@ -5,17 +5,37 @@
 ?>
 <h1>Warning Status</h1>
 
+<h2>Numeric type modules</h2>
+
 <p>
-These field have two values, minimum and maximum. Configuring them correctly you will get that some values will show a module as warning status.
+These field have three values: minimum, maximum and Inverse interval. Configuring them correctly you will get that some values will show a module as warning status.
 </p>
 
 <p>
 To understand better these options is better to see an example. The CPU module will be always on green in the agent status , so it simply informs of a value between 0% and 100%. If we want that the module of CPU usage will be shown in yellow (warning) when they reached the 70% of its use. We should configure:
 </p>
 
-<li>Warning status:70.</li>
+<li>Warning Min:70</li>
+<li>Warning Max:0 (limitless)</li>
 
 <p>
 With this, if its value is greater 70 it will be in yellow (WARNING), and under 70 in green (NORMAL). 
 </p>
 
+<p>
+If we enable the Inverse interval checkbox, the module will change to Warning status when the value doesnt be between the setted interval. In the example when were below 70.
+</p>
+
+<h2>String type modules</h2>
+
+<p>
+These field have two values: String and Inverse interval. Configuring them correctly you will get that some values will show a module as Warning status.
+</p>
+
+<p>
+In the field String we will set a regular expression. When the data matchs with it, the module will turn into Warning status.
+</p>
+
+<p>
+If we enable the Inverse interval checkbox, the module will change to the Warning status when the value doesnt match with the regular expression.
+</p>
--- pandora_console/include/help/es/help_critical_status.php
+++ pandora_console/include/help/es/help_critical_status.php
@@ -5,17 +5,37 @@
 ?>
 <h1>Critical Status</h1>
 
+<h2>Módulos de tipo numérico</h2>
+
 <p>
-Estos campos tiene dos valores, mínimo y máximo. Configurandolos correctamente se conseguirá que algunos valores se muestren en estado crítico.
+Estos campos tiene cuatro valores: mínimo, máximo, cadena e intervalo inverso. Configurandolos correctamente se conseguirá que algunos valores se muestren en estado crítico.
 </p>
 
 <p>
 Para entender mejor estas opciones mejor se puede usar un ejemplo. El módulo de CPU estará siempre en verde en el estado del agente, asi que simplemente informará de un valor entre 0% y 100%. Si se quiere que el módulo de uso de la CPU se muestre como rojo (crítico) cuando se llegue al 90% de uso. Se debería configurar:
 </p>
 
-<li>Critical status:90.</li>
+<li>Critical Min:90</li>
+<li>Critical Max:0 (sin límite)</li>
 
 <p>
 Con esto, si el valor es mayor que 90 el indicador estatá en rojo (CRÍTICO), y por debajo de 70 estatá en verde (NORMAL).
 </p>
 
+<p>
+Si activamos la casilla de intervalo inverso el módulo cambiará a estado Crítico cuando no se encuentre en el intervalo indicado. En el caso del ejemplo cuando esté por debajo de 90.
+</p>
+
+<h2>Módulos de tipo cadena</h2>
+
+<p>
+Estos campos tiene dos valores: Cadena e intervalo inverso. Configurandolos correctamente se conseguirá que algunos valores se muestren en estado de alerta.
+</p>
+
+<p>
+En el campo Cadena podremos poner una expresión regular para que cuando el dato del módulo coincida con ella, el módulo pase a estado Crítico.
+</p>
+
+<p>
+Si activamos la casilla de intervalo inverso el módulo cambiará a estado Crítico cuando no coincida con la expresión regular.
+</p>

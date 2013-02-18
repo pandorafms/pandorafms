21d3cdbe12552e8711595d7630826b86fd781128
zarzuelo <zarzuelo@gmail.com>
2013-02-18 11:39:32 +0000

    2013-02-18  Sergio Martin <sergio.martin@artica.es>
    	* lib/PandoraFMS/DataServer.pm: Fixed the exception of "status" 
    	field of the XML deleted from the supported tags in the last 
    	commit for bug 3604850
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@7668 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/DataServer.pm
+++ pandora_server/lib/PandoraFMS/DataServer.pm
@@ -467,7 +467,7 @@ sub process_module_data ($$$$$$$$$) {
 	my $tags = {'name' => 0, 'data' => 0, 'type' => 0, 'description' => 0, 'max' => 0,
 	            'min' => 0, 'descripcion' => 0, 'post_process' => 0, 'module_interval' => 0, 'min_critical' => 0,
 	            'max_critical' => 0, 'min_warning' => 0, 'max_warning' => 0, 'disabled' => 0, 'min_ff_event' => 0,
-	            'datalist' => 0, 'unit' => 0, 'timestamp' => 0, 'id_module_group' => 0};
+	            'datalist' => 0, 'status' => 0, 'unit' => 0, 'timestamp' => 0, 'id_module_group' => 0};
 
 	# Other tags will be saved here
 	$module_conf->{'extended_info'} = '';

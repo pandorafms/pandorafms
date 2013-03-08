05f3f7872cb3da0f6c71e58ea4e609c178a55cb6
mariopulido <mario.pulido@artica.es>
2013-03-08 11:34:48 +0000

    2013-03-07 Mario Pulido <mario.pulido@artica.es>
           *images/help/historyddbb.png,images/help/plugin1.png
           images/help/plugin2.png, images/help/plugin3.png
           include/help/es/help_plugin_definition.php
           include/help/es/help_history_database.php
           include/help/en/help_history_database.php
           include/help/en/help_plugin_definition.php: Modified plugin definition help.
           Add history database help.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7813 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_plugin_definition.php
+++ pandora_console/include/help/en/help_plugin_definition.php
@@ -5,47 +5,59 @@
 ?>
 <h1>Plugin registration</h1>
 
-Pandora FMS Plugin registration tool is used to define what parameters Pandora FMS Plugin server needs to be used in each plugin, and what kind of data will pass after that parameter.
+Unlike with the rest of components, in a default way Pandora FMS does not include any pre-configured complement, so first you should create and configure a complement to could after add it to the module of an agent. But Pandora FMS includes plugins in the installation directories, but as have already been said, they are not configured in the database. 
 <br><br>
-For example, if you have a plugin to check Informix tablespace called "Aleph", under IP address "192.168.50.2" and with username "Calabria" and password "malcolm45". This plugin could return if tablespace is ok, the number of queries per second, load, fragmentation level and memory usage.
+To add a plugin that already exists to Pandora FMS, go to the console administration section, and in it, click on Manage servers. After doing this, click on Manage plugins: 
 <br><br>
-This plugin has the following interface:
-<br>
-<pre>
-	informix_plugin_pandora -H ip_address -U user -P password -T tablespace -O operation
-</pre>
-<br>
-Operation could be "status", "qps", "load", "fragment" and "memory". It returns a single value, used by Pandora FMS. To define this plugin in pandora, you would need to fill fields as follows:
+Once you are in the screen of the plugin management, click on Create a new plugin, so there will be no one. 
 <br><br>
+Fill in the plugin creation form with the following data:
+<?php html_print_image ("images/help/plugin1.png", false, array('width' => '550px')); ?>
+<br><br>
+<b>Name</b><br>
+Name of the plugin, in this case Nmap.
+<br><br>
+<b>Plugin type </b><br>
+There are two kinds of plugins, the standard ones and the kind Nagios. The standard plugins are scripts that execute actions and accept parameters. The Nagios plugins are, as their name shows, Nagios plugins that could be being used in Pandora FMS.The difference is mainly on that the Nagios plugins return an error level to show if the test has been successful or not.
+<br><br>
+If you want to use a plugin kind Nagios and you want to get a data, not an state (good/Bad), then you can use a plugin kind Nagios is the "Standard" mode.
+<br><br>
+In this case (for the NMAP example plugin), we have to select Standard. 
+<br><br>
+<b>Max. timeout</b><br>
 
-<table cellpadding=4 cellspacing=4 class=databox width=80%>
-<tr>
-<td valign='top'>Plugin command</td><td>/usr/share/pandora/util/plugins/informix_plugin_pandora (default location for plugins)</td>
-</tr>
-<tr>
-<td>Max. timeout:</td><td> 15 (for example).</td>
-</tr>
-<tr>
-<td>IP Address option</td><td> -H</td>
-</tr>
-<tr>
-<td>Port option</td><td> Leave it blank.</td>
-</tr>
-<tr>
-<td>User option</td><td> -U</td>
-</tr>
-
-<tr>
-<td>Password option</td><td> -P</td>
-</tr>
-
-</table>
-<br>
-
-If you needed to create a module that uses this plugin, you would need to choose the plugin (this new plugin will show up in the combo to be selected). Afterwards only to fill the IP Address, Username, and Password would be required. Pandora FMS will put this data into the appropriate fields when executing external plugins.
+It is the time of expiration of the plugin. If you do not receive a response in this time, you should select the module as unknown, and its value will be not updated. It is a very important factor when implementing monitoring with plugins, so if the time it takes at executing the plugin is bigger than this number, we never could obtain values with it. This value should always be bigger than the time it takes usually to return a value the script/executable that is used as plugin. In there is nothing said, then you should used the value that in the configuration is named plugin_timeout.
 <br><br>
-There are always some sort of parameters which cannot be "generic", in this scenario, you would have "tablespace" parameter. This one is very particular for Informix, but each example could have its own exception. You would have a field called "Plugin parameter", that is used to pass "as is" to the plugin. In this particular case you would need to put "-T tablespace" there. 
+In this case, we write 15.
 <br><br>
-If you want to use another tablespace, just create another module with different string after "-T".
+<b>Description</b><br>
+
+Plugin description. Write a short description, as for example:Test # UDP open ports, and if it is possible, specify the complete interface of parameters to help to someone that will after check the plugin definition to know which parameters accept.
 <br><br>
-Of course, in "Plugin parameter" field, you can put more than one parameter. All data entered there is passed to the plugin "as is".
+<b>Plug-in command</b><br>
+
+It is the path where the plugin command is. In a default way, if the installation has been an standard one, there will be in the directory /usr/share/pandora_server/util/plugin/. Though it could be any path of the system. For this case, writte /usr/share/pandora_server/util/plugin/udp_nmap_plugin.shin the field.
+<br><br>
+Pandora server will execute this script, so this should have permissions of access and execution on it.
+<br><br>
+<b>Plug-in parameters</b><br>
+
+A string with the parameters of the command that will be after command and a blank space. This parameters field accepts macros as _field1_ _field2_ ... _fieldN_.
+<br><br>
+<b>Parameters macros</b><br>
+
+Is possible to add unlimited macros to use it in Plug-in parameters field. This macros will appear as normal text fields in the module configuration.
+<br><br>
+Each macro has 3 fields:
+<br><br>
+    <i>Description:</i> A short string descripting the macro. Will be the label near the field.<br>
+    <i>Default value:</i> Value asigned to the field by default<br>
+    <i>Help:</i> A text with a explanation of the macro. <br>
+<br><br>
+Example of a macro configuration: 
+<br><br>
+<?php html_print_image ("images/help/plugin2.png", false, array('width' => '550px')); ?>
+<br><br>
+Example of this macro in the module editor:
+<br><br>
+<?php html_print_image ("images/help/plugin3.png", false, array('width' => '550px')); ?>
--- pandora_console/include/help/es/help_plugin_definition.php
+++ pandora_console/include/help/es/help_plugin_definition.php
@@ -5,48 +5,60 @@
 ?>
 <h1>Registro de complementos</h1>
 
-La herramienta de registro de Pandora FMS se usa para definir los parámetros que el servidor de complementos de Pandora FMS necestia usar con cada complemento, y qué tipo de datos se le pasarán posteriormente después del parámetro.
+A diferencia del resto de componentes, de forma predeterminada Pandora FMS no incluye ningún complemento pre-configurado, por lo tanto primero se deberá crear y configurar un complemento, para después añadírselo al módulo de un agente. No obstante Pandora FMS sí incluye complementos en los directorios de instalación, pero como ya se ha dicho no están configurados en la base de datos. 
 <br><br>
-Por ejemplo, si tiene un complemento para comprobar la <i>tablespace</i> de Informix llamada &laquo;Aleph&raquo;, bajo la dirección IP &laquo;192.168.1.2&raquo; con el nombre de usuario &laquo;Calabria&raquo; y la contraseña &laquo;malcolm45&raquo;. Este complemento podría devolver si el <i>tablespace</i> está correcto, el número de consultas por segundo, la carga, el nivel de fragmentación, y el uso de memoria.
+Para añadir un complemento existente a Pandora FMS, ir a la sección de administración de la consola, y en ella, pulsar sobre Manage servers; después pulsar Manage plugins: 
 <br><br>
-Este complemento tiene la siguiente interfaz:
-<br>
-<pre>
-	informix_plugin_pandora -H dirección_ip -U usuario -P contraseña -T tablespace -O operación
-</pre>
-<br>
-La operación podría ser &laquo;status&raquo;, &laquo;qps&raquo;, &laquo;load&raquo;, &laquo;fragment&raquo; y &laquo;memory&raquo;. Devuelve un solo valor, que usa Pandora FMS. Para definir este complemento en Pandora FMS, debe rellenar los campos como sigue:
+Una vez en la pantalla de gestión de los complementos, pulsar el botón Create para crear un nuevo complemento, ya que no habrá ninguno.  
 <br><br>
-
-<table cellpadding=4 cellspacing=4 class=databox width=80%>
-<tr>
-<td valign='top'>Comando del complemento</td><td>/usr/share/pandora/util/plugins/informix_plugin_pandora (ubicación predeterminada para los complementos)</td>
-</tr>
-<tr>
-<td>Máx. tiempo de expiración</td><td> 15 (por ejemplo).</td>
-<tr>
-<td>Dirección IP</td><td> -H</td>
-</tr>
-
-<tr>
-<td>Opción de puerto</td><td> Déjela en blanco.</td>
-</tr>
-
-<tr>
-<td>Opción de usuario</td><td> -U</td>
-</tr>
-
-<tr>
-<td>Opción de contraseña</td><td> -P</td>
-</tr>
-
-</table>
-<br>
-
-Si necesita crear un módulo que use este complemento, deberá elegir el complemento (este nuevo complemento aparecerá en la caja de combinación para poder seleccionarse). Después, sólo debe rellenar los campos necesarios IP destino, Usuario y Contraseña. Pandora FMS pondrá estos datos en los campos apropiados al ejecutar el complemento externo.
+Rellenar el formulario de creación de complementos con los siguientes datos: 
 <br><br>
-Siempre hay algunos parámetros que no pueden ser &laquo;genéricos&raquo;, en este caso está el parámetro &laquo;tablespace&raquo;. Éste es muy particular para Informix, pero cada ejemplo puede tener su propia excepción. Podría tener un campo llamado &raquo;Parámetros del complemento&raquo; usado para pasar parámetros &laquo;tal cual&raquo; al complemento. En este caso en particular podría poner &laquo;-T tablespace&raquo; en él.
+<?php html_print_image ("images/help/plugin1.png", false, array('width' => '550px')); ?>
 <br><br>
-Si quiere usar otra tabla distinta, simplemente cree otro módulo con una cadena diferente después de &laquo;-T&raquo;.
+<b>Name</b><br>
+Nombre del plugin, en este caso Nmap.
 <br><br>
-Por supuesto, en el campo &laquo;Parámetros del complemento&raquo; puede introducir más de un parámetro todos los datos introducidos en el campo se pasan al complemento &laquo;tal cual&raquo;.
+<b>Plugin type </b><br>
+Hay dos tipos de complementos, los estándar (standard) y los de tipo Nagios. Los complementos estándar son scripts que ejecutan acciones y admiten parámetros. Los complementos de Nagios son, como su nombre indica, complementos de Nagios que se pueden usar en Pandora FMS. La diferencia estriba principalmente en que los plugins de nagios devuelven un error level para indicar si la prueba ha tenido éxito o no.
+<br><br>
+Si quiere usar un plugin de tipo nagios y quiere obtener un dato, no un estado (Bien/Mal), puede utilizar un plugin de tipo nagios en el modo "Standard". 
+<br><br>
+En este caso (para el plugin de ejemplo, NMAP), seleccionaremos Standard. 
+<br><br>
+<b>Max. timeout</b><br>
+
+Es el tiempo de expiración del complemento. Si no se recibe una respuesta en ese tiempo, se marcará el módulo como desconocido y no se actualizará su valor. Este es un factor muy importante a la hora de implementar monitorización con plugins, ya que si el tiempo que tarda en ejecutar el plugin es mayor que este numero, nunca podremos obtener valores con él. Este valor siempre debe ser mayor que el tiempo que tarde normalmente en devolver un valor el script/ejecutable usado como plugin. Si no se indica nada, se utilizará el valor indicado en la configuracion como plugin_timeout. 
+<br><br>
+En este caso, escribimos 15.
+<br><br>
+<b>Description</b><br>
+
+Descripción del complemento. Escribir una breve descripción, como por ejemplo: Test # UDP open ports y si es posible especificar la interfaz completa de parámetros como ayuda para que alguien que revise posteriormente la definición del plugin sepa que parámetros acepta. 
+<br><br>
+<b>Plug-in command</b><br>
+
+Es la ruta a donde está el comando del complemento. De forma predeterminada, si la instalación ha sido estándar, estarán en el directorio /usr/share/pandora_server/util/plugin/. Aunque puede ser cualquier ruta del sistema. Para este caso, escribir /usr/share/pandora_server/util/plugin/udp_nmap_plugin.sh en el campo.
+<br><br>
+El servidor de pandora ejecutará ese script, por lo que éste debe tener permisos de acceso y de ejecución sobre él. 
+<br><br>
+<b>Plug-in parameters</b><br>
+
+Una cadena con los parámetros del plugin, que irán tras el comando y un espacio en blanco. Este campo acepta macros tales como _field1_ _field2_ ... _fieldN_. 
+<br><br>
+<b>Parameters macros</b><br>
+
+Es posible agregar macros ilimitadas para usarlas en el campo de los parámetros del plugin. Estas macros aparecerán como campos de texto en la configuración del módulo. 
+<br><br>
+Cada macro tiene 3 campos: 
+<br><br>
+    <i>Description:</i> Una cadena corta descriptiva de la macro. Será la etiqueta que aparecerá junto al campo en el formulario. <br>
+    <i>Default value:</i> Valor asignado al campo por defecto. <br>
+    <i>Help:</i> Un texto explicativo de la macro.  <br>
+<br><br>
+Ejemplo de la configuración de macros:  
+<br><br>
+<?php html_print_image ("images/help/plugin2.png", false, array('width' => '550px')); ?>
+<br><br>
+Ejemplo de esta misma macro en el editor del módulo: 
+<br><br>
+<?php html_print_image ("images/help/plugin3.png", false, array('width' => '550px')); ?>

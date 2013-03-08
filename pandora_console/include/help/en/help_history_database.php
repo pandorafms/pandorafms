7c830c5342d685d20fe27f9027144afbae2abc0c
mariopulido87 <noreply@pandorafms.org>
2013-03-08 11:34:48 +0000

    2013-03-07 Mario Pulido <mario.pulido@artica.es>
           *images/help/historyddbb.png,images/help/plugin1.png
           images/help/plugin2.png, images/help/plugin3.png
           include/help/es/help_plugin_definition.php
           include/help/es/help_history_database.php
           include/help/en/help_history_database.php
           include/help/en/help_plugin_definition.php: Modified plugin definition help.
           Add history database help.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7813 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_history_database.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>History database</h1>
+
+A history database is a database where old module data is moved to make the main Pandora FMS database more responsive for everyday operations. That data will still be available seamlessly to the Pandora FMS console when viewing reports, module charts etc. 
+<br><br>
+<b>SETTING UP A HISTORY DATABASE</b>
+<br><br>
+To configure a history database follow these simple steps:
+<br><br>
+<ol>
+<li>Create the new history database. 
+<br><br>
+<li>Create the necessary tables in the new database. You can use the pandoradb.sql script provided with the Pandora FMS console: 
+<br><br>
+<i>cat pandoradb.sql | mysql -u user -p -D history_db</i>
+<br><br>
+<li>Give pandora user permissions to access pandora history database
+<br><br>
+<i>Mysql Example: GRANT ALL PRIVILEGES ON pandora.* TO 'pandora'@'IP' IDENTIFIED BY 'password'</i>
+<br><br>
+<li>In your Pandora FMS console navigate to Setup->History database and enter the host, port, database name, user and password of the new database.
+</ol>
+<br><br>
+<?php html_print_image ("images/help/historyddbb.png", false, array('width' => '550px')); ?>
+<br><br>
+Data older than Days days will be moved to the history database in blocks of Step rows, waiting Delay seconds between one block and the next to avoid overload. 
+<br><br>
+Here are explained the fields:
+<br><br>
+<ol>
+   <b>Enable history database:</b> Enabled history database feature. 
+<br><br>
+   <b>Host:</b> Hostname of history database. 
+<br><br>
+    <b>Port:</b> Port of history database. 
+<br><br>
+   <b>Database name:</b> Database name for history database. 
+<br><br>
+   <b>Database user:</b> User to access to history database. 
+<br><br>
+   <b>Database password:</b> Password to access to history database. 
+<br><br>
+   <b>Days:</b> Number of days since data will be transfered to history database. 
+<br><br>
+   <b>Step:</b> Size of buffer for data transferring. The lower step the slower data transferring, but lower performance reduction on main database. 
+<br><br>
+   <b>Delay:</b> delay time between data transferences between main and history database.
+<br><br>
+</ol> 
---
+++ pandora_console/include/help/es/help_history_database.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @package Include/help/es
+ */
+?>
+<h1>Base de datos de histórico</h1>
+
+Una base de datos de histórico es una base de datos a la que se mueven datos antiguos de módulos para mejorar la respuesta de la base de datos principal de Pandora FMS. Estos datos seguirán estando disponibles para la consola de Pandora FMS de forma transparente al ver informes, gráficas de módulos etc. 
+<br><br>
+<b>CONFIGURANDO UNA BASE DE DATOS DE HISTÓRICO</b>
+<br><br>
+Para configurar una base de datos de histórico siga los siguientes pasos: 
+<br><br>
+<ol>
+<li>Cree la nueva base de datos de histórico.  
+<br><br>
+<li>Cree las tablas necesarias en la nueva base de datos. Puede utilizar el script pandoradb.sql incluido en la consola de Pandora FMS:  
+<br><br>
+<i>cat pandoradb.sql | mysql -u user -p -D history_db</i>
+<br><br>
+<li>Dar los permisos necesarios para que el usuario de pandora tenga acceso a la base de datos de histórico
+<br><br>
+<i>Mysql Example: GRANT ALL PRIVILEGES ON pandora.* TO 'pandora'@'IP' IDENTIFIED BY 'password'</i>
+<br><br>
+<li>En la consola de Pandora FMS vaya a Setup->History database y configure el host, port, database name, user y password de la nueva base de datos.
+</ol>
+<br><br>
+<?php html_print_image ("images/help/historyddbb.png", false, array('width' => '550px')); ?>
+<br><br>
+Los datos con más días de antigüedad se moverán a la base de datos de histórico en bloques de Step filas, esperando Delay segundos entre un bloque y el siguiente para evitar sobrecargas. 
+<br><br>
+Aquí se detallan los campos a rellenar: 
+<br><br>
+<ol>
+   <b>Enable history database:</b> Permite utilizar la funcionalidad de base de datos histórica. 
+<br><br>
+   <b>Host:</b> Nombre de host de la base de datos histórica.  
+<br><br>
+    <b>Port:</b> Puerto de conexión de la base de datos histórica.  
+<br><br>
+   <b>Database name:</b> Nombre de la base de datos histórica.  
+<br><br>
+   <b>Database user:</b> Usuario de la base de datos histórica. 
+<br><br>
+   <b>Database password:</b> Password de la base de datos histórica. 
+<br><br>
+   <b>Days:</b> A partir de cuantos días los datos serán transferidos a la base de datos histórica.  
+<br><br>
+   <b>Step:</b> Mecanismo para la transferencia de datos (similar un buffer de datos) a la base de datos histórica. Cuanto menor sea menos eficiente será la transferencia pero afectará menos al rendimiento de la base de datos principal. 
+<br><br>
+   <b>Delay:</b>  tiempo de espera entre transferencias de datos entre la base de datos principal y la histórica. 
+<br><br>
+</ol> 

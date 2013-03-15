1bdb6f1c4e7e4d0cd517a7428ab5211c7f44b0c9
mariopulido87 <noreply@pandorafms.org>
2013-03-15 12:41:45 +0000

    2013-03-15  Mario Pulido <mario.pulido@artica.es>
    	* include/help/en/help_performance.php, include/help/es/help_performance.php
     	godmode/setup/setup.php: Add help in performance configuration.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7856 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_performance.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Performance Configuration</h1>
+
+
+<b>Max. days before delete events</b>
+<br><br>
+Maximum number of days before delete events.
+<br><br>
+<b>Max. days before delete traps</b>
+<br><br>
+Maximum number of days before delete traps.
+<br><br>
+<b>Max. days before delete audit events</b>
+<br><br>
+Maximum number of days before delete audited evetns.
+<br><br>
+<b>Max. days before delete string data</b>
+<br><br>
+Maximum number of days before delete string data.
+<br><br>
+<b>Max. days before delete GIS data</b>
+<br><br>
+Maximum number of days before delete GIS data.
+<br><br>
+<b>Max. days before purge</b>
+<br><br>
+Maximum number of days before purge database. This parameter is also used to specify max. number of days before deleting inventory data. If you have installed a history database, this number must be higher than the number of days before data is transferred to history database. Remember that in the history database never deleted data.
+<br><br>
+<b>Max. days before compact data</b>
+<br><br>
+Maximum number of days before compact data.
+<br><br>
+<b>Compact interpolation in hours (1 Fine-20 bad)</b>
+<br><br>
+Interpolation range. 1 is the best and 20 the worst. It is recommended to use 1 or values near to 1.
+<br><br>
+<b>SLA period (seconds)</b>
+<br><br>
+Default time, in seconds, to calculate SLA in agents SLA tab. Calculates the SLA automatically in modules defined in an agent based on Critical or Normal values.
+<br><br>
+<b>Default hours for event view</b>
+<br><br>
+Default number of hours for event filter. If the value is 24 hours, the event views will only show the events which happened in the last 24 hours.
+<br><br>
+<b>Use realtime statistics</b>
+<br><br>
+Enabled/Disabled real time statistics.
+<br><br>
+<b>Batch statistics period (secs)</b>
+<br><br>
+If realtime statistics are disaabled, here you define the refresh time for batch statistics.
+<br><br>
+<b>Use agent access graph</b>
+<br><br>
+Agent access graph, renders the number of agent contacts per hour in a graph with a daily scale (24h). This is use to know the frecuency of contact for each agent. It could take a long time to processs the date, so if you have low resources its recommended to disable it.
+<br><br>
+<b>Max. days before delete unknown modules</b>
+<br><br>
+Maximum number of days before delete unknown modules. 
+<br><br>
+<i>**All these parameters are made when running the pandora_db.pl tool</i>
+
---
+++ pandora_console/include/help/es/help_performance.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package Include/help/es
+ */
+?>
+<h1>Configuración de Rendimiento</h1>
+
+
+<b>Max. days before delete events</b>
+<br><br>
+Campo donde se definen el número máximo de días antes de borrar los eventos.
+<br><br>
+<b>Max. days before delete traps</b>
+<br><br>
+Campo donde se definen el número máximo de días antes de borrar los traps. 
+<br><br>
+<b>Max. days before delete audit events</b>
+<br><br>
+Campo donde se definen el número máximo de días antes de borrar los eventos de auditoría. 
+<br><br>
+<b>Max. days before delete string data</b>
+<br><br>
+Campo donde se definen el número máximo de días antes de borrar las cadenas de datos. 
+<br><br>
+<b>Max. days before delete GIS data</b>
+<br><br>
+Campo donde se definen el número máximo de días antes de borrar los datos GIS. 
+<br><br>
+<b>Max. days before purge</b>
+<br><br>
+Campo donde se definen el número máximo de días antes de borrar datos. Esto tambien especifica el nº maximo de dias a mantener datos de histórico de inventario (a partir de la version 4.0.3). Si tiene instalada una base de datos de histórico, este número tiene que ser más alto que el de dias en los que se transfiere la información a la base de datos de histórico. Recuerda que en la base de datos de histórico nunca se eliminan los datos, por lo tanto este número no indica que se borren los datos en la base de histórico.
+<br><br>
+<b>Max. days before compact data</b>
+<br><br>
+Campo donde se define el número máximo de días antes de compactar datos. 
+<br><br>
+<b>Compact interpolation in hours (1 Fine-20 bad)</b>
+<br><br>
+Campo donde se define el grado de interpolación, donde 1 es el mejor ajuste, y 20 el peor. Se recomienda usar 1 o valores próximos a uno.
+<br><br>
+<b>SLA period (seconds)</b>
+<br><br>
+El tiempo por defecto para calcular la vista de SLA en la solapa SLA de la vista de agentes. Calcula automáticamente los SLA de los Monitores definidos en ese agente en el intervalo en segundos que se da, diferenciando entre los valores Critical y Normal.
+<br><br>
+<b>Default hours for event view</b>
+<br><br>
+Campo donde se define el campo horas del filtro por defecto en la vista de eventos. Si por defecto es 24, la vista de eventos mostrará únicamente los eventos sucedidos en las últimas 24 horas.
+<br><br>
+<b>Use realtime statistics</b>
+<br><br>
+Habilitar/Deshabilitar el uso de estadísticas en tiempo real. 
+<br><br>
+<b>Batch statistics period (secs)</b>
+<br><br>
+Si las estadísticas en tiempo real están deshabilitadas, se definirá aquí el tiempo de refresco para las estadísticas.
+<br><br>
+<b>Use agent access graph</b>
+<br><br>
+El gráfico de accesos del agente, renderiza el número de contactos por hora en un gráfico con una escala diaria (24h). Esto se usa para conocer la frecuencia de contacto de cada agente. Puede necesitar mucho tiempo de procesado, por lo que si dispone de bajos recursos se recomienda deshabilitarlo. 
+<br><br>
+<b>Max. days before delete unknown modules</b>
+<br><br>
+Campo donde se define el número máximo de días antes de borrar los módulos desconocidos. 
+<br><br>
+<i>**Con todos estos parámetros trabaja la herramienta pandora_db.pl. </i>
+

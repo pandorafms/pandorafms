5a393ac8566f4b0933dfadf70153d532eb409a71
mariopulido87 <noreply@pandorafms.org>
2013-03-15 13:41:38 +0000

    2013-03-15  Mario Pulido <mario.pulido@artica.es>
    	* include/help/en/help_plugin_policy.php
    	include/help/es/help_plugin_policy.php: Added plugin policy help.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7857 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/es/help_plugin_policy.php
@@ -0,0 +1,14 @@
+<?php
+/*
+* @package Include/help/es/
+*/
+?>
+
+<h1>Plugins de agente</h1>
+
+<p>A partir de Pandora FMS 5.0, con el editor de plugins en políticas se pueden propagar los plugins de agentes con facilidad.
+<br><br>
+Se pueden añadir plugins de agente en una política para que se creen en cada agente local al ser aplicada. 
+<br><br>
+Ejemplo: cscript.exe //B "%ProgramFiles%\Pandora_Agent\util\df.vbs" 
+

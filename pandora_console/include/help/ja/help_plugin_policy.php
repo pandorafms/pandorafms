403cb51f43080c773b6aadcaf53aa7a151cf643a
jsatoh <junichi@rworks.jp>
2013-03-18 04:41:08 +0000

    2013-03-18  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/ja/help_performance.php,
    	include/help/ja/help_plugin_policy.php: Added new help files.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7864 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/ja/help_plugin_policy.php
@@ -0,0 +1,13 @@
+<?php
+/*
+* @package Include/help/ja/
+*/
+?>
+
+<h1>エージェントプラグイン</h1>
+
+<p>Pandora FMS 5.0 から、ポリシーのプラグインエディタで、エージェントのプラグインを簡単に展開することができます。
+<br><br>
+ポリシーを適用したときに、ローカルエージェントにエージェントプラグイン作成するようポリシーに追加することができます。
+<br><br>
+例: cscript.exe //B "%ProgramFiles%\Pandora_Agent\util\df.vbs" 

3509fd1b3d3994b0f283c6aba5aa97775c8d464a
mdtrooper <tres.14159@gmail.com>
2013-04-02 13:44:48 +0000

    2013-04-02 Miguel de Dios <miguel.dedios@artica.es>
    	* mobile/operation/groups.php, mobile/include/style/main.css,
    	include/functions_ui.php, include/functions_groups.php: change the
    	skin of list groups.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7911 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/mobile/operation/groups.php
+++ pandora_console/mobile/operation/groups.php
@@ -27,11 +27,10 @@ class Groups {
 			$this->correct_acl = true;
 			
 			$this->groups = $this->getListGroups();
-			
-			foreach ($this->groups as $key => $group) {
-				$this->status[$key] = $group['status'];
-				unset($this->groups[$key]['status']);
-			}
+			//~ foreach ($this->groups as $key => $group) {
+				//~ $this->status[$key] = $group['status'];
+				//~ unset($this->groups[$key]['status']);
+			//~ }
 		}
 		else {
 			$this->correct_acl = false;
@@ -68,11 +67,68 @@ class Groups {
 		$ui->showFooter(false);
 		$ui->beginContent();
 			
-			$table = new Table();
-			$table->setClass('group_view');
-			$table->importFromHash($this->groups);
-			$table->setRowClass($this->status);
-			$ui->contentAddHtml($table->getHTML());
+			$ui->contentAddHtml('<div data-role="collapsible-set" data-theme="a" data-content-theme="d">');
+				$count = 0;
+				foreach ($this->groups as $group) {
+					$ui->contentAddHtml('
+						<style type="text/css">
+							.ui-icon-group_' . $count . ' {
+								background: url("' . $group['group_icon'] . '") no-repeat scroll 0 0 transparent !important;
+							}
+						</style>
+						');
+					$ui->contentAddHtml('<div data-collapsed-icon="group_' . $count . '" ' .
+						'data-expanded-icon="group_' . $count . '" ' .
+						'data-iconpos="right" data-role="collapsible" ' .
+						'data-collapsed="true" data-theme="' . $group['status'] . '" data-content-theme="d">');
+					$ui->contentAddHtml('<h4>' . $group['group_name'] . '</h4>');
+					$ui->contentAddHtml('<ul data-role="listview">');
+					$ui->contentAddHtml('<li>' .
+						__('Agents') .
+						' ' . $group[__('Agents')] .
+						'</li>');
+					$ui->contentAddHtml('<li>' .
+						__('Agents unknown') .
+						' ' . $group[__('Agents unknown')] .
+						'</li>');
+					$ui->contentAddHtml('<li>' .
+						__('Unknown') .
+						' ' . $group[__('Unknown')] .
+						'</li>');
+					$ui->contentAddHtml('<li>' .
+						__('Not init') .
+						' ' . $group[__('Not init')] .
+						'</li>');
+					$ui->contentAddHtml('<li>' .
+						__('Normal') .
+						' ' . $group[__('Normal')] .
+						'</li>');
+					$ui->contentAddHtml('<li>' .
+						__('Warning') .
+						' ' . $group[__('Warning')] .
+						'</li>');
+					$ui->contentAddHtml('<li>' .
+						__('Critical') .
+						' ' . $group[__('Critical')] .
+						'</li>');
+					$ui->contentAddHtml('<li>' .
+						__('Alerts fired') .
+						' ' . $group[__('Alerts fired')] .
+						'</li>');
+					$ui->contentAddHtml('</ul>');
+					$ui->contentAddHtml('</div>');
+					
+					$count++;
+				}
+			$ui->contentAddHtml('</div>');
+			
+			//$ui->contentAddHtml(ob_get_clean());
+			//~ $table = new Table();
+			//~ $table->setId('list_groups');
+			//~ $table->setClass('group_view');
+			//~ $table->importFromHash($this->groups);
+			//~ $table->setRowClass($this->status);
+			//~ $ui->contentAddHtml($table->getHTML());
 			
 		$ui->endContent();
 		$ui->showPage();
@@ -105,6 +161,7 @@ class Groups {
 		// For each valid group for this user, take data from agent and modules
 		foreach ($groups as $id_group => $group) {
 			$rows = groups_get_group_row($id_group, $groups, $group, $printed_groups, false);
+			
 			if (!empty($rows))
 				$return = array_merge($return, $rows);
 		}

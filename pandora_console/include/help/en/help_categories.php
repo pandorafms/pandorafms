fefe09c4244a5a3158e362f87751f7d3c53a3c0f
mariopulido <mario.pulido@artica.es>
2013-04-02 12:00:10 +0000

    2013-04-02  Mario Pulido <mario.pulido@artica.es>
    	* godmode/category/category.php,
           include/help/en/help_categories.php,
           include/help/es/help_categories.php: Added help in categories menu
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7910 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_categories.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Categories in Pandora FMS</h1>
+
+A category are configurated on the system, and be assigned to the choosed modules.  <br>
+Administrator is the only user who has permissions to create and configure categories. Can be used for charging modules depending on the category to which they belong.
+

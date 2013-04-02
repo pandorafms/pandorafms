613e1bad903e88683a1df1b2571c78cebb2fb673
mariopulido <mario.pulido@artica.es>
2013-04-02 16:38:38 +0000

    2013-04-02  Mario Pulido <mario.pulido@artica.es>
    	* include/help/en/help_inventory_tab.php,
    	include/help/es/help_inventory_tab.php: Changes in help of inventory.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7914 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_inventory_tab.php
+++ pandora_console/include/help/en/help_inventory_tab.php
@@ -4,5 +4,13 @@
 ?>
 
 <p>
-Inventory allow you to have a complete list of devices of the monitored servers. With this you can have information of CPU, RAM memory, patches, software, etc. of your servers. 
+Inventory allow you to have a complete list of devices of the monitored servers. With this you can have information of CPU, RAM memory, patches, software, etc. of your servers. <br><br>
+Next are described the fields that you should complete to add an Inventory Module.<br>
+    <ul><b>Module:</b> Combo where you can choose the Inventory Module that you want to add. It will only show the modules which Operative System will match up with that from the agent.</ul>
+    <ul><b>Target:</b> IP or servername from you want to get the inventory.</ul>
+    <ul><b>Interval:</b> Combo where you choose the time interval in which the Inventory Module will ve executed.</ul>
+    <ul><b>Username:</b> User that will ve used to execute the Inventory Module.</ul>
+    <ul><b>Password:</b> User Password that will be used to execute the Inventory Module.</ul>
+
+<p>You can apply changes in all modules clicking in "Update all" when update any module.
 </p>
--- pandora_console/include/help/es/help_inventory_tab.php
+++ pandora_console/include/help/es/help_inventory_tab.php
@@ -3,6 +3,19 @@
 */
 ?>
 
-<p>El inventario te permite tener una lista completa de todos los dispositivos de los servidores monitorizados. Con esto se puede tener informacion de CPU, memoria RAM, parches, software, etc. de tus servidores.
-</p>
+<p>El inventario te permite tener una lista completa de todos los dispositivos de los servidores monitorizados. Con esto se puede tener informacion de CPU, memoria RAM, parches, software, etc. de tus servidores.<br></p>
+
+<p>A continuación se describen los campos que hay que completar para añadir un módulo de inventario.<br>
+
+    <ul><b>Module:</b> Combo donde se elige el módulo de inventario que se quiere añadir. Sólo aparecerán los módulos cuyo Sistema Operativo coincide con el del agente.</ul>
+    <ul><b>Target:</b> IP o nombre del servidor del que se quiere sacar el inventario.</ul>
+    <ul><b>Interval:</b> Combo donde se elige el intervalo de tiempo en que se ejecutará el módulo de inventario.</ul>
+    <ul><b>Username:</b> Usuario que se usara para ejecutar el módulo de inventario.</ul>
+    <ul><b>Password:</b> Password del usuario que se usara para ejecutar el módulo de inventario. </ul>
+
+<p>Una vez completado el formulario se pulsa en “Add” para que el módulo se añada a los módulos de inventario.<br></p>
+
+<p>Puede aplicar cambios en todos los módulos haciendo clic en "Actualizar todo" cuando se actualice cualquiera de los módulos.</p>
+
+
 

19d9b869146f0a52a2fb2ee2e57b425476d0386a
mariopulido87 <noreply@pandorafms.org>
2013-04-12 11:03:12 +0000

    2013-04-12  Mario Pulido <mario.pulido@artica.es>
    	* godmode/users/configure_user.php,
    	godmode/modules/manage_network_components_form_common.php,
            godmode/modules/manage_network_components_form_network.php,
            include/help/en/help_meta_access.php,
            include/help/es/help_meta_access.php: Added meta_access help in 
    	user editor and module network creation.
    
    	*godmode/modules/manage_network_components_form_network.php: Added 
    	help tips in module network creation 
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@7971 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_meta_access.php
@@ -0,0 +1,14 @@
+<?php
+/* Include package help/en
+*/
+?>
+
+<p> Users have assigned a level of access for the Wizard of metaconsole. This level can be Basic or Advanced.
+<ol>
+<li><b>Basic Access</b></li>
+Basic access users may only see the Wizard alerts for alert templates with Basic level and modules created from components with Basic level.
+<li><b>Advanced Access</b></li>
+Advanced users will see, in Wizard, modules and alerts modules in basic and advanced level.
+</ol>
+</p>
+
---
+++ pandora_console/include/help/es/help_meta_access.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<p> Los usuarios tienen asignado un un nivel de acceso respecto al Wizard de la Metaconsola. Este nivel puede ser Básico o Avanzado.
+<ol>
+<li><b>Acceso Básico</b></li>
+Los usuarios de acceso Básico, solo podrán ver en el Wizard las alertas correspondientes a las plantillas de alerta con nivel Básico y los módulos creados desde componentes de nivel Básico. 
+<li><b>Acceso Avanzado </b></li>
+Los usuarios de acceso Avanzado verán en el Wizard las alertas y módulos tanto de nivel Básico como Avanzado. 
+</ol>
+</p>
+

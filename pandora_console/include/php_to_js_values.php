c1e37d9ea500eaff62449a943828200a57bf6fe2
zarzuelo <noreply@pandorafms.org>
2013-04-29 09:24:49 +0000

    2013-04-29  Sergio Martin <sergio.martin@artica.es>
    	* include/php_to_js_values.php: Added a separated file
    	to store from php interesting values to ve retrieved 
    	from javascript
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8079 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/php_to_js_values.php
@@ -0,0 +1,29 @@
+<?php
+// Pandora FMS - http://pandorafms.com
+// ==================================================
+// Copyright (c) 2005-2012 Artica Soluciones Tecnologicas
+// Please see http://pandorafms.org for full contribution list
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the  GNU Lesser General Public License
+// as published by the Free Software Foundation; version 2
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+/**
+ * @package Include
+ * @subpackage php_to_js_values
+ */
+
+// Hidden div to forced title
+html_print_div(array('id' => 'forced_title_layer', 'class' => 'forced_title_layer', 'hidden' => true));
+
+//======= Store values to be retrieved from javascript code ============
+set_js_value('absolute_homeurl', ui_get_full_url(false, false, false, false));
+set_js_value('homeurl', $config["homeurl"]);
+set_js_value('homedir', $config["homedir"] . '/');
+//======================================================================
+?>

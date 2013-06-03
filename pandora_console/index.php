1a7469b7263772078799f95755a16d9c8ab8281e
zarzuelo <zarzuelo@gmail.com>
2013-06-03 11:10:20 +0000

    2013-06-03  Sergio Martin <sergio.martin@artica.es>
    	* include/styles/pandora.css
    	index.php: Fix footer layout merged from 4.1
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8246 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/index.php
+++ pandora_console/index.php
@@ -549,10 +549,10 @@ else {
 
 
 if ($config["pure"] == 0) {
+	echo '</div>'; //container div
 	echo '<div id="foot">';
 	require ("general/footer.php");
 	echo '</div>';
-	echo '</div>'; //container div
 }
 while (@ob_end_flush ());
 
@@ -565,3 +565,20 @@ echo "\n<!-- Page generated in $run_time seconds -->\n";
 // Values from PHP to be recovered from JAVASCRIPT
 require('include/php_to_js_values.php');
 ?>
+
+<script type="text/javascript" language="javascript">
+//Initial load of page
+$(document).ready(sizeContent);
+
+//Every resize of window
+$(window).resize(sizeContent);
+
+//Dynamically assign height
+function sizeContent() {
+	var footposition_bottom = $('#foot').position().top + $("#foot").height();
+
+	if(footposition_bottom < $(window).height()) {		
+		$('#container').css('height', $(window).height() - $("#foot").height() - 15 + 'px');
+	}
+}
+</script>

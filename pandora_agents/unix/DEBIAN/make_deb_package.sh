f9b7142392a531270f8a95c16ffeeffa8a9bcc11
Ramon Novoa <rnovoa@artica.es>
2013-06-07 08:51:15 +0000

    2013-06-07  Ramon Novoa  <rnovoa@artica.es>
    	* unix/DEBIAN/make_deb_package.sh: Create the agent's temp dir.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@8267 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/unix/DEBIAN/make_deb_package.sh
+++ pandora_agents/unix/DEBIAN/make_deb_package.sh
@@ -38,6 +38,7 @@ mkdir -p temp_package/etc/pandora/plugins
 mkdir -p temp_package/etc/pandora/collections
 mkdir -p temp_package/etc/init.d/
 mkdir -p temp_package/var/log/pandora/
+mkdir -p temp_package/var/spool/pandora/data_out
 mkdir -p temp_package/usr/share/man/man1/
 mkdir -p temp_package/usr/share/pandora_agent/plugins
 mkdir -p temp_package/tmp

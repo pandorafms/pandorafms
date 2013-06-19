1e3454ab09f15588eed81e052621b71c97979bc3
mariopulido <mario.pulido@artica.es>
2013-06-19 10:23:49 +0000

    2013-06-19 Mario Pulido <mario.pulido@artica.es>
    	* include/help/ja/help_module_definition.php
            include/help/en/help_module_definition.php,
            include/help/es/help_module_definition.php: Changes in help
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@8352 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_module_definition.php
+++ pandora_console/include/help/en/help_module_definition.php
@@ -4,9 +4,12 @@
  */
 ?>
 <h1>Module definition</h1>
-
+<p>
 There are two modes for an agent:
+</p>
 <ul>
-    <li>Learning mode: all the modules sent by the agent are accepted. If modules are not defined, they will be automatically defined by the system. It is recommended to activate the agents in this mode and change it once the user is familiar with Pandora FMS.</li>
-    <li>Normal mode: the modules in this mode must be conﬁgured manually. The self definition of the modules is not allowed in this mode.</li>
+    <li><i>Learning mode:</i> all the modules sent by the agent are accepted. If modules are not defined, they will be automatically defined by the system. It is recommended to activate the agents in this mode and change it once the user is familiar with Pandora FMS.<br>From version 4.0.3, in this mode,  Pandora console collect all the configuration specified by the agent configuration file the first time and thereafter any changes should be made through console, will not catch changes in config file.
+</li>
+<br>
+    <li><i>Normal mode:</i> the modules in this mode must be conﬁgured manually. The self definition of the modules is not allowed in this mode.</li>
 </ul>

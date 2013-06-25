c855e455cf7248274f3d7ef2142997725b49db28
mdtrooper <tres.14159@gmail.com>
2013-06-25 10:45:01 +0000

    2013-06-25 Miguel de Dios <miguel.dedios@artica.es>
    	* extensions/vnc_view.php: added the changes from the branch of
    	pandora_4 made for Sancho.
    	
    	MERGED FROM THE BRANCH PANDORA_4.0
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8396 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/extensions/vnc_view.php
+++ pandora_console/extensions/vnc_view.php
@@ -18,7 +18,14 @@ function vnc_view() {
 	$idAgent = (int)get_parameter('id_agente', 0);
 	$ipAgent = db_get_value('direccion', 'tagente', 'id_agente', $idAgent);
 
-	echo "<iframe src='http://$ipAgent:5800' width='100%' height=550>";
+	$display = get_parameter("display", 0);
+	echo "<form method=post>";
+	echo __("VNC Display (:0 by default)");
+	echo "<input type=text name=display value=$display>";
+	echo "<input type=submit value='".__("Send")."'>";
+	echo "</form>";
+	$port = 5800 + $display;
+	echo "<iframe src='http://$ipAgent:$port' width='100%' height=600>";
 	echo "</iframe>";
 }
 

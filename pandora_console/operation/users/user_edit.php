7405a089add30e776143b815b4599e7903998db3
mdtrooper <tres.14159@gmail.com>
2013-06-25 11:19:06 +0000

    2013-06-25 Miguel de Dios <miguel.dedios@artica.es>
    	* operation/users/user_edit.php: merge the code for made more
    	easy the merges from the trunk.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@8397 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/users/user_edit.php
+++ pandora_console/operation/users/user_edit.php
@@ -35,7 +35,9 @@ $status = get_parameter ("status", -1); // Flag to print action status message
 $user_info = get_user_info ($id);
 $id = $user_info["id_user"]; //This is done in case there are problems with uppercase/lowercase (MySQL auth has that problem)
 
-if ((!check_acl ($config["id_user"], users_get_groups ($id), "UM")) AND ($id != $config["id_user"])){
+if ((!check_acl ($config["id_user"], users_get_groups ($id), "UM"))
+	AND ($id != $config["id_user"])) {
+	
 	db_pandora_audit("ACL Violation","Trying to view a user without privileges");
 	require ("general/noaccess.php");
 	exit;
@@ -44,13 +46,17 @@ if ((!check_acl ($config["id_user"], users_get_groups ($id), "UM")) AND ($id != 
 //If current user is editing himself or if the user has UM (User Management) rights on any groups the user is part of AND the authorization scheme allows for users/admins to update info
 if (($config["id_user"] == $id || check_acl ($config["id_user"], users_get_groups ($id), "UM")) && $config["user_can_update_info"]) {
 	$view_mode = false;
-} else {
+}
+else {
 	$view_mode = true;
 }
+
 
 // Header
 ui_print_page_header (__('User detail editor'), "images/group.png", false, "", false, "");
 
+
+// Update user info
 if (isset ($_GET["modified"]) && !$view_mode) {
 	$upd_info = array ();
 	$upd_info["fullname"] = get_parameter_post ("fullname", $user_info["fullname"]);
@@ -62,26 +68,27 @@ if (isset ($_GET["modified"]) && !$view_mode) {
 	$upd_info["phone"] = get_parameter_post ("phone", $user_info["phone"]);
 	$upd_info["comments"] = get_parameter_post ("comments", $user_info["comments"]);
 	$upd_info["language"] = get_parameter_post ("language", $user_info["language"]);
-	$upd_info["id_skin"] = get_parameter ("skin", $user_info["id_skin"]);	
+	$upd_info["id_skin"] = get_parameter ("skin", $user_info["id_skin"]);
 	$upd_info["block_size"] = get_parameter ("block_size", $config["block_size"]);
 	$default_block_size = get_parameter ("default_block_size", 0);
-	if($default_block_size) {
+	if ($default_block_size) {
 		$upd_info["block_size"] = 0;
 	}
 	
 	$upd_info["flash_chart"] = get_parameter ("flash_charts", $config["flash_charts"]);
-
+	
 	// This only will be running on 4.1 or higher.
 	if (isset($user_info['section'])){	
 		$upd_info["section"] = get_parameter ("section", $user_info["section"]);
 		$upd_info["data_section"] = get_parameter ("data_section", '');
-	} else {
+	}
+	else {
 		$upd_info["section"] = "";
-                $upd_info["data_section"] = "";
-	}	
-
+		$upd_info["data_section"] = "";
+	}
+	
 	$section = io_safe_output($upd_info["section"]);
-
+	
 	if (($section == 'Event list') || ($section == 'Group view') || ($section == 'Alert detail') || ($section == 'Tactical view') || ($section == 'Default')) {
 		$upd_info["data_section"] = '';
 	}
@@ -91,45 +98,39 @@ if (isset ($_GET["modified"]) && !$view_mode) {
 	else if ($section == 'Visual console') {
 		$upd_info["data_section"] = $visual_console;
 	}
-
-	if ( !empty ($password_new)) {
+	
+	if (!empty ($password_new)) {
 		if ($config["user_can_update_password"] && $password_confirm == $password_new) {
 			$return = update_user_password ($id, $password_new);
 			ui_print_result_message ($return,
 				__('Password successfully updated'),
 				__('Error updating passwords: %s', $config['auth_error']));
-		} elseif ($password_new !== "NON-INIT") {
+			
+		}
+		elseif ($password_new !== "NON-INIT") {
 			ui_print_error_message (__('Passwords didn\'t match or other problem encountered while updating passwords'));
 		}
 	}
-
+	
 	// No need to display "error" here, because when no update is needed (no changes in data) 
 	// SQL function returns	0 (FALSE), but is not an error, just no change. Previous error
 	// message could be confussing to the user.
-
+	
 	$return = update_user ($id, $upd_info);
 	if ($return > 0) {
 		ui_print_result_message ($return,
 			__('User info successfully updated'),
 			__('Error updating user info'));
 	}
-
-	// Reload page to update skin
-     /*   if ($return){
-                header ('location:' . $config['homeurl'] . '/index.php?sec=workspace&sec2=operation/users/user_edit&status=1');
-        }
-        else{
-                header ('location:' . $config['homeurl'] . '/index.php?sec=workspace&sec2=operation/users/user_edit&status=0');
-        }*/
-
+	
 	$user_info = $upd_info;
 }
 
 // Prints action status for current message 
-if ($status != -1){
+if ($status != -1) {
 	ui_print_result_message ($status,
-       		__('User info successfully updated'),
-             	__('Error updating user info'));
+		__('User info successfully updated'),
+		__('Error updating user info'));
 }
 
 
@@ -157,21 +158,14 @@ html_print_input_text_extended ("fullname", $user_info["fullname"], '', '', 35, 
 // Not used anymore. In 3.0 database schema continues storing it, but will be removed in the future, or we will 'reuse'
 // the database fields for anything more useful.
 
-/*
-echo '</td></tr><tr><td class="datos">'.__('First name').'</td><td class="datos">';
-html_print_input_text_extended ("firstname", $user_info["firstname"], '', '', 25, 100, $view_mode, '', 'class="input"');
-
-echo '</td></tr><tr><td class="datos2">'.__('Last name').'</td><td class="datos2">';
-html_print_input_text_extended ("lastname", $user_info["lastname"], '', '', 25, 100, $view_mode, '', 'class="input"');
-
-*/
 if ($view_mode === false) {
 	if ($config["user_can_update_password"]) {
 		echo '</td></tr><tr><td class="datos">'.__('New Password').'</td><td class="datos">';
 		html_print_input_text_extended ("password_new", "", '', '', '15', '25', $view_mode, '', 'class="input"', false, true);
 		echo '</td></tr><tr><td class="datos">'.__('Password confirmation').'</td><td class="datos">';
 		html_print_input_text_extended ("password_conf", "", '', '', '15', '25', $view_mode, '', 'class="input"', false, true);
-	} else {
+	}
+	else {
 		echo '<i>'.__('You can not change your password from Pandora FMS under the current authentication scheme').'</i>';
 	}
 }
@@ -189,19 +183,19 @@ echo html_print_select_from_sql ('SELECT id_language, name FROM tlanguage',
 echo '</td></tr><tr><td class="datos2">'.__('Comments').'</td><td class="datos">';
 html_print_textarea ("comments", 2, 60, $user_info["comments"], ($view_mode ? 'readonly="readonly"' : ''));
 html_print_input_hidden('quick_language_change', 1);
-   
+
 $own_info = get_user_info ($config['id_user']);
 if ($own_info['is_admin'] || check_acl ($config['id_user'], 0, "PM"))
 	$display_all_group = true;
 else
-	$display_all_group = false;		
+	$display_all_group = false;
 
 $usr_groups = (users_get_groups($config['id_user'], 'AR', $display_all_group));
 $id_usr = $config['id_user'];
 
 // User only can change skins if has more than one group 
-if (count($usr_groups) > 1){
-
+if (count($usr_groups) > 1) {
+	
 	$isFunctionSkins = enterprise_include_once ('include/functions_skins.php');
 	if ($isFunctionSkins !== ENTERPRISE_NOT_HOOK) {
 		echo '</td></tr><tr><td class="datos">' . __('Skin') . '</td><td class="datos2">';
@@ -213,7 +207,7 @@ echo '</td></tr><tr><td class="datos">'.__('Flash charts').'</td><td class="dato
 $values = array(-1 => __('Default'),1 => __('Yes'),0 => __('No'));
 echo html_print_select($values, 'flash_charts', $user_info["flash_chart"], '', '', -1, true, false, false);
 echo '</td></tr><tr><td class="datos">'.__('Block size for pagination'). ui_print_help_tip(__('If checkbox is clicked then block size global configuration is used'), true) . '</td><td class="datos2">';
-if($user_info["block_size"] == 0) {
+if ($user_info["block_size"] == 0) {
 	$block_size = $config["global_block_size"];
 }
 else {
@@ -226,18 +220,18 @@ echo __('Default').' ('.$config["global_block_size"].')';
 
 
 // This only will be running on 4.1 or higher.
-if (isset($user_info['section'])){
-
-	echo '</td></tr><tr><td class="datos">'.__('Home screen'). ui_print_help_tip(__('User can customize the home page. By default, will display \'Agent Detail\'. Example: Select \'Other\' and type sec=estado&sec2=operation/agentes/estado_agente to show agent detail view'), true) .'</td><td class="datos2">';
+if (isset($user_info['section'])) {
+	
+	echo '</td></tr><tr><td class="datos">' . __('Home screen') . ui_print_help_tip(__('User can customize the home page. By default, will display \'Agent Detail\'. Example: Select \'Other\' and type sec=estado&sec2=operation/agentes/estado_agente to show agent detail view'), true) .'</td><td class="datos2">';
 	$values = array ('Default' =>__('Default'), 'Visual console'=>__('Visual console'), 'Event list'=>__('Event list'),
 	'Group view'=>__('Group view'), 'Tactical view'=>__('Tactical view'), 'Alert detail' => __('Alert detail'), 'Other'=>__('Other'));
-
+	
 	if ($is_enterprise) {
 		array_push($values, array('Dashboard' => __('Dashboard')));
 	}
 	echo html_print_select($values, 'section', io_safe_output($user_info["section"]), 'show_data_section();', '', -1, true, false, false);
 	echo "&nbsp;&nbsp;";
-
+	
 	if ($is_enterprise) {
 		$dashboards = get_user_dashboards ($config['id_user']);
 		$dashboards_aux = array();
@@ -251,7 +245,7 @@ if (isset($user_info['section'])){
 		}
 		echo html_print_select ($dashboards_aux, 'dashboard', $user_info["data_section"], '', '', '', true);
 	}
-
+	
 	$layouts = visual_map_get_user_layouts ($config['id_user'], true);
 	$layouts_aux = array();
 	if ($layouts === false) {
@@ -271,10 +265,12 @@ echo '</td></tr></table>';
 echo '<div style="width:90%; text-align:right;">';
 if (!$config["user_can_update_info"]) {
 	echo '<i>'.__('You can not change your user info from Pandora FMS under the current authentication scheme').'</i>';
-} else {
+}
+else {
 	html_print_submit_button (__('Update'), 'uptbutton', $view_mode, 'class="sub upd"');
 }
 echo '</div></form>';
+
 
 echo '<h4>'.__('Profiles/Groups assigned to this user').'</h4>';
 
@@ -300,14 +296,19 @@ if ($result === false) {
 foreach ($result as $profile) {
 	$data[0] = '<b>'.profile_get_name ($profile["id_perfil"]).'</b>';
 	$data[1] = ui_print_group_icon ($profile["id_grupo"], true).' <a href="index.php?sec=estado&sec2=operation/agentes/estado_agente&refr=60&group_id='.$profile['id_grupo'].'"></a>';
+	
+	
 	array_push ($table->data, $data);
 }
 
 if (!empty ($table->data)) {
 	html_print_table ($table);
-} else {
+}
+else {
 	echo '<div class="nf">'.__('This user doesn\'t have any assigned profile/group').'</div>'; 
 }
+
+
 ?>
 
 <script language="javascript" type="text/javascript">
@@ -318,7 +319,7 @@ $(document).ready (function () {
 	});
 	
 	function check_default_block_size() {
-		if($("#checkbox-default_block_size").attr('checked')) {
+		if ($("#checkbox-default_block_size").attr('checked')) {
 			$("#text-block_size").attr('disabled', true);
 		}
 		else {
@@ -330,7 +331,8 @@ $(document).ready (function () {
 });
 
 function show_data_section () {
-	section=$("#section").val();
+	section = $("#section").val();
+	
 	switch (section) {
 		case <?php echo "'" . __('Dashboard') . "'"; ?>:
 			$("#text-data_section").css("display", "none");

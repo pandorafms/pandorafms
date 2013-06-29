d387b9f1e25458b5c8acadf4797bda129873304d
pandora-release <noreply@pandorafms.org>
2013-06-29 23:41:57 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@8435 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="4.1RC2-130629"
+pandora_version="4.1RC2-130630"
 
 package_cpan=0
 package_pandora=1

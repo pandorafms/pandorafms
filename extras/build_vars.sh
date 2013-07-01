3b7a406c789ba98263bb540f893aa9879916ffd8
ramonn <noreply@pandorafms.org>
2013-07-01 09:14:00 +0000

    2013-07-01  Ramon Novoa  <rnovoa@artica.es>
    	* build_vars.sh: Use absolute paths.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8437 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- extras/build_vars.sh
+++ extras/build_vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CODEHOME=~/code/pandora/trunk
-CODEHOME_ENT=~/code/artica/code
+CODEHOME=/root/code/pandora/trunk
+CODEHOME_ENT=/root/code/artica/code
 PANDHOME_ENT=$CODEHOME_ENT/pandora/trunk
 RPMHOME=/usr/src/packages
 VERSION=$(grep 'my $pandora_version =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')

a1a66d058efdd105b208bd72e8788ebaf9fc8d1f
zarzuelo <zarzuelo@gmail.com>
2013-07-22 07:31:22 +0000

    2013-07-22  Sergio Martin <sergio.martin@artica.es>
    	* include/help/en/help_module_interval_factor.php
    	include/help/es/help_module_interval_factor.php
    	godmode/agentes/module_manager_editor_common.php: Change the
    	input field of module interval on data modules form by 
    	a non editable info and improve the help explaining deeply the
    	way of change the interval and how to do it. For bug #2308
    
    	* images/cancel.png
    	godmode/massive/massive_operations.php: Add missed icon
    	and change another one
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8551 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/es/help_module_interval_factor.php
+++ pandora_console/include/help/es/help_module_interval_factor.php
@@ -4,6 +4,23 @@
 ?>
 
 <h1>El intervalo de módulo como factor</h1>
+<h2>Dónde se cambia el intervalo</h2>
+<p>
+El intervalo de los módulos de tipo data <b>se cambia en la definición del módulo en 
+el fichero de configuración del agente</b>.<br><br>
+El token de configuración del intervalo es <b>module_interval</b>.<br><br>
+Por ejemplo:<br><br>
+<i>
+module_begin<br>
+module_name Ejemplo de modulo<br>
+module_type generic_data<br>
+module_exec echo 100<br>
+module_interval 2<br>
+module_description Este modulo devuelve siempre 100<br>
+module_end<br>
+</i>
+</p>
+<h2>Cómo se define el intervalo en este tipo de módulos</h2>
 <p> En los <b>módulos de tipo data</b>, el intervalo <b>no se define en segundos</b>.<br><br>
 
 intervalo se calcula como un <b>factor multiplicador</b> para el intervalo del agente.<br><br>
@@ -11,5 +28,5 @@ intervalo se calcula como un <b>factor multiplicador</b> para el intervalo del a
 Por ejemplo, si el agente tiene intervalo 300 (5 minutos), y se quiere un módulo que 
 sea procesado sólo cada 15 minutos, se debe establecer un intervalo de módulo 3<br><br>
 
-This module will be preocessed every 300sec x 3 = 900sec (15 minutes).
+Este módulo será procesado cada 300seg x 3 = 900seg (15 minutos).
 </p>

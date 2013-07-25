e835f469784bd005d356da5b61c126da391fb0a6
Ramon Novoa <rnovoa@artica.es>
2013-07-25 10:05:21 +0000

    2013-07-25  Ramon Novoa  <rnovoa@artica.es>
    	* godmode/servers/manage_recontask_form.php: Merged from trunk. Added a
    	  help tip for parent recursion.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@8572 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/servers/manage_recontask_form.php
+++ pandora_console/godmode/servers/manage_recontask_form.php
@@ -258,7 +258,7 @@ $table->data[20][1] =  html_print_checkbox ('parent_detection', 1, $parent_detec
 
 // Parent recursion
 $table->data[21][0] = "<b>".__('Parent recursion');
-$table->data[21][1] =  html_print_input_text ('parent_recursion', $parent_recursion, '', 5, 0, true);
+$table->data[21][1] =  html_print_input_text ('parent_recursion', $parent_recursion, '', 5, 0, true) . ui_print_help_tip (__('Maximum number of parent hosts that will be created if parent detection is enabled.'), true);
 
 // Different Form url if it's a create or if it's a update form
 echo '<form name="modulo" method="post" action="index.php?sec=gservers&sec2=godmode/servers/manage_recontask&'.(($id_rt != -1) ? 'update='.$id_rt : 'create=1').'">';

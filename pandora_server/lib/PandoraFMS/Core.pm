e9d539874b296f1a56b9bea50cb723176b086cf6
ramonn <noreply@pandorafms.org>
2013-08-01 11:03:50 +0000

    2013-08-01  Ramon Novoa  <rnovoa@artica.es>
    	* lib/PandoraFMS/Core.pm: Changed the name of the self-monitoring XML
    	  files. Fixes bug #2365.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8613 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/Core.pm
+++ pandora_server/lib/PandoraFMS/Core.pm
@@ -3825,7 +3825,7 @@ sub pandora_self_monitoring ($$) {
 	
 	$xml_output .= "</agent_data>";
 	
-	my $filename = $pa_config->{"incomingdir"}."/".$pa_config->{'servername'}.".".$utimestamp.".self.data";
+	my $filename = $pa_config->{"incomingdir"}."/".$pa_config->{'servername'}.".self.".$utimestamp.".data";
 	
 	open (XMLFILE, ">> $filename") or die "[FATAL] Could not open internal monitoring XML file for deploying monitorization at '$filename'";
 	print XMLFILE $xml_output;

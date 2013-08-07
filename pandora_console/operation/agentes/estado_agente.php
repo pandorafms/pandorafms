93589e7fd482acc36cc928e14276daa73d4da5c3
mdtrooper <tres.14159@gmail.com>
2013-08-07 10:59:09 +0000

    2013-08-07 Miguel de Dios <miguel.dedios@artica.es>
    	* operation/agentes/estado_agente.php: fixed the call to
    	enterprise function.
    	
    	Fixes: #2367 
    	
    	* godmode/agentes/module_manager.php: make more easy the merges
    	from the last branch.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8637 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/operation/agentes/estado_agente.php
+++ pandora_console/operation/agentes/estado_agente.php
@@ -491,13 +491,15 @@ foreach ($agents as $agent) {
 		// Has remote configuration ?
 		$data[9]="";
 		
-		if (config_agents_has_remote_configuration($agent["id_agente"])) {
-			$data[9] = "<a href='index.php?sec=estado&sec2=godmode/agentes/configurar_agente&tab=main&id_agente=".$agent["id_agente"]."&disk_conf=1'>".
-			html_print_image("images/application_edit.png", true, array("align" => 'middle', "title" => __('Edit remote config')))."</a>";
+		if (enterprise_installed()) {
+			if (config_agents_has_remote_configuration($agent["id_agente"])) {
+				$data[9] = "<a href='index.php?sec=estado&sec2=godmode/agentes/configurar_agente&tab=main&id_agente=".$agent["id_agente"]."&disk_conf=1'>".
+				html_print_image("images/application_edit.png", true, array("align" => 'middle', "title" => __('Edit remote config')))."</a>";
+			}
 		}
 		
 	}
-
+	
 	array_push ($table->data, $data);
 }
 

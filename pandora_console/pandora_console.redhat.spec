01cf6c3b8766543f30de29a9f4b87382ba712508
pandora-release <noreply@pandorafms.org>
2013-08-15 23:20:38 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@8666 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/pandora_console.redhat.spec
+++ pandora_console/pandora_console.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     4.1
-%define release     130815
+%define release     130816
 
 # User and Group under which Apache is running
 %define httpd_name  httpd
--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="4.1-130815"
+pandora_version="4.1-130816"
 
 package_cpan=0
 package_pandora=1

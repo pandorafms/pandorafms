2a73f68ae28bc5e69a892fc2399f026a50530f2d
jsatoh <junichi@rworks.jp>
2013-09-22 14:08:53 +0000

    2013-09-22  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/ja/help_alert_config.php: Translated new sentences.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8795 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/ja/help_alert_config.php
+++ pandora_console/include/help/ja/help_alert_config.php
@@ -3,66 +3,64 @@
  * @package Include/help/ja
  */
 ?>
-<h1>Configure Alert Action</h1>
-<br>Actions are the components of alerts where a command (which is described in the previous section) is linked with the generic variables Field 1, Field 2, ..., Field 10. These actions will be used later in the alert templates that are the ones that associate a data condition with an specific action. <br>
-Next are the fields that you should fill in:<br><br>
+<h1>アラートアクションの設定</h1>
+<br>アクションは、一般的な変数、フィールド1、フィールド2、... フィールド10 と共に(前の節で説明した)コマンドにリンクしたアラートのコンポーネントです。これらのアクションは、データの状態と関連づけるアラートテンプレートで利用します。<br>
+以下に、設定するフィールドを説明します。<br><br>
 
-    <b>Name:</b> Name of the action.<br>
-    <b>Group:</b> Group of the action. <br>
-    <b>Command:</b> In this field is defined the command that will be used in case the alert will be executed . You can choose between the different commands that are defined in Pandora.Depending of the command chosen we will have some or others fields to fill. <br>
-    <b>Threshold:</b> The action execution threshold. <br>
-    <b>Command Preview:</b>  In this field, not editable, will automatically appear the command that will be executed in the system. <br>
-    <b>Field X:</b> In this fields are defined the values of the macros _field1_ to _field10_, that will be used in the command if necessary. These fields can be a text field or combo box if configured. Depending on the selected command will appear a number of fields to fill in as needed or not. For example:<br><br>
+    <b>名前(Name):</b> アクションの名前。<br>
+    <b>グループ(Group):</b> アクションのグループ。<br>
+    <br>コマンド(Command):</b> アラートが実行された時に利用されるコマンドの定義です。Pandora で定義されたコマンドを選択することができます。選択するコマンドによって、入力が必要なフィールドが異なります。<br>
+    <b>閾値(Threshold):</b> アクション実行の閾値。<br>
+    <b>コマンドプレビュー(Command Preview):</b> このフィールドには、システムが実行するコマンドが表示されます。編集はできません。<br>
+    <b>フィールドX(Field X):</b> このフィールドでは、マクロ _field1_ から _field10_ までのマクロの値を定義します。これらは、必要に応じてコマンドで利用されます。これらのフィールドは、設定によりテキスト入力または選択となります。選択したコマンドに応じて、表示されるフィールドの数が変化します。以下に例を示します。<br><br>
 
-To configure email action, you can only fill _field1_ (Destination address), _field2_ (Subject) y _field3_ (Message)<br><br>
+email アクションを設定するには、_field1_ (送信先アドレス)、_field2_ (件名)、および _filed3_ (本文) のみを設定します。<br><br>
 
-Within these fields we can configure the macros listed below.
+これらのフィールド内では、以下のマクロを利用できます。
 <br><br>
 <?php html_print_image ("images/help/actions.png", false, array('width' => '550px')); ?>
 <br><br>
 <br>
 
 <p>
-Besides the defined module macros, the following macros are available:
+定義したモジュールマクロ以外に、次のマクロが利用できます:
 <ul>
-<li>_field1_ : User defined field 1.</li>
-<li>_field2_ : User defined field 2.</li>
-<li>_field3_ : User defined field 3.</li>
-<li>_agent_ : Name of the agent that fired the alert.</li>
-<li>_agentdescription_ : Description of the agent who fired alert.</li>
-<li>_agentgroup_ : Agent group name.</li>
-<li>_agentstatus_ : Current status of the agent.</li>
-<li>_address_ : Address of the agent that fired the alert.</li>
-<li>_timestamp_ : Time when the alert was fired (yy-mm-dd hh:mm:ss).</li>
-<li>_data_ : Module data that caused the alert to fire.</li>
-<li>_alert_description_ : Alert description.</li>
-<li>_alert_threshold_ : Alert threshold.</li>
-<li>_alert_times_fired_ : Number of times the alert has been fired.</li>
-<li>_module_ : Module name.</li>
-<li>_modulegroup_ : Module group name.</li>
-<li>_moduledescription_ : Description of the module who fired the alert.</li>
-<li>_modulestatus_ : Status of the module.</li>
-<li>_moduletags_ : Tags associated to the module.</li>
-<li>_alert_name_ : Alert name.</li>
-<li>_alert_priority_ : Numerical alert priority.</li>
-<li>_alert_text_severity_ : Text alert severity (Maintenance, Informational, Normal Minor, Warning, Major, Critical).</li>
-<li>_event_text_severity_ : (Only event alerts) Text event (who fire the alert) severity (Maintenance, Informational, Normal Minor, Warning, Major, Critical).</li>
-<li>_event_id_ : (Only event alerts) Id of the event that fired the alert.</li>
-<li>_id_agent_ : Id of agent, useful to build direct URL to redirect to a Pandora FMS console webpage.</li>
-<li>_id_alert_ : Numerical ID of the alert (unique), used to correlate on third party software</li>
-<li>_policy_ : Name of the policy the module belongs to (if applies).</li>
-<li>_interval_ : Execution interval of the module. </li>
-<li>_target_ip_ : IP address of the target of the module.</li>
-<li>_target_port_ : Port number of the target of the module.</li>
-<li>_plugin_parameters_ : Plug-in Parameters of the module.</li>
-<li>_groupcontact_ : Group contact information.	Configured when the group is created.</li>
-<li>_groupother_ : Other information about the group. Configured when the group is created.</li>
-<li>_email_tag_ : Emails associated to the module tags.</li>
+<li>_field2_ : ユーザ定義フィールド2</li>
+<li>_field3_ : ユーザ定義フィールド3</li>
+<li>_agent_ : アラートが発生したエージェント</li>
+<li>_agentdescription_ : 発生したアラートの説明</li>
+<li>_agentgroup_ : エージェントグループ名</li>
+<li>_agentstatus : エージェントの現在の状態</li>
+<li>_address_ : アラートが発生したエージェントのアドレス</li>
+<li>_timestamp_ : アラートが発生した日時 (yy-mm-dd hh:mm:ss).</li>
+<li>_data_ : アラート発生時のモジュールのデータ(値)</li>
+<li>_alert_description_ : アラートの説明</li>
+<li>_alert_threshold_ : アラートのしきい値</li>
+<li>_alert_times_fired_ : アラートが発生した回数</li>
+<li>_module_ : モジュール名</li>
+<li>_modulegroup_ : モジュールグループ名</li>
+<li>_moduledescription_ : アラートが発生したモジュールの説明</li>
+<li>_modulestatus_ : モジュールの状態</li>
+<li>_moduletags_ : モジュールに関連付けられたタグ</li>
+<li>_alert_name_ : アラート名</li>
+<li>_alert_priority_ : アラート優先順位(数値)</li>
+<li>_alert_text_severity_ : テキストでのアラートの重要度 (Maintenance, Informational, Normal Minor, Warning, Major, Critical)</li>
+<li>_event_text_severity_ : (イベントアラートのみ) イベント(アラートの発生元)のテキストでの重要度 (Maintenance, Informational, Normal Minor, Warning, Major, Critical)</li>
+<li>_event_id_ : (イベントアラートのみ) アラート発生元のイベントID</li>
+<li>_id_agent_ : エージェントのID / Webコンソールへのリンクを生成するのに便利です</li>
+<li>_id_alert_ : アラートの(ユニークな)ID / 他のソフトウエアパッケージとの連携に利用できます</li>
+<li>_policy_ : モジュールが属するポリシー名 (存在する場合)</li>
+<li>_interval_ : モジュールの実行間隔</li>
+<li>_target_ip_ : モジュールの対象IPアドレス</li>
+<li>_target_port_ : モジュールの対象ポート</li>
+<li>_plugin_parameters_ : モジュールのプラグインパラメータ</li>
+<li>_groupcontact_ : グループコンタクト情報。グループの作成時に設定されます。</li>
+<li>_groupother_ : グループに関するその他情報。グループの作成時に設定されます。</li>
+<li>_email_tag_ : モジュールタグに関連付けられた Email。</li>
 
 </ul>
 <p>
 </p>
 <p>
-Example: Agent _agent_ has fired alert _alert_ with data _data_
+例: Agent _agent_ has fired alert _alert_ with data _data_
 </p>
-

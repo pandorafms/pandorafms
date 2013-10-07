b3c1dd9c45f9a41431e4f33a0ff20986fa6eb8fe
jsatoh <junichi@rworks.jp>
2013-10-07 08:03:05 +0000

    2013-10-07  Junichi Satoh  <junichi@rworks.jp>
    	* extras/pandoradb_migrate_4.0.x_to_5.0.mysql.sql,
    	extras/pandoradb_migrate_4.0.x_to_5.0.postgreSQL.sql: Removed
    	duplicated queries.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8857 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/extras/pandoradb_migrate_4.0.x_to_5.0.mysql.sql
+++ pandora_console/extras/pandoradb_migrate_4.0.x_to_5.0.mysql.sql
@@ -455,17 +455,6 @@ ALTER TABLE `ttag` ADD COLUMN `email` text NULL;
 ALTER TABLE `ttag` ADD COLUMN `phone` text NULL;
 
 -- ---------------------------------------------------------------------
--- Add more fields in snmp alerts (05/03/13)
--- ---------------------------------------------------------------------
-ALTER TABLE `talert_snmp` ADD COLUMN `al_field4` text;
-ALTER TABLE `talert_snmp` ADD COLUMN `al_field5` text;
-ALTER TABLE `talert_snmp` ADD COLUMN `al_field6` text;
-ALTER TABLE `talert_snmp` ADD COLUMN `al_field7` text;
-ALTER TABLE `talert_snmp` ADD COLUMN `al_field8` text;
-ALTER TABLE `talert_snmp` ADD COLUMN `al_field9` text;
-ALTER TABLE `talert_snmp` ADD COLUMN `al_field10` text;
-
--- ---------------------------------------------------------------------
 -- Add metaconsole fields to user table (17/04/13)
 -- ---------------------------------------------------------------------
 ALTER TABLE `tusuario` ADD COLUMN `metaconsole_agents_manager` tinyint(1) unsigned NOT NULL default 0;

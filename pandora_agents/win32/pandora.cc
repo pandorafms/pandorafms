8af4dc13d8ce80e33b13c8cbe73d11186886bf1e
pandora-release <noreply@pandorafms.org>
2013-10-08 12:12:08 +0000

    Updated version and build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8870 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("5.0RC1(Build 130930)")
+#define PANDORA_VERSION ("5.0RC2(Build 131008)")
 
 string pandora_path;
 string pandora_dir;

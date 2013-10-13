1914ddf983d8c4a23266c40cf14ea6c2b67e7c91
pandora-release <noreply@pandorafms.org>
2013-10-13 23:29:29 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@8903 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="4.1-131013"
+pandora_version="4.1-131014"
 
 package_cpan=0
 package_pandora=1
--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -33,7 +33,7 @@ use PandoraFMS::Tools;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "4.1 PS131013";
+my $version = "4.1 PS131014";
 
 # Pandora server configuration
 my %conf;

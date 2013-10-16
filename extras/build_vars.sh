cffde24bbc76409386409e0eaebfb95205dcf4f5
ramonn <noreply@pandorafms.org>
2013-10-16 09:20:04 +0000

    2013-10-16  Ramon Novoa  <rnovoa@artica.es>
    	* build_vars.sh: Added arch information.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@8922 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- extras/build_vars.sh
+++ extras/build_vars.sh
@@ -5,4 +5,4 @@ PANDHOME_ENT=$CODEHOME_ENT/pandora/trunk
 RPMHOME=/usr/src/packages
 VERSION=$(grep 'my $pandora_version =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
 BUILD=$(grep 'my $pandora_build =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
-KEYGEN_VERSION=$(grep "%define version" $CODEHOME_ENT/updatemanager/keygen/pandora/pandora_keygen.spec | awk '{print $3}')
+X86_64=`uname -m | grep x86_64`

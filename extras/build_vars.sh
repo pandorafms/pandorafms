be64e0fab0b1997b0308d54060cb585adb9eb3cd
ramonn <noreply@pandorafms.org>
2013-11-06 08:59:09 +0000

    2013-11-06  Ramon Novoa  <rnovoa@artica.es>
    	* build_vars.sh: Added console dirs to auto-create OUM packages.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9011 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- extras/build_vars.sh
+++ extras/build_vars.sh
@@ -6,3 +6,5 @@ RPMHOME=/usr/src/packages
 VERSION=$(grep 'my $pandora_version =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
 BUILD=$(grep 'my $pandora_build =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
 X86_64=`uname -m | grep x86_64`
+CONSOLEHOME=$CODEHOME/pandora_console
+CONSOLEHOME_ENT=$PANDHOME_ENT/pandora_console

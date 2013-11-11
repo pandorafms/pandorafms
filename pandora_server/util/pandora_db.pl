dab8b7beb600c89e86c0b00e8066946fbdf91fcd
pandora-release <noreply@pandorafms.org>
2013-11-11 00:28:33 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9044 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -33,7 +33,7 @@ use PandoraFMS::Tools;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "5.0 PS131110";
+my $version = "5.0 PS131111";
 
 # Pandora server configuration
 my %conf;

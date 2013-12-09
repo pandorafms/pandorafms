9276ffa32695faa71707aa1aaabe0c2eadf7e85c
mdtrooper <tres.14159@gmail.com>
2013-12-09 11:13:23 +0000

    2013-08-09 Miguel de Dios <miguel.dedios@artica.es>
    	* mobile: moved the all code of android to this folder.
    
    	
    
    	* tablet: copy the mobile coded to tablet to starting to developer
    
    	the version for android tablets.
    
    
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9191 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_agents/android/tablet/src/pandroid/agent/PandroidAgentTentacle.java
@@ -0,0 +1,204 @@
+// Pandora FMS - http://pandorafms.com
+// ==================================================
+// Copyright (c) 2005-2011 Artica Soluciones Tecnologicas
+// Please see http://pandorafms.org for full contribution list
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; version 2
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package pandroid.agent;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+
+class tentacle_client {
+
+	// Return 0 when success, -1 when error
+	public int send(String args[]) {
+
+		int port = 41121;
+		String send = null;
+		String serverResponse = null;
+		String address = "127.0.0.1";
+		byte[] data = null;
+		String parameter = "";
+		String filePath = null;
+		boolean verbose = false;
+		File file = new File("");
+
+		for (int i = 0; i < args.length; i++) {
+			if (i == (args.length - 1)) {
+				filePath = args[i];
+			}
+			else {
+				// Get the param if is -* format or empty string otherwise
+				String last_parameter = parameter;
+				parameter = isParameter(args[i]);
+
+				// If is not -* param check the previous param
+				if (parameter.equals("")) {
+					if (last_parameter.equals("-a")) {
+						address = args[i];
+					}
+					else if(last_parameter.equals("-p")) {
+						port = Integer.parseInt(args[i]);
+					}
+				}
+
+				// The solo params are checked otherwise
+				if (parameter.equals("-v")) {
+					verbose = true;
+				}
+			}
+		}
+
+		if  (filePath == null) {
+			getError("Incorrect parameters. File path is necessary.");
+		}
+
+		getInfo("\n*** Starting tentacle client ***\n",verbose);
+
+		Socket socketCliente = new Socket();
+
+		// TODO Maybe change socket to higher timeout value
+
+		try {
+			socketCliente.connect(new InetSocketAddress(address, port), 2000);
+
+		}
+		catch (UnknownHostException e) {
+			getError("Host doesn't exist");
+			return -1;
+		}
+		catch (IOException e) {
+			getError("Could not connect: The host is down");
+			return -1;
+        }
+
+
+		DataOutputStream serverOutput = null;
+
+		try {
+			serverOutput = new DataOutputStream(socketCliente.getOutputStream());
+		}
+		catch (IOException e1) {
+			getError("Could not get Data output stream");
+		}
+
+		BufferedReader serverInput = null;
+
+		try {
+			serverInput = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+		}
+		catch (IOException e1) {
+			getError("Could not get Buffered reader");
+		}
+
+
+        file = new File(filePath);
+        int size = (int) file.length();
+        data = new byte[size];
+        try {
+             BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+             buf.read(data, 0, data.length);
+             buf.close();
+        }
+        catch (FileNotFoundException e) {
+             getError("File not found");
+        }
+        catch (IOException e) {
+             getError("Could not read from file");
+        }
+
+		getInfo("*** Start of transference ***\n",verbose);
+		// Send the file name and length
+		try {
+			send = "SEND <" + file.getName() + "> SIZE " + Integer.toString(data.length) + '\n';
+			getInfo("Client -> Server: " + send, verbose);
+			serverOutput.writeBytes(send);
+		}
+		catch (IOException e) {
+			getError("Could not write on server");
+		}
+		try {
+			serverResponse = serverInput.readLine();
+		}
+		catch (IOException e) {
+			getError("Could not get server response");
+		}
+
+		getInfo("Server -> Client: " + serverResponse + "\n", verbose);
+		if (serverResponse != null && serverResponse.equals("SEND OK")) {
+			try {
+				getInfo("Client -> Server: [file data]\n", verbose);
+                serverOutput.write(data);
+
+			} catch (IOException e) {
+				getError("Could not write on server");
+			}
+			try {
+				serverResponse = serverInput.readLine();
+			} catch (IOException e) {
+				getError("Could not get server response");
+			}
+
+			getInfo("Server -> Client: " + serverResponse + "\n", verbose);
+			if (serverResponse != null && serverResponse.equals("SEND OK")) {
+				try {
+					send = "QUIT\n";
+					getInfo("Client -> Server: " + send, verbose);
+					serverOutput.writeBytes("QUIT\n");
+				} catch (IOException e) {
+					getError("Could not write on server");
+				}
+				getInfo("*** End of transference ***\n", verbose);
+			}
+			else {
+				getError("Bad server response, execution aborted.\n");
+			}
+		}
+		else {
+			getError("Bad server response, execution aborted.\n");
+		}
+
+		return 0;
+	}
+
+	private String isParameter(String str) {
+		if(str.equals("-a") || str.equals("-p") || str.equals("-v")) {
+			return str;
+		}
+		else {
+			return "";
+		}
+	}
+
+	private void getError(String error_str) {
+		log("[ERROR] " + error_str + '\n');
+	}
+
+	private void getInfo(String error_str, boolean verbose) {
+		if(verbose) {
+			log("[INFO] " + error_str + '\n');
+		}
+	}
+
+	private void log (String msg) {
+		//Log.e("Tentacle",msg);
+		//Context context = getApplicationContext();
+		//int duration = Toast.LENGTH_SHORT;
+
+		//Toast toast = Toast.makeText(context, msg, duration);
+		//toast.show();
+	}
+}
---
+++ pandora_agents/android/tablet/src/pandroid/agent/Status.java
@@ -0,0 +1,380 @@
+// Pandora FMS - http://pandorafms.com
+// ==================================================
+// Copyright (c) 2005-2011 Artica Soluciones Tecnologicas
+// Please see http://pandorafms.org for full contribution list
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; version 2
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details. 
+
+package pandroid.agent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Date;
+
+
+public class Status  extends Activity {
+	Handler h = new Handler();
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if(Core.hasSim)
+			setContentView(R.layout.status);
+		else
+			setContentView(R.layout.statusnosim);
+
+		//        Core.loadLastValues(getApplicationContext());
+		//        showLastValues();
+		//        updateLastContactInfo();
+		//setButtonEvents();
+	}
+	//TODO maybe remove duplicate from onCreate
+	@Override
+	public void onResume(){
+		super.onResume();
+
+		if(Core.hasSim)
+			setContentView(R.layout.status);
+		else
+			setContentView(R.layout.statusnosim);
+
+		new GetLastValuesAsyncTask().execute();
+
+		updateLastContactInfo();
+
+	}
+
+   
+
+	public void onStart(){
+		super.onStart();
+
+		// Update the UI each second 
+		h.post(new Runnable() {
+			@Override
+			public void run() {
+
+				new GetLastValuesAsyncTask().execute();
+
+				updateLastContactInfo();
+
+				h.postDelayed(this, 1000);
+			}
+		});
+	}
+
+	//For options
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Intent i;
+		switch (item.getItemId()) {
+		case R.id.help_button_menu_options:
+			i = new Intent(this, Help.class);
+			startActivity(i);
+			break;
+		case R.id.about_button_menu_options:
+			i = new Intent(this, About.class);
+			startActivity(i);
+			break;
+		}
+		return true;
+	}
+
+	public class GetLastValuesAsyncTask extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			Core.loadLastValues(getApplicationContext());
+			return null;
+		}
+
+
+		@Override
+		protected void onPostExecute(Void unused)
+		{
+			showLastValues();
+		}
+
+
+	}// end onPostExecute
+
+
+
+	private void updateLastContactInfo() {
+		long lastContact = Core.lastContact;
+		int contactError = Core.contactError;
+		boolean alarmEnabled = Core.alarmEnabled;
+		Date date = new Date();
+		long timestamp = date.getTime() / 1000;
+		long timeAgo = -1;
+
+		//loading until error or connects
+		TextView lastContactInfo = (TextView) this.findViewById(R.id.lastContactInfo_label_str);
+		lastContactInfo.setTextColor(Color.parseColor("#FF0000"));
+		lastContactInfo.setText(getString(R.string.loading));
+
+		if(lastContact != -1){
+			timeAgo = timestamp - lastContact;
+		}
+
+		int interval = Core.interval;
+
+		if(timeAgo >= interval) {
+			timeAgo = 0;
+		}
+
+		String stringAgo = "";
+
+		if(!alarmEnabled){
+			lastContactInfo = (TextView) this.findViewById(R.id.lastContactInfo_label_str);
+			lastContactInfo.setTextColor(Color.parseColor("#FF0000"));
+			lastContactInfo.setText(getString(R.string.contact_stopped_str));
+		}
+
+		else if(contactError == 1) {
+			lastContactInfo = (TextView) this.findViewById(R.id.lastContactInfo_label_str);
+			lastContactInfo.setTextColor(Color.parseColor("#FF0000"));
+			lastContactInfo.setText(getString(R.string.conctact_error_str));
+
+		}
+		else if(lastContact == -1) {
+			stringAgo = getString(R.string.never_str);
+		}
+		else if(timeAgo == 0) {
+			stringAgo = getString(R.string.now_str);
+		}
+		//TODO
+		else if(contactError == 0) {
+			stringAgo = timeAgo + " " + getString(R.string.seconds_str);
+			lastContactInfo = (TextView) this.findViewById(R.id.lastContactInfo_label_str);
+			lastContactInfo.setTextColor(Color.parseColor("#00FF00"));
+			lastContactInfo.setText(getString(R.string.last_contact_str) + stringAgo);
+		}
+
+	}//end updateLastContactInfo
+
+	//Set layout values to current
+	private void showLastValues() {
+
+		// latitude
+		TextView textView = (TextView)findViewById(R.id.latitude_value_str);
+		textView.setText("");
+		if (Core.latitude != Core.CONST_INVALID_COORDS) {
+			textView.setText("" + Core.latitude);
+		}
+		// longitude
+		textView = (TextView)findViewById(R.id.longitude_value_str);
+		textView.setText("");
+		if (Core.longitude != Core.CONST_INVALID_COORDS) {
+			textView.setText("" + Core.longitude);
+		}
+		// battery level
+		textView = (TextView)findViewById(R.id.battery_value_str);
+		textView.setText("");
+		if (Core.batteryLevel != Core.CONST_INVALID_BATTERY_LEVEL) {
+			textView.setText("" + Core.batteryLevel);
+		}
+
+
+		/*
+		textView = (TextView)findViewById(R.id.orientation_value_str);
+		textView.setText("");
+
+		if (Core.orientation != Core.CONST_INVALID_ORIENTATION) {
+			textView.setText("" + Core.orientation);
+		}
+
+		textView = (TextView)findViewById(R.id.proximity_value_str);
+		textView.setText("");
+		if (Core.proximity != Core.CONST_INVALID_PROXIMITY) {
+			textView.setText("" + Core.proximity);
+		}
+		 */
+
+		// task
+		textView = (TextView)findViewById(R.id.task_value_str);
+		textView.setText("");
+		if (Core.taskStatus.equals("enabled") && Core.taskHumanName.length() != 0) {
+			String text = Core.taskHumanName + " ( " + Core.task + " ): ";
+			if (Core.taskRun.equals("true")) {
+				text = text + getString(R.string.running);
+			}
+			else {
+				text = text + getString(R.string.stopped);
+			}
+			textView.setText(text);		
+		}
+
+		// freeMemory
+		textView = (TextView)findViewById(R.id.memory_value_str);
+		textView.setText("");
+		if (Core.memoryStatus.equals("enabled")) {
+			String textMemory = getString(R.string.memory_avaliable_str);
+			textMemory = textMemory.replaceFirst("%i", "" + Core.availableRamKb);
+			textMemory = textMemory.replaceFirst("%i", "" + Core.totalRamKb);
+			textView.setText(textMemory);
+		}
+		// upTime
+		textView = (TextView)findViewById(R.id.uptime_value);
+		textView.setText("");
+		if (Core.upTime != 0) {
+			textView.setText("" + Core.upTime+" "+ getString(R.string.seconds));
+		}
+
+		//Only set values if sim present
+		//if no sim these views aren't present as different layout is loaded
+		if (Core.hasSim) {
+			// simID
+			textView = (TextView)findViewById(R.id.sim_id_value);
+			textView.setText("");
+			textView.setText("" + Core.simID);
+
+			// networkOperator
+			textView = (TextView)findViewById(R.id.network_operator_value);
+			textView.setText("");
+			if (Core.networkOperator != null) {
+				textView.setText("" + Core.networkOperator);
+			}
+
+			// networkType
+			textView = (TextView)findViewById(R.id.network_type_value);
+			textView.setText("");
+			if (Core.networkType != null) {
+				textView.setText("" + Core.networkType);
+			}
+
+			// phoneType
+			textView = (TextView)findViewById(R.id.phone_type_value);
+			textView.setText("");
+            if(Core.phoneType != null)
+				textView.setText("" + Core.phoneType);
+
+			// signalStrength
+			textView = (TextView)findViewById(R.id.signal_strength_value);
+			textView.setText("");
+			if(Core.signalStrength != 0)
+				textView.setText("" + Core.signalStrength+"dB");
+
+			// SMSReceived
+			textView = (TextView)findViewById(R.id.sms_received_value);
+			textView.setText("");
+			textView.setText("" + Core.SMSReceived);
+
+			// SMSSent
+			textView = (TextView)findViewById(R.id.sms_sent_value);
+			textView.setText("");
+			textView.setText("" + Core.SMSSent);
+
+			// incomingCalls
+			textView = (TextView)findViewById(R.id.incoming_calls_value);
+			textView.setText("");
+			textView.setText("" + Core.incomingCalls);
+
+			// missedCalls
+			textView = (TextView)findViewById(R.id.missed_calls_value);
+			textView.setText("");
+			textView.setText("" + Core.missedCalls);
+
+			// outgoingCalls
+			textView = (TextView)findViewById(R.id.outgoing_calls_value);
+			textView.setText("");
+			textView.setText("" + Core.outgoingCalls);
+
+			// receiveBytes
+			textView = (TextView)findViewById(R.id.receive_bytes_value);
+			textView.setText("");
+			textView.setText("" + Core.receiveBytes);
+
+			// transmiteBytes
+			textView = (TextView)findViewById(R.id.transmit_bytes_value);
+			textView.setText("");
+			textView.setText("" + Core.transmitBytes);
+
+			// roaming
+			textView = (TextView)findViewById(R.id.roaming_value);
+			textView.setText("");
+			textView.setText("" + Core.roaming);
+
+		}//end simID if
+
+	}
+
+	//For debugging
+
+	/* For debugging
+	private void setButtonEvents() {
+        // Set update button events
+        Button start = (Button) findViewById(R.id.start);
+        Button xml = (Button) findViewById(R.id.get_xml);
+        Button hidexml = (Button) findViewById(R.id.hide_xml);
+        Button stop = (Button) findViewById(R.id.stop);
+
+
+        xml.setOnClickListener(new OnClickListener() {
+        	public void onClick(View view) {
+        		updateLastXML();
+        	}
+        });
+
+        hidexml.setOnClickListener(new OnClickListener() {
+        	public void onClick(View view) {
+        		hideLastXML();
+        	}
+        });
+
+        stop.setOnClickListener(new OnClickListener() {
+        	public void onClick(View view) {
+        		Core.stopAgentListener();
+        	}
+        });
+
+        start.setOnClickListener(new OnClickListener() {
+        	public void onClick(View view) {
+        		Core.restartAgentListener(getApplicationContext());
+        	}
+        });
+
+    }//end button events
+
+
+
+	private void updateLastXML() {
+
+		TextView xml = (TextView) this.findViewById(R.id.xml);
+		SharedPreferences agentPreferences = PandroidAgent.getSharedPrefs();
+
+		String lastXML = agentPreferences.getString("lastXML", "[no data]");
+		xml.setText("Last XML builded: \n\n" + lastXML);
+
+	}
+	private void hideLastXML(){
+		TextView xml = (TextView) this.findViewById(R.id.xml);
+		xml.setText("");
+	}
+
+	 */
+}

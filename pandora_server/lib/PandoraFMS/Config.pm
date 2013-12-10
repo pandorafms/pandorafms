56fb64dcb184009645f66e2e481e80568c56a498
pandora-release <noreply@pandorafms.org>
2013-12-10 17:27:20 +0000

    2013-12-10  Sancho Lerena <slerena@artica.es>
            * Config.pm: Updated to 4.1.1 version string (RELEASE CHANGE)
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@9203 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -41,7 +41,7 @@ our @EXPORT = qw(
 	);
 
 # version: Defines actual version of Pandora Server for this module only
-my $pandora_version = "4.1";
+my $pandora_version = "4.1.1";
 my $pandora_build = "131210";
 our $VERSION = $pandora_version." ".$pandora_build;
 

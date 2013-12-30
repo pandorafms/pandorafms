ab571632a9b0a06652eac9c5fb3eedac063b46e6
pandora-release <noreply@pandorafms.org>
2013-12-30 00:41:20 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9272 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/unix/DEBIAN/make_deb_package.sh
+++ pandora_agents/unix/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="5.0SP2-131229"
+pandora_version="5.0SP2-141230"
 
 echo "Test if you has the tools for to make the packages."
 whereis dpkg-deb | cut -d":" -f2 | grep dpkg-deb > /dev/null

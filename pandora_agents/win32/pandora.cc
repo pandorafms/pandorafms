8d641e2a0950424dcead8f952dbf560dbd4e176e
pandora-release <noreply@pandorafms.org>
2014-01-22 00:47:16 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9350 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("5.0SP2(Build 140121)")
+#define PANDORA_VERSION ("5.0SP2(Build 140122)")
 
 string pandora_path;
 string pandora_dir;

d0459270e9a11d317c6972e728cd47b8363708cf
pandora-release <noreply@pandorafms.org>
2014-01-28 00:49:49 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9378 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/DEBIAN/make_deb_package.sh
+++ pandora_console/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="5.0SP2-140127"
+pandora_version="5.0SP2-140128"
 
 package_pear=0
 package_pandora=1
--- pandora_server/pandora_server.redhat.spec
+++ pandora_server/pandora_server.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     5.0SP2
-%define release     140127
+%define release     140128
 
 Summary:            Pandora FMS Server
 Name:               %{name}

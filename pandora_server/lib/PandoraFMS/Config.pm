eae01c48d3dcdecc5037b9211ff0ae59899b5f06
pandora-release <noreply@pandorafms.org>
2014-02-07 00:49:55 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9410 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -43,7 +43,7 @@ our @EXPORT = qw(
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "5.0SP3";
-my $pandora_build = "140206";
+my $pandora_build = "140207";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash

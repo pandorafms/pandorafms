0589f7c38cbe245e8dcc9137daead89ed28a2871
pandora-release <noreply@pandorafms.org>
2014-02-11 00:55:11 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@9426 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC140210';
+$build_version = 'PC140211';
 $pandora_version = 'v4.1.1';
 
 // Do not overwrite default timezone set if defined.
--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -42,7 +42,7 @@ our @EXPORT = qw(
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "4.1.1";
-my $pandora_build = "140210";
+my $pandora_build = "140211";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash

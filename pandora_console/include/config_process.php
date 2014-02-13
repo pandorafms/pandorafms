086872c20b80544e25fb4cf7582902306ff60573
pandora-release <noreply@pandorafms.org>
2014-02-13 00:49:58 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9434 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC140212';
+$build_version = 'PC140213';
 $pandora_version = 'v5.0SP3';
 
 // Do not overwrite default timezone set if defined.

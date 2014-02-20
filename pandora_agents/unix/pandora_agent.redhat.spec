24da3324e7067f1d7111754185fb75d09d969a10
pandora-release <noreply@pandorafms.org>
2014-02-20 00:50:21 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9449 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/unix/pandora_agent.redhat.spec
+++ pandora_agents/unix/pandora_agent.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_agent_unix
 %define version     5.0SP3
-%define release     140217
+%define release     140220
 
 Summary:            Pandora FMS Linux agent, PERL version
 Name:               %{name}
--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="5.0SP3-140217"
+pandora_version="5.0SP3-140220"
 
 package_cpan=0
 package_pandora=1

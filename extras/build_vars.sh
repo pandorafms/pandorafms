b2cc0d27478181e8dcbc16a1e9c92c046a66afa9
pandora-release <noreply@pandorafms.org>
2014-02-24 14:59:25 +0000

    Reverted the previous commit. This changes belong into the 5.0 branch.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9463 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- extras/build_vars.sh
+++ extras/build_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CODEHOME=/root/code/pandora/branches/pandora_5.0
+CODEHOME=/root/code/pandora/trunk
 CODEHOME_ENT=/root/code/artica/code
-PANDHOME_ENT=$CODEHOME_ENT/pandora/branches/5.0
+PANDHOME_ENT=$CODEHOME_ENT/pandora/trunk
 RPMHOME=/usr/src/packages
 VERSION=$(grep 'my $pandora_version =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')
 BUILD=$(grep 'my $pandora_build =' $CODEHOME/pandora_server/lib/PandoraFMS/Config.pm | awk '{print substr($4, 2, length($4) - 3)}')

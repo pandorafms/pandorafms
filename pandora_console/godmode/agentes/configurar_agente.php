6df29def51777348f3d0e05aeaf71001f7019afb
vgilc <noreply@pandorafms.org>
2014-03-17 12:32:48 +0000

    2014-03-17  Vanessa Gil <vanessa.gil@artica.es>
    	* pandoradb.sql
    	  extras/pandoradb_migrate_5.0_to_5.1.mysql.sql
    	  godmode/agentes/configurar_agente.php: Fixed bug:
    	'post_process' out of range.
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9604 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/godmode/agentes/configurar_agente.php
+++ pandora_console/godmode/agentes/configurar_agente.php
@@ -741,7 +741,7 @@ if ($update_module || $create_module) {
 	// where are very big and PHP uses scientific notation, p.e:
 	// 1.23E-10 is 0.000000000123
 	
-	$post_process = (string) get_parameter ('post_process', 0.0);
+	$post_process = get_parameter ('post_process', 0);
 	$prediction_module = 0;
 	$max_timeout = (int) get_parameter ('max_timeout');
 	$max_retries = (int) get_parameter ('max_retries');

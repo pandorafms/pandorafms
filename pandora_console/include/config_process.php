9b1e4ba8679be4dff5d5bc8b2ff5601d6b50d65f
pandora-release <noreply@pandorafms.org>
2014-03-21 00:33:57 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9624 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC140318';
+$build_version = 'PC140321';
 $pandora_version = 'v5.1dev';
 
 // Do not overwrite default timezone set if defined.
--- pandora_server/pandora_server.spec
+++ pandora_server/pandora_server.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     5.1dev
-%define release     140318
+%define release     140321
 
 Summary:            Pandora FMS Server
 Name:               %{name}

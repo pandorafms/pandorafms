6f22f8db6b70cb0b5629034f9737c2bcc8feb2ea
vgilc <noreply@pandorafms.org>
2014-03-27 11:31:01 +0000

    2014-03-27  Vanessa Gil <vanessa.gil@artica.es>
    	* images/help/custom_oid2.png: Changed image.
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9654 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_field_match_snmp.php
+++ pandora_console/include/help/en/help_field_match_snmp.php
@@ -33,7 +33,7 @@ That is, if we want to make an alert seeking values ​​"Uno" in the first var
 	<p>Alert generated (an internal audit) will have this text:</p>
  
 <p>SNMP Alert of 192.168.5.2 with OID .1.3.6.1.4.1.2789.2005
-Varbind 1: “cien” Varbind 3: “tres” Varbind 5: “veinte” Varbind 7: “sesenta”</p>
+Varbind 100: “cien” Varbind 3: “tres” Varbind 20: “veinte” Varbind 60: “sesenta”</p>
 
 <p>Thus, if the trap has 200 variables, you can use up to 20 filters variables (varbinds) and take the value of up to 20 variables, regardless if they are in the 10, 50, or 170 position.</p>
 </tr>

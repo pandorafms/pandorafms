69c762dd336ea11fdc70b55bf4e4ef198886cc4d
vgilc <vanessa.gil@artica.es>
2014-03-27 11:21:48 +0000

    2014-03-27  Vanessa Gil <vanessa.gil@artica.es>
    	* extras/pandoradb_migrate_5.0_to_5.1.mysql.sql:
    	Fixed error with 'talert_snmp' order fields.
    
    	* include/help/en/help_field_match_snmp.php
    	  include/help/es/help_field_match_snmp.php: Changed help.
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9653 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_field_match_snmp.php
+++ pandora_console/include/help/en/help_field_match_snmp.php
@@ -6,7 +6,7 @@
 
 <h1>Custom OID/Data #1-20</h1>
 
-<p>These are regular expressions that try to match binding variables 1 to 20. If there is a match, the alert is fired. The value of the variable is stored in the corresponding _snmp_fx_ macro (_snmp_f1_, _snmp_f2_, ...). Even though only twenty variables can be searched for matches, _snmp_fx_ macros are set for all of them (_snmp_f11_, _snmp_f12_, ...).</p>
+<p>These are regular expressions that try to match varbinds 1 to 20. If there is a match, the alert is fired. The value of the variable is stored in the corresponding _snmp_fx_ macro (_snmp_f1_, _snmp_f2_, ...). Even though only twenty variables can be searched for matches, _snmp_fx_ macros are set for all of them (_snmp_f11_, _snmp_f12_, ...).</p>
 
 
 <table width=100%>
@@ -30,11 +30,11 @@ That is, if we want to make an alert seeking values ​​"Uno" in the first var
 	<img src="../images/help/trap.png" width='520' height='220'>
 </tr>
 <tr>
-	<p>Alert generated (an internal audit) will have this text:
+	<p>Alert generated (an internal audit) will have this text:</p>
  
-SNMP Alert of 192.168.5.2 with OID .1.3.6.1.4.1.2789.2005
-Binding 1: “uno” Binding 3: “tres” Binding 5: “cinco” Binding 7: “siete”
+<p>SNMP Alert of 192.168.5.2 with OID .1.3.6.1.4.1.2789.2005
+Varbind 1: “cien” Varbind 3: “tres” Varbind 5: “veinte” Varbind 7: “sesenta”</p>
 
-Thus, if the trap has 200 variables, you can use up to 20 filters variables (bindings) and take the value of up to 20 variables, regardless if they are in the 10, 50, or 170 position.</p>
+<p>Thus, if the trap has 200 variables, you can use up to 20 filters variables (varbinds) and take the value of up to 20 variables, regardless if they are in the 10, 50, or 170 position.</p>
 </tr>
 </table>

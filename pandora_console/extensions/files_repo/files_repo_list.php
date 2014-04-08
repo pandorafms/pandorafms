347e24e76182cc31b0556db97c617b871601dc24
alexhigh <alex@artica.es>
2014-04-08 16:26:23 +0000

    2014-04-08  Alejandro Gallardo <alejandro.gallardo@artica.es>
    	* include/functions.php: Added the functions
    	"get_file_upload_status" and "translate_file_upload_status".
    
    	* extensions/files_repo.php,
    	  extensions/files_repo/functions_files_repo.php,
    	  extensions/files_repo/files_repo_list.php,
    	  extensions/files_repo/files_repo_form.php,
    	  extensions/files_repo/sql/files_repo.sql,
    	  extensions/files_repo/sql/files_repo.postgreSQL.sql,
    	  extensions/files_repo/sql/files_repo.oracle.sql: Added
    	new files. New extension "Files repository".
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9741 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/extensions/files_repo/files_repo_list.php
@@ -0,0 +1,91 @@
+<?php
+
+// Pandora FMS - http://pandorafms.com
+// ==================================================
+// Copyright (c) 2005-2011 Artica Soluciones Tecnologicas
+// Please see http://pandorafms.org for full contribution list
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+global $config;
+
+$full_extensions_dir = $config['homedir'].DIRECTORY_SEPARATOR.EXTENSIONS_DIR.DIRECTORY_SEPARATOR;
+require_once ($full_extensions_dir."files_repo".DIRECTORY_SEPARATOR."functions_files_repo.php");
+
+$offset = (int) get_parameter('offset');
+$filter = array();
+$filter['limit'] = $config['block_size'];
+$filter['offset'] = $offset;
+$filter['order'] = array('field' => 'id', 'order' => 'DESC');
+$files = files_repo_get_files($filter);
+
+if (!empty($files)) {
+
+	echo "<br>";
+
+	// Pagination
+	if ($manage) {
+		$url = ui_get_full_url("index.php?sec=gextensions&sec2=extensions/files_repo");
+	} else {
+		$url = ui_get_full_url("index.php?sec=extensions&sec2=extensions/files_repo");
+	}
+	$total_files = files_repo_get_files(false, true);
+	ui_pagination($total_files, $url, $offset);
+
+	$table = new stdClass();
+	$table->width = '98%';
+	$table->style = array();
+	$table->style[1] = "max-width: 200px;";
+	$table->style[2] = "text-align: center;";
+	$table->style[3] = "text-align: center;";
+	$table->style[4] = "text-align: center;";
+	$table->head = array();
+	$table->head[0] = __("Name");
+	$table->head[1] = __("Description");
+	$table->head[2] = __("Size");
+	$table->head[3] = __("Last modification");
+	$table->head[4] = "";
+	$table->data = array();
+
+	foreach ($files as $file_id => $file) {
+		$data = array();
+
+		// Prepare the filename for the get_file.php script
+		$relative_path = str_replace($_SERVER['DOCUMENT_ROOT'], '', $file['location']);
+		$file_path = base64_encode($relative_path);
+		$hash = md5($relative_path . $config['dbpass']);
+		$url = ui_get_full_url("include/get_file.php?file=$file_path&hash=$hash");
+
+		$data[0] = "<a href=\"$url\" target=\"_blank\">" . $file['name'] . "</a>"; // Name
+		$data[1] = $file['description']; // Description
+		$data[2] = ui_format_filesize($file['size']); // Size
+		$data[3] = date('F j, Y - H:m', $file['mtime']); // Last modification
+		$data[4] = "<a href=\"$url\" target=\"_blank\">";
+		$data[4] .= html_print_image('images/download.png', true, array('title' => __('Download'))); // Download image
+		$data[4] .= "</a>";
+		if ($manage) {
+			$url = ui_get_full_url("index.php?sec=gextensions&sec2=extensions/files_repo&file_id=$file_id");
+			$data[4] .= " <a href=\"$url\">";
+			$data[4] .= html_print_image('images/config.png', true, array('title' => __('Edit'))); // Edit image
+			$data[4] .= "</a>";
+			$url = ui_get_full_url("index.php?sec=gextensions&sec2=extensions/files_repo&delete=1&file_id=$file_id");
+			$data[4] .= " <a href=\"$url\" onClick=\"if (!confirm('".__('Are you sure?')."')) return false;\">";
+			$data[4] .= html_print_image('images/cross.png', true, array('title' => __('Delete'))); // Delete image
+			$data[4] .= "</a>";
+		}
+		$table->data[] = $data;
+	}
+	html_print_table($table);
+
+} else {
+	ui_print_info_message(__('No items'));
+}
+
+?>
---
+++ pandora_console/extensions/files_repo/functions_files_repo.php
@@ -0,0 +1,279 @@
+<?php
+
+// Pandora FMS - http://pandorafms.com
+// ==================================================
+// Copyright (c) 2005-2011 Artica Soluciones Tecnologicas
+// Please see http://pandorafms.org for full contribution list
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+
+function files_repo_check_directory ($print_messages = false) {
+	global $config;
+
+	$attachment_path = realpath($config['attachment_store']);
+	$files_repo_path = $attachment_path.DIRECTORY_SEPARATOR."files_repo";
+
+	$result = false;
+	$messages = "";
+
+	// attachment/ check
+	if (!is_writable($attachment_path)) {
+		$messages .= ui_print_error_message(array('message' => __('Attachment directory is not writable by HTTP Server')
+			.'</h3>'.'<p>'.__('Please check that the web server has write rights on the {HOMEDIR}/attachment directory'),
+				'no_close' => true, 'force_style' => 'color: #000000 !important'), '', true);
+	} else {
+		// attachment/agent_packages/ check
+		if (!file_exists($files_repo_path) || !is_writable($files_repo_path)) {
+			// Create the directoty if not exist
+			if (!file_exists($files_repo_path)) {
+				mkdir($files_repo_path);
+			}
+			if (!is_writable($files_repo_path)) {
+				$messages .= ui_print_error_message(array('message' => __('Attachment directory is not writable by HTTP Server')
+					.'</h3>'.'<p>'.__('Please check that the web server has write rights on the {HOMEDIR}/attachment directory'),
+						'no_close' => true, 'force_style' => 'color: #000000 !important'), '', true);
+			} else {
+				$result = true;
+			}
+		} else {
+			$result = true;
+		}
+	}
+
+	if ($print_messages) {
+		echo $messages;
+	}
+	return $result;
+}
+
+function files_repo_check_file_acl ($file_id, $user_id = false, $file_groups = false, $user_groups = false) {
+	global $config;
+
+	$result = false;
+	if (!$user_id) {
+		$user_id = $config['id_user'];
+	}
+	if (is_user_admin($user_id)) {
+		return true;
+	}
+	if (!$file_groups) {
+		$file_groups = files_repo_get_file_groups($file_id);
+		if (empty($file_groups)) {
+			$file_groups = array();
+		}
+	}
+	if (!$user_groups) {
+		$user_groups = users_get_groups ($user_id, false, true);
+		if (empty($user_groups)) {
+			$user_groups = array();
+		}
+	}
+
+	foreach ($file_groups as $group_id) {
+		// $user_groups has the id in the array keys
+		if (in_array($group_id, $user_groups)) {
+			$result = true;
+			break;
+		}
+	}
+	
+	return $result;
+}
+
+function files_repo_get_file_groups ($file_id) {
+
+	$groups = array();
+	$filter = array('id_file' => $file_id);
+	$result = db_get_all_rows_filter("tfiles_repo_group", $filter, "id_group");
+
+	if (!empty($result)) {
+		foreach ($result as $key => $value) {
+			$groups[] = $value['id_group'];
+		}
+	}
+
+	return $groups;
+}
+
+function files_repo_get_user_groups ($user_id) {
+
+	$groups = array();
+	$filter = array('id_usuario' => $user_id);
+	$result = db_get_all_rows_filter("tusuario_perfil", $filter, "id_grupo");
+
+	if (!empty($result)) {
+		foreach ($result as $key => $value) {
+			$groups[] = $value['id_grupo'];
+		}
+	}
+
+	return $groups;
+}
+
+function files_repo_get_files ($filter = false, $count = false) {
+	global $config;
+
+	// Don't use the realpath for the download links!
+	$files_repo_path = $config['attachment_store'].DIRECTORY_SEPARATOR."files_repo";
+
+	$sql = "SELECT * FROM tfiles_repo " . db_format_array_where_clause_sql($filter, "AND", "WHERE");
+	$files = db_get_all_rows_sql($sql);
+
+	if ($files === false)
+		$files = array();
+
+	$user_groups = files_repo_get_user_groups($config['id_user']);
+
+	$files_data = array();
+	foreach ($files as $file) {
+
+		$file_groups = files_repo_get_file_groups($file['id']);
+		$permission = files_repo_check_file_acl ($file['id'], $config['id_user'], $file_groups, $user_groups);
+		if (!$permission) {
+			continue;
+		}
+
+		$data = array();
+		$data['name'] = $file['name'];
+		$data['description'] = $file['description'];
+		$data['location'] = $files_repo_path.DIRECTORY_SEPARATOR.$file['id']."_".$data['name'];
+		// Size in bytes
+		$data['size'] = filesize($data['location']);
+		// Last modification time in unix timestamp
+		$data['mtime'] = filemtime($data['location']);
+		$data['groups'] = $file_groups;
+		$files_data[$file['id']] = $data;
+	}
+
+	if ($count) {
+		$files_data = count($files_data);
+	}
+	
+	return $files_data;
+}
+
+function files_repo_add_file ($file_input_name = "upfile", $description = "", $groups = array()) {
+	global $config;
+
+	$attachment_path = realpath($config['attachment_store']);
+	$files_repo_path = $attachment_path.DIRECTORY_SEPARATOR."files_repo";
+
+	$result = array();
+	$result["status"] = false;
+	$result["message"] = "";
+
+	$upload_status = get_file_upload_status($file_input_name);
+	$upload_result = translate_file_upload_status($upload_status);
+
+	if ($upload_result === true) {
+		$filename = $_FILES[$file_input_name]['name'];
+		// Replace conflictive characters
+		$filename = str_replace (array(" ", "=", "?", "&"), "_", $filename);
+		$filename = filter_var($filename, FILTER_SANITIZE_URL);
+		// The filename should not be larger than 200 characters
+		if (mb_strlen($filename, "UTF-8") > 200) {
+			$filename = mb_substr($filename, 0, 200, "UTF-8");
+		}
+
+		$values = array(
+				'name' => $filename,
+				'description' => $description
+			);
+		$file_id = db_process_sql_insert('tfiles_repo', $values);
+
+		if ($file_id) {
+			
+			$file_tmp = $_FILES[$file_input_name]['tmp_name'];
+			$destination = $files_repo_path.DIRECTORY_SEPARATOR.$file_id."_".$filename;
+			
+			if (move_uploaded_file($file_tmp, $destination)) {
+
+				if (is_array($groups) && !empty($groups)) {
+					db_process_sql_delete('tfiles_repo_group', array('id_file' => $file_id));
+					foreach ($groups as $group) {
+						$values = array(
+								'id_file' => $file_id,
+								'id_group' => $group
+							);
+						db_process_sql_insert('tfiles_repo_group', $values);
+					}
+				}
+				
+				$result["status"] = true;
+			} else {
+				db_process_sql_delete('tfiles_repo', array('id' => $file_id));
+				unlink ($file_tmp);
+				$result["message"] = __('The file could not be copied');
+			}
+			
+		} else {
+			$result["message"] = __('There was an error creating the file');
+		}
+	} else {
+		$result["message"] = $upload_result;
+	}
+
+	return $result;
+}
+
+function files_repo_update_file ($file_id, $description = "", $groups = array()) {
+	global $config;
+
+	$result = array();
+	$result["status"] = false;
+	$result["message"] = "";
+
+	$values = array('description' => $description);
+	$filter = array('id' => $file_id);
+	$res = db_process_sql_update('tfiles_repo', $values, $filter);
+	if ($res !== false) {
+		if (is_array($groups) && !empty($groups)) {
+			db_process_sql_delete('tfiles_repo_group', array('id_file' => $file_id));
+			foreach ($groups as $group) {
+				$values = array(
+						'id_file' => $file_id,
+						'id_group' => $group
+					);
+				db_process_sql_insert('tfiles_repo_group', $values);
+			}
+		}
+		$result["status"] = true;
+	} else {
+		$result["message"] = __('There was an error updating the file');
+	}
+
+	return $result;
+}
+
+function files_repo_delete_file ($file_id) {
+	global $config;
+
+	$result = -1;
+
+	$filename = db_get_value("name", "tfiles_repo", "id", $file_id);
+
+	if ($filename) {
+		$attachment_path = realpath($config['attachment_store']);
+		$files_repo_path = $attachment_path.DIRECTORY_SEPARATOR."files_repo";
+		$location = $files_repo_path.DIRECTORY_SEPARATOR.$file_id."_".$filename;
+
+		if (file_exists($location)) {
+			$result = false;
+			if (unlink($location)) {
+				$result = (bool) db_process_sql_delete('tfiles_repo', array('id' => $file_id));
+			}
+		}
+	}
+	
+	return $result;
+}
+
+?>

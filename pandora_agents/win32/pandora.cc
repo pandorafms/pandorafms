e40c835c4f6c8df549bddfc494ea483c208a0b1d
pandora-release <noreply@pandorafms.org>
2014-05-01 23:37:31 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@9839 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("5.1dev(Build 140501)")
+#define PANDORA_VERSION ("5.1dev(Build 140502)")
 
 string pandora_path;
 string pandora_dir;
--- pandora_console/pandora_console.redhat.spec
+++ pandora_console/pandora_console.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     5.1dev
-%define release     140501
+%define release     140502
 
 # User and Group under which Apache is running
 %define httpd_name  httpd

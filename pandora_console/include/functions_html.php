ebef3f11e0b17bce2b082f3a8dbcf65d020f785a
jsatoh <junichi@rworks.jp>
2014-05-17 10:57:41 +0000

    2014-05-17  Junichi Satoh  <junichi@rworks.jp>
    	* include/functions_html.php: Fixed the PHP warning.
    
    	MERGED FROM TRUNK.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_5.0@9947 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/functions_html.php
+++ pandora_console/include/functions_html.php
@@ -1404,6 +1404,7 @@ function html_print_table (&$table, $return = false) {
 			if (isset ($table->headstyle[$key])) {
 				$headStyle = ' style = "' . $table->headstyle[$key] . '" ';
 			}
+			else $headStyle = '';
 	
 			$output .= '<th class="'.$table->headclass[$key].'" ' . $headColspan . $headStyle . ' scope="col">'. $heading .'</th>';
 		}

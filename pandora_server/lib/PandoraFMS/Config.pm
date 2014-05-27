ac06e41be1ce49ea0fb12e0a9f70c8945ea19be2
pandora-release <noreply@pandorafms.org>
2014-05-27 23:41:50 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@10024 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -43,7 +43,7 @@ our @EXPORT = qw(
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "5.1dev";
-my $pandora_build = "140527";
+my $pandora_build = "140528";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash
--- pandora_server/pandora_server.redhat.spec
+++ pandora_server/pandora_server.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     5.1dev
-%define release     140527
+%define release     140528
 
 Summary:            Pandora FMS Server
 Name:               %{name}

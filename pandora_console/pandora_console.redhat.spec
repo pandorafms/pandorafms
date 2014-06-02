03dc21f1677db5820b411b0a6af1cd12622f08e9
pandora-release <noreply@pandorafms.org>
2014-06-02 23:45:28 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@10082 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/pandora_console.redhat.spec
+++ pandora_console/pandora_console.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     5.1dev
-%define release     140602
+%define release     140603
 
 # User and Group under which Apache is running
 %define httpd_name  httpd
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -30,7 +30,7 @@ use PandoraFMS::Core;
 use PandoraFMS::Config;
 
 # version: define current version
-my $version = "5.1dev PS140602";
+my $version = "5.1dev PS140603";
 
 # save program name for logging
 my $progname = basename($0);

950a9fdd5b7ecd68337b66d7090bdf62ae43cb9f
zarzuelo <noreply@pandorafms.org>
2014-06-04 15:27:09 +0000

    2014-06-04  Sergio Martin <sergio.martin@artica.es>
    	* include/help/es/help_plugin_macros.php
    	include/help/es/help_alert_macros.php
    	include/help/en/help_plugin_macros.php
    	include/help/en/help_alert_macros.php: Added missed and
    	new macros to help
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@10106 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/en/help_plugin_macros.php
+++ pandora_console/include/help/en/help_plugin_macros.php
@@ -17,15 +17,16 @@ The following macros are available:
 <li>_modulegroup_ : Module group name.</li>
 <li>_moduledescription_ : Description of the module who fired the alert.</li>
 <li>_modulestatus_ : Status of the module.</li>
-<li>_moduletags_ : Tags associated to the module.</li>
 <li>_id_agent_ : Id of agent, useful to build direct URL to redirect to a Pandora FMS console webpage.</li>
 <li>_policy_ : Name of the policy the module belongs to (if applies).</li>
 <li>_interval_ : Execution interval of the module. </li>
 <li>_target_ip_ : IP address of the target of the module.</li>
 <li>_target_port_ : Port number of the target of the module.</li>
 <li>_plugin_parameters_ : Plug-in Parameters of the module.</li>	
-<li>_email_tag_ : Emails associated to the module tags.</li>
-
+<li>_name_tag_ : Nombre de los tags asociados al módulo.</li>
+<li>_email_tag_ : Emails asociados a los tags de módulos.</li>
+<li>_phone_tag_ : Teléfonos asociados a los tags de módulos.</li>
+<li>_moduletags_ : Teléfonos asociados a los tags de módulos.</li>
 </ul>
 <p>
 </p>

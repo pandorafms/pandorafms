47eacaa72e908b5ab2e3525a01fb780ac801e8bf
pandora-release <noreply@pandorafms.org>
2014-06-08 23:47:01 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/branches/pandora_4.0@10142 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/pandoradb_data.sql
+++ pandora_console/pandoradb_data.sql
@@ -48,7 +48,7 @@ INSERT INTO `tconfig` (`token`, `value`) VALUES
 ('graph_res','5'),
 ('step_compact','1'),
 ('db_scheme_version','4.1.1'),
-('db_scheme_build','PD140608'),
+('db_scheme_build','PD140609'),
 ('show_unknown','0'),
 ('show_lastalerts','1'),
 ('style','pandora'),
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -26,7 +26,7 @@ use PandoraFMS::Core;
 use PandoraFMS::Config;
 
 # version: define current version
-my $version = "4.1.1 PS140608";
+my $version = "4.1.1 PS140609";
 
 # Parameter
 my $param;

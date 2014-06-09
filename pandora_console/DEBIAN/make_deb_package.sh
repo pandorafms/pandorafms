95d574a5c7bec870395619711b64eec4343dc844
pandora-release <noreply@pandorafms.org>
2014-06-09 23:47:10 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@10148 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/DEBIAN/make_deb_package.sh
+++ pandora_console/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="5.1BETA1-140608"
+pandora_version="5.1BETA1-140610"
 
 package_pear=0
 package_pandora=1
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -30,7 +30,7 @@ use PandoraFMS::Core;
 use PandoraFMS::Config;
 
 # version: define current version
-my $version = "5.1BETA1 PS140608";
+my $version = "5.1BETA1 PS140610";
 
 # save program name for logging
 my $progname = basename($0);

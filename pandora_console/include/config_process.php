836948293cd9c7c9f4e9005da4cd8794b824ae6c
pandora-release <noreply@pandorafms.org>
2014-06-18 22:01:26 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@10236 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC140618';
+$build_version = 'PC140619';
 $pandora_version = 'v5.1RC1';
 
 // Do not overwrite default timezone set if defined.
--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="5.1RC1-140618"
+pandora_version="5.1RC1-140619"
 
 package_cpan=0
 package_pandora=1

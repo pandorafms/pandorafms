5cb5106b1d841075a5914e1dfa95d58903b3b377
Ramon Novoa <rnovoa@artica.es>
2014-06-24 09:02:18 +0000

    2014-06-24  Ramon Novoa  <rnovoa@artica.es>
    	* godmode/agentes/module_manager_editor_common.php,
    	  include/help/en/help_ff_interval.php,
    	  include/help/en/help_ff_threshold.php,
    	  include/help/en/help_ff_timeout.php,
    	  include/help/es/help_ff_interval.php,
    	  include/help/es/help_ff_threshold.php,
    	  include/help/es/help_ff_timeout.php,
    	  include/help/ja/help_ff_interval.php,
    	  include/help/ja/help_ff_threshold.php,
    	  include/help/ja/help_ff_timeout.php: Updated help files for the FF threshold.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@10270 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

---
+++ pandora_console/include/help/en/help_ff_interval.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Module Flip Flop Interval</h1>
+
+<br><br>
+
+If FF threshold is greater than 0, several consecutive values are required to change the status of a module. But if you want subsequent cheks to be performed at a different interval you can specify it with FF interval.
+
+For example, a ping module with a 5 minute interval, a FF threshold of 1 and a FF interval of 60 seconds would behave in the following way:
+
+<br><br>
+<table>
+<th>Time</th>
+<th>Data</th>
+<th>Status change</th>
+<tr>
+	<td>12:00</td>
+	<td>1</td>
+	<td>No</td>
+</tr>
+<tr>
+	<td>12:05</td>
+	<td>1</td>
+	<td>No</td>
+</tr>
+<tr>
+	<td>12:10</td>
+	<td>0</td>
+	<td>No</td>
+</tr>
+<tr>
+	<td><b>12:11</b></td>
+	<td>1</td>
+	<td>No</td>
+</tr>
+<tr>
+	<td>12:16</td>
+	<td>1</td>
+	<td>No</td>
+</tr>
+<tr>
+	<td>12:21</td>
+	<td>0</td>
+	<td>No</td>
+</tr>
+<tr>
+	<td><b>12:22</b></td>
+	<td>0</td>
+	<td><b>Yes</b></td>
+</tr>
+</table>
+
+<br><br>
+

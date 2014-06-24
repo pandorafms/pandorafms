f1a942ae93a6faa9d902359cc2983b1a09bb82aa
pandora-release <noreply@pandorafms.org>
2014-06-24 22:01:24 +0000

    Auto-updated build strings.
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@10280 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/install.php
+++ pandora_console/install.php
@@ -63,7 +63,7 @@
 		<div style='height: 10px'>
 			<?php
 $version = '5.1RC1';
-$build = '140624';
+$build = '140625';
 			$banner = "v$version Build $build";
 			
 			error_reporting(0);
--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="5.1RC1-140624"
+pandora_version="5.1RC1-140625"
 
 package_cpan=0
 package_pandora=1

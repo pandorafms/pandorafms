f88178897d56b1e2973f60b60940a8d026a2d9af
jsatoh <junichi@rworks.jp>
2014-07-09 06:43:30 +0000

    2014-07-09  Junichi Satoh  <junichi@rworks.jp>
    	* include/help/ja/help_snmp_alert_field1.php: Updated help.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@10329 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_console/include/help/ja/help_snmp_alert_field1.php
+++ pandora_console/include/help/ja/help_snmp_alert_field1.php
@@ -7,10 +7,17 @@
 <p>
 
 データフィールドがある場合、アラートでそれを利用する必要があります。この目的のために、特別な _snmp_fX_ というマクロを利用できます。これらのマクロは、SNMP トラップアラート以外では無効です。
-
+<br><br>
 メッセージを生成するには、フィールド1に次のような設定をします。
-
-Chassis Alert: _snmp_f2_ in device _snmp_f1_
-
-これらのマクロは、任意のアラートのフィールドX (1 から 10) で利用できます。
+<br><br>
+&nbsp;Chassis Alert: _snmp_f2_ in device _snmp_f1_
+<br><br>
+任意のアラートのフィールドX (1 から 10) で以下のマクロを利用できます。
+<br><br>
+<b>_data_</b>: トラップ全体<br>
+<b>_agent_</b>: エージェント名<br>
+<b>_address_</b>: IP アドレス<br>
+<b>_timestamp_</b>: トラップのタイムスタンプ<br>
+<b>_snmp_oid_</b>: トラップの OID<br>
+<b>_snmp_value_</b>: トラップ OID の値<br>
 </p>

b7183f4daf4efe86123cd01fe2d20584ab58a325
Ramon Novoa <rnovoa@artica.es>
2014-08-07 10:43:25 +0000

    2014-08-07  Ramon Novoa  <rnovoa@artica.es>
    	* conf/pandora_server.conf.new,
    	  lib/PandoraFMS/Config.pm,
    	  lib/PandoraFMS/ReconServer.pm,
    	  util/recon_scripts/snmp-recon.pl,
    	  util/recon_scripts/wmi-recon.pl: Added a new parameter to control how
    	  aggressive nmap should be.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/pandora/code/trunk@10391 c3f86ba8-e40f-0410-aaad-9ba5e7f4b01f

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -264,6 +264,7 @@ sub pandora_load_config {
 
 	# nmap for recon OS fingerprinting and tcpscan (optional)
 	$pa_config->{"nmap"} = "/usr/bin/nmap";
+	$pa_config->{"nmap_timing_template"} = 2; # > 5.1
 
 	# braa for enterprise snmp server
 	$pa_config->{"braa"} = "/usr/bin/braa";
@@ -606,6 +607,9 @@ sub pandora_load_config {
 		}
 		elsif ($parametro =~ m/^nmap\s(.*)/i) {
 			$pa_config->{'nmap'}= clean_blank($1); 
+		}
+		elsif ($parametro =~ m/^nmap_timing_template\s([0-9]*)/i) {
+			$pa_config->{'nmap_timing_template'}= clean_blank($1); 
 		}
 		elsif ($parametro =~ m/^braa\s(.*)/i) {
 			$pa_config->{'braa'}= clean_blank($1); 

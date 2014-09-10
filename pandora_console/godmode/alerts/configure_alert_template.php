d01b82bc3b834ac8c192d0de3466d4480c4d616b
mdtrooper <tres.14159@gmail.com>
2014-09-10 18:04:59 +0200

    Fixed the english message of my last commit. Thanks Sancho.

--- pandora_console/godmode/alerts/configure_alert_template.php
+++ pandora_console/godmode/alerts/configure_alert_template.php
@@ -783,7 +783,7 @@ var critical = <?php echo "'" . __("The alert would fire when the module is in c
 var onchange_msg = <?php echo "'" . __("The alert would fire when the module value changes") . "'";?>;
 var onchange_not = <?php echo "'" . __("The alert would fire when the module value does not change") . "'";?>;
 var unknown = <?php echo "'" . __("The alert would fire when the module is in unknown status") . "'";?>;
-var error_message_min_max_zero = <?php echo "'" . __("The alert template has not a min an max condition equal 0.") . "'";?>;
+var error_message_min_max_zero = <?php echo "'" . __("The alert template cannot have the same value for min and max thresholds.") . "'";?>;
 
 function check_fields_step2() {
 	var correct = true;

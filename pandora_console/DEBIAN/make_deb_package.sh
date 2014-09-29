2b2f7b1c5623f54962aa651a4184b531e1cd1b4d
artica <artica.devel@gmail.com>
2014-09-30 00:01:19 +0200

    Auto-updated build strings.

--- pandora_console/DEBIAN/make_deb_package.sh
+++ pandora_console/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="5.0SP4-140929"
+pandora_version="5.0SP4-140930"
 
 package_pear=0
 package_pandora=1
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -33,7 +33,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "5.0SP4 PS140929";
+my $version = "5.0SP4 PS140930";
 
 # save program name for logging
 my $progname = basename($0);

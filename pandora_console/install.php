b3e277570b62af2f75b9802e542598b91ba544e9
artica <artica.devel@gmail.com>
2014-10-05 00:01:08 +0200

    Auto-updated build strings.

--- pandora_console/install.php
+++ pandora_console/install.php
@@ -63,7 +63,7 @@
 		<div style='height: 10px'>
 			<?php
 $version = '5.1SP1';
-$build = '141004';
+$build = '141005';
 			$banner = "v$version Build $build";
 			
 			error_reporting(0);
--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -43,7 +43,7 @@ our @EXPORT = qw(
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "5.1SP1";
-my $pandora_build = "141004";
+my $pandora_build = "141005";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash

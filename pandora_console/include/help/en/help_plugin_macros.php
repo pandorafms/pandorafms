9473270d44b5028a3d6fd562176fa49685ec9373
mdtrooper <tres.14159@gmail.com>
2014-10-21 14:38:17 +0200

    Updated the list of module macros in the helps

--- pandora_console/include/help/en/help_plugin_macros.php
+++ pandora_console/include/help/en/help_plugin_macros.php
@@ -8,21 +8,28 @@
 <p>
 The following macros are available:
 <ul>
-<li>_agent_ : Name of the agent that fired the alert.</li>
-<li>_agentdescription_ : Description of the agent who fired alert.</li>
+<li>_agent_ : Name of the agent.</li>
+<li>_agentdescription_ : Description of the agent.</li>
 <li>_agentgroup_ : Agent group name.</li>
 <li>_agentstatus_ : Current status of the agent.</li>
-<li>_address_ : Address of the agent that fired the alert.</li>
+<li>_address_ : Address of the agent.</li>
 <li>_module_ : Module name.</li>
 <li>_modulegroup_ : Module group name.</li>
-<li>_moduledescription_ : Description of the module who fired the alert.</li>
+<li>_moduledescription_ : Description of the module.</li>
 <li>_modulestatus_ : Status of the module.</li>
 <li>_id_agent_ : Id of agent, useful to build direct URL to redirect to a Pandora FMS console webpage.</li>
 <li>_policy_ : Name of the policy the module belongs to (if applies).</li>
 <li>_interval_ : Execution interval of the module. </li>
+
+<!--
+Hidden this macros because they cannot edit in the module form
+-->
+<!--
 <li>_target_ip_ : IP address of the target of the module.</li>
 <li>_target_port_ : Port number of the target of the module.</li>
-<li>_plugin_parameters_ : Plug-in Parameters of the module.</li>	
+-->
+
+<li>_plugin_parameters_ : Plug-in Parameters of the module.</li>
 <li>_name_tag_ : Nombre de los tags asociados al módulo.</li>
 <li>_email_tag_ : Emails asociados a los tags de módulos.</li>
 <li>_phone_tag_ : Teléfonos asociados a los tags de módulos.</li>

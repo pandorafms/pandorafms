96e66a9c89b526d19ff60f7a7141838a82823abc
mdtrooper <tres.14159@gmail.com>
2014-10-21 14:38:17 +0200

    Updated the list of module macros in the helps

--- pandora_console/include/help/es/help_plugin_macros.php
+++ pandora_console/include/help/es/help_plugin_macros.php
@@ -9,10 +9,10 @@
 Las siguientes macros están disponibles:
 </p>
 <ul>
-<li>_agentdescription_ : Descripción del agente que disparó la alerta.</li>
+<li>_agentdescription_ : Descripción del agente.</li>
 <li>_agentgroup_ : Nombre del grupo del agente.</li>
 <li>_agentstatus_ : Estado actual del agente.</li>
-<li>_address_: Dirección del agente que disparó la alerta.</li>
+<li>_address_: Dirección del agente.</li>
 <li>_module_: Nombre del módulo</li>
 <li>_modulegroup_ : Nombre del grupo del módulo.</li>
 <li>_moduledescription_: Descripcion del modulo.</li>
@@ -20,8 +20,15 @@ Las siguientes macros están disponibles:
 <li>_id_agent_: ID del agente, util para construir URL de acceso a la consola de Pandora.</li>
 <li>_policy_: Nombre de la política a la que pertenece el módulo (si aplica).</li>
 <li>_interval_ : Intervalo de la ejecución del módulo. </li>
+
+<!--
+Hidden this macros because they cannot edit in the module form
+-->
+<!--
 <li>_target_ip_ : Dirección IP del objetivo del módulo.</li>
 <li>_target_port_ : Puerto del objetivo del módulo.</li>
+-->
+
 <li>_plugin_parameters_ : Parámetros del Plug-in del módulo.</li>
 <li>_name_tag_ : Names of the tags associated to the module.</li>
 <li>_email_tag_ : Emails associated to the module tags.</li>

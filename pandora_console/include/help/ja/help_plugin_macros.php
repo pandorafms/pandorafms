244b02516d7f380bbec13bd7d63f7fbf6cd1262a
Junichi Satoh <junichi@rworks.jp>
2014-10-22 18:32:47 +0900

    Updated help.

--- pandora_console/include/help/ja/help_plugin_macros.php
+++ pandora_console/include/help/ja/help_plugin_macros.php
@@ -9,25 +9,33 @@
 次のマクロが利用できます:
 </p>
 <ul>
-<li>_agent_ : アラートが発生したエージェント</li>
-<li>_agentdescription_ : 発生したアラートの説明</li>
-<li>_agentgroup_ : エージェントグループ名</li>
-<li>_agentstatus : エージェントの現在の状態</li>
-<li>_address_ : アラートが発生したエージェントのアドレス</li>
-<li>_module_ : モジュール名</li>
-<li>_modulegroup_ : モジュールグループ名</li>
-<li>_moduledescription_ : アラートが発生したモジュールの説明</li>
-<li>_modulestatus_ : モジュールの状態</li>
-<li>_moduletags_ : モジュールに関連付けられたタグ</li>
-<li>_id_agent_ : エージェントのID / Webコンソールへのリンクを生成するのに便利です</li>
-<li>_policy_ : モジュールが属するポリシー名 (存在する場合)</li>
-<li>_interval_ : モジュールの実行間隔</li>
-<li>_target_ip_ : モジュールの対象IPアドレス</li>
-<li>_target_port_ : モジュールの対象ポート</li>
-<li>_plugin_parameters_ : モジュールのプラグインパラメータ</li>
-<li>_name_tag_ : モジュールに関連付けられたタグの名前</li>
+<li>_agent_ : エージェント名。</li>
+<li>_agentdescription_ : エージェントの説明。</li>
+<li>_agentgroup_ : エージェントグループ名。</li>
+<li>_agentstatus : エージェントの現在の状態。</li>
+<li>_address_ : エージェントのアドレス。</li>
+<li>_module_ : モジュール名。</li>
+<li>_modulegroup_ : モジュールグループ名。</li>
+<li>_moduledescription_ : モジュールの説明。</li>
+<li>_modulestatus_ : モジュールの状態。</li>
+<li>_id_agent_ : エージェントのID / Webコンソールへのリンクを生成するのに便利です。</li>
+<li>_policy_ : モジュールが属するポリシー名。(存在する場合)</li>
+<li>_interval_ : モジュールの実行間隔。</li>
+
+<!--
+Hidden this macros because they cannot edit in the module form
+-->
+<!--
+<li>_target_ip_ : モジュールの対象IPアドレス。</li>
+<li>_target_port_ : モジュールの対象ポート。</li>
+-->
+
+<li>_plugin_parameters_ : モジュールのプラグインパラメータ。</li>
+<li>_name_tag_ : モジュールに関連付けられたタグの名前。</li>
 <li>_email_tag_ : モジュールタグに関連付けられた Email。</li>
 <li>_phone_tag_ : モジュールタグに関連付けられた電話番号。</li>
 <li>_moduletags_ : モジュールタグに関連付けられた URL。</li>
 
 </ul>
+<p>
+</p>

352c63087c12ef387111ffb6c9e83f5628b8130f
mdtrooper <tres.14159@gmail.com>
2014-11-26 16:03:40 +0100

    The last work for the new elements into the visualmap. Ticket  #1478

--- pandora_console/godmode/reporting/visual_console_builder.elements.php
+++ pandora_console/godmode/reporting/visual_console_builder.elements.php
@@ -147,6 +147,11 @@ foreach ($layoutDatas as $layoutData) {
 			$table->data[$i + 1]['icon'] =
 				html_print_image('images/box_item.png', true, array('title' => __('Box')));
 			break;
+		case LINE_ITEM:
+			$table->data[$i + 1]['icon'] =
+				html_print_image('images/line_item.png', true,
+					array('title' => __('Line')));
+			break;
 		default:
 			if (enterprise_installed()) {
 				$table->data[$i + 1]['icon'] =
@@ -166,6 +171,7 @@ foreach ($layoutDatas as $layoutData) {
 	switch ($layoutData['type']) {
 		case ICON:
 		case BOX_ITEM:
+		case LINE_ITEM:
 			// hasn't the label.
 			$table->data[$i + 1][0] = '';
 			break;
@@ -196,18 +202,35 @@ foreach ($layoutDatas as $layoutData) {
 	
 	
 	//Width and height
-	$table->data[$i + 1][2] = html_print_input_text('width_' . $idLayoutData, $layoutData['width'], '', 2, 5, true) .
-		'x' .
-		html_print_input_text('height_' . $idLayoutData, $layoutData['height'], '', 2, 5, true);
+	switch ($layoutData['type']) {
+		case LINE_ITEM:
+			// hasn't the width and height.
+			$table->data[$i + 1][2] = '';
+			break;
+		default:
+			$table->data[$i + 1][2] = html_print_input_text('width_' . $idLayoutData, $layoutData['width'], '', 2, 5, true) .
+				'x' .
+				html_print_input_text('height_' . $idLayoutData, $layoutData['height'], '', 2, 5, true);
+			break;
+	}
 	
 	//Position
-	$table->data[$i + 1][3] = '(' . html_print_input_text('left_' . $idLayoutData, $layoutData['pos_x'], '', 2, 5, true) .
-		',' . html_print_input_text('top_' . $idLayoutData, $layoutData['pos_y'], '', 2, 5, true) .
-		')';
+	switch ($layoutData['type']) {
+		case LINE_ITEM:
+			// hasn't the width and height.
+			$table->data[$i + 1][3] = '';
+			break;
+		default:
+			$table->data[$i + 1][3] = '(' . html_print_input_text('left_' . $idLayoutData, $layoutData['pos_x'], '', 2, 5, true) .
+				',' . html_print_input_text('top_' . $idLayoutData, $layoutData['pos_y'], '', 2, 5, true) .
+				')';
+			break;
+	}
 	
 	//Parent
 	switch ($layoutData['type']) {
 		case BOX_ITEM:
+		case LINE_ITEM:
 			$table->data[$i + 1][4] = "";
 			break;
 		default:
@@ -238,6 +261,7 @@ foreach ($layoutDatas as $layoutData) {
 		case BOX_ITEM:
 		case ICON:
 		case LABEL:
+		case LINE_ITEM:
 			$table->data[$i + 2][0] = '';
 			break;
 		default:
@@ -288,6 +312,7 @@ foreach ($layoutDatas as $layoutData) {
 		case ICON:
 		case LABEL:
 		case BOX_ITEM:
+		case LINE_ITEM:
 			$table->data[$i + 2][1] = '';
 			break;
 		default:
@@ -349,6 +374,7 @@ foreach ($layoutDatas as $layoutData) {
 	
 	//Map linked
 	switch ($layoutData['type']) {
+		case LINE_ITEM:
 		case BOX_ITEM:
 			$table->data[$i + 2][4] = "";
 			break;

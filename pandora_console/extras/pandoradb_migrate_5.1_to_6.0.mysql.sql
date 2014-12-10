8c3a5cedb749c2eccdf5600efca47ca6990a4319
mdtrooper <tres.14159@gmail.com>
2014-12-10 13:36:42 +0100

    Working in the tags out the policies. Ticket: #1577

--- pandora_console/extras/pandoradb_migrate_5.1_to_6.0.mysql.sql
+++ pandora_console/extras/pandoradb_migrate_5.1_to_6.0.mysql.sql
@@ -20,3 +20,13 @@ ALTER TABLE tlayout_data ADD COLUMN `fill_color` varchar(200) DEFAULT "";
 
 INSERT INTO `tconfig_os` (`name`, `description`, `icon_name`) VALUES ('Mainframe', 'Mainframe agent', 'so_mainframe.png');
 
+-- ---------------------------------------------------------------------
+-- Table `ttag_module`
+-- ---------------------------------------------------------------------
+ALTER TABLE ttag_module ADD COLUMN `id_policy_module` int(10) NOT NULL DEFAULT 0;
+
+UPDATE ttag_module AS t1
+SET t1.id_policy_module = (
+	SELECT t2.id_policy_module
+	FROM tagente_modulo AS t2
+	WHERE t1.id_agente_modulo = t2.id_agente_modulo);
--- pandora_console/extras/pandoradb_migrate_5.1_to_6.0.oracle.sql
+++ pandora_console/extras/pandoradb_migrate_5.1_to_6.0.oracle.sql
@@ -13,3 +13,9 @@ ALTER TABLE tlayout_data DROP COLUMN label_color;
 ALTER TABLE tlayout_data ADD COLUMN border_width INTEGER NOT NULL default 0;
 ALTER TABLE tlayout_data ADD COLUMN border_color varchar(200) DEFAULT "";
 ALTER TABLE tlayout_data ADD COLUMN fill_color varchar(200) DEFAULT "";
+
+-- ---------------------------------------------------------------------
+-- Table `ttag_module`
+-- ---------------------------------------------------------------------
+
+ALTER TABLE tlayout_data ADD COLUMN id_policy_module NUMBER(10, 0) DEFAULT 0 NOT NULL;

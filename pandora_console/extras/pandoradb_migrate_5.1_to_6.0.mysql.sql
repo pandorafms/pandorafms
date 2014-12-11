8a294c4d68db8e3d0436917cf5cefcc36f8e684e
Alejandro Gallardo Escobar <alex@artica.es>
2014-12-11 16:11:21 +0100

    Added a new table called 'tuser_double_auth'

--- pandora_console/extras/pandoradb_migrate_5.1_to_6.0.mysql.sql
+++ pandora_console/extras/pandoradb_migrate_5.1_to_6.0.mysql.sql
@@ -14,3 +14,15 @@ ALTER TABLE tlayout_data ADD COLUMN `border_width` INTEGER UNSIGNED NOT NULL def
 ALTER TABLE tlayout_data ADD COLUMN `border_color` varchar(200) DEFAULT "";
 ALTER TABLE tlayout_data ADD COLUMN `fill_color` varchar(200) DEFAULT "";
 
+/* 2014/12/10 */
+-- ----------------------------------------------------------------------
+-- Table `tuser_double_auth`
+-- ----------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tuser_double_auth` (
+	`id` int(10) unsigned NOT NULL auto_increment,
+	`id_user` varchar(60) NOT NULL,
+	`secret` varchar(20) NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE (`id_user`),
+	FOREIGN KEY (`id_user`) REFERENCES tusuario(`id_user`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

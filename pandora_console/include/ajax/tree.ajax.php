a045faeb6dcad23f54fe183d6f58d0aff60fc5e5
Alejandro Gallardo Escobar <alex@artica.es>
2014-12-18 09:55:54 +0100

    Minor changes

--- pandora_console/include/ajax/tree.ajax.php
+++ pandora_console/include/ajax/tree.ajax.php
@@ -13,23 +13,33 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
-require_once("include/class/Tree.class.php");
+// Only accesible by ajax
+if (is_ajax ()) {
+	global $config;
 
-$getChildren = (bool)get_parameter('getChildren', 0);
-
-if ($getChildren) {
-	$type = get_parameter('type', 'group');
-	$filter = get_parameter('filter',
-		array('search' => '',
-			'status' => AGENT_STATUS_ALL));
-	$id = (int)get_parameter('id', 0);
-	$method = get_parameter('method', 'on_demand');
+	// Login check
+	check_login ();
 	
-	$tree = new Tree($type, $method, $id);
-	$tree->setFilter(array(
-		'status' => $filter['status'],
-		'search' => $filter['search']));
-	echo json_encode(array('success' => 1, 'tree' => $tree->getArray()));
+	require_once("include/class/Tree.class.php");
+
+	$getChildren = (bool)get_parameter('getChildren', 0);
+
+	if ($getChildren) {
+		$type = get_parameter('type', 'group');
+		$filter = get_parameter('filter',
+			array('search' => '',
+				'status' => AGENT_STATUS_ALL));
+		$id = (int)get_parameter('id', 0);
+		$method = get_parameter('method', 'on_demand');
+		
+		$tree = new Tree($type, $method, $id);
+		$tree->setFilter(array(
+			'status' => $filter['status'],
+			'search' => $filter['search']));
+		echo json_encode(array('success' => 1, 'tree' => $tree->getArray()));
+		return;
+	}
+
 	return;
 }
 ?>

ed9cd8f999878cd478ddd8fc8aa794f0d218eeec
Alejandro Gallardo Escobar <alex@artica.es>
2015-01-08 11:45:31 +0100

    Improved the search filter and added new info icons to the modules in the tree view

--- pandora_console/operation/tree2.php
+++ pandora_console/operation/tree2.php
@@ -132,6 +132,8 @@ html_print_table($table);
 echo '</form>';
 
 // --------------------- form filter -----------------------------------
+ui_include_time_picker();
+ui_require_jquery_file("ui.datepicker-" . get_user_language(), "include/javascript/i18n/");
 
 ui_require_javascript_file("TreeController", "include/javascript/tree/");
 html_print_image('images/spinner.gif', false,
@@ -231,6 +233,95 @@ echo "</div>";
 				}
 			},
 			dataType: "json"
+		});
+	}
+
+	// Show the modal window of an module
+	var moduleDetailsWindow = $("<div></div>");
+	moduleDetailsWindow
+		.hide()
+		.prop("id", "module_details_window")
+		.appendTo('body');
+
+	function show_module_detail_dialog(module_id, id_agent, server_name, offset, period) {
+		var params = {};
+
+		if (period == -1) {
+			period = $('#period').val();
+			params.selection_mode = $('input[name=selection_mode]:checked').val();
+			params.date_from = $('#text-date_from').val();
+			params.time_from = $('#text-time_from').val();
+			params.date_to = $('#text-date_to').val();
+			params.time_to = $('#text-time_to').val();
+		}
+
+		params.page = "include/ajax/module";
+		params.get_module_detail = 1;
+		params.server_name = server_name;
+		params.id_agent = id_agent;
+		params.id_module = module_id;
+		params.offset = offset;
+		params.period = period;
+		
+		$.ajax({
+			type: "POST",
+			url: "<?php echo ui_get_full_url('ajax.php', false, false, false); ?>",
+			data: params,
+			dataType: "html",
+			success: function(data) {
+				$("#module_details_window").hide ()
+					.empty ()
+					.append (data)
+					.dialog ({
+						resizable: true,
+						draggable: true,
+						modal: true,
+						overlay: {
+							opacity: 0.5,
+							background: "black"
+						},
+						width: 650,
+						height: 500
+					})
+					.show ();
+					refresh_pagination_callback(module_id, id_agent, server_name);
+					datetime_picker_callback();
+					forced_title_callback();
+			}
+		});
+	}
+	
+	function datetime_picker_callback() {
+		$("#text-time_from, #text-time_to").timepicker({
+			showSecond: true,
+			timeFormat: '<?php echo TIME_FORMAT_JS; ?>',
+			timeOnlyTitle: '<?php echo __('Choose time');?>',
+			timeText: '<?php echo __('Time');?>',
+			hourText: '<?php echo __('Hour');?>',
+			minuteText: '<?php echo __('Minute');?>',
+			secondText: '<?php echo __('Second');?>',
+			currentText: '<?php echo __('Now');?>',
+			closeText: '<?php echo __('Close');?>'});
+			
+		$.datepicker.setDefaults($.datepicker.regional[ "<?php echo get_user_language(); ?>"]);
+		$("#text-date_from, #text-date_to").datepicker({dateFormat: "<?php echo DATE_FORMAT_JS; ?>"});
+		
+	}
+	
+	function refresh_pagination_callback (module_id, id_agent, server_name) {
+		
+		$(".binary_dialog").click( function() {
+			
+			var classes = $(this).attr('class');
+			classes = classes.split(' ');
+			var offset_class = classes[2];
+			offset_class = offset_class.split('_');
+			var offset = offset_class[1];
+			
+			var period = $('#period').val();
+			
+			show_module_detail_dialog(module_id, id_agent, server_name, offset, period);
+			return false;
 		});
 	}
 

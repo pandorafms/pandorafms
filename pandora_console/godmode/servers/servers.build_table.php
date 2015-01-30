89faf1f7f57383ce975a6bfd47d98808bea44940
m-lopez-f <miguel.lopez@artica.es>
2015-01-30 10:07:16 +0100

    Change order to show info in 'Tactical server information' about data server
    (cherry picked from commit f1f1e7ba940c71cb556d2ac3e4f6a94ab807af18)

--- pandora_console/godmode/servers/servers.build_table.php
+++ pandora_console/godmode/servers/servers.build_table.php
@@ -83,12 +83,18 @@ foreach ($servers as $server) {
 	
 	// Type
 	if ($server['type'] == 'data'){
-		$data[2] = '<span style="white-space:nowrap;">'.$server["img"].'</span> (<span style="font-size:8px;">' . $server["version"]. '</span>)';
+		$data[2] = '<span style="white-space:nowrap;">'.$server["img"];
+		if ($server["master"] == 1){
+			$data[2] .= ui_print_help_tip (__("This is a master server"), true). '</span> <span style="font-size:8px;"> v' . $server["version"]. '</span>';
+		}
+		else{
+			$data[2] .= '</span> (<span style="font-size:8px;"> v' . $server["version"]. '</span>)';
+		}
 	}
 	else{
 		$data[2] = '<span style="white-space:nowrap;">'.$server["img"].'</span> ('.ucfirst($server["type"]).")";
 	}
-	if ($server["master"] == 1)
+	if ($server["master"] == 1 and $server['type'] != 'data')
 		$data[2] .= ui_print_help_tip (__("This is a master server"), true);
 	
 	switch ($server['type']) {

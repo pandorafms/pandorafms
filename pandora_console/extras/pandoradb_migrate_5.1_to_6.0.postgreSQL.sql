f41079bb8de90fb19fe7fee918cb61be7c298b42
Alejandro Gallardo Escobar <alex@artica.es>
2015-02-04 16:48:03 +0100

    Modified the email alert command to add the html editor activator to the field 3

--- pandora_console/extras/pandoradb_migrate_5.1_to_6.0.postgreSQL.sql
+++ pandora_console/extras/pandoradb_migrate_5.1_to_6.0.postgreSQL.sql
@@ -15,10 +15,22 @@ ALTER TABLE "tlayout_data" ADD COLUMN "border_color" varchar(200) DEFAULT "";
 ALTER TABLE "tlayout_data" ADD COLUMN "fill_color" varchar(200) DEFAULT "";
 
 -- ---------------------------------------------------------------------
+-- Table `tconfig_os`
+-- ---------------------------------------------------------------------
+
+INSERT INTO "tconfig_os" ("name", "description", "icon_name") VALUES ('Mainframe', 'Mainframe agent', 'so_mainframe.png');
+
+-- ---------------------------------------------------------------------
 -- Table `ttag_module`
 -- ---------------------------------------------------------------------
 
 ALTER TABLE tlayout_data ADD COLUMN "id_policy_module" INTEGER NOT NULL DEFAULT 0;
+
+UPDATE ttag_module AS t1
+SET t1.id_policy_module = (
+	SELECT t2.id_policy_module
+	FROM tagente_modulo AS t2
+	WHERE t1.id_agente_modulo = t2.id_agente_modulo);
 
 /* 2014/12/10 */
 -- ----------------------------------------------------------------------
@@ -39,3 +51,8 @@ INSERT INTO "ttipo_modulo" VALUES (5,'generic_data_inc_abs',0,'Generic numeric i
 -- Table `tusuario`
 -- ---------------------------------------------------------------------
 ALTER TABLE "tusuario" ADD COLUMN "strict_acl" SMALLINT DEFAULT 0;
+
+-- ---------------------------------------------------------------------
+-- Table `talert_commands`
+-- ---------------------------------------------------------------------
+UPDATE "talert_commands" SET "fields_descriptions" = '[\"Destination&#x20;address\",\"Subject\",\"Text\",\"\",\"\",\"\",\"\",\"\",\"\",\"\"]', "fields_values" = '[\"\",\"\",\"_html_editor_\",\"\",\"\",\"\",\"\",\"\",\"\",\"\"]' WHERE "id" = 1 AND "name" = 'eMail';

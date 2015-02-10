658bb921e7de3720df4b97d069ed9290a4f79fbe
Alejandro Gallardo Escobar <alex@artica.es>
2015-02-10 19:06:19 +0100

    Improved the help

--- pandora_console/include/help/en/help_alert_macros.php
+++ pandora_console/include/help/en/help_alert_macros.php
@@ -7,52 +7,63 @@
 
 <p>
 Besides the defined module macros, the following macros are available:
-<ul>
-<li>_field1_ : User defined field 1.</li>
-<li>_field2_ : User defined field 2.</li>
-<li>_field3_ : User defined field 3.</li>
-<li>_agent_ : Name of the agent that fired the alert.</li>
-<li>_agentcustomfield_<i>n</i>_: Agent custom field number <i>n</i> (eg. _agentcustomfield_9_). </li>
-<li>_agentcustomid_: Agent custom ID. </li>
-<li>_agentdescription_ : Description of the agent who fired alert.</li>
-<li>_agentgroup_ : Agent group name.</li>
-<li>_agentstatus_ : Current status of the agent.</li>
-<li>_address_ : Address of the agent that fired the alert.</li>
-<li>_timestamp_ : Time when the alert was fired (yy-mm-dd hh:mm:ss).</li>
-<li>_timezone_ : Timezone name that _timestamp_ represents in.</li>
-<li>_data_ : Module data that caused the alert to fire.</li>
-<li>_alert_description_ : Alert description.</li>
-<li>_alert_threshold_ : Alert threshold.</li>
-<li>_alert_times_fired_ : Number of times the alert has been fired.</li>
-<li>_module_ : Module name.</li>
-<li>_modulecustomid_: Module custom ID. </li>
-<li>_modulegroup_ : Module group name.</li>
-<li>_moduledescription_ : Description of the module who fired the alert.</li>
-<li>_modulestatus_ : Status of the module.</li>
-<li>_alert_name_ : Alert name.</li>
-<li>_alert_priority_ : Numerical alert priority.</li>
-<li>_alert_text_severity_ : Text alert severity (Maintenance, Informational, Normal Minor, Warning, Major, Critical).</li>
-<li>_event_text_severity_ : (Only event alerts) Text event (who fire the alert) severity (Maintenance, Informational, Normal Minor, Warning, Major, Critical).</li>
-<li>_event_id_ : (Only event alerts) Id of the event that fired the alert.</li>
-<li>_id_agent_ : Id of agent, useful to build direct URL to redirect to a Pandora FMS console webpage.</li>
-<li>_id_alert_ : Numerical ID of the alert (unique), used to correlate on third party software</li>
-<li>_policy_ : Name of the policy the module belongs to (if applies).</li>
-<li>_interval_ : Execution interval of the module. </li>
-<li>_target_ip_ : IP address of the target of the module.</li>
-<li>_target_port_ : Port number of the target of the module.</li>
-<li>_plugin_parameters_ : Plug-in Parameters of the module.</li>
-<li>_groupcontact_ : Group contact information.	Configured when the group is created.</li>
-<li>_groupcustomid_: Group custom ID. </li>
-<li>_groupother_ : Other information about the group. Configured when the group is created.</li>
-<li>_name_tag_ : Names of the tags associated to the module.</li>
-<li>_email_tag_ : Emails associated to the module tags.</li>
-<li>_phone_tag_ : Phone numbers associated to the module tags.</li>
-<li>_moduletags_ : URLs associated to the module tags.</li>
-<li>_alert_critical_instructions_: Instructions for CRITICAL status, as contained in module.</li>
-<li>_alert_warning_instructions_: Instrucciones for WARNING status, as contained in module.</li>
-</ul>
-<p>
 </p>
+<ul>
+<li>_field1_: User defined field 1.</li>
+<li>_field2_: User defined field 2.</li>
+<li>_field3_: User defined field 3.</li>
+<li>_field4_: User defined field 4.</li>
+<li>_field5_: User defined field 5.</li>
+<li>_field6_: User defined field 6.</li>
+<li>_field7_: User defined field 7.</li>
+<li>_field8_: User defined field 8.</li>
+<li>_field9_: User defined field 9.</li>
+<li>_field10_: User defined field 10.</li>
+<li>_agent_: Name of the agent that fired the alert.</li>
+<li>_agentcustomfield_<i>n</i>_: Agent custom field number <i>n</i> (eg. _agentcustomfield_9_).</li>
+<li>_agentcustomid_: Agent custom ID.</li>
+<li>_agentdescription_: Description of the agent who fired alert.</li>
+<li>_agentgroup_: Agent group name.</li>
+<li>_agentstatus_: Current status of the agent.</li>
+<li>_address_: Address of the agent that fired the alert.</li>
+<li>_timestamp_: Time when the alert was fired (yy-mm-dd hh:mm:ss).</li>
+<li>_timezone_: Timezone name that _timestamp_ represents in.</li>
+<li>_data_: Module data that caused the alert to fire.</li>
+<li>_alert_description_: Alert description.</li>
+<li>_alert_threshold_: Alert threshold.</li>
+<li>_alert_times_fired_: Number of times the alert has been fired.</li>
+<li>_module_: Module name.</li>
+<li>_modulecustomid_: Module custom ID.</li>
+<li>_modulegroup_: Module group name.</li>
+<li>_moduledescription_: Description of the module who fired the alert.</li>
+<li>_modulestatus_: Status of the module.</li>
+<li>_alert_name_: Alert name.</li>
+<li>_alert_priority_: Numerical alert priority.</li>
+<li>_alert_text_severity_: Text alert severity (Maintenance, Informational, Normal Minor, Warning, Major, Critical).</li>
+<li>_event_text_severity_: (Only event alerts) Text event (who fire the alert) severity (Maintenance, Informational, Normal Minor, Warning, Major, Critical).</li>
+<li>_event_id_: (Only event alerts) Id of the event that fired the alert.</li>
+<li>_id_agent_: Id of agent, useful to build direct URL to redirect to a Pandora FMS console webpage.</li>
+<li>_id_alert_: Numerical ID of the alert (unique), used to correlate on third party software.</li>
+<li>_policy_: Name of the policy the module belongs to (if applies).</li>
+<li>_interval_: Execution interval of the module </li>
+<li>_target_ip_: IP address of the target of the module.</li>
+<li>_target_port_: Port number of the target of the module.</li>
+<li>_plugin_parameters_: Plug-in Parameters of the module.</li>
+<li>_groupcontact_: Group contact information. Configured when the group is created.</li>
+<li>_groupcustomid_: Group custom ID.</li>
+<li>_groupother_: Other information about the group. Configured when the group is created.</li>
+<li>_name_tag_: Names of the tags associated to the module.</li>
+<li>_email_tag_: Emails associated to the module tags.</li>
+<li>_phone_tag_: Phone numbers associated to the module tags.</li>
+<li>_moduletags_: URLs associated to the module tags.</li>
+<li>_alert_critical_instructions_: Instructions for the CRITICAL status contained in the module.</li>
+<li>_alert_warning_instructions_: Instructions for the WARNING status contained in the module.</li>
+<li>_modulegraph_<i>n</i>h_: (>=6.0) (Only for alerts that use the command <i>eMail</i>)
+Returns an image codified in base64 of a module graph with a period of <i>n</i> hours (eg. _modulegraph_24h_).
+A correct setup of the connection between the server and the console's api is required.
+This setup is done into the server's configuration file.</li>
+</ul>
+
 <p>
 Example: Agent _agent_ has fired alert _alert_ with data _data_
 </p>
--- pandora_console/include/help/es/help_alert_macros.php
+++ pandora_console/include/help/es/help_alert_macros.php
@@ -12,12 +12,19 @@ Además de las macros de módulo definidas, las siguientes macros están disponi
 <li>_field1_: Campo 1 definido por el usuario.</li>
 <li>_field2_: Campo 2 definido por el usuario.</li>
 <li>_field3_: Campo 3 definido por el usuario.</li>
+<li>_field4_: Campo 4 definido por el usuario.</li>
+<li>_field5_: Campo 5 definido por el usuario.</li>
+<li>_field6_: Campo 6 definido por el usuario.</li>
+<li>_field7_: Campo 7 definido por el usuario.</li>
+<li>_field8_: Campo 8 definido por el usuario.</li>
+<li>_field9_: Campo 9 definido por el usuario.</li>
+<li>_field10_: Campo 10 definido por el usuario.</li>
 <li>_agent_: Nombre del agente que disparó la alerta.</li>
 <li>_agentcustomfield_<i>n</i>_: Campo personalizado número <i>n</i> del agente (eg. _agentcustomfield_9_). </li>
-<li>_agentcustomid_ : ID personalizado del agente. </li>
-<li>_agentdescription_ : Descripción del agente que disparó la alerta.</li>
-<li>_agentgroup_ : Nombre del grupo del agente.</li>
-<li>_agentstatus_ : Estado actual del agente.</li>
+<li>_agentcustomid_: ID personalizado del agente. </li>
+<li>_agentdescription_: Descripción del agente que disparó la alerta.</li>
+<li>_agentgroup_: Nombre del grupo del agente.</li>
+<li>_agentstatus_: Estado actual del agente.</li>
 <li>_address_: Dirección del agente que disparó la alerta.</li>
 <li>_timestamp_: Hora y fecha en que se disparó la alerta.</li>
 <li>_timezone_: Area Nombre _timestamp_ que representa en.</li>
@@ -25,33 +32,36 @@ Además de las macros de módulo definidas, las siguientes macros están disponi
 <li>_alert_description_: Descripción de la alerta.</li>
 <li>_alert_threshold_: Umbral de la alerta.</li>
 <li>_alert_times_fired_: Número de veces que se ha disparado la alerta.</li>
-<li>_module_: Nombre del módulo</li>
-<li>_modulecustomid_: ID personalizado del módulo. </li>
-<li>_modulegroup_ : Nombre del grupo del módulo.</li>
+<li>_module_: Nombre del módulo.</li>
+<li>_modulecustomid_: ID personalizado del módulo.</li>
+<li>_modulegroup_: Nombre del grupo del módulo.</li>
 <li>_moduledescription_: Descripcion del modulo.</li>
-<li>_modulestatus_ : Estado del módulo.</li>
+<li>_modulestatus_: Estado del módulo.</li>
 <li>_alert_name_: Nombre de la alerta.</li>
 <li>_alert_priority_: Prioridad numérica de la alerta.</li>
-<li>_alert_text_severity_: Prioridad en texto de la alerta. (Maintenance, Informational, Normal Minor, Warning, Major, Critical)</li>
-<li>_eventt_text_severity_: (Solo alertas de evento) Prioridad en texto de el evento que dispara la alerta. (Maintenance, Informational, Normal Minor, Warning, Major, Critical)</li>
-<li>_event_id_ : (Solo alertas de evento) Id del evento que disparó la alerta.</li>
+<li>_alert_text_severity_: Prioridad en texto de la alerta (Maintenance, Informational, Normal Minor, Warning, Major, Critical).</li>
+<li>_eventt_text_severity_: (Solo alertas de evento) Prioridad en texto de el evento que dispara la alerta (Maintenance, Informational, Normal Minor, Warning, Major, Critical).</li>
+<li>_event_id_: (Solo alertas de evento) Id del evento que disparó la alerta.</li>
 <li>_id_agent_: ID del agente, util para construir URL de acceso a la consola de Pandora.</li>
 <li>_id_alert_: ID de la alerta, util para correlar la alerta en herramientas de terceros.</li>
 <li>_policy_: Nombre de la política a la que pertenece el módulo (si aplica).</li>
-<li>_interval_ : Intervalo de la ejecución del módulo. </li>
-<li>_target_ip_ : Dirección IP del objetivo del módulo.</li>
-<li>_target_port_ : Puerto del objetivo del módulo.</li>
-<li>_groupcontact_ : Información de contacto del grupo.	Se configura al crear el grupo.</li>
-<li>_groupcustomid_ : ID personalizado del grupo. </li>
-<li>_groupother_ : Otra información sobre el grupo. Se configura al crear el grupo.</li>
-<li>_name_tag_ : Nombre de los tags asociados al módulo.</li>
-<li>_email_tag_ : Emails asociados a los tags de módulos.</li>
-<li>_phone_tag_ : Teléfonos asociados a los tags de módulos.</li>
-<li>_moduletags_ : Teléfonos asociados a los tags de módulos.</li>
-<li>_plugin_param1_ - _plugin_param10_ (podria ser mas): El valor de campo de los parametros de plug-in correspondiente.</li>
-<li>i_plugin_param1_desc_ - _plugin_param10_desc_ (podria ser mas): La descripcion del campo de los parametros de plug-in correspondiente.</li>
+<li>_interval_: Intervalo de la ejecución del módulo. </li>
+<li>_target_ip_: Dirección IP del objetivo del módulo.</li>
+<li>_target_port_: Puerto del objetivo del módulo.</li>
+<li>_plugin_parameters_: Parámetros del plugin del módulo.</li>
+<li>_groupcontact_: Información de contacto del grupo. Se configura al crear el grupo.</li>
+<li>_groupcustomid_: ID personalizado del grupo.</li>
+<li>_groupother_: Otra información sobre el grupo. Se configura al crear el grupo.</li>
+<li>_name_tag_: Nombre de los tags asociados al módulo.</li>
+<li>_email_tag_: Emails asociados a los tags de módulos.</li>
+<li>_phone_tag_: Teléfonos asociados a los tags de módulos.</li>
+<li>_moduletags_: URLs asociadas a los tags de módulos.</li>
 <li>_alert_critical_instructions_: Instrucciones contenidas en el módulo para un estado CRITICAL.</li>
 <li>_alert_warning_instructions_: Instrucciones contenidas en el módulo para un estado WARNING.</li>
+<li>_modulegraph_<i>n</i>h_: (>=6.0) (Solo para alertas que usen el comando <i>eMail</i>)
+Devuelve una imagen codificada en base64 de una gráfica del módulo con un período de <i>n</i> horas (eg. _modulegraph_24h_).
+Requiere de una configuración correcta de la conexión del servidor a la consola vía api,
+la cual se realiza en el fichero de configuración del servidor.</li>
 </ul>
 
 <p>

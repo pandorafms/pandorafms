308c538dd7a58ea03b13369b86e867a872dbde51
Junichi Satoh <junichi@junichi.org>
2015-02-22 15:50:29 +0900

    Updated help.

--- pandora_console/include/help/ja/help_alert_macros.php
+++ pandora_console/include/help/ja/help_alert_macros.php
@@ -12,12 +12,19 @@
 <li>_field1_ : ユーザ定義フィールド1</li>
 <li>_field2_ : ユーザ定義フィールド2</li>
 <li>_field3_ : ユーザ定義フィールド3</li>
+<li>_field4_ : ユーザ定義フィールド4</li>
+<li>_field5_ : ユーザ定義フィールド5</li>
+<li>_field6_ : ユーザ定義フィールド6</li>
+<li>_field7_ : ユーザ定義フィールド7</li>
+<li>_field8_ : ユーザ定義フィールド8</li>
+<li>_field9_ : ユーザ定義フィールド9</li>
+<li>_field10_ : ユーザ定義フィールド10</li>
 <li>_agent_ : アラートが発生したエージェント</li>
 <li>_agentcustomfield_<i>n</i>_ : エージェントカスタムフィールド番号<i>n</i> (例: _agentcustomfield_9_). </li>
 <li>_agentcustomid_: エージェントカスタムID</li>
 <li>_agentdescription_ : 発生したアラートの説明</li>
 <li>_agentgroup_ : エージェントグループ名</li>
-<li>_agentstatus : エージェントの現在の状態</li>
+<li>_agentstatus_ : エージェントの現在の状態</li>
 <li>_address_ : アラートが発生したエージェントのアドレス</li>
 <li>_timestamp_ : アラートが発生した日時 (yy-mm-dd hh:mm:ss).</li>
 <li>_timezone_: _timestamp_ で使用されるタイムゾーン名.</li>
@@ -30,7 +37,6 @@
 <li>_modulegroup_ : モジュールグループ名</li>
 <li>_moduledescription_ : アラートが発生したモジュールの説明</li>
 <li>_modulestatus_ : モジュールの状態</li>
-<li>_moduletags_ : モジュールに関連付けられたタグ</li>
 <li>_alert_name_ : アラート名</li>
 <li>_alert_priority_ : アラート優先順位(数値)</li>
 <li>_alert_text_severity_ : テキストでのアラートの重要度 (Maintenance, Informational, Normal Minor, Warning, Major, Critical)</li>
@@ -52,8 +58,10 @@
 <li>_moduletags_ : モジュールタグに関連付けられた URL。</li>
 <li>_alert_critical_instructions_: モジュールの障害状態時手順。</li>
 <li>_alert_warning_instructions_: モジュールの警告状態時手順。</li>
-
+<li>_modulegraph_<i>n</i>h_: (>=6.0) (<i>eMail</i>コマンドを使うアラートのみ)
+n で示す期間の base64 でエンコードされたモジュールグラフを返します。(例: _modulegraph_24h_) サーバとコンソールの API の設定が正しくできている必要があります。この設定は、サーバの設定ファイルで行います。 </li>
 </ul>
+
 <p>
 例: Agent _agent_ has fired alert _alert_ with data _data_
 </p>

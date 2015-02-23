12dacaa9cb877ca4f6e32a25d5c9ec4d581dd4f9
artica <artica.devel@gmail.com>
2015-02-24 00:01:25 +0100

    Auto-updated build strings.

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("5.1SP2(Build 150223)")
+#define PANDORA_VERSION ("5.1SP2(Build 150224)")
 
 string pandora_path;
 string pandora_dir;

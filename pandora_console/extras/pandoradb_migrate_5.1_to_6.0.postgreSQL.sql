fa58fb25ed392cc053cdc148ee71f2b304372b32
Alejandro Gallardo Escobar <alex@artica.es>
2015-03-25 20:55:02 +0100

    Added new columns to the table 'tnetwork_map'

--- pandora_console/extras/pandoradb_migrate_5.1_to_6.0.postgreSQL.sql
+++ pandora_console/extras/pandoradb_migrate_5.1_to_6.0.postgreSQL.sql
@@ -61,3 +61,10 @@ UPDATE "talert_commands" SET "fields_descriptions" = '[\"Destination&#x20;addres
 -- Table `tconfig`
 -- ---------------------------------------------------------------------
 INSERT INTO "tconfig" ("token", "value") VALUES ('post_process_custom_values', '{"0.00000038580247":"Seconds&#x20;to&#x20;months","0.00000165343915":"Seconds&#x20;to&#x20;weeks","0.00001157407407":"Seconds&#x20;to&#x20;days","0.01666666666667":"Seconds&#x20;to&#x20;minutes","0.00000000093132":"Bytes&#x20;to&#x20;Gigabytes","0.00000095367432":"Bytes&#x20;to&#x20;Megabytes","0.0009765625":"Bytes&#x20;to&#x20;Kilobytes","0.00000001653439":"Timeticks&#x20;to&#x20;weeks","0.00000011574074":"Timeticks&#x20;to&#x20;days"}');
+
+-- ---------------------------------------------------------------------
+-- Table `tnetwork_map`
+-- ---------------------------------------------------------------------
+ALTER TABLE "tnetwork_map" ADD COLUMN "id_tag" INTEGER DEFAULT 0;
+ALTER TABLE "tnetwork_map" ADD COLUMN "store_group" INTEGER DEFAULT 0;
+UPDATE "tnetwork_map" SET "store_group" = "id_group";

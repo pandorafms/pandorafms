d9fb4b9b1bb9c2734b5090f667e86b5927a0ccaf
artica <artica.devel@gmail.com>
2015-03-29 00:01:29 +0100

    Auto-updated build strings.

--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -33,7 +33,7 @@ use PandoraFMS::Tools;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "5.1SP2 PS150328";
+my $version = "5.1SP2 PS150329";
 
 # Pandora server configuration
 my %conf;
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -34,7 +34,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "5.1SP2 PS150328";
+my $version = "5.1SP2 PS150329";
 
 # save program name for logging
 my $progname = basename($0);

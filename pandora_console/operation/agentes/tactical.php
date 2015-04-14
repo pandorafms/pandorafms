1a612ff3a8f09ea42e5b1216b7534119f638de12
m-lopez-f <miguel.lopez@artica.es>
2015-04-14 09:56:49 +0200

    Changed style in tactical view

--- pandora_console/operation/agentes/tactical.php
+++ pandora_console/operation/agentes/tactical.php
@@ -246,13 +246,13 @@ $out .= '<table cellpadding=0 cellspacing=0 class="databox" style="margin-top:15
 			<legend>' . 
 				__('Event graph') . 
 			'</legend>' . 
-			grafico_eventos_total("", 180, 80) . '</fieldset>';
+			grafico_eventos_total("", 250, 80) . '</fieldset>';
 	$out .="</td><td>";
 	$out .= '<fieldset class="databox tactical_set">
 			<legend>' . 
 				__('Event graph by agent') . 
 			'</legend>' . 
-			grafico_eventos_grupo(180, 80) . '</fieldset>';
+			grafico_eventos_grupo(250, 80) . '</fieldset>';
 	$out .= '</td></tr></table>';
 echo $out;
 

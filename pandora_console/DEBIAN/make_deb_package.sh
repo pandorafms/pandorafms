9927fa0040c62e894bcfed5402edc412ff328308
artica <artica.devel@gmail.com>
2015-04-18 00:01:30 +0200

    Auto-updated build strings.

--- pandora_console/DEBIAN/make_deb_package.sh
+++ pandora_console/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="5.1SP2-150417"
+pandora_version="5.1SP2-150418"
 
 package_pear=0
 package_pandora=1
--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC150417';
+$build_version = 'PC150418';
 $pandora_version = 'v5.1SP2';
 
 // Do not overwrite default timezone set if defined.

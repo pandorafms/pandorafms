484a5c1c2ca0fcf66a0e962a62e5699608581a5f
Ramon Novoa <rnovoa@artica.es>
2015-04-28 14:59:57 +0200

    Added missing help files.

---
+++ pandora_console/include/help/es/help_metaconsole_agent_cache.php
@@ -0,0 +1,7 @@
+<?php
+/* Include package help/es
+*/
+?>
+
+<p>Para que funcione la caché de agentes de la metaconsola, asegúrese de que <b>Metaconsole DB host</b>, <b>Metaconsole DB name</b>, <b>Metaconsole DB user</b>, <b>Metaconsole DB password</b> y <b>Metaconsole DB port</b> están bien configurados.</p>
+
---
+++ pandora_console/include/help/ja/help_metaconsole_agent_cache.php
@@ -0,0 +1,6 @@
+<?php
+/* Include package help/ja
+*/
+?>
+
+<p>For the metaconsole agent cache to work, Make sure <b>Metaconsole DB host</b>, <b>Metaconsole DB name</b>, <b>Metaconsole DB user</b>, <b>Metaconsole DB password</b> and <b>Metaconsole DB port</b> are properly configured.</p>

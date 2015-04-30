27420a08682f7bd885bb1839b9cb9ddb8d5ac8a1
Ramon Novoa <rnovoa@artica.es>
2015-04-30 13:36:58 +0200

    Execute all previously run actions when an alert recovers.

--- pandora_server/lib/PandoraFMS/Tools.pm
+++ pandora_server/lib/PandoraFMS/Tools.pm
@@ -57,6 +57,8 @@ our @EXPORT = qw(
 	ICMPSERVER
 	SNMPSERVER
 	$DEVNULL
+	RECOVERED_ALERT
+	FIRED_ALERT
     cron_get_closest_in_range
 	cron_next_execution
 	cron_next_execution_date
@@ -105,6 +107,10 @@ use constant WEBSERVER => 9;
 use constant EVENTSERVER => 10;
 use constant ICMPSERVER => 11;
 use constant SNMPSERVER => 12;
+
+# Alert modes
+use constant RECOVERED_ALERT => 0;
+use constant FIRED_ALERT => 1;
 
 # /dev/null
 our $DEVNULL = ($^O eq 'MSWin32') ? '/Nul' : '/dev/null';

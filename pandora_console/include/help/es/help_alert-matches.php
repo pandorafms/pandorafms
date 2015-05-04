f16ba3e1da5879f9e03f36667d27ed0138ffbd9c
Ramon Novoa <rnovoa@artica.es>
2015-05-04 10:54:54 +0200

    Updated the alert-matches help files with documentation from the Wiki.

--- pandora_console/include/help/es/help_alert-matches.php
+++ pandora_console/include/help/es/help_alert-matches.php
@@ -7,7 +7,7 @@
 
 <p>
 Define el número de alertas que deben ocurrir antes de ejecutar la acción. Es un parámetro de ajuste fino.<br><br>
-Esto permite "redefinir" un poco más el comportamiento de la alerta, de forma que si hemos definido un máximo de 5 veces las veces que se puede disparar una alerta, y sólo queremos que nos envie un email, pondremos aquí un 0 y un 1, para decirle que sólo nos envie un email desde la vez 0 a la 1 (osea, una vez).<br><br>
+Esto permite "redefinir" un poco más el comportamiento de la alerta, de forma que si hemos definido un máximo de 5 veces las veces que se puede disparar una alerta, y sólo queremos que nos envie un email, pondremos aquí un 0 y un 1, para decirle que sólo nos envie un email desde la vez 0 a la 1 (osea, una vez). Cuando una alerta se recupera, todas las acciones que se hayan ejecutado hasta ese momento se volverán a ejecutar.<br><br>
 
 Ahora veremos que podemos añadir más acciones a la misma alerta, definiendo con estos campos "Number of alerts match from" el comportamiento de la alerta en función de cuantas veces se dispare.<br><br>
 

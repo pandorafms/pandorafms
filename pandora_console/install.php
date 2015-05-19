0dc669178ba9800918536ef6df979372d3371cb2
artica <artica.devel@gmail.com>
2015-05-20 00:01:17 +0200

    Auto-updated build strings.

--- pandora_console/install.php
+++ pandora_console/install.php
@@ -63,7 +63,7 @@
 		<div style='height: 10px'>
 			<?php
 $version = '6.0dev';
-$build = '150519';
+$build = '150520';
 			$banner = "v$version Build $build";
 			
 			error_reporting(0);
--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -33,7 +33,7 @@ use PandoraFMS::Tools;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "6.0dev PS150519";
+my $version = "6.0dev PS150520";
 
 # Pandora server configuration
 my %conf;

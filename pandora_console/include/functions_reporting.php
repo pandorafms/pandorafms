71ca2f1de74d6caefbd35c475740504887033d44
mdtrooper <tres.14159@gmail.com>
2015-06-01 12:45:42 +0200

    Fixed the SLA item in the reports into metaconsole when some agents into this is disabled.

--- pandora_console/include/functions_reporting.php
+++ pandora_console/include/functions_reporting.php
@@ -4368,6 +4368,12 @@ function reporting_render_report_html_item ($content, $table, $report, $mini = f
 				}
 				
 				if (modules_is_disable_agent($sla['id_agent_module'])) {
+					
+					if (($config ['metaconsole'] == 1) && defined('METACONSOLE')) {
+						//Restore db connection
+						metaconsole_restore_db();
+					}
+					
 					continue;
 				}
 				

73ae001a3fcb8e23b95e6f972b385e9d2acb14ef
m-lopez-f <miguel.lopez@artica.es>
2015-06-01 10:06:42 +0200

    Change privilege in call function to return groups of view this user. Tiquet: #2259

--- pandora_console/godmode/alerts/alert_list.list.php
+++ pandora_console/godmode/alerts/alert_list.list.php
@@ -88,7 +88,7 @@ $form_filter .= "</tr>\n";
 
 $all_groups = db_get_value('is_admin', 'tusuario', 'id_user', $config['id_user']);
 
-$groups_user = users_get_groups($config['id_user'], 'AR', $all_groups);
+$groups_user = users_get_groups($config['id_user'], 'LW', $all_groups);
 if ($groups_user === false) {
 	$groups_user = array();
 }

f2181d66d738c892f404ffc03b8870f8bdd1b22f
m-lopez-f <miguel.lopez@artica.es>
2015-07-21 08:41:26 +0200

    Fixed problems with visual console. Tiquets: #2465,#2483,#2482,#2486

--- pandora_console/include/ajax/skins.ajax.php
+++ pandora_console/include/ajax/skins.ajax.php
@@ -22,7 +22,6 @@ if ($get_image_path) {
 	$img_src = get_parameter("img_src");
 	$only_src = get_parameter("only_src", 0);
 	
-	return html_print_image($img_src, false, '', $only_src);
+	echo html_print_image($img_src, true, '', $only_src);
 }
-
 ?>

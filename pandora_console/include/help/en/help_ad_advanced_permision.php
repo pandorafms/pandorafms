3aac54a84bf0f6d84543587a9d3accb30827d004
m-lopez-f <miguel.lopez@artica.es>
2015-08-06 13:22:44 +0200

    Change help of advance permissions of AD.

--- pandora_console/include/help/en/help_ad_advanced_permision.php
+++ pandora_console/include/help/en/help_ad_advanced_permision.php
@@ -9,5 +9,5 @@
 
 <table width="750px" style="display:inline">
 <tr>
-	<td class=""> If you add a new permission, you was added of next form: Name of Profile,Name of Group,[Name of Group AD n1,Name of Group AD n2,Name of Group AD n3,...]</td>
+	<td class=""> If you add a new permission, you was added of next form: Name of Profile,Name of Group,[Name of Group AD n1-Name of Group AD n2-Name of Group AD n3-...],[Nametag1-NameTag2-NameTagN-...]</td>
 </table>

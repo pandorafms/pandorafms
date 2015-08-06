740536a03482fb69c32f5b383f4d8198d325887d
m-lopez-f <miguel.lopez@artica.es>
2015-08-06 14:55:55 +0200

    Changed text in firts task

--- pandora_console/general/firts_task/fields_manager.php
+++ pandora_console/general/firts_task/fields_manager.php
@@ -26,11 +26,8 @@ ui_require_css_file ('firts_task');
 	</div>
 	<div class="text_task">
 		<h3> <?php echo __('Create Fields Manager'); ?></h3>
-		<p id="description_task"> <?php echo __('The Recon Task definition of Pandora FMS is used to find new elements in the network. 
-		If it detects any item, it will add that item to the monitoring, and if that item it is already being monitored, then it will 
-		ignore it or will update its information.There are three types of detection: Based on <strong id="fuerte"> ICMP </strong>(pings), 
-		<strong id="fuerte">SNMP</strong> (detecting the topology of networks and their interfaces), and other <strong id="fuerte"> customized </strong>
-		type. You can define your own customized recon script.'); ?></p>
+		<p id="description_task"> <?php echo __("Custom fields are an easy way to personalized agent's information.
+				You're able to create custom fields by klicking on 'Administration' -> 'Manage monitoring' -> 'Manage custom fields'. "); ?></p>
 		<form action="index.php?sec=gservers&sec2=godmode/servers/manage_recontask_form&create" method="post">
 			<input type="submit" class="button_task" value="<?php echo __('Create Fields '); ?>" />
 		</form>

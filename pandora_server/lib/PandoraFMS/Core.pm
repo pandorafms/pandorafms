f49cc7b7b88c86cdf58693bf2fd2c246efb514df
Ramon Novoa <rnovoa@artica.es>
2015-08-12 14:40:28 +0200

    Removed an unused variable.

--- pandora_server/lib/PandoraFMS/Core.pm
+++ pandora_server/lib/PandoraFMS/Core.pm
@@ -3317,7 +3317,7 @@ sub process_data ($$$$$$$) {
 
 	# Process INC modules
 	if ($module_type =~ m/_inc$/) {
-		$data = process_inc_data ($pa_config, $data, $agent, $module, $utimestamp, $dbh);
+		$data = process_inc_data ($pa_config, $data, $module, $utimestamp, $dbh);
 		
 		# No previous data or error.
 		return undef unless defined ($data);
@@ -3341,7 +3341,7 @@ sub process_data ($$$$$$$) {
 # Process data of type *_inc.
 ##########################################################################
 sub process_inc_data ($$$$$) {
-	my ($pa_config, $data, $agent, $module, $utimestamp, $dbh) = @_;
+	my ($pa_config, $data, $module, $utimestamp, $dbh) = @_;
 
 	my $data_inc = get_db_single_row ($dbh, 'SELECT * FROM tagente_datos_inc WHERE id_agente_modulo = ?', $module->{'id_agente_modulo'});
 

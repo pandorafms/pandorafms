f5fad7a670f95dd52d528756f2eecff51b8118d1
mdtrooper <tres.14159@gmail.com>
2015-08-21 14:02:55 +0200

    Re-Fixed the availability item in report, now it does not show the diable. TICKET: #2510

--- pandora_console/include/functions_reporting.php
+++ pandora_console/include/functions_reporting.php
@@ -5739,11 +5739,13 @@ function reporting_render_report_html_item ($content, $table, $report, $mini = f
 					}
 				}
 				
-				if (modules_is_disable_agent($item['id_agent_module'])) {
+				if (modules_is_disable_agent($item['id_agent_module'])
+					|| modules_is_not_init($item['id_agent_module'])) {
 					//Restore dbconnection
 					if (($config ['metaconsole'] == 1) && $server_name != '' && defined('METACONSOLE')) {
 						metaconsole_restore_db();
 					}
+					
 					
 					continue;
 				}

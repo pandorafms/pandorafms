e95935244cd388f9ca448756d987bfeb96e6c25e
Arturo Gonzalez Diaz <arturo.gonzalez@artica.es>
2015-09-15 18:09:49 +0200

    Modified help message

--- pandora_console/include/help/en/help_ad_advanced_permision.php
+++ pandora_console/include/help/en/help_ad_advanced_permision.php
@@ -9,5 +9,15 @@
 
 <table width="750px" style="display:inline">
 <tr>
-	<td class=""> If you add a new permission, you was added of next form: Name of Profile,Name of Group,[Name of Group AD n1-Name of Group AD n2-Name of Group AD n3-...],[Nametag1-NameTag2-NameTagN-...]</td>
+	<td class=""> If you add a new permission, you must added the text as follows:
+		<nav>
+		</nav>
+		"Name of Profile, Name of Group, [Name of Group AD n1|Name of Group AD n2|
+		<nav>
+		</nav>
+		Name of Group AD n3|...],[Nametag1|NameTag2|NameTagN|...]"
+		<nav>
+		</nav>
+		Between groups and tags must be "|" as showed in the top example
+	</td>
 </table>

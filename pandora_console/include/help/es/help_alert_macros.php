84c7837935ef87262957c446302ad411fadb1e31
m-lopez-f <miguel.lopez@artica.es>
2015-11-04 14:52:25 +0100

    Translate to spanish from commit Added a new alert and plugin macro, _id_group_. by jsatoh
    (cherry picked from commit e4361bd8600a23e1677de31ec0db12776e5ee288)
    (cherry picked from commit 5b78afc360c6ff3f8d03f192fbb2ee7de7048b77)

--- pandora_console/include/help/es/help_alert_macros.php
+++ pandora_console/include/help/es/help_alert_macros.php
@@ -36,6 +36,7 @@ Además de las macros de módulo definidas, las siguientes macros están disponi
 <li>_eventt_text_severity_: (Solo alertas de evento) Prioridad en texto de el evento que dispara la alerta. (Maintenance, Informational, Normal Minor, Warning, Major, Critical)</li>
 <li>_event_id_ : (Solo alertas de evento) Id del evento que disparó la alerta.</li>
 <li>_id_agent_: ID del agente, util para construir URL de acceso a la consola de Pandora.</li>
+<li>_id_group_ : Id del grupo de agente.</li>
 <li>_id_alert_: ID de la alerta, util para correlar la alerta en herramientas de terceros.</li>
 <li>_policy_: Nombre de la política a la que pertenece el módulo (si aplica).</li>
 <li>_interval_ : Intervalo de la ejecución del módulo. </li>

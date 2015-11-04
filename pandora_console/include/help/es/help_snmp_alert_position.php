4ba1794e056b2b5daa2b643e2ad052fb7ec13f7f
fermin831 <fermin.hernandez@artica.es>
2015-11-04 18:33:50 +0100

    Added help to SNMP alert position

---
+++ pandora_console/include/help/es/help_snmp_alert_position.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* @package Include /help/en
+*/
+?>
+
+<h1>Alert SNMP position</h1>
+
+<p>Las alertas con menor posición se evalúan primero. Si hay varias alertas con la misma posición que coinciden con el trap, se lanzarán todas las alertas coincidentes con la misma posición. Las de menor posición, aunque coincidan, no se lanzarán.</p>
+
+<p>Como regla genereal, las alertas más restrictivas deben tener una posición menor</p>

8fb8d979424b4f3ca416d135985e0737f322384e
Hirofumi Kosaka <kosaka@rworks.jp>
2015-12-01 15:23:15 +0900

    Fixed to allow empty descripcion/module_macros at create/update plugin/data/network modules APIs.

--- pandora_console/include/functions_api.php
+++ pandora_console/include/functions_api.php
@@ -1552,7 +1552,14 @@ function api_set_create_network_module($id, $thrash1, $other, $thrash3) {
 		'min_ff_event_warning' => $other['data'][26],
 		'min_ff_event_critical' => $other['data'][27]
 	);
-	
+
+	if ( ! $values['descripcion'] ) {
+		$values['descripcion'] = '';	// Column 'descripcion' cannot be null
+	}
+	if ( ! $values['module_macros'] ) {
+		$values['module_macros'] = '';	// Column 'module_macros' cannot be null
+	}
+
 	$idModule = modules_create_agent_module($idAgent, $name, $values, true);
 	
 	if (is_error($idModule)) {
@@ -1733,6 +1740,13 @@ function api_set_create_plugin_module($id, $thrash1, $other, $thrash3) {
 		'min_ff_event_critical' => $other['data'][32]
 	);
 	
+	if ( ! $values['descripcion'] ) {
+		$values['descripcion'] = '';	// Column 'descripcion' cannot be null
+	}
+	if ( ! $values['module_macros'] ) {
+		$values['module_macros'] = '';	// Column 'module_macros' cannot be null
+	}
+
 	$idModule = modules_create_agent_module($idAgent, $name, $values, true);
 	
 	if (is_error($idModule)) {
@@ -1912,6 +1926,13 @@ function api_set_create_data_module($id, $thrash1, $other, $thrash3) {
 		'ff_timeout' => $other['data'][23]
 	);
 	
+	if ( ! $values['descripcion'] ) {
+		$values['descripcion'] = '';	// Column 'descripcion' cannot be null
+	}
+	if ( ! $values['module_macros'] ) {
+		$values['module_macros'] = '';	// Column 'module_macros' cannot be null
+	}
+
 	$idModule = modules_create_agent_module($idAgent, $name, $values, true);
 	
 	if (is_error($idModule)) {
@@ -2154,6 +2175,10 @@ function api_set_create_snmp_module($id, $thrash1, $other, $thrash3) {
 		);
 	}
 	
+	if ( ! $values['descripcion'] ) {
+		$values['descripcion'] = '';	// Column 'descripcion' cannot be null
+	}
+
 	$idModule = modules_create_agent_module($idAgent, $name, $values, true);
 	
 	if (is_error($idModule)) {

3109026029ff3733122b7ed83a127ea9dd218b60
artica <artica.devel@gmail.com>
2015-12-05 00:01:38 +0100

    Auto-updated build strings.

--- pandora_console/DEBIAN/make_deb_package.sh
+++ pandora_console/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="5.1SP3-151204"
+pandora_version="5.1SP3-151205"
 
 package_pear=0
 package_pandora=1

56c75d600e535b04acdfdc33429ce9e9e606b8d8
Arturo Gonzalez <arturo.gonzalez@artica.es>
2016-01-29 14:46:50 +0100

    Fixed bug in tree when shows modules with tag configuration. Ticket#3307

--- pandora_console/include/javascript/tree/TreeController.js
+++ pandora_console/include/javascript/tree/TreeController.js
@@ -489,8 +489,10 @@ var TreeController = {
 					// Load the status counters
 					var hasCounters = _processNodeCounters($content, element.counters, element.type);
 					//Don't show empty groups
-					if (!hasCounters) {
-						return;
+					if (element.type == 'agent') {
+						if (!hasCounters) {
+							return;
+						}
 					}
 					// If exist the detail container, show the data
 					if (typeof controller.detailRecipient !== 'undefined') {

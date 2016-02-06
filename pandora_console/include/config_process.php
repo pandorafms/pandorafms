4b96c68af672d7aac0feefa338b1e111f963994f
artica <artica.devel@gmail.com>
2016-02-07 00:01:10 +0100

    Auto-updated build strings.

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC160206';
+$build_version = 'PC160207';
 $pandora_version = 'v6.1dev';
 
 // Do not overwrite default timezone set if defined.
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -35,7 +35,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "6.1dev PS160206";
+my $version = "6.1dev PS160207";
 
 # save program name for logging
 my $progname = basename($0);

42e962c5ede1ae64fe30b3112392cb5ed78bfc07
Arturo Gonzalez <arturo.gonzalez@artica.es>
2016-02-18 10:51:33 +0100

    New maps in progress. Added tooltip plugin to test.

--- pandora_console/include/javascript/map/MapController.js
+++ pandora_console/include/javascript/map/MapController.js
@@ -12,7 +12,8 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
-// Constructor
+/*-------------------Constructor-------------------*/
+
 var MapController = function(target) {
 	this._target = target;
 	
@@ -21,13 +22,20 @@ var MapController = function(target) {
 	this._marginConstant = 50;
 }
 
-// Atributes
+/*--------------------Atributes--------------------*/
+
 MapController.prototype._id = null;
 MapController.prototype._dialogNodeMargin = 0; //To be beauty
 MapController.prototype._marginConstant = 0; //To be beauty
 
 
-// Methods
+/*--------------------Methods----------------------*/
+
+/*
+Function init_map
+Return void
+This function init the map
+*/
 MapController.prototype.init_map = function() {
 	var svg = d3.select("#map svg");
 	
@@ -95,13 +103,28 @@ MapController.prototype.init_map = function() {
 		.attr("style", "fill: rgb(50, 50, 128);")
 		.attr("r", "6");
 
+	//Runs tooltipster plugin
+	$(document).ready(function() {
+            $('.tooltip').tooltipster();
+    });
+
 	this.init_events();
 };
 
+/*
+Function init_events
+Return boolean
+This function init click events in the map
+*/
 MapController.prototype.init_events = function(principalObject) {
 	$(this._target + " svg *, " + this._target + " svg").on("mousedown", {map: this}, this.click_event);
 }
 
+/*
+Function click_event
+Return void
+This function manages mouse clicks and run events in consecuence
+*/
 MapController.prototype.click_event = function(event) {
 	var self = event.data.map;
 	event.preventDefault();
@@ -109,7 +132,7 @@ MapController.prototype.click_event = function(event) {
 	switch (event.which) {
         case 1:
 			if ($(event.currentTarget).attr("class") == "node") {
-				self.popup_map(self, event);
+				self.tooltip_map(self, event);
 			}
             break;
         case 2:
@@ -121,81 +144,22 @@ MapController.prototype.click_event = function(event) {
     }
 }
 
-MapController.prototype.popup_map = function(self, event) {
-	// Node position and radius
-	nodeX = parseInt($(event.currentTarget).attr("cx"));
-	nodeY = parseInt($(event.currentTarget).attr("cy"));
-	nodeR = parseInt($(event.currentTarget).attr("r"));
+/*
+Function popup_map
+Return void
+This function manages nodes tooltips
+*/
+MapController.prototype.tooltip_map = function(self, event) {
+	/*------------------- PRUEBA CON TOOLTIPSTER ----------------------*/
+	$(this._target + " svg").after($("<div>").attr("id", "tooltipster_node"));
 
-	// Map dimensions in pixels
-	var map_width_with_px = $(self._target + " svg").attr("width");
-	var map_height_with_px = $(self._target + " svg").attr("height");
-
-	// Map dimensions in numbers
-	var map_width = parseInt(map_width_with_px.slice(0, map_width_with_px.length - 2));
-	var map_height = parseInt(map_height_with_px.slice(0, map_height_with_px.length - 2));
-
-	// Dialog dimensions
-	var dialog_width = 300;
-	var dialog_height = 200;
-
-	//To know the position of the dialog box
-	if (self.xOffset(map_width, nodeX, dialog_width) && self.yOffset(map_height, nodeY, dialog_height)) {
-		var dialogClass = "nodeDialogBottom";
-		var xPos = event.pageX - dialog_width + nodeR;
-		var yPos = event.clientY - dialog_height - nodeR - self._dialogNodeMargin;
-	}
-	else if (self.yOffset(map_height, nodeY, dialog_height)) {
-		var dialogClass = "nodeDialogBottom2";
-		var xPos = event.pageX - nodeR;
-		var yPos = event.clientY + -dialog_height - nodeR - self._dialogNodeMargin;
-	}
-	else if (self.xOffset(map_width, nodeX, dialog_width)) {
-		var dialogClass = "nodeDialogTop2";
-		var xPos = event.pageX - dialog_width + nodeR;
-		var yPos = event.clientY + nodeR + self._dialogNodeMargin;
-	}
-	else {
-		var dialogClass = "nodeDialogTop";
-		var xPos = event.pageX - nodeR;
-		var yPos = event.clientY + nodeR + self._dialogNodeMargin;
-	}
-
-	$(self._target + " svg").after($("<div>").attr("id", "dialog_popup"));
-	$("#dialog_popup").dialog({
-		dialogClass: dialogClass,
-	  	modal: false,
-		closeOnEscape: true,
-		show: {effect: 'fade', speed: 1000},
-		title: "Node dialog",
-		resizable: false,
-		position: [xPos,yPos],
-		height: dialog_height,
-		width: dialog_width
+	$('#tooltipster_node').tooltipster({
+		arrow: true,
+		autoClose: false,
+		multiple: true,
+		content: 'I\'M A FUCKING TOOLTIP!!'
 	});
 
-}
-
-/*
-Function xOffset
-Return boolean
-This function returns true if dialog cuts map's x axis
-*/
-MapController.prototype.xOffset = function(map_w, node_x, dialog_w) {
-	if ((map_w - node_x - this._marginConstant) < dialog_w) {
-		return true;
-	}
-	return false;
-}
-
-/*
-Function yOffset
-Return boolean
-This function returns true if dialog cuts map's y axis
-*/
-MapController.prototype.yOffset = function(map_h, node_y, dialog_h) {
-	if ((map_h - node_y - this._marginConstant) < dialog_h) {
-		return true;
-	}
-	return false;
+	$('#tooltipster_node').tooltipster("show");
+	/*-----------------------------------------------------------------*/
 }

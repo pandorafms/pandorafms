ad71f09a4abef8d1317a944ddce11685fa466461
artica <artica.devel@gmail.com>
2016-03-27 00:01:11 +0100

    Auto-updated build strings.

--- pandora_agents/unix/pandora_agent.spec
+++ pandora_agents/unix/pandora_agent.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_agent_unix
 %define version     6.1dev
-%define release     160326
+%define release     160327
 
 Summary:            Pandora FMS Linux agent, PERL version
 Name:               %{name}
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -35,7 +35,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "6.1dev PS160326";
+my $version = "6.1dev PS160327";
 
 # save program name for logging
 my $progname = basename($0);

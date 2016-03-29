f59665fe4bdc5bad2bfe7072f2f1fef33fcc1afe
Arturo Gonzalez <arturo.gonzalez@artica.es>
2016-03-29 16:34:41 +0200

    Revert "New login method (saml). Ticket#3393"
    This reverts commit 08569e64e3d47828abfbf7f77d6ca9550a106fb8.
    
    Conflicts:
    	pandora_console/general/login_page.php
    	pandora_console/godmode/setup/setup_auth.php
    	pandora_console/include/constants.php
    	pandora_console/index.php

--- pandora_console/general/login_page.php
+++ pandora_console/general/login_page.php
@@ -124,12 +124,7 @@ echo '<div id="login_in">';
 				'', 'class="login login_password" placeholder="'.__('Password').'"', false, true);
 			echo '</div>';
 			echo '<div class="login_button">';
-			if ($config['auth'] == 'saml') {
-				html_print_submit_button(__("Login with SAML"), "login_button_saml", false, 'class="sub login_boton"');
-			}
-			else {
-				html_print_submit_button(__("Login"), "login_button", false, 'class="sub next_login"');
-			}
+			html_print_submit_button(__("Login"), "login_button", false, 'class="sub next_login"');
 			echo '</div>';
 			break;
 		case 'logout':

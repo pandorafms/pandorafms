6f60aa013f7d25bd82ef18e0ae828893958c3463
artica <artica.devel@gmail.com>
2016-03-30 00:01:56 +0200

    Auto-updated build strings.

--- pandora_agents/unix/pandora_agent.spec
+++ pandora_agents/unix/pandora_agent.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_agent_unix
 %define version     6.0SP1
-%define release     160329
+%define release     160330
 
 Summary:            Pandora FMS Linux agent, PERL version
 Name:               %{name}
--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="6.0SP1-160329"
+pandora_version="6.0SP1-160330"
 
 package_cpan=0
 package_pandora=1

794c2e2e1b06b08697da748ba964d3d6c388d96f
artica <artica.devel@gmail.com>
2016-04-03 00:01:22 +0200

    Auto-updated build strings.

--- pandora_console/pandora_console.redhat.spec
+++ pandora_console/pandora_console.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     6.0SP1
-%define release     160402
+%define release     160403
 
 # User and Group under which Apache is running
 %define httpd_name  httpd
--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="6.0SP1-160402"
+pandora_version="6.0SP1-160403"
 
 package_cpan=0
 package_pandora=1

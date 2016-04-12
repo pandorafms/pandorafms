744e6adaebd71f7512afef9784abdb26a0fce0de
artica <artica.devel@gmail.com>
2016-04-13 00:01:23 +0200

    Auto-updated build strings.

--- pandora_server/pandora_server.redhat.spec
+++ pandora_server/pandora_server.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     6.0SP2
-%define release     160412
+%define release     160413
 
 Summary:            Pandora FMS Server
 Name:               %{name}
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -35,7 +35,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "6.0SP2 PS160412";
+my $version = "6.0SP2 PS160413";
 
 # save program name for logging
 my $progname = basename($0);

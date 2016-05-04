e3cfca24ed82a49b7e3c8114369087fb8e8ed3cf
Alejandro Gallardo Escobar <alex@artica.es>
2016-05-04 19:19:07 +0200

    Added a setup section for ehorus

--- pandora_console/include/functions_config.php
+++ pandora_console/include/functions_config.php
@@ -636,6 +636,20 @@ function config_update_config () {
 				if (!config_update_value ('history_db_delay', get_parameter ('history_db_delay')))
 					$error_update[] = __('Delay');
 				break;
+			case 'ehorus':
+				if (!config_update_value('ehorus_enabled', (int) get_parameter('ehorus_enabled')))
+					$error_update[] = __('Enable eHorus');
+				if (!config_update_value('ehorus_user', get_parameter('ehorus_user')))
+					$error_update[] = __('eHorus user');
+				if (!config_update_value('ehorus_pass', io_input_password(get_parameter('ehorus_pass'))))
+					$error_update[] = __('eHorus password');
+				if (!config_update_value('ehorus_hostname', get_parameter('ehorus_hostname')))
+					$error_update[] = __('eHorus API hostname');
+				if (!config_update_value('ehorus_port', (int) get_parameter('ehorus_port')))
+					$error_update[] = __('eHorus API port');
+				if (!config_update_value('ehorus_req_timeout', (int) get_parameter('ehorus_req_timeout')))
+					$error_update[] = __('eHorus request timeout');
+				break;
 			
 		}
 		
@@ -1435,7 +1449,19 @@ function config_process_config () {
 		config_update_value ('max_file_size', "2M");
 	}
 	
-	
+	// eHorus
+	if (!isset($config['ehorus_enabled'])) {
+		config_update_value('ehorus_enabled', 0);
+	}
+	if (!isset($config['ehorus_hostname'])) {
+		config_update_value('ehorus_hostname', 'switch.ehorus.com');
+	}
+	if (!isset($config['ehorus_port'])) {
+		config_update_value('ehorus_port', 18080);
+	}
+	if (!isset($config['ehorus_req_timeout'])) {
+		config_update_value('ehorus_req_timeout', 5);
+	}
 	
 	/* Finally, check if any value was overwritten in a form */
 	config_update_config();

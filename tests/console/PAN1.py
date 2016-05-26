37697d9df657d212181b3161eb206ff23d9c0acd
axl89 <axl89@users.noreply.github.com>
2016-05-26 22:31:00 +0200

    Added several functions and refactorized all console CI tests

--- tests/console/PAN1.py
+++ tests/console/PAN1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from include.common_classes_60 import PandoraWebDriverTestCase
-from include.common_functions_60 import login
+from include.common_functions_60 import login, click_menu_element
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -17,8 +17,7 @@ class PAN1(PandoraWebDriverTestCase):
 	def test_pan1(self):
 		driver = self.driver
 		login(driver,"admin","pandora",self.base_url)
-		element = driver.find_element_by_css_selector("#subSetup > li.sub_subMenu > a > div.submenu_text.submenu2_text_middle")
-		driver.execute_script("arguments[0].click();", element)
+		click_menu_element(driver,"General Setup")
 		self.assertEqual("IP list with API access", driver.find_element_by_id("table2-15-0").text)
 	
 if __name__ == "__main__":

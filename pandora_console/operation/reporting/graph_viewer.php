63b7e5c43c741e6523f7ec84f28f0eb991caff62
Alejandro Gallardo Escobar <alex@artica.es>
2016-05-30 19:31:01 +0200

    Fixed the custom line thickness of the flot line graphs (cherry picked from commit 6e2cddc48d9a01058837e82f0e4110f037241f6a)

--- pandora_console/operation/reporting/graph_viewer.php
+++ pandora_console/operation/reporting/graph_viewer.php
@@ -116,7 +116,7 @@ if ($view_graph) {
 		include ("general/noaccess.php");
 		exit;
 	}
-	html_print_input_hidden ('lineWidhtGraph', $config['custom_graph_width']);
+	html_print_input_hidden ('line_width_graph', $config['custom_graph_width']);
 	html_print_input_hidden ('custom_graph', 1);
 	$url = "index.php?" .
 		"sec=reporting&" .

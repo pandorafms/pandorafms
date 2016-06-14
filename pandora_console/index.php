5e6080c354c1952790734fb102b89fc77299daf3
fermin831 <fermin.hernandez@artica.es>
2016-06-14 15:12:39 +0200

    Fixed download messages for first time

--- pandora_console/index.php
+++ pandora_console/index.php
@@ -536,9 +536,11 @@ $config['logged'] = false;
 extensions_load_extensions ($process_login);
 
 // Check for update manager messages
-if (license_free() && is_user_admin ($config['id_user']) && $config['last_um_check'] > (time() + 2 * SECONDS_1HOUR)) {
+if (license_free() && is_user_admin ($config['id_user']) && 
+	(($config['last_um_check'] > (time() + 2 * SECONDS_1HOUR)) || 
+	(!isset($config['last_um_check'])))) {
+		
 	require_once("include/functions_update_manager.php");
-	
 	update_manager_download_messages ();
 }
 

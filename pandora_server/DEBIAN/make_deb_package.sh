7c0c461920362a1c449bc8981960662286780f5e
artica <artica.devel@gmail.com>
2016-06-17 00:01:25 +0200

    Auto-updated build strings.

--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="6.0SP2-160616"
+pandora_version="6.0SP2-160617"
 
 package_cpan=0
 package_pandora=1

3e46dcd52ff0ddc1ed67878ac0c0b371b992b737
cesar991 <cesar.espinosa@artica.es>
2016-06-20 17:13:01 +0200

    Add function for delete report

--- tests/console/PAN4.py
+++ tests/console/PAN4.py
@@ -1,7 +1,25 @@
 
+
+# -*- coding: utf-8 -*-
+from include.common_classes_60 import PandoraWebDriverTestCase
+from include.common_functions_60 import login, click_menu_element, refresh_N_times_until_find_element, detect_and_pass_all_wizards, create_user, is_element_present, create_report, logout, delete_report
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import StaleElementReferenceException
+import unittest, time, re
+ 
+ 
+class PAN4(PandoraWebDriverTestCase):
+
+	test_name = u'PAN_4'
+	test_description = u'Creates a user with Chief Operator permissions over the Applications group. Then creates two reports: one in the Applications group and other in the Servers group. Then, it checks that the given user can only see the Application report'
+	tickets_associated = []
+
 def test_pan4(self):
 	driver = self.driver
-	login(driver,"admin","pandora",self.base_url)
+	login(driver)
 	detect_and_pass_all_wizards(driver)
 
 	#Creates a user with Chief Operator - Applications profile
@@ -14,10 +32,10 @@ def test_pan4(self):
 	create_report(driver,"PAN_4_Servers","Servers")
 
 	#Logout
-	logout(driver,'http://127.0.0.1:85')
+	logout(driver,self.base_url)
 
 	#Login
-	login(driver,user='PAN_4',passwd='PAN_4',self.base_url)
+	login(driver,user='PAN_4',passwd='PAN_4')
 	detect_and_pass_all_wizards(driver)
 
 	#Check that the report is visible
@@ -35,14 +53,15 @@ def test_pan4(self):
 	driver.find_element_by_id('submit-search_submit').click()
 	
 	self.assertEqual("No data found." in driver.page_source,True)
-	
+
 	#Delete reports
 	logout(driver,self.base_url)
-	login(driver,self.base_url)
+	login(driver)
 
 	delete_report(driver,"PAN_4_Servers")
 	delete_report(driver,"PAN_4_Applications")
 
 
 if __name__ == "__main__":
-        unittest.main()
+	unittest.main()
+

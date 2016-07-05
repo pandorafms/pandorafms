487d36936e807369f1cd589c6441d1aeaa0227fa
cesar991 <cesar.espinosa@artica.es>
2016-07-05 10:55:56 +0200

    Complete function create_agent

--- tests/console/include/common_functions_60.py
+++ tests/console/include/common_functions_60.py
@@ -183,10 +183,22 @@ def search_agent(driver,agent_name):
 	driver.find_element_by_id("submit-srcbutton").click()
 	driver.find_element_by_css_selector("b").click()
 
-def create_agent(driver,agent_name):
+def create_agent(driver,agent_name,ip=None,description=None,group=None,os_id=None):
+
 	click_menu_element(driver,"Agent detail")
 	driver.find_element_by_id("submit-crt").click()
-	driver.find_element_by_id("text-agente").click()
-	driver.find_element_by_id("text-agente").clear()
 	driver.find_element_by_id("text-agente").send_keys(agent_name)
-	driver.find_element_by_id("submit-crtbutton").click()
+        
+	if ip != None:
+		driver.find_element_by_id("text-direccion").clear()
+		driver.find_element_by_id("text-direccion").send_keys(ip)
+			
+	if description != None:
+		driver.find_element_by_id("text-comentarios").clear()
+		driver.find_element_by_id("text-comentarios").send_keys(description)
+			
+	if group != None:
+		Select(driver.find_element_by_id("grupo")).select_by_visible_text(group)
+		
+	if os_id !=None:
+		Select(driver.find_element_by_id("id_os")).select_by_visible_text(os_id)

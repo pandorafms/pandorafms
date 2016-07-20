de9d88f6fd2bacfe1f647574bcc7f818cd07d650
cesar991 <cesar.espinosa@artica.es>
2016-07-20 13:45:51 +0200

    Added search_policy and add_module_policy functions in policy_functions.py

--- tests/console/include/policy_functions.py
+++ tests/console/include/policy_functions.py
@@ -21,3 +21,20 @@ def create_policy(driver,policy_name,group,description=None):
 		driver.find_element_by_id("textarea_description").send_keys(description)
 	
 	driver.find_element_by_id("submit-crt").click()
+
+
+def search_policy(driver,policy_name,go_to_policy=True):
+	click_menu_element(driver,"Manage policies")
+	driver.find_element_by_id("text-text_search").clear()
+	driver.find_element_by_id("text-text_search").send_keys(policy_name)
+	driver.find_element_by_id("submit-submit").click()
+	# If go_to_policy is True, this function enter in options of this policy
+	
+	if go_to_policy == True:
+		driver.find_element_by_xpath('//*[@id="policies_list-0-1"]/span/strong/a/span').click()
+	
+
+def add_module_policy(driver,policy_name,module_type,*args,**kwargs):
+	search_policy(driver,policy_name,go_to_policy=True)
+	driver.find_element_by_xpath('//*[@id="menu_tab"]/ul/li[2]/a/img').click()
+	create_module(module_type,*args,**kwargs)

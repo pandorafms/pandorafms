9865bf7afffca8c22dbb3cbd5da96fa6e7336206
fbsanchez <fborja.sanchez@artica.es>
2016-07-29 15:44:07 +0200

    Ticket 3462: Provided snmp help/std help based on referer

--- pandora_console/godmode/alerts/alert_commands.php
+++ pandora_console/godmode/alerts/alert_commands.php
@@ -110,7 +110,14 @@ if (is_ajax ()) {
 				}
 				//If the field is the number one, print the help message
 				if ($i == 1) {
-					$fdesc .= ui_print_help_icon ('alert_config', true);
+					// If our context is snmpconsole, show snmp_alert helps
+					if ((isset ($_SERVER["HTTP_REFERER"])) && ( preg_match ("/snmp_alert/", $_SERVER["HTTP_REFERER"]) > 0 )){
+                        $fdesc .= ui_print_help_icon ('snmp_alert_field1',true);
+                    }
+                    else {
+                        $fdesc .= ui_print_help_icon ('alert_config', true);
+                    }
+
 				}
 			}
 			else {

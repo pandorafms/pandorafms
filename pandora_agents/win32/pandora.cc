c3c94682d3b8ed58a8fb748ad789a0b97d339311
artica <artica.devel@gmail.com>
2016-08-07 00:02:34 +0200

    Auto-updated build strings.

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("5.1SP4(Build 160806)")
+#define PANDORA_VERSION ("5.1SP4(Build 160807)")
 
 string pandora_path;
 string pandora_dir;
--- pandora_server/pandora_server.redhat.spec
+++ pandora_server/pandora_server.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     5.1SP4
-%define release     160806
+%define release     160807
 
 Summary:            Pandora FMS Server
 Name:               %{name}

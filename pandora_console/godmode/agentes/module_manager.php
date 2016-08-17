68f5c5a3ba71648ab4374a6965943d01ca0b03c0
m-lopez-f <miguel.lopez@artica.es>
2016-08-17 12:07:48 +0200

    Fixed problems with  search form in Policies form. Tiquet: #3246

--- pandora_console/godmode/agentes/module_manager.php
+++ pandora_console/godmode/agentes/module_manager.php
@@ -29,9 +29,17 @@ require_once ('include/functions_servers.php');
 
 $search_string = io_safe_output(urldecode(trim(get_parameter ("search_string", ""))));
 
+global $policy_page;
+
+if (!isset($policy_page))
+	$policy_page = false;
+
 // Search string filter form
 //echo '<form id="create_module_type" method="post" action="index.php?sec=gagente&sec2=godmode/agentes/configurar_agente&tab=module&id_agente='.$id_agente.'">';
-echo '<form id="create_module_type" method="post" action="'.$url.'">';
+if (($policy_page) || (isset($agent)))
+	echo '<form id="" method="post" action="">';
+else
+	echo '<form id="create_module_type" method="post" action="'.$url.'">';
 echo '<table width="98%" cellpadding="2" cellspacing="2" class="databox" >';
 echo "<tr><td class='datos' style='width:20%'>";
 echo __('Search') . ' ' .
@@ -91,11 +99,6 @@ if (strstr($sec2, "enterprise/godmode/policies/policies") !== false) {
 	//modules from the same policy.
 	unset($modules['predictionserver']);
 }
-
-global $policy_page;
-
-if (!isset($policy_page))
-	$policy_page = false;
 
 $show_creation = false;
 

b82fbe2c5365085541f5801ff2c64a684e0c41bc
artica <artica.devel@gmail.com>
2016-09-12 00:01:31 +0200

    Auto-updated build strings.

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("6.0SP3(Build 160910)")
+#define PANDORA_VERSION ("6.0SP3(Build 160912)")
 
 string pandora_path;
 string pandora_dir;
--- pandora_console/pandora_console.redhat.spec
+++ pandora_console/pandora_console.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     6.0SP3
-%define release     160910
+%define release     160912
 
 # User and Group under which Apache is running
 %define httpd_name  httpd

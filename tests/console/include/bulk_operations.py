885854ab93e4c9d467ee578d0dc40ed220cf5ead
cesar991 <cesar.espinosa@artica.es>
2016-09-13 12:06:27 +0200

    Add delete_module_in_bulk_operation funtion and add test_D in Bulk_operations file

--- tests/console/include/bulk_operations.py
+++ tests/console/include/bulk_operations.py
@@ -43,3 +43,48 @@ def edit_agents_in_bulk(driver,agent_names_list,new_group=None,new_description=N
 
 	driver.find_element_by_id("submit-updbutton").click()
 
+
+def delete_modules_in_bulk(driver,agent_name_list,module_name_list,select_agent_first=None):
+
+	#If select_agent_first is None, select the modules first
+	
+	#To erase all modules with this names, add "Any" in agent_name_list
+	#To erase all modules of the agent put "Any" in module_name_list
+
+	click_menu_element(driver,"Module operations")
+	
+	driver.find_element_by_id("option").click()
+	Select(driver.find_element_by_id("option")).select_by_visible_text("Delete modules in bulk")
+	
+	if select_agent_first != None:
+		
+		driver.find_element_by_id("radiobtn0002").click()
+		
+		for agent_name in agent_name_list:
+			Select(driver.find_element_by_id("id_agents")).select_by_visible_text(agent_name)
+
+		time.sleep(3)
+
+		
+		for module_name in module_name_list:
+			Select(driver.find_element_by_id("module")).select_by_visible_text(module_name)
+
+	else:
+
+		#driver.find_element_by_id("module_type").click()
+		Select(driver.find_element_by_id("module_type")).select_by_visible_text("All")
+
+		time.sleep(3)
+
+		for module_name in module_name_list:
+			Select(driver.find_element_by_id("module_name")).select_by_visible_text(module_name)
+		
+		time.sleep(3)
+		
+		for agent_name in agent_name_list:
+			Select(driver.find_element_by_id("agents")).select_by_visible_text(agent_name)
+
+
+	driver.find_element_by_id("submit-go").click()
+
+

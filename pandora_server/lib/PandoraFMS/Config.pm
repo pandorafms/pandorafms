b41c9aace276d49ec838c010b952018202892b52
artica <artica.devel@gmail.com>
2016-09-15 00:01:37 +0200

    Auto-updated build strings.

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -43,7 +43,7 @@ our @EXPORT = qw(
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "6.0SP3";
-my $pandora_build = "160914";
+my $pandora_build = "160915";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash

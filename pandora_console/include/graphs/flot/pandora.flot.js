2e270c75697df4b99bb1228c3ba9695d2c729542
m-lopez-f <miguel.lopez@artica.es>
2016-09-16 11:19:52 +0200

    Change transparency of colors in graphs. Ticket: #4046
    (cherry picked from commit 6f89eba5192edbbee29637c3d236e99984a7f78c)

--- pandora_console/include/graphs/flot/pandora.flot.js
+++ pandora_console/include/graphs/flot/pandora.flot.js
@@ -980,9 +980,9 @@ function pandoraFlotArea(graph_id, values, labels, labels_long, legend,
 			//threshold: [{ below: 80, color: "rgb(200, 20, 30)" } , { below: 65, color: "rgb(30, 200, 30)" }, { below: 50, color: "rgb(30, 200, 30)" }],
 			lines: {
 				show: line_show,
-				fill: filled,
+				fill: 0.2,
 				fillColor: {
-					colors: [ { opacity: 0.9 }, { opacity: 0.9 } ]
+					colors: [ { opacity: 0.9 }, { opacity: 0.6 } ]
 				},
 				lineWidth: lineWidth,
 				steps: steps_chart

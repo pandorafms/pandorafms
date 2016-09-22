6f33c0eaf877030ef6ecea00b73cdc74c3609969
enriquecd <enrique.camargo@artica.com>
2016-09-22 11:12:19 +0200

    modificacion de estilos de ventana modal y editor de usuarios

--- pandora_console/operation/system_alert.php
+++ pandora_console/operation/system_alert.php
@@ -2,6 +2,23 @@
 
 // TODO: Poner esto resizable y dragable: http://jqueryui.com/demos/dialog
 
-echo get_pandora_error_for_header();
+echo "
+<div class='modalheader'>
+<span class='modalheadertext'>Community version</span>
+<img class='modalclosex cerrar' src='".$config['homeurl']."/images/icono_cerrar.png'>
+</div>".get_pandora_error_for_header()."
+
+<div class='modalokbutton cerrar'>
+<span class='modalokbuttontext close'>OK</span>
+</div>";
 
 ?>
+
+<script>
+
+$(".cerrar").click(function(){
+  $("#alert_messages").hide();
+  $( "#opacidad" ).remove();
+});
+
+</script>

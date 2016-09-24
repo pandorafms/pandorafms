e08a7f2505d115ba7b016feecd8e481704de81d5
artica <artica.devel@gmail.com>
2016-09-25 00:01:11 +0200

    Auto-updated build strings.

--- pandora_agents/unix/DEBIAN/make_deb_package.sh
+++ pandora_agents/unix/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="6.1dev-160924"
+pandora_version="6.1dev-160925"
 
 echo "Test if you has the tools for to make the packages."
 whereis dpkg-deb | cut -d":" -f2 | grep dpkg-deb > /dev/null
--- pandora_console/pandora_console.spec
+++ pandora_console/pandora_console.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     6.1dev
-%define release     160924
+%define release     160925
 %define httpd_name      httpd
 # User and Group under which Apache is running
 %define httpd_name  apache2

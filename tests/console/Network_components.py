c3d07bfe12562e77d21e609aa8d781693f2c81b5
cesar991 <cesar.espinosa@artica.es>
2016-09-26 13:32:09 +0200

    Add a new tests file Network_components.py and repair errors in network_components_functions.py

---
+++ tests/console/Network_components.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from include.common_classes_60 import PandoraWebDriverTestCase
+from include.common_functions_60 import login, click_menu_element, detect_and_pass_all_wizards, gen_random_string
+from include.agent_functions import search_agent,create_agent,delete_agent
+from include.api_functions import *
+from include.module_functions import search_module
+from include.network_components_functions import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.remote.webelement import WebElement
+
+import unittest2, time, re
+
+
+class PAN13(PandoraWebDriverTestCase):
+
+
+	test_name = u'Planned_downtime'
+	test_description = u'Planed downtime test'
+	tickets_associated = []
+
+	def test_A_create_network_component(self):
+
+		u"""
+		Create and search new network component module
+		"""
+		
+		driver = self.driver
+		self.login()
+		detect_and_pass_all_wizards(driver)
+		
+		agent_name = gen_random_string(6)
+		network_component_name = gen_random_string(6)
+
+		activate_api(driver,"1234")
+
+		params = [agent_name,"127.0.0.1","0","4","0","300","2","pandorafms","2","0","0","pruebas"]
+		create_agent_api(driver,params,user="admin",pwd="pandora")
+
+		lista = driver.current_url.split('/')
+
+		url = lista[0]+'//'+lista[2]+'/pandora_console'
+
+		driver.get(url)
+			
+		create_network_component(driver,network_component_name,"Remote TCP network agent, boolean data","Network Management","Application",min_warning=10,max_critical=100,description="New network component by test")
+
+		search_agent(driver,agent_name,go_to_agent=True)
+
+		driver.find_element_by_xpath('//ul[@class="mn"]/li/a/img[@data-title="Manage"]').click()
+		driver.find_element_by_xpath('//ul[@class="mn"]/li/a/img[@data-title="Modules"]').click()
+
+		Select(driver.find_element_by_id("moduletype")).select_by_visible_text("Create a new network server module")
+
+		driver.find_element_by_xpath('//*[@id="main"]/form/table/tbody/tr/td[5]/input').click()
+
+		driver.find_element_by_xpath('//a[contains(.,"Advanced options")]').click()
+
+		Select(driver.find_element_by_id("network_component_group")).select_by_visible_text("Network Management")
+
+		time.sleep(3)
+			
+		Select(driver.find_element_by_id("network_component")).select_by_visible_text(network_component_name)
+
+		driver.find_element_by_id("submit-crtbutton").click()
+
+		search_module (driver,agent_name,network_component_name,go_to_module=False)
+
+		self.assertEqual(network_component_name in driver.page_source,True)
+
+
+
+if __name__ == "__main__":
+	unittest2.main()
+
+
+
+	

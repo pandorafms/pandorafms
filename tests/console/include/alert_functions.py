b4079041a4d35e65d4c39e1c7f9084e8c9ebbec3
cesar991 <cesar.espinosa@artica.es>
2016-09-28 12:53:30 +0200

    Add test alert and new functions in alert_functions.py
    (cherry picked from commit 00519707ba42a44ed768ca724b09fa627fde76e3)

--- tests/console/include/alert_functions.py
+++ tests/console/include/alert_functions.py
@@ -31,6 +31,62 @@ def force_alert_of_module(driver,agent_name,module_name,template_name):
         time.sleep(10)
 
 
+def create_new_action_to_alert(driver,action_name,action_group,command,threshold=None,field1=None,field2=None,field3=None):
+
+	click_menu_element(driver,"Actions")
+	driver.find_element_by_id("submit-create").click()
+
+	driver.find_element_by_id("text-name").clear()
+	driver.find_element_by_id("text-name").send_keys(action_name)
+
+	driver.find_element_by_xpath('//option[contains(.,"'+action_group+'")]').click()
+
+	driver.find_element_by_xpath('//option[contains(.,"'+command+'")]').click()
+
+	if threshold != None:
+
+		driver.find_element_by_id("text-action_threshold").clear()
+		driver.find_element_by_id("text-action_threshold").send_keys(threshold)
+
+	if command == "eMail" and field1 != None and field2 != None and field3 != None:
+
+		driver.find_element_by_id("textarea_field1_value").clear()
+		driver.find_element_by_id("textarea_field1_value").send_keys(field1)
+
+		driver.find_element_by_id("textarea_field2_value").clear()
+		driver.find_element_by_id("textarea_field2_value").send_keys(field2)
+
+		driver.find_element_by_id("textarea_field3_value").clear()
+		driver.find_element_by_id("textarea_field3_value").send_keys(field3)
 
 
+	driver.find_element_by_id("submit-create").click()
+
+
+def create_new_command_to_alert(driver,command_name,command,list_field_description,list_field_values,description=None):
+
+	click_menu_element(driver,"Commands")
+	driver.find_element_by_id("submit-create").click()
+	
+	driver.find_element_by_id("text-name").clear()
+	Select(driver.find_element_by_id("text-name")).send_keys(command_name)
+	
+	driver.find_element_by_id("textarea_command").clear()
+	Select(driver.find_element_by_id("textarea_command")).send_keys(command)
+	
+	if description != None:
+		
+		driver.find_element_by_id("textarea_description").clear()
+		Select(driver.find_element_by_id("textarea_description")).send_keys(description)	
+	i=1
+	for field_description in list_field_description:
+		driver.find_element_by_id("text-field"+i+"_description").clear()
+		Select(driver.find_element_by_id("text-field"+i+"_description")).send_keys(field_description)
+	
+	i=1
+	for field_value in list_field_values:
+		driver.find_element_by_id("text-field"+i+"_description").clear()
+		Select(driver.find_element_by_id("text-field"+i+"_description")).send_keys(field_value)
+		
+	driver.find_element_by_id("submit-create").click()
 

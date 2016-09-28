4baea754bb714a962e8359bafc7ebdbe9ca6c0ae
Arturo Gonzalez <arturo.gonzalez@artica.es>
2016-09-28 10:45:46 +0200

    New networkmaps in progress... (Added filter to links)

--- pandora_console/include/functions_pandora_networkmap.php
+++ pandora_console/include/functions_pandora_networkmap.php
@@ -676,6 +676,8 @@ function networkmap_write_js_array($id, $nodes_and_relations = array()) {
 			continue;
 		if ($link_js['source'] == -1)
 			continue;
+		if ($link_js['target'] == $link_js['source']) 
+			continue;
 		
 		echo "networkmap.links.push(" . json_encode($link_js) . ");\n";
 	}

33e680c8ad7ebf2b47d9e6469c25e3d3514364d6
enriquecd <enrique.camargo@artica.com>
2016-10-03 12:27:59 +0200

    add qr icon, separate menu bar, favicon and optional graph logo 46,64,45,80,55
    (cherry picked from commit 359f0aaac126e1f22ac682a1d48e7a667187d354)

--- pandora_console/include/functions_config.php
+++ pandora_console/include/functions_config.php
@@ -1027,7 +1027,12 @@ function config_process_config () {
 
 	
 	if (!isset ($config["custom_logo"])) {
+		if(enterprise_installed()){
+		config_update_value ('custom_logo', 'pandora_logo_head.png');
+	}
+	else{
 		config_update_value ('custom_logo', 'pandora_logo_head_3.png');
+	}
 	}
 	if (!isset ($config["custom_logo_login"])) {
 		config_update_value ('custom_logo_login', 'login_logo.png');

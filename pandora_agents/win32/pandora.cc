4cb0f50d50d1f973a2204c2c241242a02a3e728e
artica <artica.devel@gmail.com>
2016-10-23 00:01:36 +0200

    Auto-updated build strings.

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("6.0SP3(Build 161022)")
+#define PANDORA_VERSION ("6.0SP3(Build 161023)")
 
 string pandora_path;
 string pandora_dir;
--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC161022';
+$build_version = 'PC161023';
 $pandora_version = 'v6.0SP3';
 
 // Do not overwrite default timezone set if defined.

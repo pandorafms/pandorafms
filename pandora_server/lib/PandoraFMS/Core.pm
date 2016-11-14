8b81dcd1f92dc56cc5151aef69d3e7fa33753e01
Ramon Novoa <rnovoa@artica.es>
2016-11-14 13:53:00 +0100

    Remove the timestamp when creating modules from a hash.

--- pandora_server/lib/PandoraFMS/Core.pm
+++ pandora_server/lib/PandoraFMS/Core.pm
@@ -2662,6 +2662,9 @@ sub pandora_create_module_from_hash ($$$) {
 	if (defined $parameters->{'id_network_component_group'}) {
 		delete $parameters->{'id_network_component_group'};
 	}
+	if (defined $parameters->{'timestamp'}) {
+		delete $parameters->{'timestamp'};
+	}
 
 	# Encrypt plug-in passwords.
 	if (defined($parameters->{'plugin_pass'})) {

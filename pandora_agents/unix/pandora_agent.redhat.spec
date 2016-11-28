26b50bd95ecb3033e19c37de1067071a2b73925d
artica <artica.devel@gmail.com>
2016-11-29 00:01:17 +0100

    Auto-updated build strings.

--- pandora_agents/unix/pandora_agent.redhat.spec
+++ pandora_agents/unix/pandora_agent.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_agent_unix
 %define version     7.0dev
-%define release     161126
+%define release     161129
 
 Summary:            Pandora FMS Linux agent, PERL version
 Name:               %{name}
--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("7.0dev(Build 161126)")
+#define PANDORA_VERSION ("7.0dev(Build 161129)")
 
 string pandora_path;
 string pandora_dir;

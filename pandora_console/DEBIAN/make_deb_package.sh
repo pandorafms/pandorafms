5d3f859942a0c7a68df3bcba620480e319a9c9cb
artica <artica.devel@gmail.com>
2016-12-18 00:01:41 +0100

    Auto-updated build strings.

--- pandora_console/DEBIAN/make_deb_package.sh
+++ pandora_console/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="5.1SP4-161217"
+pandora_version="5.1SP4-161218"
 
 package_pear=0
 package_pandora=1
--- pandora_server/pandora_server.redhat.spec
+++ pandora_server/pandora_server.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     5.1SP4
-%define release     161217
+%define release     161218
 
 Summary:            Pandora FMS Server
 Name:               %{name}

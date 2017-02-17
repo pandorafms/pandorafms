f02827d235cbce75233e372bfd1ef0f93d8487c6
Arturo Gonzalez <arturo.gonzalez@artica.es>
2017-02-17 12:43:12 +0100

    Changed field type in db to get negative numbers. Ticket #120

--- pandora_console/pandoradb.sql
+++ pandora_console/pandoradb.sql
@@ -1068,7 +1068,7 @@ CREATE TABLE IF NOT EXISTS `tusuario` (
 	`metaconsole_access_node` tinyint(1) unsigned NOT NULL default 0,
 	`strict_acl` tinyint(1) unsigned NOT NULL DEFAULT 0,
 	`id_filter`  int(10) unsigned NULL default NULL,
-	`session_time` int(10) unsigned NOT NULL default 0,
+	`session_time` int(10) signed NOT NULL default 0,
 	CONSTRAINT `fk_filter_id` FOREIGN KEY (`id_filter`) REFERENCES tevent_filter (`id_filter`) ON DELETE SET NULL,
 	UNIQUE KEY `id_user` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

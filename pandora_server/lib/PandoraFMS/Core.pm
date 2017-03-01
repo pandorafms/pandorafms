1c88b6370b082e653ac84fcd58be311864201913
Ramon Novoa <rnovoa@artica.es>
2017-03-01 11:18:29 +0100

    Make sure the alias is always set.

--- pandora_server/lib/PandoraFMS/Core.pm
+++ pandora_server/lib/PandoraFMS/Core.pm
@@ -2896,6 +2896,7 @@ sub pandora_create_agent ($$$$$$$$$$;$$$$$$$$$) {
 	}
 	
 	$agent_mode = 1 unless defined($agent_mode);
+	$alias = $agent_name unless defined($alias);
 
 	$description = "Created by $server_name" unless ($description ne '');	
 	my ($columns, $values) = db_insert_get_values ({ 'nombre' => safe_input($agent_name),

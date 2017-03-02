31ea5bf5ecb61dfc4ad254ff3cee643e7d03eb18
fermin831 <fermin.hernandez@artica.es>
2017-03-02 16:36:54 +0100

    Added Time lapse in #434

---
+++ pandora_console/include/help/es/help_reporting_time_lapse.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Reports - Time lapse</h1>
+<br>
+Este es el lapso de tiempo que refleja el informe. Si es una semana, por ejemplo, el informe mostrará los datos desde hace una semana hasta ahora
---
+++ pandora_console/include/help/ja/help_reporting_time_lapse.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package Include/help/en
+ */
+?>
+<h1>Reports - Time lapse</h1>
+<br>
+This is the range, or period of time over which the report renders the information for this report type. For example, a week means data from a week ago from now. 

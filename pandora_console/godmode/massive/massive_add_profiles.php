1dce7623f830bb04f66d69a6752a4b5ad4c231d0
enriquecd <enrique.camargo@artica.com>
2017-03-10 13:58:36 +0100

    Massive operation profile actions - users select box  now is  ordered by user_id(primary name) - #92

--- pandora_console/godmode/massive/massive_add_profiles.php
+++ pandora_console/godmode/massive/massive_add_profiles.php
@@ -103,8 +103,8 @@ $data[2] = '<span id="alerts_loading" class="invisible">';
 $data[2] .= html_print_image('images/spinner.png', true);
 $data[2] .= '</span>';
 $users_profiles = "";
-
-$data[2] .= html_print_select (users_get_info(), 'users_id[]', '', '', '', 
+$users_order = array('field' => 'full_name', 'order' => 'ASC');
+$data[2] .= html_print_select (users_get_info($users_order), 'users_id[]', '', '', '', 
 	'', true, true, true, '', false, 'width: 100%');
 
 

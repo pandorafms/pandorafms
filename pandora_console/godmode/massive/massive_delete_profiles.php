d87dae44db19989a6d18cef13bb6d26af51bd571
enriquecd <enrique.camargo@artica.com>
2017-03-13 11:50:37 +0100

    Select user_id(name inserted manually by user) info to show and order (fullname is empty often) - 92

--- pandora_console/godmode/massive/massive_delete_profiles.php
+++ pandora_console/godmode/massive/massive_delete_profiles.php
@@ -118,7 +118,7 @@ $data[2] .= html_print_image('images/spinner.png', true);
 $data[2] .= '</span>';
 $users_profiles = "";
 $users_order = array('field' => 'id_user', 'order' => 'ASC');
-$data[2] .= html_print_select (users_get_info($users_order), 'users_id[]', '', '', '', 
+$data[2] .= html_print_select (users_get_info($users_order,'id_user'), 'users_id[]', '', '', '', 
 	'', true, true, true, '', false, 'width: 100%');
 
 

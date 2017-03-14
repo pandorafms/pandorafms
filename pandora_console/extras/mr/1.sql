01945c3cb59f922685e3adea65ba8fb22d7e9be3
vanessa gil <vanessa.gil@artica.es>
2017-03-14 16:56:00 +0100

    new mr

--- pandora_console/extras/mr/1.sql
+++ pandora_console/extras/mr/1.sql
@@ -1,4 +1,5 @@
 START TRANSACTION;
+
 CREATE TABLE IF NOT EXISTS `ttable_test_nueva` (
 	`id` int(10) unsigned NOT NULL auto_increment,
 	`field1` varchar(60) NOT NULL default '',
@@ -6,4 +7,5 @@ CREATE TABLE IF NOT EXISTS `ttable_test_nueva` (
 	PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 ALTER TABLE `tusuario` ADD COLUMN `test_nuevo` tinyint(1) NOT NULL DEFAULT 0;
+
 COMMIT;

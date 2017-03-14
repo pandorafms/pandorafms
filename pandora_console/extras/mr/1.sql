cc39f6d1a83aab54e2f43fd84a8b353a63ff387b
vanessa gil <vanessa.gil@artica.es>
2017-03-14 16:46:21 +0100

    New file.

--- pandora_console/extras/mr/1.sql
+++ pandora_console/extras/mr/1.sql
@@ -1,12 +1,9 @@
-BEGIN
-
+BEGIN transaction;
 CREATE TABLE IF NOT EXISTS `ttable_test_nueva` (
 	`id` int(10) unsigned NOT NULL auto_increment,
 	`field1` varchar(60) NOT NULL default '',
 	`field2` int(10) unsigned NOT NULL default '0',
 	PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
 ALTER TABLE `tusuario` ADD COLUMN `test_nuevo` tinyint(1) NOT NULL DEFAULT 0;
-
-COMMIT
+COMMIT;

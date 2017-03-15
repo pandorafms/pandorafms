f00cf520cecedec73c7e698578f1e00d82856da7
Daniel Maya <daniel.maya@artica.es>
2017-03-15 11:43:13 +0100

    Fixed problem with the module graph type line in the public visual console

--- pandora_console/include/graphs/fgraph.php
+++ pandora_console/include/graphs/fgraph.php
@@ -573,7 +573,11 @@ function line_graph($flash_chart, $chart_data, $width, $height, $color,
 		$graph['percentil'] = $percentil_values;
 		$id_graph = serialize_in_temp($graph, null, $ttl);
 		
-		return "<img src='" . $homeurl . "include/graphs/functions_pchart.php?static_graph=1&graph_type=line&ttl=".$ttl."&id_graph=" . $id_graph . "' />";
+		if(empty($homeurl)){
+			return "<img src='" . ui_get_full_url (false, false, false, false) . "include/graphs/functions_pchart.php?static_graph=1&graph_type=line&ttl=".$ttl."&id_graph=" . $id_graph . "' />";
+		}else{
+			return "<img src='" . $homeurl . "include/graphs/functions_pchart.php?static_graph=1&graph_type=line&ttl=".$ttl."&id_graph=" . $id_graph . "' />";
+		}
 	}
 }
 

132560108296ea50fbe595795d9359919836b669
daniel <daniel.barbero@artica.es>
2017-03-16 18:41:53 +0100

    fixed errors

--- pandora_console/general/login_page.php
+++ pandora_console/general/login_page.php
@@ -58,7 +58,7 @@ if (!empty ($page) && !empty ($sec)) {
 $login_body_style = '';
 // Overrides the default background with the defined by the user
 if (!empty($config['login_background'])) {
-	$background_url = ui_get_full_url("images/backgrounds/") . $config['login_background'];
+	$background_url = "../../images/backgrounds/" . $config['login_background'];
 	$login_body_style = "style=\"background-image: url('$background_url');\"";
 }
 echo '<div id="login_body" ' . $login_body_style . '></div>';

cb1cd58d83976b7018980714feb97c1b50613e72
vanessa gil <vanessa.gil@artica.es>
2017-03-17 11:36:10 +0100

    new file

---
+++ pandora_console/extras/mr/2.sql
@@ -0,0 +1,11 @@
+START TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS `ttable_test` (
+	`id` int(10) unsigned NOT NULL auto_increment,
+	`field1` varchar(60) NOT NULL default '',
+	`field2` int(10) unsigned NOT NULL default '0',
+	PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+ALTER TABLE `tusuario` ADD COLUMN `test` tinyint(1) NOT NULL DEFAULT 0;
+
+COMMIT;

40b3c318cd4acc7ddbf19e526e85e19c89a820df
artica <artica.devel@gmail.com>
2017-03-18 00:01:19 +0100

    Auto-updated build strings.

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("7.0NG(Build 170317)")
+#define PANDORA_VERSION ("7.0NG(Build 170318)")
 
 string pandora_path;
 string pandora_dir;
--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -43,7 +43,7 @@ our @EXPORT = qw(
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "7.0NG";
-my $pandora_build = "170317";
+my $pandora_build = "170318";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash

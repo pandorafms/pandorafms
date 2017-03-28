f2b0d28d63c3074b60590abca366eccce3508ec5
mlopez <miguel.lopez@artica.es>
2017-03-28 14:54:57 +0200

    Update functions_flot.php

--- pandora_console/include/graphs/functions_flot.php
+++ pandora_console/include/graphs/functions_flot.php
@@ -54,8 +54,6 @@ function include_javascript_dependencies_flot_graph($return = false) {
 				ui_get_full_url($metaconsole_hack . '/include/graphs/flot/jquery.flot.exportdata.pandora.js') .'"></script>
 			<script language="javascript" type="text/javascript" src="'.
 				ui_get_full_url($metaconsole_hack . '/include/graphs/flot/jquery.flot.axislabels.js') .'"></script>
-			<script language="javascript" type="text/javascript" src="'.
-				ui_get_full_url($metaconsole_hack . '/include/graphs/flot/jquery.flot.image.js') .'"></script>
 			<script language="javascript" type="text/javascript" src="'.				
 				ui_get_full_url($metaconsole_hack . '/include/graphs/flot/pandora.flot.js') .'"></script>';
 		$output .= "

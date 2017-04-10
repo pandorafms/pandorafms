f8d3c8804ecc917da5478dbfbae52f393e2f58e3
m-lopez-f <miguel.lopez@artica.es>
2017-04-10 17:46:32 +0200

    Gis Maps Feature: When scrolling in map, change font-size of labels.

--- pandora_console/operation/gis_maps/public_console.php
+++ pandora_console/operation/gis_maps/public_console.php
@@ -14,7 +14,7 @@
 
 // Real start
 
-require_once ("../../include/config.php");
+include_once ("../../include/config.php");
 
 // Set root on homedir, as defined in setup
 chdir ($config["homedir"]);

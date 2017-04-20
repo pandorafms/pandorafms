82530c9b597762e846fe5fd363829d134b43346b
Arturo Gonzalez <arturo.gonzalez@artica.es>
2017-04-20 16:18:37 +0200

    Added external query to mr

--- pandora_console/extras/mr/1.sql
+++ pandora_console/extras/mr/1.sql
@@ -2,4 +2,12 @@ START TRANSACTION;
 
 ALTER TABLE tusuario add default_event_filter int(10) unsigned NOT NULL DEFAULT 0;
 
+CREATE TABLE IF NOT EXISTS `treset_pass` (
+    `id` bigint(10) unsigned NOT NULL auto_increment,
+    `id_user` varchar(100) NOT NULL default '',
+    `cod_hash` varchar(100) NOT NULL default '',
+    `reset_time` int(10) unsigned NOT NULL default 0,
+    PRIMARY KEY (`id`) 
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 COMMIT;

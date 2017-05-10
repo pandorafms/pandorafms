530eb2e6e86bc008b05234cf27cf6d66dca571c7
Arturo Gonzalez <arturo.gonzalez@artica.es>
2017-05-10 13:37:14 +0200

    Added boolean charts to agent graphs section

--- pandora_console/operation/agentes/graphs.php
+++ pandora_console/operation/agentes/graphs.php
@@ -78,6 +78,31 @@ $modules_others = agents_get_modules(
 if (empty($modules_others))
 	$modules_others = array();
 
+$modules_boolean = agents_get_modules(
+	$id_agente, false, array(
+		'id_tipo_modulo' => array(
+			'<>1',
+			'<>3',
+			'<>4',
+			'<>5',
+			'<>7',
+			'<>8',
+			'<>10',
+			'<>11',
+			'<>15',
+			'<>16',
+			'<>17',
+			'<>22',
+			'<>23',
+			'<>24',
+			'<>30',
+			'<>32',
+			'<>33',
+			'<>100')
+		));
+if (empty($modules_boolean))
+	$modules_boolean = array();
+
 //Cleaned the duplicate $modules and other things
 $modules_others = array_diff_key($modules_others,
 	$modules_networkmap_no_proc);
@@ -91,10 +116,14 @@ foreach ($modules_networkmap_no_proc as $i => $m) {
 		'optgroup' => __('Modules network no proc'),
 		'name' => $m);
 }
-
+foreach ($modules_boolean as $i => $m) {
+	$modules_boolean[$i] = array(
+		'optgroup' => __('Modules boolean'),
+		'name' => $m);
+}
 
 $list_modules = $modules_networkmap_no_proc +
-	$modules_others;
+	$modules_others + $modules_boolean;
 //----------------------------------------------------------------------
 
 if (empty($modules)) {

54b70e5927e22e750028fcef976e463cbabe9705
Arturo Gonzalez <arturo.gonzalez@artica.es>
2017-05-26 12:55:37 +0200

    Changed custom graph size specs in visual console

--- pandora_console/operation/visual_console/render_view.php
+++ pandora_console/operation/visual_console/render_view.php
@@ -254,7 +254,7 @@ $ignored_params['refr'] = '';
 			}
 			
 			startCountDown(refr, false);
-			//~ // Auto hide controls
+			
 			var controls = document.getElementById('vc-controls');
 			autoHideElement(controls, 1000);
 			
@@ -315,24 +315,6 @@ $ignored_params['refr'] = '';
 			height = parseInt($(this).css("height")) - 30;
 			$(this).css('height', height);
 		});
-		
-		/*
-		$('.percentile_item a > img').each(function(){
-			
-			if($(this).css('float')=='left' || $(this).css('float')=='right'){
-				
-				
-			$(this).css('margin-top',(parseInt($(this).parent().parent().css('height'))/2-parseInt($(this).css('height'))/2)+'px');
-			$(this).css('margin-left','');
-			
-			}
-			else{
-				$(this).css('margin-left',(parseInt($(this).parent().parent().css('width'))/2-parseInt($(this).css('width'))/2)+'px');
-				$(this).css('margin-top','');
-			}
-			
-		});
-		*/
 	
 	});
 </script>

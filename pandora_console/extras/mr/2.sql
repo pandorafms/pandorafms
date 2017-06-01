efd28c0513b582f8e92c980a48dec24e378d0e91
Arturo Gonzalez <arturo.gonzalez@artica.es>
2017-06-01 11:28:24 +0200

    Added Enrique Camargo db changes to mr number 2

--- pandora_console/extras/mr/2.sql
+++ pandora_console/extras/mr/2.sql
@@ -2,4 +2,6 @@ START TRANSACTION;
 
 ALTER TABLE tagent_custom_fields ADD is_password_type tinyint(1) NOT NULL DEFAULT 0;
 
+ALTER TABLE treport_content ADD COLUMN historical_db tinyint(1) NOT NULL DEFAULT 0;
+
 COMMIT;

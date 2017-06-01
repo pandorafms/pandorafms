7a9f63f2122aa2fcca72039ab0a21bfce59a5aff
Arturo Gonzalez <arturo.gonzalez@artica.es>
2017-06-01 14:30:34 +0200

    Added change to db

--- pandora_console/extras/mr/2.sql
+++ pandora_console/extras/mr/2.sql
@@ -4,4 +4,6 @@ ALTER TABLE tagent_custom_fields ADD is_password_type tinyint(1) NOT NULL DEFAUL
 
 ALTER TABLE treport_content ADD COLUMN historical_db tinyint(1) NOT NULL DEFAULT 0;
 
+ALTER TABLE tpolicy_modules ADD COLUMN ip_target varchar(100) default '';
+
 COMMIT;

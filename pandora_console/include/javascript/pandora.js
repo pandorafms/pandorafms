ced80503172da172f15b76710b34b357be6fbeb1
enriquecd <enrique.camargo@artica.com>
2017-06-05 12:50:22 +0200

    Decode html entities in module plugin selector for edit bulk operation - #959

--- pandora_console/include/javascript/pandora.js
+++ pandora_console/include/javascript/pandora.js
@@ -1184,3 +1184,13 @@ var autoHideElement = function (element, hideTime) {
 	// Start hide
 	startHideTimeout(hideTime);
 }
+
+function htmlEncode(value){
+  // Create a in-memory div, set its inner text (which jQuery automatically encodes)
+  // Then grab the encoded contents back out. The div never exists on the page.
+  return $('<div/>').text(value).html();
+}
+
+function htmlDecode(value){
+  return $('<div/>').html(value).text();
+}

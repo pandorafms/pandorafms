d2d56b4b89893be9a1b73513017360fc29b1d7f3
Ramon Novoa <rnovoa@artica.es>
2017-06-15 13:47:20 +0200

    Add a missing MR.

--- pandora_console/extras/mr/2.sql
+++ pandora_console/extras/mr/2.sql
@@ -1,9 +1,25 @@
 START TRANSACTION;
 
-ALTER TABLE tagent_custom_fields ADD is_password_type tinyint(1) NOT NULL DEFAULT 0;
-
-ALTER TABLE treport_content ADD COLUMN historical_db tinyint(1) NOT NULL DEFAULT 0;
-
-ALTER TABLE tpolicy_modules ADD COLUMN ip_target varchar(100) default '';
-
+DROP PROCEDURE IF EXISTS addcol_oum706;
+																																																										 
+delimiter '//'
+CREATE PROCEDURE addcol_oum706() BEGIN
+	   IF NOT EXISTS (
+			   SELECT * FROM information_schema.columns WHERE table_name='treport_content' AND column_name='historical_db'
+	   ) THEN
+			   ALTER TABLE treport_content ADD COLUMN historical_db tinyint(1) UNSIGNED NOT NULL default 0;
+	   END IF;																																																						   
+	   IF NOT EXISTS (
+			   SELECT * FROM information_schema.columns WHERE table_name='tpolicy_modules' AND column_name='ip_target'
+	   ) THEN
+			ALTER TABLE tpolicy_modules ADD COLUMN ip_target varchar(100) default '';
+	   END IF;																																																						   
+END;																																																									 
+//																																																									   
+																																																										 
+delimiter ';'																																																							
+CALL addcol_oum706();
+DROP PROCEDURE addcol_oum706;
+																																																										 
 COMMIT;
+

89ea98f5054100db1b459b8e9278516311bb8552
artica <artica.devel@gmail.com>
2017-06-18 00:01:14 +0200

    Auto-updated build strings.

--- pandora_console/pandora_console.redhat.spec
+++ pandora_console/pandora_console.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     7.0NG.705
-%define release     170617
+%define release     170618
 
 # User and Group under which Apache is running
 %define httpd_name  httpd
--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -43,7 +43,7 @@ our @EXPORT = qw(
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "7.0NG.705";
-my $pandora_build = "170617";
+my $pandora_build = "170618";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash

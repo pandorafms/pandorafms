45c40b400d819e1a627f9f444ab4d179bf09becf
artica <artica.devel@gmail.com>
2017-07-18 00:02:24 +0200

    Auto-updated build strings.

--- pandora_console/pandoradb_data.sql
+++ pandora_console/pandoradb_data.sql
@@ -39,7 +39,7 @@ INSERT INTO `tconfig` (`token`, `value`) VALUES
 ('step_compact','1'),
 ('db_scheme_first_version', '5.1dev'),
 ('db_scheme_version','5.1SP4'),
-('db_scheme_build','PD170717'),
+('db_scheme_build','PD170718'),
 ('show_unknown','0'),
 ('show_lastalerts','1'),
 ('style','pandora'),

240b6e017fe39e2eb71cfbdf66879b6e946709ec
enriquecd <enrique.camargo@artica.com>
2017-07-26 13:50:55 +0200

    Adapt historical_db checkbox for sql reports to sql templates - #1157

--- pandora_console/godmode/reporting/reporting_builder.php
+++ pandora_console/godmode/reporting/reporting_builder.php
@@ -1124,8 +1124,8 @@ switch ($action) {
 							$values['treport_custom_sql_id'] = get_parameter('id_custom');
 							if ($values['treport_custom_sql_id'] == 0) {
 								$values['external_source'] = get_parameter('sql');
-								$values['historical_db'] = get_parameter('historical_db_check');
 							}
+							$values['historical_db'] = get_parameter('historical_db_check');
 						}
 						else if ($values['type'] == 'url') {
 							$values['external_source'] = get_parameter('url');
@@ -1213,7 +1213,6 @@ switch ($action) {
 									}
 									break;
 							}
-							
 							
 							$resultOperationDB = db_process_sql_update(
 								'treport_content',
@@ -1446,8 +1445,8 @@ switch ($action) {
 							$values['treport_custom_sql_id'] = get_parameter('id_custom');
 							if ($values['treport_custom_sql_id'] == 0) {
 								$values['external_source'] = get_parameter('sql');
-								$values['historical_db'] = get_parameter('historical_db_check');
 							}
+							$values['historical_db'] = get_parameter('historical_db_check');
 						}
 						elseif ($values['type'] == 'url') {
 							$values['external_source'] = get_parameter('url');

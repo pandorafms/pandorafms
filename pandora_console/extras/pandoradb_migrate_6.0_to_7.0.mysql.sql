ea06ba213bb387060143b37c27bc64b4c3f88ee7
Arturo Gonzalez <arturo.gonzalez@artica.es>
2017-08-03 13:05:57 +0200

    Added registration method to reset pass

--- pandora_console/extras/pandoradb_migrate_6.0_to_7.0.mysql.sql
+++ pandora_console/extras/pandoradb_migrate_6.0_to_7.0.mysql.sql
@@ -1357,7 +1357,8 @@ INSERT INTO `tcontainer` SET `name` = 'Default graph container';
 CREATE TABLE IF NOT EXISTS `treset_pass_history` (
 	`id` int(10) unsigned NOT NULL auto_increment,
 	`id_user` varchar(60) NOT NULL,
-	`reset_moment` date default NULL,
+	`reset_moment` datetime NOT NULL,
+	`success` tinyint(1) NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 

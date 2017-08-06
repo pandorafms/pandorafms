5164624dcad97bd3f6b93ff46adfdf310ba11aa1
artica <artica.devel@gmail.com>
2017-08-07 00:01:14 +0200

    Auto-updated build strings.

--- pandora_agents/unix/DEBIAN/make_deb_package.sh
+++ pandora_agents/unix/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="7.0NG.709-170806"
+pandora_version="7.0NG.709-170807"
 
 echo "Test if you has the tools for to make the packages."
 whereis dpkg-deb | cut -d":" -f2 | grep dpkg-deb > /dev/null

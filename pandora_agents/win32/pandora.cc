b973e12a511d480cba6d170109eb2e6ba424850d
artica <artica.devel@gmail.com>
2017-08-13 00:01:12 +0200

    Auto-updated build strings.

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("7.0NG.710(Build 170812)")
+#define PANDORA_VERSION ("7.0NG.710(Build 170813)")
 
 string pandora_path;
 string pandora_dir;
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -35,7 +35,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "7.0NG.710 PS170812";
+my $version = "7.0NG.710 PS170813";
 
 # save program name for logging
 my $progname = basename($0);

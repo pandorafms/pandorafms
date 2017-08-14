c08e17084f65af5ca68f2ec1f56ddfa8a613c1f4
artica <artica.devel@gmail.com>
2017-08-15 00:01:13 +0200

    Auto-updated build strings.

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC170814';
+$build_version = 'PC170815';
 $pandora_version = 'v7.0NG.710';
 
 // Do not overwrite default timezone set if defined.
--- pandora_server/pandora_server.redhat.spec
+++ pandora_server/pandora_server.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     7.0NG.710
-%define release     170814
+%define release     170815
 
 Summary:            Pandora FMS Server
 Name:               %{name}

19f1c3610e2665d091de2a1a70e259bea99b322b
artica <artica.devel@gmail.com>
2017-08-19 00:01:13 +0200

    Auto-updated build strings.

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC170818';
+$build_version = 'PC170819';
 $pandora_version = 'v7.0NG.711';
 
 // Do not overwrite default timezone set if defined.

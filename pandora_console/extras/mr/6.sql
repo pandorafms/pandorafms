068be133eda14c89c4bc79dbe0b4e7245120d26d
enriquecd <enrique.camargo@artica.com>
2017-08-28 12:22:59 +0200

    Add password reset table in MR - #1211

--- pandora_console/extras/mr/6.sql
+++ pandora_console/extras/mr/6.sql
@@ -3,3 +3,11 @@ START TRANSACTION;
 ALTER TABLE tagente MODIFY COLUMN `cascade_protection_module` int(10) unsigned NOT NULL default '0';
 
 COMMIT;
+
+CREATE TABLE IF NOT EXISTS treset_pass_history (
+    id int(10) unsigned NOT NULL auto_increment,
+    id_user varchar(60) NOT NULL,
+    reset_moment datetime NOT NULL,
+    success tinyint(1) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

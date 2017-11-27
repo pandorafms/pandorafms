192175a4cc8521b48193541893e44a95fffcc235
Arturo Gonzalez <arturo.gonzalez@artica.es>
2017-11-27 13:52:31 +0100

    Added custom graphviz fields to map creation

--- pandora_console/operation/agentes/pandora_networkmap.php
+++ pandora_console/operation/agentes/pandora_networkmap.php
@@ -218,6 +218,16 @@ if ($new_networkmap || $save_networkmap) {
 		$offset_x = get_parameter('pos_x');
 		$offset_y = get_parameter('pos_y');
 		$scale_z = get_parameter('scale_z', 0.5);
+
+		$node_sep = get_parameter('node_sep', "0.25");
+		if ($method == "twopi") {
+			$rank_sep = get_parameter('rank_sep', "1.0");
+		}
+		else {
+			$rank_sep = get_parameter('rank_sep', "0.5");
+		}
+		$mindist = get_parameter('mindist', "1.0");
+		$kval = get_parameter('kval', "0.3");
 		
 		$values = array();
 		$values['name'] = $name;
@@ -278,6 +288,11 @@ if ($new_networkmap || $save_networkmap) {
 		$filter['x_offs'] = $offset_x;
 		$filter['y_offs'] = $offset_y;
 		$filter['z_dash'] = $scale_z;
+		$filter['node_sep'] = $node_sep;
+		$filter['rank_sep'] = $rank_sep;
+		$filter['mindist'] = $mindist;
+		$filter['kval'] = $kval;
+
 		$values['filter'] = json_encode($filter);
 		
 		$result = false;

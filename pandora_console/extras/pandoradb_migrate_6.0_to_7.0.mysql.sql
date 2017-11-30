688736288c53425c95fa3d64acf62ed91b1a043c
enriquecd <enrique.camargo@artica.com>
2017-11-30 12:46:29 +0100

    Change treport_content_template fileds order - #1326

--- pandora_console/extras/pandoradb_migrate_6.0_to_7.0.mysql.sql
+++ pandora_console/extras/pandoradb_migrate_6.0_to_7.0.mysql.sql
@@ -725,10 +725,10 @@ CREATE TABLE IF NOT EXISTS `treport_content_template` (
 	`module_free_text` TEXT,
 	`each_agent` tinyint(1) default 1,
 	`historical_db` tinyint(1) UNSIGNED NOT NULL default 0,
-	`hide_no_data` tinyint(1) default 0,
 	`lapse_calc` tinyint(1) UNSIGNED NOT NULL default '0',
 	`lapse` int(11) UNSIGNED NOT NULL default '300',
 	`visual_format` tinyint(1) UNSIGNED NOT NULL default '0',
+	`hide_no_data` tinyint(1) default 0,
 	PRIMARY KEY(`id_rc`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 

fbab43b45137a231c035096ffa0889199478efdf
Arturo Gonzalez <arturo.gonzalez@artica.es>
2017-11-30 08:04:56 +0100

    In progress

--- pandora_console/include/help/es/help_network_map_enterprise.php
+++ pandora_console/include/help/es/help_network_map_enterprise.php
@@ -4,21 +4,19 @@
 */
 ?>
 
-<h1>Networkmap console</h1>
+<h1>Mapas de red</h1>
 
-<p>Con la version Enterprise puede crear mapas de red editables de una manera mas interactiva comparado con la version Open que esta actualmente en la subseccion &#34;Ver agentes&#34;.</p>
+<p>Con la version Enterprise puede crear mapas de red editables de una manera mas interactiva comparado con la version Open que no cuenta con muchas de las opciones de manipulación de elementos.</p>
 
-<p>En contraste con la version Open, el mapa de red proprociona mas funcionalidades como:</p>
+<p>El mapa de red proprociona algunas funcionalidades como:</p>
 
 
-<li>Mapa de red mas grande, con mas de 1000 agentes para monitorizar.</li>
 <li>Monitorizar en tiempo real toda la topologia de la red con sus sistemas.</li>
 <li>Diferentes vistas de la topologia de red, definidas de una forma manual o generadas automaticamente con grupos de agentes.</li>
 <li>Enlazar diferentes vistas mediante el uso de puntos ficticios.</li>
-<li>Manipular la topologia representada en cada una de las vistas.</li>
 <li>A&ntilde;adir nuevos nodos, uno por uno o de forma masiva.</li>
-<li>Editando las caracteristicas de los nodos.</li>
-<li>Organizandolos dentro de la vista:<br>
+<li>Editar las caracteristicas de los nodos.</li>
+<li>Organizar dentro de la vista:<br>
             - La posición de los nodos.<br>
             - Las relaciones entre los nodos.</li>
 

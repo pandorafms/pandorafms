f61f2914904e594c292d1a44418d119bc734bbfb
fbsanchez <fborja.sanchez@artica.es>
2017-12-18 19:41:23 +0100

    1532 Minor fix

--- pandora_server/util/pandora_perf_report.pl
+++ pandora_server/util/pandora_perf_report.pl
@@ -339,8 +339,8 @@ Memory: $mem
 Metric                              Value             Reference value for small/medium/large systems (*)
 ------                              -----             --------------------------------------------------
 
-CPU ops/ms                     $cpu_score             (NA/NA/NA)
-DB INSERT/s                    $db_scores->{'insert_score'}             (1584.42/2326.07/3128.54)
+CPU ops/ms                     $cpu_score             (1624.84/2347.42/3102.42)
+DB INSERT/s                    $db_scores->{'insert_score'}             (2884.42/2326.07/3128.54)
 DB UPDATE/s                    $db_scores->{'update_score'}             (2820.54/6954.10/8580.51)
 DB SELECT/s                    $db_scores->{'select_score'}             (4632.42/7194.24/10051.32)
 DB DELETE/s                    $db_scores->{'delete_score'}             (3192.37/6657.79/8933.77)

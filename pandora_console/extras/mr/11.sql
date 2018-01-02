ff60278060c7648e5a5058e3ab4a78097133d0b8
daniel <daniel.barbero@artica.es>
2018-01-02 13:54:08 +0100

    fixed error in bbdd tsnmp_filter

--- pandora_console/extras/mr/11.sql
+++ pandora_console/extras/mr/11.sql
@@ -2,4 +2,7 @@ START TRANSACTION;
 
 ALTER TABLE `tlayout` ADD `is_favourite` int(1) NOT NULL DEFAULT 0;
 
+SELECT max(unified_filters_id) INTO @max FROM tsnmp_filter;
+UPDATE tsnmp_filter tsf,(SELECT @max:= @max) m SET tsf.unified_filters_id = @max:= @max + 1 where tsf.unified_filters_id=0;
+
 COMMIT;

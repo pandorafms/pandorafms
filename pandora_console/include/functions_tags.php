407fbf450f84eb26d8ad9ad73628a4b1d227494a
fbsanchez <fborja.sanchez@artica.es>
2018-02-16 15:09:37 +0100

    Fixed permission checks in event_acl Added event response macro _group_custom_id_

--- pandora_console/include/functions_tags.php
+++ pandora_console/include/functions_tags.php
@@ -1482,7 +1482,7 @@ function tags_checks_event_acl($id_user, $id_group, $access, $tags = array(), $c
 			$group_ids = implode(',', $childrens_ids);
 		}
 		$sql = "SELECT id_usuario FROM tusuario_perfil
-					WHERE id_usuario = '".$config["id_user"]."' AND tags IN ('$tags_str')
+					WHERE id_usuario = '".$config["id_user"]."' AND (tags IN ('$tags_str') OR tags = '')
 					AND id_perfil IN (SELECT id_perfil FROM tperfil WHERE ".get_acl_column($access)."=1)
 					AND id_grupo IN ($group_ids)";
 		$has_perm = db_get_value_sql ($sql);

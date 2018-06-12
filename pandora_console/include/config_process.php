e4461273ab6d9816b4560b0d21321dad1761f5de
artica <git@artica.es>
2018-06-12 17:28:15 +0200

    Updated version and build strings.

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -23,7 +23,7 @@
  * Pandora build version and version 
  */
 $build_version = 'PC180612';
-$pandora_version = 'v7.0NG.723';
+$pandora_version = 'v7.0NG.724';
 
 // Do not overwrite default timezone set if defined.
 $script_tz = @date_default_timezone_get();
--- pandora_console/install.php
+++ pandora_console/install.php
@@ -70,7 +70,7 @@
 	<body>
 		<div style='height: 10px'>
 			<?php
-$version = '7.0NG.723';
+$version = '7.0NG.724';
 $build = '180612';
 			$banner = "v$version Build $build";
 			

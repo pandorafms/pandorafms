a258dc7e43fe8f0c3f3f1c049dbeccedee5a1970
Alejandro Gallardo Escobar <alejandro.gallardo@artica.es>
2018-06-12 10:04:33 +0200

    Fixed a message

--- pandora_console/include/functions_ui.php
+++ pandora_console/include/functions_ui.php
@@ -3922,7 +3922,7 @@ function ui_get_using_system_timezone_warning ($tag = "h3", $return = true) {
 	$user_offset = (-get_fixed_offset() / 60) / 60;
 
 	$message = sprintf(
-		__("This controls are using the timezone of the system (%s) instead of yours (%s). The difference in hours to your timezone is %s."),
+		__("These controls are using the timezone of the system (%s) instead of yours (%s). The difference with your time zone in hours is %s."),
 		$config["timezone"],
 		date_default_timezone_get(),
 		$user_offset > 0 ? "+" . $user_offset : $user_offset

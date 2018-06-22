e19f1166370b30e89747950b32c4c0634d9c110f
fermin831 <fermin.hernandez@artica.es>
2018-06-22 13:33:04 +0200

    Fixed add particular agents to GIS layers

--- pandora_console/include/functions_gis.php
+++ pandora_console/include/functions_gis.php
@@ -446,37 +446,27 @@ function gis_add_agent_point($layerName, $pointName, $lat, $lon, $icon = null, $
  * Get the agents in layer but not by group in layer.
  * 
  * @param integer $idLayer Layer ID.
- * @param array $fields Fields of row tagente to return.
  * 
  * @return array The array rows of tagente of agents in the layer.
  */
-function gis_get_agents_layer($idLayer, $fields = null) {
-	
-	if ($fields === null) {
-		$select = '*';
-	}
-	else {
-		$select = implode(',',$fields);
-	}
-	
-	$sql = "SELECT $select
+function gis_get_agents_layer($idLayer) {
+
+	$sql = "SELECT id_agente, nombre
 			FROM tagente
 			WHERE id_agente IN (
 				SELECT tagente_id_agente
 				FROM tgis_map_layer_has_tagente
 				WHERE tgis_map_layer_id_tmap_layer = $idLayer)";
 	$agents = db_get_all_rows_sql($sql);
-	
+
+	$returned_agents = array();
 	if ($agents !== false) {
 		foreach ($agents as $index => $agent) {
-			$agents[$index] = $agent['nombre'];
+			$returned_agents[$agent['id_agente']] = $agent['nombre'];
 		}
 	}
-	else {
-		return array();
-	}
-	
-	return $agents;
+
+	return $returned_agents;
 }
 
 function gis_add_point_path($layerName, $lat, $lon, $color, $manual = 1, $id) {

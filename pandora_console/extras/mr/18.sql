f430b8998f6074c7338f2c8f0c271d829f40c2b0
fbsanchez <fborja.sanchez@artica.es>
2018-07-03 12:01:37 +0200

    Added DB changes to support ProvisioningServer 2

--- pandora_console/extras/mr/18.sql
+++ pandora_console/extras/mr/18.sql
@@ -10,4 +10,40 @@ ALTER TABLE `tmetaconsole_agent` ADD COLUMN `cps` int NOT NULL DEFAULT '0';
 
 ALTER TABLE `tagente_modulo` ADD COLUMN `cps` int NOT NULL DEFAULT '0';
 
+CREATE TABLE `tautoconfig` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `order` int(11) NOT NULL DEFAULT '0',
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `tautoconfig_rules` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_autoconfig` int(10) unsigned NOT NULL,
+  `order_by` int(11) NOT NULL DEFAULT '0',
+  `operator` enum('AND','OR') DEFAULT 'OR',
+  `type` enum('alias','ip-range','group','os','custom-field','script') DEFAULT 'alias',
+  `value` varchar(100) NOT NULL DEFAULT '',
+  `custom` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `id_autoconfig` (`id_autoconfig`),
+  CONSTRAINT `tautoconfig_rules_ibfk_1` FOREIGN KEY (`id_autoconfig`) REFERENCES `tautoconfig` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `tautoconfig_actions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_autoconfig` int(10) unsigned NOT NULL,
+  `order` int(11) NOT NULL DEFAULT '0',
+  `action_type` enum('set-group', 'set-secondary-group','apply-policy','apply-all-policies','launch-event','launch-alert-action','raw-config') DEFAULT 'launch-event',
+  `value` varchar(100) NOT NULL DEFAULT '',
+  `custom` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `id_autoconfig` (`id_autoconfig`),
+  CONSTRAINT `tautoconfig_action_ibfk_1` FOREIGN KEY (`id_autoconfig`) REFERENCES `tautoconfig` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 COMMIT;

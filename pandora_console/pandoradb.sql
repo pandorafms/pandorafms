c800061bc23673dd285242b54f4170b9fa300ac3
fbsanchez <fborja.sanchez@artica.es>
2018-07-04 18:39:06 +0200

    Autoconfiguration v1

--- pandora_console/pandoradb.sql
+++ pandora_console/pandoradb.sql
@@ -3215,8 +3215,8 @@ CREATE TABLE IF NOT EXISTS `tautoconfig_rules` (
   `order_by` int(11) NOT NULL DEFAULT '0',
   `operator` enum('AND','OR') DEFAULT 'OR',
   `type` enum('alias','ip-range','group','os','custom-field','script') DEFAULT 'alias',
-  `value` varchar(100) NOT NULL DEFAULT '',
-  `custom` varchar(100) NOT NULL DEFAULT '',
+  `value` text,
+  `custom` text,
   PRIMARY KEY (`id`),
   KEY `id_autoconfig` (`id_autoconfig`),
   CONSTRAINT `tautoconfig_rules_ibfk_1` FOREIGN KEY (`id_autoconfig`) REFERENCES `tautoconfig` (`id`) ON DELETE CASCADE
@@ -3229,9 +3229,9 @@ CREATE TABLE IF NOT EXISTS `tautoconfig_actions` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_autoconfig` int(10) unsigned NOT NULL,
   `order` int(11) NOT NULL DEFAULT '0',
-  `action_type` enum('set-group', 'set-secondary-group','apply-policy','apply-all-policies','launch-event','launch-alert-action','raw-config') DEFAULT 'launch-event',
-  `value` varchar(100) NOT NULL DEFAULT '',
-  `custom` varchar(100) NOT NULL DEFAULT '',
+  `action_type` enum('set-group', 'set-secondary-group', 'apply-policiy', 'launch-script', 'launch-event', 'launch-alert-action', 'raw-config') DEFAULT 'launch-event',
+  `value` text,
+  `custom` text,
   PRIMARY KEY (`id`),
   KEY `id_autoconfig` (`id_autoconfig`),
   CONSTRAINT `tautoconfig_action_ibfk_1` FOREIGN KEY (`id_autoconfig`) REFERENCES `tautoconfig` (`id`) ON DELETE CASCADE
--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -487,6 +487,8 @@ sub pandora_load_config {
 	$pa_config->{"provisioningserver_threads"} = 1; # 7.0 720
 	$pa_config->{"provisioning_cache_interval"} = 300; # 7.0 720
 
+	$pa_config->{"autoconfigure_agents"} = 1; # 7.0 725
+
 	# Check for UID0
 	if ($pa_config->{"quiet"} != 0){
 		if ($> == 0){
@@ -1112,6 +1114,9 @@ sub pandora_load_config {
 		}
 		elsif ($parametro =~ m/^provisioning_cache_interval\s+([0-9]*)/i){
 			$pa_config->{'provisioning_cache_interval'}= clean_blank($1);
+		}
+		elsif ($parametro =~ m/^autoconfigure_agents\s+([0-1])/i){
+			$pa_config->{'autoconfigure_agents'}= clean_blank($1);
 		}
 	} # end of loop for parameter #
 

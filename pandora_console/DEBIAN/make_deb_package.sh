adbfa1c56197434a84d32b4b59cf57f7774c7fda
artica <artica.devel@gmail.com>
2018-07-11 00:01:12 +0200

    Auto-updated build strings.

--- pandora_console/DEBIAN/make_deb_package.sh
+++ pandora_console/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="6.0SP8-180710"
+pandora_version="6.0SP8-180711"
 
 package_pear=0
 package_pandora=1
--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -33,7 +33,7 @@ use PandoraFMS::Tools;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "6.0SP8 PS180710";
+my $version = "6.0SP8 PS180711";
 
 # Pandora server configuration
 my %conf;

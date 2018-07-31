1088c5390daeb9258b4ab1e0472c404c086793cd
artica <artica.devel@gmail.com>
2018-08-01 00:01:22 +0200

    Auto-updated build strings.

--- pandora_server/pandora_server.spec
+++ pandora_server/pandora_server.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     7.0NG.725
-%define release     180731
+%define release     180801
 
 Summary:            Pandora FMS Server
 Name:               %{name}
--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -34,7 +34,7 @@ use PandoraFMS::Config;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "7.0NG.725 PS180731";
+my $version = "7.0NG.725 PS180801";
 
 # Pandora server configuration
 my %conf;

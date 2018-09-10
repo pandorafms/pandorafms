40ff7b9042a6f6c485aac02588fd860d07fb16b4
Alejandro Gallardo Escobar <alejandro.gallardo@artica.es>
2018-09-10 11:25:44 +0200

    Now is possible to link visual consoles from different nodes to the visual console elements

--- pandora_console/extras/mr/20.sql
+++ pandora_console/extras/mr/20.sql
@@ -12,8 +12,10 @@ ALTER TABLE tagente_modulo ALTER COLUMN `parent_module_id` SET default 0;
 ALTER TABLE `tlayout_data` ADD COLUMN `linked_layout_status_type` ENUM ('default', 'weight', 'service') DEFAULT 'default';
 ALTER TABLE `tlayout_data` ADD COLUMN `linked_layout_status_as_service_warning` FLOAT(20, 3) NOT NULL default 0;
 ALTER TABLE `tlayout_data` ADD COLUMN `linked_layout_status_as_service_critical` FLOAT(20, 3) NOT NULL default 0;
+ALTER TABLE `tlayout_data` ADD COLUMN `linked_layout_node_id` INT(10) NOT NULL default 0;
 ALTER TABLE `tlayout_template_data` ADD COLUMN `linked_layout_status_type` ENUM ('default', 'weight', 'service') DEFAULT 'default';
 ALTER TABLE `tlayout_template_data` ADD COLUMN `linked_layout_status_as_service_warning` FLOAT(20, 3) NOT NULL default 0;
 ALTER TABLE `tlayout_template_data` ADD COLUMN `linked_layout_status_as_service_critical` FLOAT(20, 3) NOT NULL default 0;
+ALTER TABLE `tlayout_template_data` ADD COLUMN `linked_layout_node_id` INT(10) NOT NULL default 0;
 
 COMMIT;

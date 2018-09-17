c9af020d91973f1e682b69225c1f21164d30f92e
fermin831 <fermin.hernandez@artica.es>
2018-09-17 10:16:12 +0200

    Remove unwanted git traces on 20.sql

--- pandora_console/extras/mr/20.sql
+++ pandora_console/extras/mr/20.sql
@@ -18,13 +18,12 @@ ALTER TABLE `tlayout_template_data` ADD COLUMN `linked_layout_status_as_service_
 ALTER TABLE `tlayout_template_data` ADD COLUMN `linked_layout_status_as_service_critical` FLOAT(20, 3) NOT NULL default 0;
 ALTER TABLE `tlayout_template_data` ADD COLUMN `linked_layout_node_id` INT(10) NOT NULL default 0;
 
-<<<<<<< Updated upstream
 -- -----------------------------------------------------
 -- Add column in table `treport`
 -- -----------------------------------------------------
 
 ALTER TABLE `treport` ADD COLUMN `hidden` tinyint(1) NOT NULL DEFAULT 0;
-=======
+
 ALTER TABLE `trecon_task` ADD COLUMN `snmp_version` varchar(5) NOT NULL default '1';
 ALTER TABLE `trecon_task` ADD COLUMN `snmp_auth_user` varchar(255) NOT NULL default '';
 ALTER TABLE `trecon_task` ADD COLUMN `snmp_auth_pass` varchar(255) NOT NULL default '';
@@ -32,6 +31,5 @@ ALTER TABLE `trecon_task` ADD COLUMN `snmp_auth_method` varchar(25) NOT NULL def
 ALTER TABLE `trecon_task` ADD COLUMN `snmp_privacy_method` varchar(25) NOT NULL default '';
 ALTER TABLE `trecon_task` ADD COLUMN `snmp_privacy_pass` varchar(255) NOT NULL default '';
 ALTER TABLE `trecon_task` ADD COLUMN `snmp_security_level` varchar(25) NOT NULL default '';
->>>>>>> Stashed changes
 
 COMMIT;

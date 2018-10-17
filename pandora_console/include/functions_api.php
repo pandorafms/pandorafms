7a44f9dcddddf2faae986908508bb83435d0b199
fbsanchez <fborja.sanchez@artica.es>
2018-10-17 13:26:41 +0200

    Minor fix in cde cluster api methods

--- pandora_console/include/functions_api.php
+++ pandora_console/include/functions_api.php
@@ -11306,6 +11306,7 @@ function api_get_cluster_status($id_cluster, $trash1, $trash2, $returnType) {
 	
 	if ($value === false) {
 		returnError('id_not_found', $returnType);
+		return;
 	}
 	
 	$data = array('type' => 'string', 'data' => $value);
@@ -11323,6 +11324,7 @@ function api_get_cluster_id_by_name($cluster_name, $trash1, $trash2, $returnType
 	$value = cluster_get_id_by_name($cluster_name);
 	if(($value === false) || ($value === null)){
 		returnError('id_not_found', $returnType);
+		return;
 	}
 
 	$cluster_group = clusters_get_group($value);

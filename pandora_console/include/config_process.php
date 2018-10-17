a46ec8278ffbbfd402fd6bcf7a3f5c5c5d97a5e9
artica <artica.devel@gmail.com>
2018-10-18 00:01:09 +0200

    Auto-updated build strings.

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -22,7 +22,7 @@
 /**
  * Pandora build version and version 
  */
-$build_version = 'PC181017';
+$build_version = 'PC181018';
 $pandora_version = 'v6.0SP8';
 
 // Do not overwrite default timezone set if defined.
--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -33,7 +33,7 @@ use PandoraFMS::Tools;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "6.0SP8 PS181017";
+my $version = "6.0SP8 PS181018";
 
 # Pandora server configuration
 my %conf;

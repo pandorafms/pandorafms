8c49b26a9149f4203a1d5566089035b522c2a1ce
daniel <daniel.barbero@artica.es>
2018-10-19 11:37:11 +0200

    php7 version

--- pandora_console/include/functions_config.php
+++ pandora_console/include/functions_config.php
@@ -43,7 +43,6 @@ function config_create_value ($token, $value) {
  */
 function config_update_value ($token, $value) {
 	global $config;
-	$config['flash_charts'] = true;
 	// Include functions_io to can call __() function
 	include_once($config['homedir'] . '/include/functions_io.php');
 	

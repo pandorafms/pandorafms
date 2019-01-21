5328a0e23984a35e565953c1a130d19c503e5343
fermin831 <fermin.hernandez@artica.es>
2019-01-21 17:42:41 +0100

    Added tnetwork_matrix data structure
    Former-commit-id: aa49de6c04a3c7a612ce7041393fe521e23e281d

--- pandora_console/pandoradb.sql
+++ pandora_console/pandoradb.sql
@@ -3378,3 +3378,17 @@ CREATE TABLE IF NOT EXISTS `tagent_custom_fields_filter` (
 	`group_search` int(10) unsigned default '0',
 	PRIMARY KEY(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+-- -----------------------------------------------------
+-- Table `tnetwork_matrix`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tnetwork_matrix` (
+	`id` int(10) unsigned NOT NULL auto_increment,
+	`source` varchar(60) default '',
+	`destination` varchar(60) default '',
+	`utimestamp` bigint(20) default 0,
+	`bytes` int(18) unsigned default 0,
+	`pkts` int(18) unsigned default 0,
+	PRIMARY KEY (`id`),
+	UNIQUE (`source`, `destination`, `utimestamp`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 ;

1e4a288f53ffa178b9e541ff06f1a5ec85b38d17
artica <artica.devel@gmail.com>
2019-01-23 00:01:23 +0100

    Auto-updated build strings.
    Former-commit-id: d02bc496b37ddd35ad1a75174daa0b3ca6d24f24

--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -34,7 +34,7 @@ use PandoraFMS::Config;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "7.0NG.730 PS190122";
+my $version = "7.0NG.730 PS190123";
 
 # Pandora server configuration
 my %conf;

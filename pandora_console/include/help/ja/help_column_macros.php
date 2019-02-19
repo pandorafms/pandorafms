7bd5851667b01318e083cc62075b31a76d70f113
Ramon Novoa <rnovoa@artica.es>
2019-02-19 13:04:57 +0100

    Add support for column macros.
    Former-commit-id: 8b826854c288754feefdbe270701655c6bbbfac9

---
+++ pandora_console/include/help/ja/help_column_macros.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Include/help/ja
+ */
+?>
+<h1>モジュールマクロ</h1>
+
+<p>
+次のマクロが利用できます:
+</p>
+<ul>
+<li>_agentcustomfield_<i>n</i>_ : エージェントカスタムフィールド番号<i>n</i> (例: _agentcustomfield_9_). </li>
+</ul>
+<p>
+</p>

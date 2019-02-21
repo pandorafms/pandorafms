b1149a053b6843d7511ee4f2e1220341b01e7969
artica <artica.devel@gmail.com>
2019-02-22 00:01:38 +0100

    Auto-updated build strings.
    Former-commit-id: f642e8fe029c7df7a017abe433d7768acd8577fd

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -20,7 +20,7 @@
 /**
  * Pandora build version and version
  */
-$build_version = 'PC190221';
+$build_version = 'PC190222';
 $pandora_version = 'v7.0NG.731';
 
 // Do not overwrite default timezone set if defined.

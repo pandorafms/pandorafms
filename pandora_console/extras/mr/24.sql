50155dd10bde4c34240b39c59f109a016a0d2aaf
fermin831 <fermin.hernandez@artica.es>
2019-02-21 18:44:40 +0100

    Added tnetwork_matrix table
    Former-commit-id: c933f5c00d68dd459ca15176dbc1a914f2b7ea32

--- pandora_console/extras/mr/24.sql
+++ pandora_console/extras/mr/24.sql
@@ -2,4 +2,16 @@ START TRANSACTION;
 
 ALTER TABLE `treport` ADD COLUMN `orientation` varchar(25) NOT NULL default 'vertical';
 
+CREATE TABLE IF NOT EXISTS `tnetwork_matrix` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+    `source` varchar(60) default '',
+    `destination` varchar(60) default '',
+    `utimestamp` bigint(20) default 0,
+    `bytes` int(18) unsigned default 0,
+    `pkts` int(18) unsigned default 0,
+    PRIMARY KEY (`id`),
+    UNIQUE (`source`, `destination`, `utimestamp`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 ;
+
+
 COMMIT;

2e9322515d2fb0c26e890a4b9336bc40f13215b0
fermin831 <fermin.hernandez@artica.es>
2019-02-22 08:45:05 +0100

    Move network matrix mr
    Former-commit-id: 18227212ce8961c9dae24939fa364c03e6f7dff8

---
+++ pandora_console/extras/mr/25.sql
@@ -0,0 +1,14 @@
+START TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS `tnetwork_matrix` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+    `source` varchar(60) default '',
+    `destination` varchar(60) default '',
+    `utimestamp` bigint(20) default 0,
+    `bytes` int(18) unsigned default 0,
+    `pkts` int(18) unsigned default 0,
+    PRIMARY KEY (`id`),
+    UNIQUE (`source`, `destination`, `utimestamp`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 ;
+
+COMMIT;

8fc432b9340caadbef304c5117e657cb94861878
artica <artica.devel@gmail.com>
2019-03-01 00:01:32 +0100

    Auto-updated build strings.
    Former-commit-id: 818dfe6138fe1837c87d96d607b15e51e06336cb

--- pandora_console/DEBIAN/make_deb_package.sh
+++ pandora_console/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="7.0NG.732-190228"
+pandora_version="7.0NG.732-190301"
 
 package_pear=0
 package_pandora=1
--- pandora_console/install.php
+++ pandora_console/install.php
@@ -128,8 +128,8 @@
         </div>
         <div style='height: 10px'>
             <?php
-            $version = '7.0NG.732';
-            $build = '190228';
+$version = '7.0NG.732';
+$build = '190301';
             $banner = "v$version Build $build";
 
             error_reporting(0);

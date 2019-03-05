88f1f16a5a87b3098041f76a5e104d8a069007d6
artica <artica.devel@gmail.com>
2019-03-06 00:01:33 +0100

    Auto-updated build strings.
    Former-commit-id: 4e42c790d0cc4e621a9cd231c77b3d04fc8443c4

--- pandora_console/install.php
+++ pandora_console/install.php
@@ -129,7 +129,7 @@
         <div style='height: 10px'>
             <?php
 $version = '7.0NG.732';
-$build = '190305';
+$build = '190306';
             $banner = "v$version Build $build";
 
             error_reporting(0);
--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -34,7 +34,7 @@ use PandoraFMS::Config;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "7.0NG.732 PS190305";
+my $version = "7.0NG.732 PS190306";
 
 # Pandora server configuration
 my %conf;

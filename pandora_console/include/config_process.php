4eb6c163398f21ba03ae145fa2f12e51c45b7fdb
artica <artica.devel@gmail.com>
2019-03-07 00:01:27 +0100

    Auto-updated build strings.
    Former-commit-id: 4c22b36dac2410ec75f5cae1c0e6ec7b81e5d78f

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -20,7 +20,7 @@
 /**
  * Pandora build version and version
  */
-$build_version = 'PC190306';
+$build_version = 'PC190307';
 $pandora_version = 'v7.0NG.732';
 
 // Do not overwrite default timezone set if defined.
--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -45,7 +45,7 @@ our @EXPORT = qw(
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "7.0NG.732";
-my $pandora_build = "190306";
+my $pandora_build = "190307";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash

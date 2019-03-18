2be3333d29dd6c538e44ec169fdc0364112b8a64
fermin831 <fermin.hernandez@artica.es>
2019-03-18 11:18:55 +0100

    Update version script cannot delete initial whitespaces in install.php
    Former-commit-id: e0b973605efb316ca73fad0a5cfeded2bffe149d

--- pandora_console/install.php
+++ pandora_console/install.php
@@ -128,8 +128,8 @@
         </div>
         <div style='height: 10px'>
             <?php
-$version = '7.0NG.732';
-$build = '190318';
+            $version = '7.0NG.732';
+            $build = '190318';
             $banner = "v$version Build $build";
 
             error_reporting(0);

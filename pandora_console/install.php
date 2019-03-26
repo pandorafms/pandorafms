8160983e367ef49e7b440f21a5e5c9e1ce559c63
artica <artica.devel@gmail.com>
2019-03-27 00:01:24 +0100

    Auto-updated build strings.
    Former-commit-id: 00a94a2b9b56bbb245f3389855c734f7af14d874

--- pandora_console/install.php
+++ pandora_console/install.php
@@ -129,7 +129,7 @@
         <div style='height: 10px'>
             <?php
             $version = '7.0NG.732';
-            $build = '190326';
+            $build = '190327';
             $banner = "v$version Build $build";
 
             error_reporting(0);

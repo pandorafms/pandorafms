6ebbe544487b0fd57ac87bf89ef63586488693e7
fbsanchez <fborja.sanchez@artica.es>
2019-03-27 17:06:14 +0100

    minor fix
    Former-commit-id: 252895bd6c64e5a1cb593ba705fb15a2ec6bed97

--- pandora_console/extras/mr/26.sql
+++ pandora_console/extras/mr/26.sql
@@ -1,10 +1,5 @@
 START TRANSACTION;
 
-ALTER TABLE `treport_content` ADD COLUMN `show_extended_events` tinyint(1) default '0';
-
--- ----------------------------------------------------------------------
--- Add column in table `tagent_custom_fields`
--- ----------------------------------------------------------------------
 ALTER TABLE tagent_custom_fields ADD COLUMN `combo_values` VARCHAR(255) DEFAULT '';
 
 ALTER TABLE `treport_content` ADD COLUMN `show_extended_events` tinyint(1) default '0';

dc4396622d2aa282d37087003a9b43c23a8a73c7
Alejandro Gallardo Escobar <alejandro.gallardo@artica.es>
2019-04-12 13:12:12 +0200

    Visual Console Refactor: acl improvements
    Former-commit-id: f2cd4e7dd12a02eb4cee34f5947566ae37c35e9d

--- pandora_console/include/rest-api/index.php
+++ pandora_console/include/rest-api/index.php
@@ -17,7 +17,24 @@ $getVisualConsoleItems = (bool) get_parameter('getVisualConsoleItems');
 ob_clean();
 
 if ($getVisualConsole === true) {
-    echo VisualConsole::fromDB(['id' => $visualConsoleId]);
+    $visualConsole = VisualConsole::fromDB(['id' => $visualConsoleId]);
+    $visualConsoleData = $visualConsole->toArray();
+    $groupId = $visualConsoleData['groupId'];
+
+    // ACL.
+    $aclRead = check_acl($config['id_user'], $groupId, 'VR');
+    $aclWrite = check_acl($config['id_user'], $groupId, 'VW');
+    $aclManage = check_acl($config['id_user'], $groupId, 'VM');
+
+    if (!$aclRead && !$aclWrite && !$aclManage) {
+        db_pandora_audit(
+            'ACL Violation',
+            'Trying to access visual console without group access'
+        );
+        exit;
+    }
+
+    echo $visualConsole;
 } else if ($getVisualConsoleItems === true) {
     echo '['.implode(VisualConsole::getItemsFromDB($visualConsoleId), ',').']';
 }

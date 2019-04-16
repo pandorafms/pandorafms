0e9be8638839b988ef4100f3ff0368c187d1a623
Daniel Maya <daniel.maya@artica.es>
2019-04-16 12:41:55 +0200

    Fixed relation line
    Former-commit-id: 82543a898b570ac648d5ec4a3d71e830b8ac5597

--- visual_console_client/src/VisualConsole.ts
+++ visual_console_client/src/VisualConsole.ts
@@ -504,12 +504,12 @@ export default class VisualConsole {
     const startX = parent.props.x + parent.elementRef.clientWidth / 2;
     const startY =
       parent.props.y +
-      (parent.elementRef.clientHeight + parent.labelElementRef.clientHeight) /
+      (parent.elementRef.clientHeight - parent.labelElementRef.clientHeight) /
         2;
     const endX = child.props.x + child.elementRef.clientWidth / 2;
     const endY =
       child.props.y +
-      (child.elementRef.clientHeight + child.labelElementRef.clientHeight) / 2;
+      (child.elementRef.clientHeight - child.labelElementRef.clientHeight) / 2;
 
     const line = new Line(
       linePropsDecoder({

1dfda59ce65d6ef922e223091f26a3c754e974d3
artica <artica.devel@gmail.com>
2019-04-20 00:01:24 +0200

    Auto-updated build strings.
    Former-commit-id: cd5e2305d2b22765879068907dd042a36f1d1524

--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -36,7 +36,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "7.0NG.733 PS190419";
+my $version = "7.0NG.733 PS190420";
 
 # save program name for logging
 my $progname = basename($0);

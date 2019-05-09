5126c651f7ce8ac32baf5e5d4ac658dbee87878c
Marcos Alconada <marcos.alconada@artica.es>
2019-05-09 17:38:40 +0200

    Changed '' of query

--- pandora_console/extras/mr/28.sql
+++ pandora_console/extras/mr/28.sql
@@ -1,5 +1,5 @@
 START TRANSACTION;
 
-DELETE FROM 'tevent_response' WHERE 'name' LIKE 'Create&#x20;Integria&#x20;IMS&#x20;incident&#x20;from&#x20;event';
+DELETE FROM `tevent_response` WHERE `name` LIKE 'Create&#x20;Integria&#x20;IMS&#x20;incident&#x20;from&#x20;event';
 
 COMMIT;

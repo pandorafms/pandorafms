0008f57e35f6f6f96ceadd24a9f8355479d7375a
artica <artica.devel@gmail.com>
2019-05-11 00:01:09 +0200

    Auto-updated build strings.
    Former-commit-id: 62f8df997289fe4c8e8e63eedbb8dcdb1a08da1f

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("7.0NG.734(Build 190510)")
+#define PANDORA_VERSION ("7.0NG.734(Build 190511)")
 
 string pandora_path;
 string pandora_dir;
--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -20,7 +20,7 @@
 /**
  * Pandora build version and version
  */
-$build_version = 'PC190510';
+$build_version = 'PC190511';
 $pandora_version = 'v7.0NG.734';
 
 // Do not overwrite default timezone set if defined.

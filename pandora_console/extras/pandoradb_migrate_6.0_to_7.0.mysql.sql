d9dbc60eb5748bc6e96defdb09b7cc95c10fc529
Marcos Alconada <marcos.alconada@artica.es>
2019-05-14 16:45:28 +0200

    Update pandoradb_migrate_6.0_to_7.0.mysql.sql
    Former-commit-id: cb2edea9dca48e33260ce2fb707ed48b04d23bde

--- pandora_console/extras/pandoradb_migrate_6.0_to_7.0.mysql.sql
+++ pandora_console/extras/pandoradb_migrate_6.0_to_7.0.mysql.sql
@@ -2121,7 +2121,6 @@ ALTER TABLE `tnetflow_filter` DROP COLUMN `output`;
 -- ----------------------------------------------------------------------
 UPDATE tuser_task set parameters = 'a:5:{i:0;a:6:{s:11:\"description\";s:28:\"Report pending to be created\";s:5:\"table\";s:7:\"treport\";s:8:\"field_id\";s:9:\"id_report\";s:10:\"field_name\";s:4:\"name\";s:4:\"type\";s:3:\"int\";s:9:\"acl_group\";s:8:\"id_group\";}i:1;a:2:{s:11:\"description\";s:46:\"Send to email addresses (separated by a comma)\";s:4:\"type\";s:4:\"text\";}i:2;a:2:{s:11:\"description\";s:7:\"Subject\";s:8:\"optional\";i:1;}i:3;a:3:{s:11:\"description\";s:7:\"Message\";s:4:\"type\";s:4:\"text\";s:8:\"optional\";i:1;}i:4;a:2:{s:11:\"description\";s:11:\"Report Type\";s:4:\"type\";s:11:\"report_type\";}}' where function_name = "cron_task_generate_report";
 
-<<<<<<< pandora_console/extras/pandoradb_migrate_6.0_to_7.0.mysql.sql
 ------------------------------------------------------------------------
 ------ ADD message in table 'tnews'
 ------------------------------------------------------------------------
@@ -2134,5 +2133,3 @@ INSERT INTO `tnews` (`id_news`, `author`, `subject`, `text`, `timestamp`) VALUES
 
  ALTER TABLE `talert_templates` MODIFY COLUMN `type` ENUM('regex','max_min','max','min','equal','not_equal','warning','critical','onchange','unknown','always','not_normal');
 
-=======
->>>>>>> pandora_console/extras/pandoradb_migrate_6.0_to_7.0.mysql.sql

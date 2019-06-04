95574adf80c6beece0c387f45510e10d47bfac02
artica <artica.devel@gmail.com>
2019-06-05 00:01:08 +0200

    Auto-updated build strings.

--- pandora_console/install.php
+++ pandora_console/install.php
@@ -129,7 +129,7 @@
         <div style='height: 10px'>
             <?php
             $version = '7.0NG.735';
-            $build = '190604';
+            $build = '190605';
             $banner = "v$version Build $build";
 
             error_reporting(0);
--- pandora_server/pandora_server.spec
+++ pandora_server/pandora_server.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     7.0NG.735
-%define release     190604
+%define release     190605
 
 Summary:            Pandora FMS Server
 Name:               %{name}

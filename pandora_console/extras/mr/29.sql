180948c51c7a33b385471072603650cc55fe4486
fbsanchez <fborja.sanchez@artica.es>
2019-06-24 19:36:50 +0200

    Import previous aws account

--- pandora_console/extras/mr/29.sql
+++ pandora_console/extras/mr/29.sql
@@ -14,4 +14,7 @@ CREATE TABLE IF NOT EXISTS `tcredential_store` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+INSERT INTO `tcredential_store` (`identifier`, `id_group`, `product`, `username`, `password`) VALUES ("imported_aws_account", 0, "AWS", (SELECT `value` FROM `tconfig` WHERE `token` = "aws_access_key_id" LIMIT 1), (SELECT `value` FROM `tconfig` WHERE `token` = "aws_secret_access_key" LIMIT 1));
+
+
 COMMIT;

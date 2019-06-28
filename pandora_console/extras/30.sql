03842df33e3b7afc549e44b8ecca6549a4ef00a0
Daniel Maya <daniel.maya@artica.es>
2019-06-28 11:19:13 +0200

    Added mr and uncommented code

---
+++ pandora_console/extras/30.sql
@@ -0,0 +1,7 @@
+START TRANSACTION;
+
+UPDATE `tlayout_data` SET `height` = 70 , `width` = 70 WHERE `height` = 0 && `width` = 0 && ((`type` IN (0,5)) ||
+(`type` = 10 && `image` IS NOT NULL && `image` != '' && `image` != 'none') ||
+(`type` = 11 && `image` IS NOT NULL && `image` != '' && `image` != 'none' && `show_statistics` = 0));
+
+COMMIT;

f6f96523a00a8d38ad1136dba26c0e2667fca63b
Luis Calvo <luis.calvo@artica.es>
2019-07-29 16:57:39 +0200

    Added html enconde

--- pandora_console/include/functions_ui.php
+++ pandora_console/include/functions_ui.php
@@ -48,18 +48,16 @@ if (isset($config['homedir'])) {
  */
 function ui_bbcode_to_html($text, $allowed_tags=['[url]'])
 {
-    $return = io_safe_output($text);
-
     if (array_search('[url]', $allowed_tags) !== false) {
         // If link hasn't http, add it.
-        if (preg_match('/https?:\/\//', $return)) {
+        if (preg_match('/https?:\/\//', $text)) {
             $html_bbcode = '<a target="_blank" rel="noopener noreferrer" href="$1">$2</a>';
         } else {
             $html_bbcode = '<a target="_blank" rel="noopener noreferrer" href="http://$1">$2</a>';
         }
 
         // Replace bbcode format [url=www.example.org] String [/url] with or without http and slashes
-        $return = preg_replace('/\[url(?|](((?:https?:\/\/)?[^[]+))|(?:=[\'"]?((?:https?:\/\/)?[^]]+?)[\'"]?)](.+?))\[\/url]/', $html_bbcode, $return);
+        $return = preg_replace('/\[url(?|](((?:https?:\/\/)?[^[]+))|(?:=[\'"]?((?:https?:\/\/)?[^]]+?)[\'"]?)](.+?))\[\/url]/', $html_bbcode, $text);
     }
 
     return $return;

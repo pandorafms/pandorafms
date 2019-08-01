3a654c7f79d7008b75b9bf1781541ce5f923fd0b
artica <artica.devel@gmail.com>
2019-08-02 00:01:11 +0200

    Auto-updated build strings.

--- pandora_agents/unix/pandora_agent.spec
+++ pandora_agents/unix/pandora_agent.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_agent_unix
 %define version     7.0NG.737
-%define release     190801
+%define release     190802
 
 Summary:            Pandora FMS Linux agent, PERL version
 Name:               %{name}
--- pandora_console/install.php
+++ pandora_console/install.php
@@ -129,7 +129,7 @@
         <div style='height: 10px'>
             <?php
             $version = '7.0NG.737';
-            $build = '190801';
+            $build = '190802';
             $banner = "v$version Build $build";
 
             error_reporting(0);

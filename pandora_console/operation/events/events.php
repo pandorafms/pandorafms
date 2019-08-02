41029196d8d7a5b7a0d3d9be03db1670975ac1e0
alejandro-campos <alejandro.campos@artica.es>
2019-08-02 10:04:10 +0200

    fix minor visual bug in events table

--- pandora_console/operation/events/events.php
+++ pandora_console/operation/events/events.php
@@ -1260,6 +1260,9 @@ try {
     // Get column names.
     $column_names = events_get_column_names($fields, true);
 
+    // First column name has a padding
+    $column_names[0]['style'] = 'padding-left: 1em !important;';
+
     // Open current filter quick reference.
     $active_filters_div = '<div class="filter_summary">';
 

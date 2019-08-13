9310fd8dc1eae1819126477d011d2f5c4ce3b606
marcos.alconada <marcos.alconada@artica.es>
2019-08-13 13:58:44 +0200

    add deleted condition

--- pandora_console/index.php
+++ pandora_console/index.php
@@ -1323,9 +1323,12 @@ require 'include/php_to_js_values.php';
 
 <script type="text/javascript" language="javascript">
 
-    // When there are less than 5 rows, all rows must be white.
-    if($('table.info_table tr').length < 5){
-        $('table.info_table tbody > tr').css('background-color', '#fff');
+       // When there are less than 5 rows, all rows must be white
+       var theme = "<?php echo $config['style']; ?>";
+        if(theme === 'pandora'){
+        if($('table.info_table tr').length < 5){
+            $('table.info_table tbody > tr').css('background-color', '#fff');
+        }
     }
 
     // When the user scrolls down 400px from the top of the document, show the

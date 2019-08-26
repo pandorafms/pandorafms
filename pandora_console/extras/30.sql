63da85798e543b59364bea7dbd399408a43c4ad1
Daniel Maya <daniel.maya@artica.es>
2019-08-26 16:10:06 +0200

    Change mr

--- pandora_console/extras/30.sql
+++
@@ -1,7 +0,0 @@
-START TRANSACTION;
-
-UPDATE `tlayout_data` SET `height` = 70 , `width` = 70 WHERE `height` = 0 && `width` = 0 && ((`type` IN (0,5)) ||
-(`type` = 10 && `image` IS NOT NULL && `image` != '' && `image` != 'none') ||
-(`type` = 11 && `image` IS NOT NULL && `image` != '' && `image` != 'none' && `show_statistics` = 0));
-
-COMMIT;
--- pandora_console/extras/mr/31.sql
+++ pandora_console/extras/mr/31.sql
@@ -9,4 +9,8 @@ DELETE FROM `tconfig` WHERE `token` LIKE 'integria_api_password';
 DELETE FROM `tconfig` WHERE `token` LIKE 'integria_inventory';
 DELETE FROM `tconfig` WHERE `token` LIKE 'integria_url';
 
+UPDATE `tlayout_data` SET `height` = 70 , `width` = 70 WHERE `height` = 0 && `width` = 0 && ((`type` IN (0,5)) ||
+(`type` = 10 && `image` IS NOT NULL && `image` != '' && `image` != 'none') ||
+(`type` = 11 && `image` IS NOT NULL && `image` != '' && `image` != 'none' && `show_statistics` = 0));
+
 COMMIT;

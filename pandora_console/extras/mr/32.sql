148cd5cce6f42fba05c3013ebe5add028bc9fcac
fbsanchez <fborja.sanchez@artica.es>
2019-09-05 10:45:17 +0200

    db updates pandora_db

--- pandora_console/extras/mr/32.sql
+++ pandora_console/extras/mr/32.sql
@@ -1,11 +1,11 @@
 START TRANSACTION
 
 ALTER TABLE `tdatabase` MODIFY `last_error` text;
-ALTER TABLE `tdatabase` MODIFY `host` VARCHAR(255);
-ALTER TABLE `tdatabase` ADD COLUMN `label` VARCHAR(255);
-ALTER TABLE `tdatabase` MODIFY `os_user` VARCHAR(255);
-ALTER TABLE `tdatabase` MODIFY `db_port` INT UNSIGNED NOT NULL;
-ALTER TABLE `tdatabase` MODIFY `os_port` INT UNSIGNED NOT NULL;
+ALTER TABLE `tdatabase` MODIFY `host` VARCHAR(255) DEFAULT '';
+ALTER TABLE `tdatabase` ADD COLUMN `label` VARCHAR(255) DEFAULT '';
+ALTER TABLE `tdatabase` MODIFY `os_user` VARCHAR(255) DEFAULT '';
+ALTER TABLE `tdatabase` MODIFY `db_port` INT UNSIGNED NOT NULL DEFAULT 3306;
+ALTER TABLE `tdatabase` MODIFY `os_port` INT UNSIGNED NOT NULL DEFAULT 22;
 ALTER TABLE `tdatabase` ADD COLUMN `ssh_key` TEXT;
 ALTER TABLE `tdatabase` ADD COLUMN `ssh_pubkey` TEXT;
 

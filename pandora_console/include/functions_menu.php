b1987001721ffb3a8c9170d2ef324ce139cb90cf
Tatiana Llorente <tatiana.llorente@artica.es>
2019-09-10 12:18:17 +0200

    Fixed duplicated names - #4280

--- pandora_console/include/functions_menu.php
+++ pandora_console/include/functions_menu.php
@@ -543,10 +543,8 @@ function menu_add_extras(&$menu)
     $menu_extra['estado']['sub']['enterprise/godmode/services/manage_services']['text'] = __('Manage services');
     $menu_extra['estado']['sub']['godmode/snmpconsole/snmp_alert']['text'] = __('SNMP alerts');
     $menu_extra['estado']['sub']['godmode/snmpconsole/snmp_filters']['text'] = __('SNMP filters');
-    $menu_extra['estado']['sub']['enterprise/godmode/snmpconsole/snmp_trap_editor']['text'] = __('SNMP trap editor');
-    $menu_extra['estado']['sub']['godmode/snmpconsole/snmp_trap_generator']['text'] = __('SNMP trap generator');
-
-    $menu_extra['estado']['sub']['operation/snmpconsole/snmp_view']['text'] = __('SNMP console');
+    $menu_extra['estado']['sub']['snmpconsole']['sub2']['godmode/snmpconsole/snmp_trap_generator']['text'] = __('SNMP trap generator');
+    $menu_extra['estado']['sub']['snmpconsole']['sub2']['operation/snmpconsole/snmp_view']['text'] = __('SNMP console');
 
     $menu_extra['workspace']['sub']['operation/incidents/incident_detail']['text'] = __('Manage incident');
 

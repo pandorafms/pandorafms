43af2269eedf04ece9d8efb3fd38c5132858c2bd
Luis Calvo <luis.calvo@artica.es>
2019-09-18 11:51:01 +0200

    Fixed default values uncompress data

--- pandora_console/extras/mr/32.sql
+++ pandora_console/extras/mr/32.sql
@@ -4,8 +4,8 @@ UPDATE `tlayout_data` SET `height` = 70 , `width` = 70 WHERE `height` = 0 && `wi
 (`type` = 10 && `image` IS NOT NULL && `image` != '' && `image` != 'none') ||
 (`type` = 11 && `image` IS NOT NULL && `image` != '' && `image` != 'none' && `show_statistics` = 0));
 
-ALTER TABLE `treport_content` ADD COLUMN `uncompressed_module` TINYINT DEFAULT '1';
-ALTER TABLE `treport_content_template` ADD COLUMN `uncompressed_module` TINYINT DEFAULT '1';
+ALTER TABLE `treport_content` ADD COLUMN `uncompressed_module` TINYINT DEFAULT '0';
+ALTER TABLE `treport_content_template` ADD COLUMN `uncompressed_module` TINYINT DEFAULT '0';
 
 
 COMMIT;

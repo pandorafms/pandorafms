ebb7cbfe6c5e2deb5a5e2861d22cc5064d246f71
Daniel Barbero Martin <daniel.barbero@artica.es>
2019-11-05 15:12:24 +0100

    Fix styles css

--- pandora_console/include/javascript/pandora.js
+++ pandora_console/include/javascript/pandora.js
@@ -1962,13 +1962,34 @@ function load_modal(settings) {
 
               $("#" + settings.form + " :input").each(function() {
                 if (this.checkValidity() === false) {
-                  $(this).prop("title", this.validationMessage);
+                  $(this).attr("title", this.validationMessage);
                   $(this).tooltip({
                     tooltipClass: "uitooltip",
-                    position: { my: "right bottom", at: "right bottom" },
-                    show: { duration: 200 }
+                    position: {
+                      my: "right bottom",
+                      at: "right top",
+                      using: function(position, feedback) {
+                        $(this).css(position);
+                        $("<div>")
+                          .addClass("arrow")
+                          .addClass(feedback.vertical)
+                          .addClass(feedback.horizontal)
+                          .appendTo(this);
+                      }
+                    }
                   });
                   $(this).tooltip("open");
+
+                  var element = $(this);
+                  setTimeout(
+                    function(element) {
+                      element.tooltip("destroy");
+                      element.removeAttr("title");
+                    },
+                    3000,
+                    element
+                  );
+
                   flagError = true;
                 }
 

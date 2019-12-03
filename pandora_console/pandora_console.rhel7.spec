0ba2d86971452e04d2bd7192513f57fb5536244b
artica <artica.devel@gmail.com>
2019-12-04 00:01:08 +0100

    Auto-updated build strings.

--- pandora_console/pandora_console.rhel7.spec
+++ pandora_console/pandora_console.rhel7.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     7.0NG.741
-%define release     191203
+%define release     191204
 
 # User and Group under which Apache is running
 %define httpd_name  httpd
--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="7.0NG.741-191203"
+pandora_version="7.0NG.741-191204"
 
 package_cpan=0
 package_pandora=1

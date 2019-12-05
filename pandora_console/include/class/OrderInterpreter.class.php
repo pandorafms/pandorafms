1430e6ee112f9d2fa380e6bd5c1bb20f20af7a7f
marcos <marcos.alconada@artica.es>
2019-12-05 16:10:31 +0100

    Update orderinterpreter class and css

--- pandora_console/include/class/OrderInterpreter.class.php
+++ pandora_console/include/class/OrderInterpreter.class.php
@@ -97,25 +97,108 @@ class OrderInterpreter extends Wizard
         $ajax_controller='include/ajax/order_interpreter'
     ) {
         $this->ajaxController = $ajax_controller;
+
+        // Example pages names.
         $this->pages_name = [
-            0 => __('Tactical View'),
-            1 => __('Agent Management'),
-            2 => __('List Alerts'),
-            3 => __('Manage Policies'),
+            0  => __('Tactical View'),
+            1  => __('Agent Management'),
+            2  => __('List Alerts'),
+            3  => __('Manage Policies'),
+            4  => __('Tactical View'),
+            5  => __('Agent Management'),
+            7  => __('List Alerts'),
+            8  => __('Manage Policies'),
+            9  => __('Tactical View'),
+            10 => __('Agent Management'),
+            11 => __('List Alerts'),
+            12 => __('Manage Policies'),
         ];
+
+        // Example ICON.
+        $this->pages_icon = [
+            0  => ui_get_full_url(
+                'images/op_monitoring.menu_gray.png'
+            ),
+            1  => ui_get_full_url(
+                'images/gm_resources.menu_gray.png'
+            ),
+            2  => ui_get_full_url(
+                'images/gm_alerts.menu_gray.png'
+            ),
+            3  => ui_get_full_url(
+                'images/gm_configuration.menu_gray.png'
+            ),
+            4  => ui_get_full_url(
+                'images/op_monitoring.menu_gray.png'
+            ),
+            5  => ui_get_full_url(
+                'images/gm_resources.menu_gray.png'
+            ),
+            6  => ui_get_full_url(
+                'images/gm_alerts.menu_gray.png'
+            ),
+            7  => ui_get_full_url(
+                'images/gm_configuration.menu_gray.png'
+            ),
+            8  => ui_get_full_url(
+                'images/op_monitoring.menu_gray.png'
+            ),
+            9  => ui_get_full_url(
+                'images/gm_resources.menu_gray.png'
+            ),
+            10 => ui_get_full_url(
+                'images/gm_alerts.menu_gray.png'
+            ),
+            11 => ui_get_full_url(
+                'images/gm_configuration.menu_gray.png'
+            ),
+            12 => ui_get_full_url(
+                'images/gm_configuration.menu_gray.png'
+            ),
+
+        ];
+        // Example URLS.
         $this->pages_url = [
-            0 => ui_get_full_url(
+            0  => ui_get_full_url(
                 'index.php?sec=view&sec2=operation/agentes/tactical'
             ),
-            1 => ui_get_full_url(
+            1  => ui_get_full_url(
                 'index.php?sec=gagente&sec2=godmode/agentes/modificar_agente'
             ),
-            2 => ui_get_full_url(
+            2  => ui_get_full_url(
                 'index.php?sec=galertas&sec2=godmode/alerts/alert_list'
             ),
-            3 => ui_get_full_url(
+            3  => ui_get_full_url(
                 'index.php?sec=gmodules&sec2=enterprise/godmode/policies/policies'
             ),
+            4  => ui_get_full_url(
+                'index.php?sec=view&sec2=operation/agentes/tactical'
+            ),
+            5  => ui_get_full_url(
+                'index.php?sec=gagente&sec2=godmode/agentes/modificar_agente'
+            ),
+            6  => ui_get_full_url(
+                'index.php?sec=galertas&sec2=godmode/alerts/alert_list'
+            ),
+            7  => ui_get_full_url(
+                'index.php?sec=gmodules&sec2=enterprise/godmode/policies/policies'
+            ),
+            8  => ui_get_full_url(
+                'index.php?sec=view&sec2=operation/agentes/tactical'
+            ),
+            9  => ui_get_full_url(
+                'index.php?sec=gagente&sec2=godmode/agentes/modificar_agente'
+            ),
+            10 => ui_get_full_url(
+                'index.php?sec=galertas&sec2=godmode/alerts/alert_list'
+            ),
+            11 => ui_get_full_url(
+                'index.php?sec=gmodules&sec2=enterprise/godmode/policies/policies'
+            ),
+            12 => ui_get_full_url(
+                'index.php?sec=gmodules&sec2=enterprise/godmode/policies/policies'
+            ),
+
         ];
 
     }
@@ -131,26 +214,40 @@ class OrderInterpreter extends Wizard
         // Take value from input search.
         $text = get_parameter('text', '');
         $array_found = [];
+        $iterator = 0;
+        $more_results = 0;
+
         if ($text !== '') {
             echo '<div id="result_order" class="show_result_interpreter">';
             echo '<ul>';
 
-            if (io_safe_output($text) === __('GO TO ')) {
-                foreach ($this->pages_name as $key => $value) {
+            foreach ($this->pages_name as $key => $value) {
+                if (preg_match(
+                    '/.*'.io_safe_output($text).'.*/i',
+                    __('GO TO '.$value)
+                )
+                ) {
+                    if ($iterator <= 9) {
                         echo '<li>';
-                        echo '<img src="http://localhost/pandora_console/images/arrow_right_green.png">';
+                        echo '
+                        Go to &nbsp;&nbsp;
+                        <img src="'.$this->pages_icon[$key].'">';
                         echo '&nbsp;
-           <a href="'.$this->pages_url[$key].'">'.$value.'</a><br>';
-                }
-            } else {
-                foreach ($this->pages_name as $key => $value) {
-                    if (preg_match('/.*'.io_safe_output($text).'.*/i', __('GO TO '.$value))) {
-                        echo '<li>';
-                        echo '<img src="http://localhost/pandora_console/images/arrow_right_green.png">';
-                        echo '&nbsp;
-                    <a href="'.$this->pages_url[$key].'">'.$value.'</a><br>';
+                        <a href="'.$this->pages_url[$key].'">
+                        '.$value.'</a><br>';
+                    }
+
+                    $iterator ++;
+
+                    if ($iterator > 10) {
+                        $more_results ++;
                     }
                 }
+            }
+
+            if ($iterator > 9) {
+                echo '<li class="more_results"><br>';
+                echo '+ '.$more_results.' results found';
             }
 
             echo '</ul></div';
@@ -161,9 +258,7 @@ class OrderInterpreter extends Wizard
 
     /**
      * Load JS content.
-     * function that enables the functions to the buttons when its action is
-     *  completed.
-     * Assign the url of each button.
+     * function to create JS actions.
      *
      * @return string HTML code for javascript functionality.
      */

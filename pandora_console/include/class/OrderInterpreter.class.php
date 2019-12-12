6a5dfb5ec9a707d2cbe66a8c67e75a64eba0a746
marcos <marcos.alconada@artica.es>
2019-12-12 17:16:09 +0100

    refactor code

--- pandora_console/include/class/OrderInterpreter.class.php
+++ pandora_console/include/class/OrderInterpreter.class.php
@@ -99,114 +99,133 @@ class OrderInterpreter extends Wizard
     ) {
         $this->ajaxController = $ajax_controller;
 
-        // Example pages names.
-        $this->pages_name = [
-            0  => __('Tactical View'),
-            1  => __('Agent Management'),
-            2  => __('General Setup'),
-            3  => __('Manage Policies'),
-            4  => __('List Alerts'),
-            5  => __('View Events'),
-            6  => __('Dashboard'),
-            7  => __('Visual Console'),
-            8  => __('Manage Servers'),
-            9  => __('Edit User'),
-            10 => __('Three View'),
-            11 => __('Network Component'),
-            12 => __('Task List'),
-            13 => __('Update Manager'),
-        ];
-
-        // Example ICON.
-        $this->pages_icon = [
-            0  => ui_get_full_url(
-                'images/op_monitoring.menu_gray.png'
-            ),
-            1  => ui_get_full_url(
-                'images/gm_resources.menu_gray.png'
-            ),
-            2  => ui_get_full_url(
-                'images/gm_setup.menu_gray.png'
-            ),
-            3  => ui_get_full_url(
-                'images/gm_configuration.menu_gray.png'
-            ),
-            4  => ui_get_full_url(
-                'images/gm_alerts.menu_gray.png'
-            ),
-            5  => ui_get_full_url(
-                'images/op_events.menu_gray.png'
-            ),
-            6  => ui_get_full_url(
-                'images/op_reporting.menu_gray.png'
-            ),
-            7  => ui_get_full_url(
-                'images/op_network.menu_gray.png'
-            ),
-            8  => ui_get_full_url(
-                'images/gm_servers.menu_gray.png'
-            ),
-            9  => ui_get_full_url(
-                'images/gm_users.menu_gray.png'
-            ),
-            10 => ui_get_full_url(
-                'images/op_monitoring.menu_gray.png'
-            ),
-            11 => ui_get_full_url(
-                'images/gm_configuration.menu_gray.png'
-            ),
-            12 => ui_get_full_url(
-                'images/gm_discovery.menu.png'
-            ),
-            13 => ui_get_full_url(
-                'images/um_messages.menu_gray.png'
-            ),
-
-        ];
-        // Example URLS.
-        $this->pages_url = [
-            0  => ui_get_full_url(
-                'index.php?sec=view&sec2=operation/agentes/tactical'
-            ),
-            1  => ui_get_full_url(
-                'index.php?sec=gagente&sec2=godmode/agentes/modificar_agente'
-            ),
-            2  => ui_get_full_url(
-                'index.php?sec=general&sec2=godmode/setup/setup&section=general'
-            ),
-            3  => ui_get_full_url(
-                'index.php?sec=gmodules&sec2=enterprise/godmode/policies/policies'
-            ),
-            4  => ui_get_full_url(
-                'index.php?sec=galertas&sec2=godmode/alerts/alert_list'
-            ),
-            5  => ui_get_full_url(
-                'index.php?sec=eventos&sec2=operation/events/events'
-            ),
-            6  => ui_get_full_url(
-                'index.php?sec=reporting&sec2=enterprise/dashboard/dashboards'
-            ),
-            7  => ui_get_full_url(
-                'index.php?sec=network&sec2=godmode/reporting/map_builder'
-            ),
-            8  => ui_get_full_url(
-                'index.php?sec=gservers&sec2=godmode/servers/modificar_server'
-            ),
-            9  => ui_get_full_url(
-                'index.php?sec=workspace&sec2=operation/users/user_edit'
-            ),
-            10 => ui_get_full_url(
-                'index.php?sec=view&sec2=operation/tree'
-            ),
-            11 => ui_get_full_url(
-                'index.php?sec=gmodules&sec2=godmode/modules/manage_network_components'
-            ),
-            12 => ui_get_full_url(
-                'index.php?sec=discovery&sec2=godmode/servers/discovery&wiz=tasklist'
-            ),
-            13 => ui_get_full_url(
-                'index.php?sec=messages&sec2=godmode/update_manager/update_manager&tab=setup'
-            ),
+        $this->pages_menu = [
+            0  => [
+                'name' => __('Tactical View'),
+                'icon' => ui_get_full_url(
+                    'images/op_monitoring.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=view&sec2=operation/agentes/tactical'
+                ),
+            ],
+            1  => [
+                'name' => __('Agent Management'),
+                'icon' => ui_get_full_url(
+                    'images/gm_resources.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=gagente&sec2=godmode/agentes/modificar_agente'
+                ),
+            ],
+            2  => [
+                'name' => __('General Setup'),
+                'icon' => ui_get_full_url(
+                    'images/gm_setup.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=general&sec2=godmode/setup/setup&section=general'
+                ),
+            ],
+            3  => [
+                'name' => __('Manage Policies'),
+                'icon' => ui_get_full_url(
+                    'images/gm_configuration.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=gmodules&sec2=enterprise/godmode/policies/policies'
+                ),
+            ],
+            4  => [
+                'name' => __('List Alerts'),
+                'icon' => ui_get_full_url(
+                    'images/gm_alerts.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=galertas&sec2=godmode/alerts/alert_list'
+                ),
+            ],
+            5  => [
+                'name' => __('View Events'),
+                'icon' => ui_get_full_url(
+                    'images/op_events.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=eventos&sec2=operation/events/events'
+                ),
+            ],
+            6  => [
+                'name' => __('Dashboard'),
+                'icon' => ui_get_full_url(
+                    'images/op_reporting.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=reporting&sec2=enterprise/dashboard/dashboards'
+                ),
+            ],
+            7  => [
+                'name' => __('Visual Console'),
+                'icon' => ui_get_full_url(
+                    'images/op_network.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=network&sec2=godmode/reporting/map_builder'
+                ),
+            ],
+            8  => [
+                'name' => __('Manage Servers'),
+                'icon' => ui_get_full_url(
+                    'images/gm_servers.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=gservers&sec2=godmode/servers/modificar_server'
+                ),
+            ],
+            9  => [
+                'name' => __('Edit User'),
+                'icon' => ui_get_full_url(
+                    'images/gm_users.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=workspace&sec2=operation/users/user_edit'
+                ),
+            ],
+            10 => [
+                'name' => __('Three View'),
+                'icon' => ui_get_full_url(
+                    'images/op_monitoring.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=view&sec2=operation/tree'
+                ),
+            ],
+            11 => [
+                'name' => __('Network Component'),
+                'icon' => ui_get_full_url(
+                    'images/gm_configuration.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=gmodules&sec2=godmode/modules/manage_network_components'
+                ),
+            ],
+            12 => [
+                'name' => __('Task List'),
+                'icon' => ui_get_full_url(
+                    'images/gm_discovery.menu.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=discovery&sec2=godmode/servers/discovery&wiz=tasklist'
+                ),
+            ],
+            13 => [
+                'name' => __('Update Manager'),
+                'icon' => ui_get_full_url(
+                    'images/um_messages.menu_gray.png'
+                ),
+                'url'  => ui_get_full_url(
+                    'index.php?sec=messages&sec2=godmode/update_manager/update_manager&tab=setup'
+                ),
+            ],
 
         ];
 
@@ -230,20 +249,20 @@ class OrderInterpreter extends Wizard
             echo '<div id="result_order" class="show_result_interpreter">';
             echo '<ul>';
 
-            foreach ($this->pages_name as $key => $value) {
+            foreach ($this->pages_menu as $key => $value) {
                 if (preg_match(
                     '/.*'.io_safe_output($text).'.*/i',
-                    __('GO TO '.$value)
+                    __('GO TO '.$value['name'])
                 )
                 ) {
                     if ($iterator <= 9) {
                         echo '<li class="list_found">';
                         echo '
                         Go to &nbsp;
-                        <img src="'.$this->pages_icon[$key].'">';
+                        <img src="'.$this->pages_menu[$key]['icon'].'">';
                         echo '&nbsp;
-                        <a href="'.$this->pages_url[$key].'">
-                        '.$value.'</a><br>';
+                        <a href="'.$this->pages_menu[$key]['url'].'">
+                        '.$value['name'].'</a><br>';
                     }
 
                     $iterator ++;

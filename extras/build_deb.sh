89b97e340e5fd353e81fee052783263c00b179fd
fbsanchez <fborja.sanchez@artica.es>
2019-12-16 12:16:42 +0100

    Revert "DEBIAN console updated with gotty binary file"
    Undo GoTTY in Pandora Console DEB packages
    
    This reverts commit d370fa560a86cb78f91fad53949a49d59cc847bf.

--- extras/build_deb.sh
+++ extras/build_deb.sh
@@ -8,18 +8,7 @@ fi
 echo "Creating DEB packages in $RPMHOME/DEB"
 
 # Console
-# Extra files to be added to rpm.
-if [ "$X86_64" == "" ]; then
-	# Fake gotty.
-	echo 'Only x86_64 is supported' > $CODEHOME/pandora_console/gotty
-	chmod +x pandora_console/gotty
-else 
-	cp /root/bin/winexe/x64/gotty $CODEHOME/pandora_console/
-fi
 cd $CODEHOME/pandora_console/DEBIAN && bash ./make_deb_package.sh && mv ../*.deb $RPMHOME/DEB || exit 1
-
-# Cleanup.
-rm -f pandora_console/gotty
 
 # Server
 cd $CODEHOME/pandora_server/DEBIAN  && bash ./make_deb_package.sh && mv ../*.deb $RPMHOME/DEB || exit 1

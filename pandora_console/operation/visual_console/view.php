dc6f1e3049a596024448bc00af2bc56a4ff97bc2
Daniel Barbero Martin <daniel.barbero@artica.es>
2020-01-29 10:56:29 +0100

    fixed errors VC

--- pandora_console/operation/visual_console/view.php
+++ pandora_console/operation/visual_console/view.php
@@ -167,11 +167,6 @@ if ($aclWrite || $aclManage) {
         true,
         ['title' => __('Wizard')]
     ).'</a>';
-    $options['editor']['text'] = '<a href="'.$baseUrl.'&tab=editor&id_visual_console='.$visualConsoleId.'">'.html_print_image(
-        'images/builder.png',
-        true,
-        ['title' => __('Builder')]
-    ).'</a>';
 }
 
 $options['view']['text'] = '<a href="index.php?sec=network&sec2=operation/visual_console/render_view&id='.$visualConsoleId.'&refr='.$refr.'">'.html_print_image(

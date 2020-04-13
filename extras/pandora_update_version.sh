88ac1c7e158a980b801b4bdbd67785d93accc036
fbsanchez <fborja.sanchez@artica.es>
2020-04-13 11:44:05 +0200

    update spec version (server enterprise)

--- extras/pandora_update_version.sh
+++ extras/pandora_update_version.sh
@@ -30,6 +30,7 @@ $CODEHOME/pandora_console/pandora_console.rhel7.spec \
 $CODEHOME/pandora_agents/unix/pandora_agent.redhat.spec \
 $CODEHOME/pandora_server/pandora_server.redhat.spec \
 $PANDHOME_ENT/pandora_agents/pandora_agent.spec \
+$PANDHOME_ENT/pandora_server/pandora_server_enterprise.redhat.spec \
 $PANDHOME_ENT/pandora_console/enterprise/pandora_console_enterprise.redhat.spec \
 $PANDHOME_ENT/pandora_console/enterprise/pandora_console_enterprise.rhel7.spec \
 $PANDHOME_ENT/pandora_server/PandoraFMS-Enterprise/pandora_server_enterprise.redhat.spec"

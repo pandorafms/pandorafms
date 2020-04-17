04ecb0afa5211cacb82b97659b51917c9bb02711
artica <artica.devel@gmail.com>
2020-04-18 01:00:19 +0200

    Auto-updated build strings.

--- pandora_console/DEBIAN/make_deb_package.sh
+++ pandora_console/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="7.0NG.744-200417"
+pandora_version="7.0NG.744-200418"
 
 package_pear=0
 package_pandora=1

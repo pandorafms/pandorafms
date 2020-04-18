2e446312534df8a4ccbaf79e6019229bf88b34c2
artica <artica.devel@gmail.com>
2020-04-19 01:00:15 +0200

    Auto-updated build strings.

--- pandora_console/include/config_process.php
+++ pandora_console/include/config_process.php
@@ -20,7 +20,7 @@
 /**
  * Pandora build version and version
  */
-$build_version = 'PC200418';
+$build_version = 'PC200419';
 $pandora_version = 'v7.0NG.744';
 
 // Do not overwrite default timezone set if defined.
--- pandora_server/DEBIAN/make_deb_package.sh
+++ pandora_server/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="7.0NG.744-200418"
+pandora_version="7.0NG.744-200419"
 
 package_cpan=0
 package_pandora=1

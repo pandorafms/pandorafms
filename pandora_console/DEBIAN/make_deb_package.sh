60f4f21297c6e94fe5acfd0ecfe4d9c6a3e64bf8
artica <artica.devel@gmail.com>
2020-04-22 01:00:15 +0200

    Auto-updated build strings.

--- pandora_console/DEBIAN/make_deb_package.sh
+++ pandora_console/DEBIAN/make_deb_package.sh
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-pandora_version="7.0NG.744-200421"
+pandora_version="7.0NG.744-200422"
 
 package_pear=0
 package_pandora=1
--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -35,7 +35,7 @@ use PandoraFMS::Config;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "7.0NG.744 PS200421";
+my $version = "7.0NG.744 PS200422";
 
 # Pandora server configuration
 my %conf;

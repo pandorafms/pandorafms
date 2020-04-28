72f9c051eb1f8af5ba13fc61b7ea6b9f8ef0c544
marcos <marcos.alconada@artica.es>
2020-04-28 10:44:13 +0200

    changed message confirmation dialog

--- pandora_console/include/javascript/pandora_dashboards.js
+++ pandora_console/include/javascript/pandora_dashboards.js
@@ -248,7 +248,7 @@ function initialiceLayout(data) {
           confirmDialog({
             title: "Are you sure?",
             message:
-              "<h4 style='text-align: center;padding-top: 20px;'>If you delete it, will can't be recovered</h4>",
+              "<h4 style='text-align: center;padding-top: 20px;'>All changes made to this widget will be lost</h4>",
             cancel: "Cancel",
             ok: "Ok",
             onAccept: function() {

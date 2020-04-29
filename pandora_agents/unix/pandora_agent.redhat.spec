8a019bd8f4e2e73422897016e99896df9f94759c
artica <artica.devel@gmail.com>
2020-04-30 01:00:16 +0200

    Auto-updated build strings.

--- pandora_agents/unix/pandora_agent.redhat.spec
+++ pandora_agents/unix/pandora_agent.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_agent_unix
 %define version     7.0NG.745
-%define release     200429
+%define release     200430
 
 Summary:            Pandora FMS Linux agent, PERL version
 Name:               %{name}
--- pandora_console/install.php
+++ pandora_console/install.php
@@ -129,7 +129,7 @@
         <div style='height: 10px'>
             <?php
             $version = '7.0NG.745';
-            $build = '200429';
+            $build = '200430';
             $banner = "v$version Build $build";
 
             error_reporting(0);

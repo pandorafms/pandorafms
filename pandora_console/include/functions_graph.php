8eb3a982e1f785ed4e22aac3c8398391004ccf1a
Daniel Barbero Martin <daniel.barbero@artica.es>
2020-06-01 14:48:53 +0200

    Fixed error in gauges

--- pandora_console/include/functions_graph.php
+++ pandora_console/include/functions_graph.php
@@ -2003,6 +2003,12 @@ function graphic_combined_module(
                 }
             }
 
+            if (isset($params['pdf']) === true && $params['pdf'] === true) {
+                $transitionDuration = 0;
+            } else {
+                $transitionDuration = 500;
+            }
+
             $output = stacked_gauge(
                 $graph_values,
                 $new_width,
@@ -2018,7 +2024,8 @@ function graphic_combined_module(
                 $config['fontpath'],
                 $fixed_font_size,
                 '',
-                $homeurl
+                $homeurl,
+                $transitionDuration
             );
         break;
 

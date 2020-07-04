ba9f22f7f936aa7fc59ee97691fff0ab0455098f
artica <artica.devel@gmail.com>
2020-07-05 01:00:12 +0200

    Auto-updated build strings.

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("7.0NG.747(Build 200704)")
+#define PANDORA_VERSION ("7.0NG.747(Build 200705)")
 
 string pandora_path;
 string pandora_dir;
--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -35,7 +35,7 @@ use PandoraFMS::Config;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "7.0NG.747 PS200704";
+my $version = "7.0NG.747 PS200705";
 
 # Pandora server configuration
 my %conf;

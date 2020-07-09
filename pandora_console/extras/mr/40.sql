19a8288f261c202bdf2256ae94cb2041299cc392
Ramon Novoa <rnovoa@artica.es>
2020-07-09 17:39:08 +0200

    Increase the size of tserver.version.
    Ref pandora_enterprise#4943

--- pandora_console/extras/mr/40.sql
+++ pandora_console/extras/mr/40.sql
@@ -2,6 +2,7 @@ START TRANSACTION;
 
 ALTER TABLE `tservice_element` ADD COLUMN `rules` text;
 ALTER TABLE `tservice` ADD COLUMN `unknown_as_critical` tinyint(1) NOT NULL default 0 AFTER `warning`;
+ALTER TABLE `tserver` MODIFY COLUMN `version` varchar(25) NOT NULL DEFAULT '';
 
 UPDATE `tservice` SET `auto_calculate`=0;
 

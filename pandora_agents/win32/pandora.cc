b6d8cc1f2ceddde3ae17071d7ee05ebc4faef775
artica <artica.devel@gmail.com>
2020-08-04 01:00:14 +0200

    Auto-updated build strings.

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("7.0NG.748(Build 200803)")
+#define PANDORA_VERSION ("7.0NG.748(Build 200804)")
 
 string pandora_path;
 string pandora_dir;
--- pandora_console/install.php
+++ pandora_console/install.php
@@ -129,7 +129,7 @@
         <div style='height: 10px'>
             <?php
             $version = '7.0NG.748';
-            $build = '200803';
+            $build = '200804';
             $banner = "v$version Build $build";
 
             error_reporting(0);

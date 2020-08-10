0b21adf150e57275a923c1b11682d986caf29bea
José González <jose.gonzalez@artica.es>
2020-08-10 10:18:50 +0200

    Fix 41.sql

--- pandora_console/extras/mr/41.sql
+++ pandora_console/extras/mr/41.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `talert_templates` ADD COLUMN `previous_name` text;
 ALTER TABLE `talert_actions` ADD COLUMN `previous_name` text;
 ALTER TABLE `talert_commands` ADD COLUMN `previous_name` text;
-ALTER TABLE `ttag` ADD COLUMN `previous_name` text default '';
-ALTER TABLE `tconfig_os` ADD COLUMN `previous_name` text default '';
+ALTER TABLE `ttag` ADD COLUMN `previous_name` text;
+ALTER TABLE `tconfig_os` ADD COLUMN `previous_name` text;

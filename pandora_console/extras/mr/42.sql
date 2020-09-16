dbb1ec6b849187fbbd171870c47be3c45bf678af
Jose Gonzalez <jose.gonzalez@artica.es>
2020-09-16 17:19:07 +0200

    Too long name fix

--- pandora_console/extras/mr/42.sql
+++ pandora_console/extras/mr/42.sql
@@ -1,14 +1,14 @@
 START TRANSACTION;
 
-CREATE INDEX `tmetaconsole_event_timestamp_idx` ON tmetaconsole_event (`timestamp`) ALGORITHM DEFAULT LOCK DEFAULT;
-CREATE INDEX `tmetaconsole_event_module_status_idx` ON tmetaconsole_event (`module_status`) ALGORITHM DEFAULT LOCK DEFAULT;
-CREATE INDEX `tmetaconsole_event_criticity_idx` ON tmetaconsole_event (`criticity`) ALGORITHM DEFAULT LOCK DEFAULT;
-CREATE INDEX `tmetaconsole_event_agent_name_idx` ON tmetaconsole_event (`agent_name`) ALGORITHM DEFAULT LOCK DEFAULT;
+CREATE INDEX `tme_timestamp_idx` ON tmetaconsole_event (`timestamp`) ALGORITHM DEFAULT LOCK DEFAULT;
+CREATE INDEX `tme_module_status_idx` ON tmetaconsole_event (`module_status`) ALGORITHM DEFAULT LOCK DEFAULT;
+CREATE INDEX `tme_criticity_idx` ON tmetaconsole_event (`criticity`) ALGORITHM DEFAULT LOCK DEFAULT;
+CREATE INDEX `tme_agent_name_idx` ON tmetaconsole_event (`agent_name`) ALGORITHM DEFAULT LOCK DEFAULT;
 
-CREATE INDEX `tmetaconsole_agent_id_os_idx` ON tmetaconsole_agent (`id_os`) ALGORITHM DEFAULT LOCK DEFAULT;
-CREATE INDEX `tmetaconsole_agent_server_name_idx` ON tmetaconsole_agent (`server_name`) ALGORITHM DEFAULT LOCK DEFAULT;
+CREATE INDEX `tma_id_os_idx` ON tmetaconsole_agent (`id_os`) ALGORITHM DEFAULT LOCK DEFAULT;
+CREATE INDEX `tma_server_name_idx` ON tmetaconsole_agent (`server_name`) ALGORITHM DEFAULT LOCK DEFAULT;
 
-CREATE INDEX `tmetaconsole_event_history_timestamp_idx` ON tmetaconsole_event_history (`timestamp`) ALGORITHM DEFAULT LOCK DEFAULT;
-CREATE INDEX `tmetaconsole_event_history_estado_idx` ON tmetaconsole_event_history (`estado`) ALGORITHM DEFAULT LOCK DEFAULT;
+CREATE INDEX `tmeh_estado_idx` ON tmetaconsole_event_history (`timestamp`) ALGORITHM DEFAULT LOCK DEFAULT;
+CREATE INDEX `tmeh_timestamp_idx` ON tmetaconsole_event_history (`estado`) ALGORITHM DEFAULT LOCK DEFAULT;
 
 COMMIT;

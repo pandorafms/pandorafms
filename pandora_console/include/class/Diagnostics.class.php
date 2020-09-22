a9b2c6beb81fedc136e9769304c8bf2ab06cd6e1
Marcos Alconada <marcos.alconada@artica.es>
2020-09-22 12:16:31 +0200

    Ent 5898 secciones en diagnostico aparecen 2 veces

--- pandora_console/include/class/Diagnostics.class.php
+++ pandora_console/include/class/Diagnostics.class.php
@@ -283,7 +283,7 @@ class Diagnostics extends Wizard
             $return .= '</div>';
         }
 
-        return $return;
+        return false;
     }
 
 
@@ -1596,7 +1596,7 @@ class Diagnostics extends Wizard
             }
         }
 
-        return $result;
+        return true;
     }
 
 

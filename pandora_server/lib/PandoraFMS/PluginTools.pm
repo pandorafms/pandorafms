3a890dc8b81ec15c905194cad58a2d2066877cbd
artica <artica.devel@gmail.com>
2020-10-15 10:19:42 +0200

    Updated version and build strings.

--- pandora_server/lib/PandoraFMS/PluginTools.pm
+++ pandora_server/lib/PandoraFMS/PluginTools.pm
@@ -33,7 +33,7 @@ use base 'Exporter';
 our @ISA = qw(Exporter);
 
 # version: Defines actual version of Pandora Server for this module only
-my $pandora_version = "7.0NG.749";
+my $pandora_version = "7.0NG.750";
 my $pandora_build = "201015";
 our $VERSION = $pandora_version." ".$pandora_build;
 
--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -35,7 +35,7 @@ use PandoraFMS::Config;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "7.0NG.749 PS201015";
+my $version = "7.0NG.750 PS201015";
 
 # Pandora server configuration
 my %conf;

75bb4338b7a36ac09af8639075769a6924b35c9c
fbsanchez <fborja.sanchez@artica.es>
2020-10-22 18:10:46 +0200

    fixes in event macros (responses)

--- pandora_console/include/functions_events.php
+++ pandora_console/include/functions_events.php
@@ -3811,7 +3811,7 @@ function events_get_response_target(
     if (strpos($target, '_alert_id_') !== false) {
         $target = str_replace(
             '_alert_id_',
-            empty($event['is_alert_am']) ? __('N/A') : $event['is_alert_am'],
+            empty($event['id_alert_am']) ? __('N/A') : $event['id_alert_am'],
             $target
         );
     }
@@ -3891,6 +3891,15 @@ function events_get_response_target(
     // This will replace the macro with the current logged user.
     if (strpos($target, '_current_user_') !== false) {
         $target = str_replace('_current_user_', $config['id_user'], $target);
+    }
+
+    // This will replace the macro with the command timeout value.
+    if (strpos($target, '_command_timeout_') !== false) {
+        $target = str_replace(
+            '_command_timeout_',
+            $event_response['command_timeout'],
+            $target
+        );
     }
 
     return $target;
@@ -6973,7 +6982,7 @@ function events_get_field_value_by_event_id(
     if (strpos($value, '_group_name_') !== false) {
         $value = str_replace(
             '_group_name_',
-            groups_get_name($event['id_grupo'], true),
+            io_safe_output(groups_get_name($event['id_grupo'], true)),
             $value
         );
     }

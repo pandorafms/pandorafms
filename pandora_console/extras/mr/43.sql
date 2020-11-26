dbfe52e383e042aaf37c83901ee6747292796b47
Jose Gonzalez <jose.gonzalez@artica.es>
2020-11-26 13:39:17 +0100

    Fix issues with alert fields

---
+++ pandora_console/extras/mr/43.sql
@@ -0,0 +1,9 @@
+START TRANSACTION;
+
+ALTER TABLE `talert_snmp` ADD COLUMN `al_field16` TEXT NOT NULL DEFAULT "" AFTER `al_field15`;
+ALTER TABLE `talert_snmp` ADD COLUMN `al_field17` TEXT NOT NULL DEFAULT "" AFTER `al_field16`;
+ALTER TABLE `talert_snmp` ADD COLUMN `al_field18` TEXT NOT NULL DEFAULT "" AFTER `al_field17`;
+ALTER TABLE `talert_snmp` ADD COLUMN `al_field19` TEXT NOT NULL DEFAULT "" AFTER `al_field18`;
+ALTER TABLE `talert_snmp` ADD COLUMN `al_field20` TEXT NOT NULL DEFAULT "" AFTER `al_field19`;
+
+COMMIT;

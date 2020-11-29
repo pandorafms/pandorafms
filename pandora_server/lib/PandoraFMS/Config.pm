9041fc31e8c87ca9fc9d2920a3ec1eed6f1d4ce8
artica <artica.devel@gmail.com>
2020-11-29 01:00:15 +0100

    Auto-updated build strings.

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -45,7 +45,7 @@ our @EXPORT = qw(
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "7.0NG.750";
-my $pandora_build = "201128";
+my $pandora_build = "201129";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash
--- pandora_server/pandora_server.redhat.spec
+++ pandora_server/pandora_server.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     7.0NG.750
-%define release     201128
+%define release     201129
 
 Summary:            Pandora FMS Server
 Name:               %{name}

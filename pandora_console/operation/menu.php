210f2057cddcfc3d1220db2f56b4eeebded6c496
Luis Calvo <luis.calvo@artica.es>
2020-12-21 17:56:24 +0100

    Removes event_statistics view

--- pandora_console/operation/menu.php
+++ pandora_console/operation/menu.php
@@ -393,8 +393,6 @@ if (check_acl($config['id_user'], 0, 'ER')
     $sub['operation/events/events']['text'] = __('View events');
     $sub['operation/events/events']['id'] = 'View events';
     $sub['operation/events/events']['pages'] = ['godmode/events/events'];
-    $sub['operation/events/event_statistics']['text'] = __('Statistics');
-    $sub['operation/events/event_statistics']['id'] = 'Statistics';
 
     // If ip doesn't is in list of allowed IP, isn't show this options.
     include_once 'include/functions_api.php';

2bd9515152333ad1b3c0543ff23cf05ca4c8cf65
artica <artica.devel@gmail.com>
2021-03-08 01:00:14 +0100

    Auto-updated build strings.

--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -36,7 +36,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "7.0NG.752 PS210307";
+my $version = "7.0NG.752 PS210308";
 
 # save program name for logging
 my $progname = basename($0);

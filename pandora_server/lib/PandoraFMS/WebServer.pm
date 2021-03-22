4b7c0fc7ee3b9df14d404c29a95d52f5c650d2ba
Jose Gonzalez <jose.gonzalez@artica.es>
2021-03-22 17:39:24 +0100

    WIP: Backup upload

--- pandora_server/lib/PandoraFMS/WebServer.pm
+++ pandora_server/lib/PandoraFMS/WebServer.pm
@@ -1,10 +1,20 @@
 package PandoraFMS::WebServer;
-
 ##########################################################################
 # Pandora FMS Web Server.
+# Pandora FMS. the Flexible Monitoring System. http://www.pandorafms.org
 ##########################################################################
 # Copyright (c) 2007-2021 Artica Soluciones Tecnologicas S.L
-# This code is not free or OpenSource. Please don't redistribute.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; version 2
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 ##########################################################################
 
 use strict;
@@ -21,13 +31,13 @@ use POSIX qw(strftime);
 # Default lib dir for RPM and DEB packages
 use lib '/usr/lib/perl5';
 
-use PandoraFMS::Goliat::GoliatTools;
-use PandoraFMS::Goliat::GoliatConfig;
-
 use PandoraFMS::Tools;
 use PandoraFMS::DB;
 use PandoraFMS::Core;
 use PandoraFMS::ProducerConsumerServer;
+
+use Goliat::GoliatTools;
+use Goliat::GoliatConfig;
 
 # Inherits from PandoraFMS::ProducerConsumerServer
 our @ISA = qw(PandoraFMS::ProducerConsumerServer);

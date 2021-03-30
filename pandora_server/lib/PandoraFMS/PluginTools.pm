321fc8aa05b1fd6a09313800ed05311948e0e3d3
artica <artica.devel@gmail.com>
2021-03-31 01:00:44 +0200

    Auto-updated build strings.

--- pandora_server/lib/PandoraFMS/PluginTools.pm
+++ pandora_server/lib/PandoraFMS/PluginTools.pm
@@ -34,7 +34,7 @@ our @ISA = qw(Exporter);
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "7.0NG.753";
-my $pandora_build = "210330";
+my $pandora_build = "210331";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
--- pandora_server/pandora_server.redhat.spec
+++ pandora_server/pandora_server.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_server
 %define version     7.0NG.753
-%define release     210330
+%define release     210331
 
 Summary:            Pandora FMS Server
 Name:               %{name}

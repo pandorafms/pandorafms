245286330ce0efa880fe82cf8837201b044ac507
Calvo <luis.calvo@artica.es>
2021-03-30 08:28:10 +0200

    Fixed treeview filter by module status

--- pandora_console/include/class/Tree.class.php
+++ pandora_console/include/class/Tree.class.php
@@ -1005,7 +1005,7 @@ class Tree
         $agent_search_filter = $this->getAgentSearchFilter();
         $agent_status_filter = $this->getAgentStatusFilter();
         $module_search_filter = $this->getModuleSearchFilter();
-        $module_status_filter = $this->getModuleStatusFilter();
+        $module_status_filter = $this->getModuleStatusFilterFromTestado($this->filter['statusModule']);
 
         $condition = $this->L2condition;
         $condition_inside = $this->L2conditionInside;

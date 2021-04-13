9bbac06771758b2e3b9f77ab7a5066e2b21a28b1
Daniel Maya <daniel.maya@artica.es>
2021-04-13 09:26:50 +0000

    Ent 7161 cambiar opciones de skin en usuarios y grupos

--- pandora_console/extras/mr/46.sql
+++ pandora_console/extras/mr/46.sql
@@ -4,4 +4,7 @@ ALTER TABLE `tagent_custom_fields` MODIFY COLUMN `combo_values` TEXT NOT NULL DE
 
 UPDATE `talert_commands` SET `fields_descriptions` = '[\"Event&#x20;name\",\"Event&#x20;type\",\"Source\",\"Agent&#x20;name&#x20;or&#x20;_agent_\",\"Event&#x20;severity\",\"ID&#x20;extra\",\"Tags&#x20;separated&#x20;by&#x20;commas\",\"Comments\",\"\",\"\"]' WHERE `name` = "Monitoring&#x20;Event";
 
+UPDATE `tskin` SET `name` = 'Default&#x20;theme' , `relative_path` = 'pandora.css' WHERE `id` = 1;
+UPDATE `tskin` SET `name` = 'Black&#x20;theme' , `relative_path` = 'Black&#x20;theme' , `description` = 'Black&#x20;theme' WHERE `id` = 2;
+
 COMMIT;
--- pandora_console/godmode/groups/configure_group.php
+++ pandora_console/godmode/groups/configure_group.php
@@ -219,12 +219,11 @@ $table->data[$i][0] = __('Other').ui_print_help_tip(__('Information accessible t
 $table->data[$i][1] = html_print_textarea('other', 4, 40, $other, "class='min-height-0px'", true);
 $i++;
 
-$isFunctionSkins = enterprise_include_once('include/functions_skins.php');
-if ($isFunctionSkins !== ENTERPRISE_NOT_HOOK && !defined('METACONSOLE')) {
-    $table->data[9][0] = __('Skin');
-    $table->data[9][1] = skins_print_select($config['id_user'], 'skin', $skin, '', __('None'), 0, true);
-}
-
+// $isFunctionSkins = enterprise_include_once('include/functions_skins.php');
+// if ($isFunctionSkins !== ENTERPRISE_NOT_HOOK && !defined('METACONSOLE')) {
+// $table->data[10][0] = __('Skin');
+// $table->data[10][1] = skins_print_select($config['id_user'], 'skin', $skin, '', __('None'), 0, true);
+// }
 if (defined('METACONSOLE')) {
     $sec = 'advanced';
 } else {

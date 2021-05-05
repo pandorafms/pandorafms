735df105d32940876b1df958b4015cef5aa79bf4
fbsanchez <fborja.sanchez@artica.es>
2021-05-05 12:40:56 +0200

    Mute some license checks while initializing enterprise component (pandora-ha)

--- pandora_server/lib/PandoraFMS/Tools.pm
+++ pandora_server/lib/PandoraFMS/Tools.pm
@@ -914,8 +914,9 @@ sub float_equal {
 # Tries to load the PandoraEnterprise module. Must be called once before
 # enterprise_hook ().
 ################################################################################
-sub enterprise_load ($) {
+sub enterprise_load ($;$) {
 	my $pa_config = shift;
+	my $muted = shift;
 	
 	# Check dependencies
 	
@@ -943,7 +944,7 @@ sub enterprise_load ($) {
 	}
 	
 	# Initialize the enterprise module.
-	PandoraFMS::Enterprise::init($pa_config);
+	PandoraFMS::Enterprise::init($pa_config, $muted);
 	
 	return 1;
 }

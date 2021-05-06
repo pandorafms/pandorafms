ce5934b9a1aa5984f0e2847f59704877e55108e3
José González <jose.gonzalez@pandorafms.com>
2021-05-06 14:46:58 +0200

    Added form-type buttons for user-actions

--- pandora_console/operation/users/user_edit.php
+++ pandora_console/operation/users/user_edit.php
@@ -644,7 +644,7 @@ foreach ($timezones as $timezone_name => $tz) {
 }
 
 if (is_metaconsole()) {
-    echo '<form name="user_mod" method="post" action="'.ui_get_full_url('index.php?sec=advanced&sec2=advanced/users_setup').'&amp;tab=user_edit&amp;modified=1&amp;id='.$id.'&amp;pure='.$config['pure'].'">';
+    echo '<form name="user_mod" method="post" action="'.ui_get_full_url('index.php?sec=advanced&sec2=advanced/users_setup').'&amp;tab=user_edit&amp;modified=1&amp;pure='.$config['pure'].'">';
 } else {
     echo '<form name="user_mod" method="post" action="'.ui_get_full_url('index.php?sec=workspace&sec2=operation/users/user_edit').'&amp;modified=1&amp;pure='.$config['pure'].'">';
 }

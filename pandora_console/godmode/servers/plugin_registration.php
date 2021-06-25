4d2f9fb4c952647c498d47e32a1e636475d3a0bc
fbsanchez <fborja.sanchez@artica.es>
2021-06-25 12:54:14 +0200

    missing messages

--- pandora_console/godmode/servers/plugin_registration.php
+++ pandora_console/godmode/servers/plugin_registration.php
@@ -73,8 +73,10 @@ if (is_metaconsole() === true) {
     if ($management_allowed === false) {
         ui_print_warning_message(
             __(
-                'This console is not manager of this environment,
-            please manage this feature from centralized manager console (Metaconsole).'
+                'This console is not manager of this environment, please manage this feature from feature from %s.',
+                '<a target="_blank" href="'.ui_get_meta_url(
+                    'index.php?sec=advanced&sec2=godmode/servers/plugin_registration&tab=plugin_registration&pure=0'
+                ).'">'.__('metaconsole').'</a>'
             )
         );
         return;

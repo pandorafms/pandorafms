d683e192b57fe92218f3309b6a18d8401b0d5705
fbsanchez <fborja.sanchez@artica.es>
2021-09-29 13:43:08 +0200

    Moved changes for CorrelationServer schema to MR50

--- pandora_console/extras/mr/49.sql
+++ pandora_console/extras/mr/49.sql
@@ -6,7 +6,4 @@ UPDATE `tconfig` set value = 'Lato-Regular.ttf' WHERE token LIKE 'custom_report_
 UPDATE `tconfig` set value = 'Lato-Regular.ttf' WHERE token LIKE 'fontpath';
 UPDATE `tlanguage` SET `name` = 'Deutsch' WHERE `id_language` = 'de';
 
-ALTER TABLE `tevent_alert` ADD COLUMN `last_evaluation` bigint(20) NOT NULL default 0;
-ALTER TABLE `tevent_alert` ADD COLUMN `pool_occurrences` int unsigned not null default 0;
-
 COMMIT;
--- pandora_console/extras/mr/50.sql
+++ pandora_console/extras/mr/50.sql
@@ -5,4 +5,7 @@ ALTER TABLE `tevent_alert` ADD COLUMN `id_template_fields` int(10) unsigned NOT 
 ALTER TABLE `tevent_filter` ADD COLUMN `time_from` TIME NULL;
 ALTER TABLE `tevent_filter` ADD COLUMN `time_to` TIME NULL;
 
+ALTER TABLE `tevent_alert` ADD COLUMN `last_evaluation` bigint(20) NOT NULL default 0;
+ALTER TABLE `tevent_alert` ADD COLUMN `pool_occurrences` int unsigned not null default 0;
+
 COMMIT;

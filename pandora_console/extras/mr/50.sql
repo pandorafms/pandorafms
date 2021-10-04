e11d8f37665286d66d82b7118302e6f0efc47eb8
fbsanchez <fborja.sanchez@artica.es>
2021-10-04 19:43:57 +0200

    minor schema update

--- pandora_console/extras/mr/50.sql
+++ pandora_console/extras/mr/50.sql
@@ -41,6 +41,8 @@ CREATE TABLE IF NOT EXISTS `tncm_agent` (
     `model` text,
     `protocol` int unsigned not null default 0,
     `cred_key` varchar(100),
+    `status` int(4) NOT NULL default 5,
+	`last_status_change` bigint(20) NOT NULL default 0,
     PRIMARY KEY (`id_agent`),
     FOREIGN KEY (`id_agent`) REFERENCES `tagente`(`id_agente`) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (`cred_key`) REFERENCES `tcredential_store`(`identifier`) ON UPDATE CASCADE ON DELETE SET NULL
@@ -49,8 +51,8 @@ CREATE TABLE IF NOT EXISTS `tncm_agent` (
 CREATE TABLE IF NOT EXISTS `tncm_agent_templates` (
     `id_agent` int(10) unsigned NOT NULL,
     `id_template` bigint(20) unsigned NOT NULL,
-    `status` int(4) NOT NULL default '0',
-	`last_update` bigint(20) NOT NULL default '0',
+    `status` int(4) NOT NULL default 5,
+	`last_status_change` bigint(20) NOT NULL default 0,
     PRIMARY KEY (`id_agent`, `id_template`),
     FOREIGN KEY (`id_agent`) REFERENCES `tagente`(`id_agente`) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (`id_template`) REFERENCES `tncm_template`(`id`) ON UPDATE CASCADE ON DELETE CASCADE

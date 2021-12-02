00d79b553c0b3fc04b77263592fe7a11b832127f
fbsanchez <fborja.sanchez@artica.es>
2021-12-02 14:46:31 +0100

    missing in merge mr

--- pandora_console/extras/mr/51.sql
+++ pandora_console/extras/mr/51.sql
@@ -1,5 +1,7 @@
 START TRANSACTION;
 
+ALTER TABLE `tagente_modulo` ADD COLUMN `percentage_critical` tinyint(1) UNSIGNED DEFAULT 0;
+ALTER TABLE `tagente_modulo` ADD COLUMN `percentage_warning` tinyint(1) UNSIGNED DEFAULT 0;
 ALTER TABLE tagente_modulo MODIFY debug_content TEXT;
 
 CREATE TABLE IF NOT EXISTS `tncm_queue` (

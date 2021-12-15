b4131431af11c4fdcf741f8aed5eb21edc47036f
Daniel Barbero Martin <daniel.barbero@artica.es>
2021-12-15 09:55:03 +0100

    fixed mr

--- pandora_console/extras/mr/51.sql
+++ pandora_console/extras/mr/51.sql
@@ -68,7 +68,6 @@ ALTER TABLE `tipam_network` ADD FOREIGN KEY (`location`) REFERENCES `tipam_netwo
 ALTER TABLE `tagent_repository` ADD COLUMN `deployment_timeout` INT UNSIGNED DEFAULT 600 AFTER `path`;
 ALTER TABLE `talert_special_days` ADD COLUMN `id_calendar` int(10) unsigned NOT NULL DEFAULT 1;
 ALTER TABLE `talert_special_days` ADD COLUMN `day_code` tinyint(2) unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `talert_special_days` DROP COLUMN `same_day`;
 ALTER TABLE `talert_special_days` ADD FOREIGN KEY (`id_calendar`) REFERENCES `talert_calendar`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE `tipam_network` ADD COLUMN `id_site` bigint unsigned;
@@ -90,6 +89,8 @@ UPDATE `talert_special_days` set `day_code` = 4 WHERE `same_day` = 'thursday';
 UPDATE `talert_special_days` set `day_code` = 5 WHERE `same_day` = 'friday';
 UPDATE `talert_special_days` set `day_code` = 6 WHERE `same_day` = 'saturday';
 UPDATE `talert_special_days` set `day_code` = 7 WHERE `same_day` = 'sunday';
+
+ALTER TABLE `talert_special_days` DROP COLUMN `same_day`;
 
 INSERT IGNORE INTO `tipam_network_location` (`name`) SELECT `location` FROM `tipam_network` WHERE `location` <> '';
 UPDATE `tipam_network` INNER JOIN `tipam_network_location` ON tipam_network_location.name=tipam_network.location SET tipam_network.location=tipam_network_location.id;

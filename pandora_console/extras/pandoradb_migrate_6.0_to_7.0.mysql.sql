46d995a9832bcae903a6b68b1671b5e8850327bb
fbsanchez <fborja.sanchez@artica.es>
2022-01-10 11:24:32 +0100

    Added index to service elements (modules)

--- pandora_console/extras/pandoradb_migrate_6.0_to_7.0.mysql.sql
+++ pandora_console/extras/pandoradb_migrate_6.0_to_7.0.mysql.sql
@@ -480,7 +480,8 @@ CREATE TABLE IF NOT EXISTS `tservice_element` (
 	`id_agent` int(10) unsigned NOT NULL default 0,
 	`id_service_child` int(10) unsigned NOT NULL default 0,
 	`id_server_meta` int(10)  unsigned NOT NULL default 0,
-	PRIMARY KEY  (`id`)
+	PRIMARY KEY  (`id`),
+	INDEX `IDX_tservice_element` (`id_service`,`id_agente_modulo`)
 ) ENGINE=InnoDB 
 COMMENT = 'Table to define the modules and the weights of the modules that define a service' 
 DEFAULT CHARSET=utf8;

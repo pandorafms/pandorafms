e8b594a03b07c9ab95b013869173a2be8c9c3675
artica <artica.devel@gmail.com>
2022-01-15 01:00:18 +0100

    Auto-updated build strings.

--- pandora_console/install.php
+++ pandora_console/install.php
@@ -129,7 +129,7 @@
         <div style='height: 10px'>
             <?php
             $version = '7.0NG.759';
-            $build = '220114';
+            $build = '220115';
             $banner = "v$version Build $build";
 
             error_reporting(0);
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -36,7 +36,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "7.0NG.759 Build 220114";
+my $version = "7.0NG.759 Build 220115";
 
 # save program name for logging
 my $progname = basename($0);

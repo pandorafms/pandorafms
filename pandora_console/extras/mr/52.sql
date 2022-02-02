293c1cfb3a39fcc58fdd133a351ba498633eb0c1
Calvo <luis.calvo@artica.es>
2022-02-02 10:36:54 +0100

    Chaged to correct MR

--- pandora_console/extras/mr/52.sql
+++ pandora_console/extras/mr/52.sql
@@ -1,5 +1,7 @@
 START TRANSACTION;
+
 ALTER TABLE `tpolicy_queue` MODIFY COLUMN `progress` int(10) NOT NULL default '0';
 CREATE INDEX `IDX_tservice_element` ON `tservice_element`(`id_service`,`id_agente_modulo`);
+ALTER TABLE `tusuario` ADD COLUMN `local_user` tinyint(1) unsigned NOT NULL DEFAULT 0;
 
 COMMIT;
--- pandora_console/extras/mr/53.sql
+++
@@ -1,5 +0,0 @@
-START TRANSACTION;
-ALTER TABLE `tusuario` ADD COLUMN `local_user` tinyint(1) unsigned NOT NULL DEFAULT 0;
-
-
-COMMIT;

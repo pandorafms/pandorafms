fcfb5e0d2aa3ee2f232eb1fcfec60dcc33eec278
fbsanchez <fborja.sanchez@artica.es>
2022-02-07 11:26:36 +0100

    Schema, allow certain autoconfigurations being disabled on MC environments

--- pandora_console/extras/mr/52.sql
+++ pandora_console/extras/mr/52.sql
@@ -3,4 +3,6 @@ ALTER TABLE `tpolicy_queue` MODIFY COLUMN `progress` int(10) NOT NULL default '0
 CREATE INDEX `IDX_tservice_element` ON `tservice_element`(`id_service`,`id_agente_modulo`);
 ALTER TABLE tevent_response ADD COLUMN display_command tinyint(1) default 0;
 
+ALTER TABLE `tautoconfig` ADD COLUMN `disabled` TINYINT DEFAULT 0;
+
 COMMIT;

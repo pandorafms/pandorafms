4a61efd7b6c4fba794d246d4d4a11a934e3b854f
fbsanchez <fborja.sanchez@artica.es>
2022-02-08 22:52:54 +0100

    added tpolicy_group_agents

--- pandora_console/extras/mr/52.sql
+++ pandora_console/extras/mr/52.sql
@@ -14,4 +14,15 @@ SOURCE procedures/alertEvents.sql;
 CALL `migrateEventRanges`();
 DROP PROCEDURE `migrateEventRanges`;
 
+CREATE TABLE IF NOT EXISTS `tpolicy_group_agents` (
+    `id` SERIAL,
+    `id_policy` INT UNSIGNED,
+    `id_agent` INT UNSIGNED,
+	`direct` TINYINT UNSIGNED DEFAULT 0,
+    FOREIGN KEY (`id_policy`) REFERENCES `tpolicies`(`id`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (`id_agent`) REFERENCES `tagente`(`id_agente`)
+        ON DELETE CASCADE ON UPDATE CASCADE		
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
 COMMIT;

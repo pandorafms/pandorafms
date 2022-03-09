0a7238fe33b6591760b569882d6deb65994a2371
Daniel Maya <daniel.maya@artica.es>
2022-03-09 12:06:21 +0100

    #8692 Fixed remote config

--- pandora_console/godmode/agentes/configurar_agente.php
+++ pandora_console/godmode/agentes/configurar_agente.php
@@ -31,6 +31,7 @@ global $config;
 
 enterprise_include('godmode/agentes/configurar_agente.php');
 enterprise_include_once('include/functions_modules.php');
+enterprise_include_once('include/functions_config_agents.php');
 require_once $config['homedir'].'/include/functions_agents.php';
 require_once $config['homedir'].'/include/functions_cron.php';
 ui_require_javascript_file('encode_decode_base64');

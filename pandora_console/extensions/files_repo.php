b7923fe4941e97c5dc76f5ee854dfc3569083263
Calvo <luis.calvo@artica.es>
2022-03-28 11:18:08 +0200

    Added files repo error information for post_max_size and upload_max_filesize

--- pandora_console/extensions/files_repo.php
+++ pandora_console/extensions/files_repo.php
@@ -145,7 +145,7 @@ function pandora_files_repo_godmode()
     // Check for an anoying error that causes the $_POST and $_FILES arrays.
     // were empty if the file is larger than the post_max_size.
     if (intval($server_content_length) > 0 && empty($_POST)) {
-        ui_print_error_message(__('The file exceeds the maximum size'));
+        ui_print_error_message(__('Problem uploading. Please check this PHP runtime variable values: <pre>  post_max_size (currently '.ini_get('post_max_size').')</pre>'));
     }
 
     // GET and POST parameters.

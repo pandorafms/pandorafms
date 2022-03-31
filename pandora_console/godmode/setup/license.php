55e847a5c98a13a2c3f9a7563906af84d43b3d43
Daniel Maya <daniel.maya@artica.es>
2022-03-31 16:52:50 +0200

    #8541 Added file customer_key

--- pandora_console/godmode/setup/license.php
+++ pandora_console/godmode/setup/license.php
@@ -74,8 +74,11 @@ if ($update_settings) {
             );
         }
 
-		// Update the license file.
-		file_put_contents($config["remote_config"] . '/' . LICENSE_FILE, $_POST['keys']['customer_key']);
+        // Update the license file.
+        $result = file_put_contents($config['remote_config'].'/'.LICENSE_FILE, $_POST['keys']['customer_key']);
+        if ($result === false) {
+            ui_print_error_message(__('Failed to Update license file'));
+        }
 
         ui_print_success_message(__('License updated'));
     }

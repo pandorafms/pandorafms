c2fe3c5d34850a7d768d4f631b6ac7381de0ff27
Daniel Maya <daniel.maya@pandorafms.com>
2022-05-23 16:03:37 +0200

    #9009 Fixed icon

--- pandora_console/include/lib/Dashboard/Widgets/module_status.php
+++ pandora_console/include/lib/Dashboard/Widgets/module_status.php
@@ -478,7 +478,7 @@ class ModuleStatusWidget extends Widget
 
             case 4:
                 // Critical (ALERT).
-                $icon = '4'.$icon.'_bad.png';
+                $icon = $icon.'_bad.png';
             break;
 
             case 0:
@@ -493,7 +493,7 @@ class ModuleStatusWidget extends Widget
 
             case 10:
                 // Warning (ALERT).
-                $icon = '4'.$icon.'_warning.png';
+                $icon = $icon.'_warning.png';
             break;
 
             case 3:

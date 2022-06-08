5297123d170e673f787691a5334ee309491ceb52
Calvo <luis.calvo@artica.es>
2022-06-08 13:10:01 +0200

    Console changes for 8950

--- pandora_console/extras/mr/55.sql
+++ pandora_console/extras/mr/55.sql
@@ -4,4 +4,8 @@ ALTER TABLE `tservice` ADD COLUMN `enable_sunburst` tinyint(1) NOT NULL default 
 
 ALTER TABLE `tdashboard` MODIFY `name` TEXT NOT NULL DEFAULT '';
 
+ALTER TABLE `tagente` ADD COLUMN `satellite_server` INT NOT NULL default 0;
+ALTER TABLE `tmetaconsole_agent` ADD COLUMN `satellite_server` INT NOT NULL default 0;
+
 COMMIT;
+

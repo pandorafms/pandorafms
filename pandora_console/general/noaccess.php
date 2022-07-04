81283e26261895bfc46e30c254eff502d02617de
edu.corral <eduardo.corral@artica.es>
2022-07-04 10:46:55 +0200

    ent-9045 error message

--- pandora_console/general/noaccess.php
+++ pandora_console/general/noaccess.php
@@ -1,98 +1,115 @@
 <html>
 <head>
-    
-<style>
+    <link rel="stylesheet" href="<?php echo $config['homeurl']; ?>include/styles/pandora_minimal.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $config['homeurl']; ?>include/styles/js/jquery-ui.min.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $config['homeurl']; ?>include/styles/js/jquery-ui_custom.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $config['homeurl']; ?>include/styles/select2.min.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $config['homeurl']; ?>include/styles/pandora.css" type="text/css" />
+    <script type='text/javascript' src='<?php echo $config['homeurl']; ?>include/javascript/jquery.current.js'></script>
+    <script type='text/javascript' src='<?php echo $config['homeurl']; ?>include/javascript/jquery.pandora.js'></script>
+    <script type='text/javascript' src='<?php echo $config['homeurl']; ?>include/javascript/jquery-ui.min.js'></script>
+    <script type='text/javascript' src='<?php echo $config['homeurl']; ?>include/javascript/select2.min.js'></script>
+    <script type='text/javascript' src='<?php echo $config['homeurl']; ?>include/javascript/pandora.js'></script>
+    <script type='text/javascript' src='<?php echo $config['homeurl']; ?>include/javascript/pandora_ui.js'></script>
 
-#alert_messages_na{
-    z-index:2;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    -webkit-transform: translate(-50%, -50%);   
-    width:650px;
-    height: 400px;
-    background:white;
-    background-image:url('images/imagen-no-acceso.jpg');
-    background-repeat:no-repeat;
-    justify-content: center;
-    display: flex;
-    flex-direction: column;
-    box-shadow:4px 5px 10px 3px rgba(0, 0, 0, 0.4);
-}
+    <style>
+    #alert_messages_na {
+        z-index: 2;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        -webkit-transform: translate(-50%, -50%);
+        width: 650px;
+        height: 400px;
+        background: white;
+        background-repeat: no-repeat;
+        justify-content: center;
+        display: flex;
+        flex-direction: column;
+        box-shadow: 4px 5px 10px 3px rgba(0, 0, 0, 0.4);
+    }
 
-.modalheade{
-    text-align:center;
-    width:100%;
-    position:absolute;
-    top:0;
-}
-.modalheadertex{
-    color:#000;
-    line-height: 40px;
-    font-size: 23pt;
-    margin-bottom:30px;
-}
-.modalclose{
-    cursor:pointer;
-    display:inline;
-    float:right;
-    margin-right:10px;
-    margin-top:10px;
-}
-.modalconten{
-    color:black;
-    width:300px;
-    margin-left: 30px;
-}
-.modalcontenttex{
-    text-align:left;
-    color:black;
-    font-size: 11pt;
-    line-height:13pt;
-    margin-bottom:30px;
-}
-.modalokbutto{
-    cursor:pointer;
-    text-align:center;
-    display: inline-block;
-    padding: 6px 45px;
-    -moz-border-radius: 3px;
-    -webkit-border-radius: 3px;
-    border-radius: 3px;
-    background-color:white;
-    border: 1px solid #82b92e;
-}
-.modalokbuttontex{
-    color:#82b92e;
-    font-size:13pt;
-}
-.modalgobutto{
-    cursor:pointer;
-    text-align:center;
-    -moz-border-radius: 3px;
-    -webkit-border-radius: 3px;
-    border-radius: 3px;
-    background-color:white;
-    border: 1px solid #82b92e;
-}
-.modalgobuttontex{
-color:#82b92e;
-font-size:10pt;
-}
+    .modalheade {
+        text-align: center;
+        width: 100%;
+        position: absolute;
+        top: 0;
+    }
+
+    .modalheadertex {
+        color: #000;
+        line-height: 40px;
+        font-size: 23pt;
+        margin-bottom: 30px;
+    }
+
+    .modalclose {
+        cursor: pointer;
+        display: inline;
+        float: right;
+        margin-right: 10px;
+        margin-top: 10px;
+    }
+
+    .modalconten {
+        color: black;
+        width: 300px;
+        margin-left: 30px;
+    }
+
+    .modalcontenttex {
+        text-align: left;
+        color: black;
+        font-size: 11pt;
+        line-height: 13pt;
+        margin-bottom: 30px;
+    }
+
+    .modalokbutto {
+        cursor: pointer;
+        text-align: center;
+        display: inline-block;
+        padding: 6px 45px;
+        -moz-border-radius: 3px;
+        -webkit-border-radius: 3px;
+        border-radius: 3px;
+        background-color: white;
+        border: 1px solid #82b92e;
+    }
+
+    .modalokbuttontex {
+        color: #82b92e;
+        font-size: 13pt;
+    }
+
+    .modalgobutto {
+        cursor: pointer;
+        text-align: center;
+        -moz-border-radius: 3px;
+        -webkit-border-radius: 3px;
+        border-radius: 3px;
+        background-color: white;
+        border: 1px solid #82b92e;
+    }
+
+    .modalgobuttontex {
+        color: #82b92e;
+        font-size: 10pt;
+    }
 
 
-#opacidad{
-    position:fixed;
-    background:black;
-    opacity:0.6;
-    z-index:-1;
-    left:0px;
-    top:0px;
-    width:100%;
-    height:100%;
-}
-/*
+    #opacidad {
+        position: fixed;
+        background: black;
+        opacity: 0.6;
+        z-index: -1;
+        left: 0px;
+        top: 0px;
+        width: 100%;
+        height: 100%;
+    }
+    /*
 .textodialog{
     margin-left: 0px;
     color:#333;
@@ -118,16 +135,16 @@ font-size:10pt;
 </style>
 </head>
 <body>
-    
-    <div id="alert_messages_na">
-        
+
+    <div id="alert_messages_na" style='background-image: url(<?php echo ui_get_full_url('images/imagen-no-acceso.jpg', false, false, false); ?>)'>
+
         <div class='modalheade'>
-            <img class='modalclose cerrar' src='<?php echo $config['homeurl']; ?>images/input_cross.png'>  
+            <img class='modalclose cerrar' src='<?php echo $config['homeurl']; ?>images/input_cross.png'>
         </div>
 
         <div class='modalconten'>
-            <div class='modalheadertex'>
-                <?php echo __("You don't have access to this page"); ?>
+            <div class='modalheadertex' style='font-size: 23pt'>
+                <?php echo __('You do not have access to this page'); ?>
             </div>
 
             <div class='modalcontenttex'>
@@ -141,7 +158,6 @@ font-size:10pt;
                     }
                 }
                 ?>
-                      
             </div>
 
             <div class='modalokbutto cerrar'>
@@ -149,18 +165,14 @@ font-size:10pt;
             </div>
         </div>
     </div>
-        
     <div id="opacidad"></div>
-    
 </body>
 </html>
 
 <script>
-
-    $(".cerrar").click(function(){
-    window.location=".";
+    $(".cerrar").click(function() {
+        window.location = "<?php echo $config['homeurl']; ?>";
     });
 
-    $('div#page').css('background-color','#d3d3d3');
-
+    $('div#page').css('background-color', '#d3d3d3');
 </script>

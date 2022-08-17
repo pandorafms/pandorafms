041e5c99627d47227a1fd2d03962e6cb08b117f3
KIKUCHI Koichiro <koichiro@rworks.jp>
2022-08-17 04:42:49 +0000

    fix GoliatLWP's get_content

--- pandora_server/lib/PandoraFMS/Goliat/GoliatLWP.pm
+++ pandora_server/lib/PandoraFMS/Goliat/GoliatLWP.pm
@@ -236,7 +236,7 @@ sub g_http_task {
 					$task_get_content[$thread_id] = $1;
 				}
 			} else {
-				$task_get_content[$thread_id] = $resp;
+				$task_get_content[$thread_id] = $resp->decoded_content;
 			}
 						 
 			# Resource bashing

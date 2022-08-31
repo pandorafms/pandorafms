855686772f7e9f852e0439d5b1ed77941ad6c8a4
artica <artica.devel@gmail.com>
2022-09-01 01:00:21 +0200

    Auto-updated build strings.

--- pandora_agents/win32/pandora.cc
+++ pandora_agents/win32/pandora.cc
@@ -30,7 +30,7 @@ using namespace Pandora;
 using namespace Pandora_Strutils;
 
 #define PATH_SIZE    _MAX_PATH+1
-#define PANDORA_VERSION ("7.0NG.764 Build 220831")
+#define PANDORA_VERSION ("7.0NG.764 Build 220901")
 
 string pandora_path;
 string pandora_dir;
--- pandora_console/pandora_console.redhat.spec
+++ pandora_console/pandora_console.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     7.0NG.764
-%define release     220831
+%define release     220901
 
 # User and Group under which Apache is running
 %define httpd_name  httpd

4a7b11395aa25be1f415336d766c1707dac84d9e
artica <artica.devel@gmail.com>
2022-09-19 01:00:16 +0200

    Auto-updated build strings.

--- pandora_server/lib/PandoraFMS/Config.pm
+++ pandora_server/lib/PandoraFMS/Config.pm
@@ -46,7 +46,7 @@ our @EXPORT = qw(
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "7.0NG.764";
-my $pandora_build = "220918";
+my $pandora_build = "220919";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 # Setup hash
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -36,7 +36,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "7.0NG.764 Build 220918";
+my $version = "7.0NG.764 Build 220919";
 
 # save program name for logging
 my $progname = basename($0);

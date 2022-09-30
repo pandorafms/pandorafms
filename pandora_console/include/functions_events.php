4609e141eeabca23f9cc15a7874a7b63bde8fb13
Daniel Barbero <daniel.barbero@artica.es>
2022-09-30 08:56:19 +0200

    remove tmetaconsole_history token pandora_enterprise#9579

--- pandora_console/include/functions_events.php
+++ pandora_console/include/functions_events.php
@@ -1679,7 +1679,7 @@ function events_get_all(
                 $result_meta = Promise\wait(
                     parallelMap(
                         $metaconsole_connections,
-                        function ($node_int) use ($sql) {
+                        function ($node_int) use ($sql, $history) {
                             try {
                                 if (is_metaconsole() === true
                                     && (int) $node_int > 0
@@ -1688,7 +1688,7 @@ function events_get_all(
                                     $node->connect();
                                 }
 
-                                $res = db_get_all_rows_sql($sql);
+                                $res = db_get_all_rows_sql($sql, $history);
                                 if ($res === false) {
                                     $res = [];
                                 }
@@ -1819,7 +1819,7 @@ function events_get_all(
         }
     }
 
-    return db_get_all_rows_sql($sql);
+    return db_get_all_rows_sql($sql, $history);
 }
 
 

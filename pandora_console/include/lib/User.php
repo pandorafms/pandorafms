f87737a0933feb0b16f12a93d004cbdfb0df75bb
alejandro.campos@artica.es <alejandro.campos@artica.es>
2022-10-19 17:07:22 +0200

    minor fix

--- pandora_console/include/lib/User.php
+++ pandora_console/include/lib/User.php
@@ -128,7 +128,7 @@ class User implements PublicLogin
     {
         $user = new self($data);
 
-        if ($user === null) {
+        if ($user->idUser === null) {
             return false;
         }
 

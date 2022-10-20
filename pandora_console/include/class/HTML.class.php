2d71c75405ab2e7af87371d7b9072c789c0aeb70
Calvo <luis.calvo@pandorafms.com>
2022-10-20 13:42:08 +0200

    Added title to printFormAsGrid

--- pandora_console/include/class/HTML.class.php
+++ pandora_console/include/class/HTML.class.php
@@ -935,6 +935,12 @@ class HTML
         $output_head = '<form class="discovery" onsubmit="'.$form['onsubmit'].'"  enctype="'.$form['enctype'].'" action="'.$form['action'].'" method="'.$form['method'];
         $output_head .= '" id="'.$form['id'].'" '.$form['extra'].'>';
 
+        if (isset($form['title']) === true && empty($form['title']) === false) {
+            $output_head .= '<div class="form_title"">';
+            $output_head .= '<span>'.$form['title'].'</span>';
+            $output_head .= '</div>';
+        }
+
         if ($return === false) {
             echo $output_head;
         }

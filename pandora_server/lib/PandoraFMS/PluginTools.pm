4118f7656b3fae51a457089ff74e623deed384fd
artica <artica.devel@gmail.com>
2022-10-22 01:00:24 +0200

    Auto-updated build strings.

--- pandora_server/lib/PandoraFMS/PluginTools.pm
+++ pandora_server/lib/PandoraFMS/PluginTools.pm
@@ -34,7 +34,7 @@ our @ISA = qw(Exporter);
 
 # version: Defines actual version of Pandora Server for this module only
 my $pandora_version = "7.0NG.765";
-my $pandora_build = "221021";
+my $pandora_build = "221022";
 our $VERSION = $pandora_version." ".$pandora_build;
 
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );

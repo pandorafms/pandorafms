ffacefa06e80d37c30d224ae79affe4755c03fe4
artica <artica.devel@gmail.com>
2022-11-19 01:00:45 +0100

    Auto-updated build strings.

--- pandora_console/install.php
+++ pandora_console/install.php
@@ -129,7 +129,7 @@
         <div style='height: 10px'>
             <?php
             $version = '7.0NG.766';
-            $build = '221118';
+            $build = '221119';
             $banner = "v$version Build $build";
 
             error_reporting(0);

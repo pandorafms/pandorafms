4d27310499b61529aab4e6020c5de4180ed127ce
daniel <daniel.barbero@artica.es>
2023-01-09 16:20:40 +0100

    fix bundle CV pandora_enterprise#9941

--- visual_console_client/src/VisualConsole.ts
+++ visual_console_client/src/VisualConsole.ts
@@ -718,8 +718,6 @@ export default class VisualConsole {
         itemInstance.onResizeFinished(context.handleElementResizementFinished);
       }
 
-      console.log(context.containerRef);
-      console.log(itemInstance.elementRef);
       // Add the item to the DOM.
       context.containerRef.append(itemInstance.elementRef);
       return itemInstance;

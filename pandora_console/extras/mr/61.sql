082d3b5532e10aa0bef38e47504ea154aa2d714e
Daniel Cebrian <daniel.cebrian@pandorafms.com>
2023-01-24 12:10:40 +0100

    #10166 added index in MR and pandoradb

--- pandora_console/extras/mr/61.sql
+++ pandora_console/extras/mr/61.sql
@@ -11,4 +11,15 @@ CREATE TABLE IF NOT EXISTS `tsesion_filter` (
     PRIMARY KEY (`id_filter`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE INDEX `tusuario_perfil_user` ON `tusuario_perfil` (`id_usuario`);
+CREATE INDEX `tusuario_perfil_group` ON `tusuario_perfil` (`id_grupo`);
+CREATE INDEX `tusuario_perfil_profile` ON `tusuario_perfil` (`id_perfil`);
+CREATE INDEX `tlayout_data_layout` ON `tlayout_data` (`id_layout`);
+CREATE INDEX `taddress_agent_agent` ON `taddress_agent` (`id_agent`);
+CREATE INDEX `ttag_name` ON `ttag` (name(15));
+CREATE INDEX `tservice_element_service` ON `tservice_element` (`id_service`);
+CREATE INDEX `tservice_element_agent` ON `tservice_element` (`id_agent`);
+CREATE INDEX `tservice_element_am` ON `tservice_element` (`id_agente_modulo`);
+CREATE INDEX `tagent_module_log_agent` ON `tagent_module_log` (`id_agent`);
+
 COMMIT;

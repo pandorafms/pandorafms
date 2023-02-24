96a5dd99919d9b78375e53b55791c0e6866738a6
Calvo <luis.calvo@pandorafms.com>
2023-02-24 15:15:06 +0100

    Fix minor bug

--- pandora_server/lib/PandoraFMS/PluginTools.pm
+++ pandora_server/lib/PandoraFMS/PluginTools.pm
@@ -33,7 +33,7 @@ use base 'Exporter';
 our @ISA = qw(Exporter);
 
 # version: Defines actual version of Pandora Server for this module only
-my $pandora_version = "7.0NG.769+ALT";
+my $pandora_version = "7.0NG.769";
 my $pandora_build = "230224";
 our $VERSION = $pandora_version." ".$pandora_build;
 

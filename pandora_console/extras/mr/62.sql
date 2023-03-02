2f4543a0234ae05ef17b283270c4c5d4c2596d16
Jonathan <jonathan.leon@pandorafms.com>
2023-03-02 08:20:43 +0100

    #9517 Change MR sql

--- pandora_console/extras/mr/62.sql
+++ pandora_console/extras/mr/62.sql
@@ -4,4 +4,6 @@ START TRANSACTION;
 CREATE INDEX agente_modulo_estado ON tevento (estado, id_agentmodule);
 CREATE INDEX idx_disabled ON talert_template_modules (disabled);
 
+INSERT INTO `treport_custom_sql` (`name`, `sql`) VALUES ('Agent&#x20;safe&#x20;mode&#x20;not&#x20;enable', 'select&#x20;alias&#x20;from&#x20;tagente&#x20;where&#x20;safe_mode_module&#x20;=&#x20;0');
+
 COMMIT;

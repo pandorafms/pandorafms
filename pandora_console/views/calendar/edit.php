d529e6f38408f9cd3ce1f961e2196c1ba0d0ba26
Pablo Aragon <pablo.aragon@pandorafms.com>
2023-03-07 15:59:36 +0100

    Alerts

--- pandora_console/views/calendar/edit.php
+++ pandora_console/views/calendar/edit.php
@@ -34,19 +34,19 @@ if (\is_metaconsole() === true) {
     \alerts_meta_print_header($tabs);
 } else {
     // Header.
-    \ui_print_page_header(
-        // Title.
-        __('Calendars Edit'),
-        // Icon.
+    \ui_print_standard_header(
+        __('Alerts'),
         'images/gm_alerts.png',
-        // Return.
         false,
-        // Help.
         'alert_special_days',
-        // Godmode.
         true,
-        // Options.
-        $tabs
+        $tabs,
+        [
+            [
+                'link'  => '',
+                'label' => __('Calendars Edit'),
+            ],
+        ]
     );
 }
 
@@ -114,19 +114,24 @@ $inputs[] = [
         'rows'     => 50,
         'columns'  => 30,
     ],
+    'class'     => 'w100p',
 ];
 
-
+$button_create = '';
 if ($is_management_allowed === true) {
     // Submit.
-    $inputs[] = [
-        'arguments' => [
-            'name'       => 'button',
-            'label'      => (($create === true) ? __('Create') : __('Update')),
-            'type'       => 'submit',
-            'attributes' => 'class="sub next"',
-        ],
-    ];
+    html_print_action_buttons(
+        html_print_submit_button(
+            (($create === true) ? __('Create') : __('Update')),
+            'button',
+            false,
+            [
+                'icon' => 'wand',
+                'form' => 'create_specia_days',
+            ],
+            true
+        )
+    );
 }
 
 // Print form.
@@ -135,6 +140,8 @@ HTML::printForm(
         'form'   => [
             'action' => $url.'&op=edit&action=save&id='.$calendar->id(),
             'method' => 'POST',
+            'id'     => 'create_specia_days',
+            'class'  => 'aaaa',
         ],
         'inputs' => $inputs,
     ],

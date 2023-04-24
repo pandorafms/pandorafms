20ff1692ae415766e440f594e2d7c0516adf1edf
Pablo Aragon <pablo.aragon@pandorafms.com>
2023-04-24 15:58:08 +0200

    10947-Change color heatmap notinit modules

--- pandora_console/include/ajax/heatmap.ajax.php
+++ pandora_console/include/ajax/heatmap.ajax.php
@@ -364,26 +364,26 @@ if (is_ajax() === true) {
                     break;
 
                     case AGENT_MODULE_STATUS_CRITICAL_BAD:
-                        $status = ui_print_status_image('module_critical.png', $data_module, false);
+                        $status = ui_print_status_image('module_critical.png', $data_module, true);
                     break;
 
                     case AGENT_MODULE_STATUS_WARNING:
-                        $status = ui_print_status_image('module_warning.png', $data_module, false);
+                        $status = ui_print_status_image('module_warning.png', $data_module, true);
                     break;
 
                     case AGENT_MODULE_STATUS_NORMAL_ALERT:
                     case AGENT_MODULE_STATUS_WARNING_ALERT:
                     case AGENT_MODULE_STATUS_CRITICAL_ALERT:
-                        $status = ui_print_status_image('module_alertsfired.png', $data_module, false);
+                        $status = ui_print_status_image('module_alertsfired.png', $data_module, true);
                     break;
 
-                    case 4:
-                        $status = ui_print_status_image('module_no_data.png', $data_module, false);
+                    case AGENT_MODULE_STATUS_NO_DATA:
+                        $status = ui_print_status_image('module_no_data.png', $data_module, true);
                     break;
 
                     default:
                     case AGENT_MODULE_STATUS_UNKNOWN:
-                        $status = ui_print_status_image('module_unknown.png', $data_module, false);
+                        $status = ui_print_status_image('module_unknown.png', $data_module, true);
                     break;
                 }
 

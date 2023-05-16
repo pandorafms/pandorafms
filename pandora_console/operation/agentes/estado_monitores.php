b16260f08c3a56ab24093cbd0e695c83d718e46c
miguel angel rasteu <miguelangel.rasteu@pandorafms.com>
2023-05-16 12:40:37 +0200

    #11143 Change dialog width to auto

--- pandora_console/operation/agentes/estado_monitores.php
+++ pandora_console/operation/agentes/estado_monitores.php
@@ -446,9 +446,9 @@ ui_require_jquery_file('ui.datepicker-'.get_user_language(), 'include/javascript
                             opacity: 0.5,
                             background: "black"
                         },
-                        width: 650,
+                        width: "auto",
                         height: 500
-                    })
+                    }).css({"min-width": "650px"})
                     .show ();
                     refresh_pagination_callback (module_id, id_agent, "",module_name);
                     datetime_picker_callback();

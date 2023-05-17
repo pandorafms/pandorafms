e87da5e1e5fffdb0f336089c9dd6abc702ecc078
Jonathan <jonathan.leon@pandorafms.com>
2023-05-17 08:09:26 +0200

    #10362 change MR after change Milestone

--- pandora_console/extras/mr/64.sql
+++ pandora_console/extras/mr/64.sql
@@ -4,4 +4,7 @@ UPDATE pandora.tnetwork_component
    SET module_enabled=1
 WHERE name='Cisco&#x20;_nameOID_&#x20;power&#x20;state';
 
+ALTER TABLE `tlayout_data`
+ADD COLUMN `recursive_group` TINYINT NOT NULL DEFAULT '0' AFTER `fill_color`;
+
 COMMIT;

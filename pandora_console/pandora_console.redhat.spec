6904cec4a6c2d366adb217f22f6d81523623ce5a
artica <artica.devel@gmail.com>
2023-05-24 01:00:21 +0200

    Auto-updated build strings.

--- pandora_console/pandora_console.redhat.spec
+++ pandora_console/pandora_console.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     7.0NG.771
-%define release     230523
+%define release     230524
 
 # User and Group under which Apache is running
 %define httpd_name  httpd
--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -35,7 +35,7 @@ use PandoraFMS::Config;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "7.0NG.771 Build 230523";
+my $version = "7.0NG.771 Build 230524";
 
 # Pandora server configuration
 my %conf;

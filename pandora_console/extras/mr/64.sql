c1dd5d39c5bfa6064d8a4787232c9e6eaab75725
miguel angel rasteu <miguelangel.rasteu@pandorafms.com>
2023-06-02 13:03:20 +0200

    #11446 Add column in tlayout_template_data

--- pandora_console/extras/mr/64.sql
+++ pandora_console/extras/mr/64.sql
@@ -71,6 +71,9 @@ WHERE name='Cisco&#x20;_nameOID_&#x20;power&#x20;state';
 ALTER TABLE `tlayout_data`
 ADD COLUMN `recursive_group` TINYINT NOT NULL DEFAULT '0' AFTER `fill_color`;
 
+ALTER TABLE `tlayout_template_data`
+ADD COLUMN `recursive_group` TINYINT NOT NULL DEFAULT '0' AFTER `fill_color`;
+
 ALTER TABLE `tusuario`
 ADD COLUMN `metaconsole_section` VARCHAR(255) NOT NULL DEFAULT 'Default' AFTER `data_section`;
 

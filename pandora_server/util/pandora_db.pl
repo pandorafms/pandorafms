28c08d34df7a0c64aa5b5c4f0ca07c6776a54702
artica <artica.devel@gmail.com>
2023-06-12 01:00:46 +0200

    Auto-updated build strings.

--- pandora_server/util/pandora_db.pl
+++ pandora_server/util/pandora_db.pl
@@ -35,7 +35,7 @@ use PandoraFMS::Config;
 use PandoraFMS::DB;
 
 # version: define current version
-my $version = "7.0NG.771 Build 230611";
+my $version = "7.0NG.771 Build 230612";
 
 # Pandora server configuration
 my %conf;

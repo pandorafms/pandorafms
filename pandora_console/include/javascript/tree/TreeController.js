94b223a4b451c2efabc1f27b8b78958f04d3b964
alejandro.campos@artica.es <alejandro.campos@artica.es>
2023-06-19 12:12:50 +0200

    fixed error

--- pandora_console/include/javascript/tree/TreeController.js
+++ pandora_console/include/javascript/tree/TreeController.js
@@ -1560,7 +1560,10 @@ var TreeController = {
         this.reload();
       },
       init: function(data) {
-        if (data.filter.statusModule === "fired") {
+        if (
+          typeof data.filter !== "undefined" &&
+          data.filter.statusModule === "fired"
+        ) {
           const newData = {
             ajaxUrl: data.ajaxURL,
             baseURL: data.baseURL,

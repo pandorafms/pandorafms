e3e270233e627a691a4090c82349d00c03e78a14
alejandro <alejandro.sanchez@pandorafms.com>
2023-06-29 16:25:14 +0200

    added inventory solarix modules in 65.sql

--- pandora_console/extras/mr/65.sql
+++ pandora_console/extras/mr/65.sql
@@ -2,4 +2,12 @@ START TRANSACTION;
 
 DELETE FROM tconfig WHERE token = 'refr';
 
+INSERT INTO `tmodule_inventory` (`id_module_inventory`, `id_os`, `name`, `description`, `interpreter`, `data_format`, `code`, `block_mode`,`script_mode`) VALUES (37,2,'CPU','CPU','','Brand;Clock;Model','',0,2);
+
+INSERT INTO `tmodule_inventory` (`id_module_inventory`, `id_os`, `name`, `description`, `interpreter`, `data_format`, `code`, `block_mode`,`script_mode`) VALUES (38,2,'RAM','RAM','','Size','',0,2);
+
+INSERT INTO `tmodule_inventory` (`id_module_inventory`, `id_os`, `name`, `description`, `interpreter`, `data_format`, `code`, `block_mode`,`script_mode`) VALUES (39,2,'NIC','NIC','','NIC;Mac;Speed','',0,2);
+
+INSERT INTO `tmodule_inventory` (`id_module_inventory`, `id_os`, `name`, `description`, `interpreter`, `data_format`, `code`, `block_mode`,`script_mode`) VALUES (40,2,'Software','Software','','PKGINST;VERSION;NAME','',0,2);
+
 COMMIT;

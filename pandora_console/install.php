ab0fda5a5d1dce255fdb8c34fbdfee78a37c66c6
artica <artica.devel@gmail.com>
2023-08-03 01:00:42 +0200

    Auto-updated build strings.

--- pandora_console/install.php
+++ pandora_console/install.php
@@ -131,7 +131,7 @@
         <div style='padding-bottom: 50px'>
             <?php
             $version = '7.0NG.772';
-            $build = '230802';
+            $build = '230803';
             $banner = "v$version Build $build";
             error_reporting(0);
 
--- pandora_console/pandora_console.redhat.spec
+++ pandora_console/pandora_console.redhat.spec
@@ -3,7 +3,7 @@
 #
 %define name        pandorafms_console
 %define version     7.0NG.772
-%define release     230802
+%define release     230803
 
 # User and Group under which Apache is running
 %define httpd_name  httpd

9f76e7cc837db56601c5e5cc1481287aa1b28ca1
alejandro.campos@artica.es <alejandro.campos@artica.es>
2023-08-03 09:09:13 +0200

    update mr

--- pandora_console/extras/mr/66.sql
+++ pandora_console/extras/mr/66.sql
@@ -1,5 +1,7 @@
 START TRANSACTION;
 
 UPDATE tagente_modulo SET `tcp_send` = '2c' WHERE `tcp_send` = '2';
+UPDATE tpolicy_modules SET `tcp_send` = '2c' WHERE `tcp_send` = '2';
+UPDATE tnetwork_component SET `tcp_send` = '2c' WHERE `tcp_send` = '2';
 
 COMMIT;

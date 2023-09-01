82db8bd4bda6e6aa4865147bbfbd583a0d2f3bf4
Jonathan <jonathan.leon@pandorafms.com>
2023-09-01 10:28:02 +0200

    #11077 fix trre view widget style modal

--- pandora_console/include/functions_treeview.php
+++ pandora_console/include/functions_treeview.php
@@ -785,7 +785,7 @@ function treeview_printTable($id_agente, $server_data=[], $no_head=false)
         false,
         false,
         '',
-        'white-box-content mrgn_top_0 mrgn_btn_0px',
+        'white-box-content-no-height mrgn_top_0 mrgn_btn_0px',
         'white_table_flex'
     );
 
@@ -805,7 +805,7 @@ function treeview_printTable($id_agente, $server_data=[], $no_head=false)
             true,
             false,
             '',
-            'white-box-content mrgn_top_0 mrgn_btn_0px border-bottom-gray',
+            'white-box-content-no-height mrgn_top_0 mrgn_btn_0px border-bottom-gray',
             'white_table_flex'
         );
     }
@@ -980,7 +980,7 @@ function treeview_printTable($id_agente, $server_data=[], $no_head=false)
         true,
         empty($table_advanced->data),
         '',
-        'white-box-content mrgn_top_0 mrgn_btn_0px border-bottom-gray',
+        'white-box-content-no-height mrgn_top_0 mrgn_btn_0px border-bottom-gray',
         'white_table_flex'
     );
 

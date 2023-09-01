dac99b7a30be4960df2832e3cb420792a886fd62
miguel angel rasteu <miguelangel.rasteu@pandorafms.com>
2023-09-01 08:55:18 +0200

    #11886 Fix minimun misprint

--- pandora_console/include/functions_reporting_html.php
+++ pandora_console/include/functions_reporting_html.php
@@ -4091,7 +4091,7 @@ function reporting_html_value(
                 $table2->head = [
                     __('Agent'),
                     __('Module'),
-                    __('Minimun'),
+                    __('Minimum'),
                 ];
             break;
 
@@ -4162,7 +4162,7 @@ function reporting_html_value(
                 case 'min_value':
                     $table1->head = [
                         __('Lapse'),
-                        __('Minimun'),
+                        __('Minimum'),
                     ];
                 break;
 

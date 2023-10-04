e5fff03608a0d62d53d1338cc51e14265609ecc6
rafael <rafael.ameijeiras@pandorafms.com>
2023-10-04 10:50:29 +0200

    adding pandora_gotty_exec compiled from installers to rpms and deb packages

--- extras/pandora_gotty/build_deb.sh
+++ extras/pandora_gotty/build_deb.sh
@@ -6,6 +6,8 @@ mkdir -p pandora_gotty/usr/bin
 mkdir -p pandora_gotty/etc/pandora_gotty
 cp -a ../src/pandora_gotty pandora_gotty/usr/bin
 cp -a ../src/pandora_gotty.conf pandora_gotty/etc/pandora_gotty
+curl -SsL --output pandora_gotty/usr/bin/pandora_gotty_exec http://192.168.50.31/installers/installers/Linux/x86_64/pandora_gotty_exec
+chmod +x pandora_gotty/usr/bin/pandora_gotty_exec
 dpkg-deb --build pandora_gotty
 mv pandora_gotty.deb ../
 rm -rf pandora_gotty/usr/ 

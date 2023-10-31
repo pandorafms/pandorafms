b327c824da86c620a3aa383dff7e7c5be9d43ca4
Jonathan <jonathan.leon@pandorafms.com>
2023-10-31 13:45:26 +0100

    #8365 NCM new special templates

--- pandora_console/extras/mr/67.sql
+++ pandora_console/extras/mr/67.sql
@@ -3,4 +3,31 @@ START TRANSACTION;
 ALTER TABLE `tncm_queue`
 ADD COLUMN `id_agent_data` bigint unsigned AFTER `id_script`;
 
+CREATE TABLE IF NOT EXISTS `tncm_special_template` (
+    `id` SERIAL,
+    `name` TEXT,
+    `vendors` TEXT,
+    `models` TEXT,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+ALTER TABLE `tncm_agent`
+ADD COLUMN `id_special_template` BIGINT UNSIGNED NULL DEFAULT NULL AFTER `id_template`;
+
+CREATE TABLE IF NOT EXISTS `tncm_special_template_scripts` (
+    `id` SERIAL,
+    `id_special_template` BIGINT UNSIGNED NOT NULL,
+    `id_script` BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`id_special_template`) REFERENCES `tncm_special_template`(`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (`id_script`) REFERENCES `tncm_script`(`id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+ALTER TABLE `tncm_agent`
+ADD COLUMN `special_cron_interval` VARCHAR(100) NULL DEFAULT '' AFTER `cron_interval`;
+
+ALTER TABLE `tncm_agent`
+ADD COLUMN `special_event_on_change` INT UNSIGNED NULL DEFAULT NULL AFTER `event_on_change`;
+
+
 COMMIT;

24c8e606f48751d24ff8832d7ec16eb9bd232cbd
Daniel Cebrian <daniel.cebrian@pandorafms.com>
2023-11-30 16:29:18 +0100

    #12569 fixed button credential boxes

--- pandora_console/godmode/servers/modificar_server.php
+++ pandora_console/godmode/servers/modificar_server.php
@@ -521,7 +521,7 @@ $(document).ready (function () {
             function (data) {
                 $(".white-box-content").html(data);
 
-                $("#submit-add").click(function (e) {
+                $("#button-add").click(function (e) {
                     add_credential_boxes();
                 });
 
@@ -550,7 +550,7 @@ $(document).ready (function () {
                 $(".white-box-content").html(data2);
 
                 // Insert credential
-                $("#submit-add").click(function (e) {
+                $("#button-add").click(function (e) {
                     save_credential_boxes();
                 })
             },

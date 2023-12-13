af99c10b1929affc080830b002a35601da2934be
daniel <daniel.barbero@artica.es>
2023-12-13 13:03:58 +0100

    fix events sound pandora_enterprise#12607

--- pandora_console/include/ajax/events.php
+++ pandora_console/include/ajax/events.php
@@ -2644,8 +2644,8 @@ if ($get_events_fired) {
     // Set time.
     $filter['event_view_hr'] = 0;
 
-    $start = (time() - $interval);
-    $end = time();
+    $start = ((time() - $interval) + 1);
+    $end = (time() + 1);
 
     $filter['date_from'] = date('Y-m-d', $start);
     $filter['date_to'] = date('Y-m-d', $end);

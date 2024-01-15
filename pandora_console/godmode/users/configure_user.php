f52700f3927f0cc156eeba17565549184cc81eac
alejandro.campos@artica.es <alejandro.campos@artica.es>
2024-01-15 17:42:41 +0100

    fix home screen

--- pandora_console/godmode/users/configure_user.php
+++ pandora_console/godmode/users/configure_user.php
@@ -430,6 +430,11 @@ if ($create_user === true) {
         $values['data_section'] = get_parameter('data_section_external');
     }
 
+    if (is_metaconsole() === true) {
+        $values['metaconsole_section'] = $values['section'];
+        $values['metaconsole_data_section'] = $values['data_section'];
+    }
+
     // $values['section'] = $homeScreenValues[$values['section']];
     if (enterprise_installed() === true) {
         $values['force_change_pass'] = 1;

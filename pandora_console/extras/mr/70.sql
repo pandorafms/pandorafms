100f437a7abc720f60319a91c2940831cea29dcf
alejandro.campos@artica.es <alejandro.campos@artica.es>
2024-02-16 10:30:05 +0100

    change regex search in events

--- pandora_console/extras/mr/70.sql
+++ pandora_console/extras/mr/70.sql
@@ -2,7 +2,8 @@ START TRANSACTION;
 
 -- Watch out! The following field migration must be done before altering the corresponding table.
 UPDATE `tevent_filter`
-SET `search` = `regex`
+SET `search` = `regex`,
+    `regex` = '1'
 WHERE `regex` IS NOT NULL AND `regex` != '';
 
 -- Watch out! The following alter command must be done after the previous update of this table.

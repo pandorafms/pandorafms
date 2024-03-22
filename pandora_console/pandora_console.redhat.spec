968c9fb2c64a01236aa81b7cd7447505b3d41f59
artica <artica.devel@gmail.com>
2024-03-22 01:00:06 +0100

    Auto-updated build strings.

--- pandora_console/pandora_console.redhat.spec
+++ pandora_console/pandora_console.redhat.spec
@@ -6,7 +6,7 @@
 %define debug_package %{nil}
 %define name        pandorafms_console
 %define version     7.0NG.776
-%define release     240321
+%define release     240322
 
 # User and Group under which Apache is running
 %define httpd_name  httpd
--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -36,7 +36,7 @@ use Encode::Locale;
 Encode::Locale::decode_argv;
 
 # version: define current version
-my $version = "7.0NG.776 Build 240321";
+my $version = "7.0NG.776 Build 240322";
 
 # save program name for logging
 my $progname = basename($0);

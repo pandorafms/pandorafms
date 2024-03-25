752aa71ecf142f32e9959a4684e119d72c45f142
Jonathan <jonathan.leon@pandorafms.com>
2024-03-25 09:20:00 +0100

    #13150 hide omnishell

--- pandora_console/operation/agentes/ver_agente.php
+++ pandora_console/operation/agentes/ver_agente.php
@@ -1653,8 +1653,7 @@ if ($policyTab === ENTERPRISE_NOT_HOOK) {
 }
 
 // Omnishell.
-$tasks = enterprise_hook('count_tasks_agent', [$id_agente]);
-
+// $tasks = enterprise_hook('count_tasks_agent', [$id_agente]);
 if ($tasks === true) {
     $omnishellTab = enterprise_hook('omnishell_tab');
     if ($omnishellTab == -1) {
